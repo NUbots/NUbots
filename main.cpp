17dd97fd8638a6e1a86fe6c394379514d1f799db
Trent Houliston <trent@houliston.me>
2013-07-31 13:23:11 +1000

    More placeholders and changes to the structure

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <NUClear.h>
 #include "DarwinMotors.h"
+#include "DarwinSensors.h"
+#include "DarwinCamera.h"
 
 int main(int argc, char *argv[]) {
     
     NUClear::PowerPlant plant;
     
     plant.install<modules::DarwinMotors>();
+    plant.install<modules::DarwinSensors>();
+    plant.install<modules::DarwinCamera>();
     
     plant.start();
 }
--- modules/DarwinCamera/src/DarwinCamera.cpp
+++ modules/DarwinCamera/src/DarwinCamera.cpp
@@ -3,7 +3,6 @@ namespace modules {
     
     DarwinCamera::DarwinCamera(NUClear::PowerPlant& plant) : Reactor(plant) {
         on<Trigger<Every<20, std::chrono::milliseconds>>>([](const time_t& time) {
-            
         });
     }
 }
