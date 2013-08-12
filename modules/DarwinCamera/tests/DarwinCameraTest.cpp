96448f6100de46857b16bc931972068b8c01865f
Michael Burton <michael.burton@uon.edu.au>
2013-08-13 09:48:16 +1000

    Added a simple test for DarwinCamera

--- modules/DarwinCamera/tests/DarwinCameraTest.cpp
+++ modules/DarwinCamera/tests/DarwinCameraTest.cpp
@@ -1,3 +1,25 @@
+#include "DarwinCamera.h"
+#include "messages/NUImage.h"
+
+namespace {
+	class ImageReactor : public NUClear::Reactor {
+	public:
+		ImageReactor(NUClear::PowerPlant& plant): Reactor(plant) {
+			on<Trigger<messages::NUImage>>([this](const messages::NUImage& image) {
+				std::cout << "Successfully retrieved an image." << std::endl;
+				this->powerPlant.shutdown();
+			});
+		}
+	};
+}
+
+
 int main(int argc, char *argv[]) {
+    NUClear::PowerPlant::Configuration config;
+    config.threadCount = 1;
+    NUClear::PowerPlant plant(config);
+	plant.install<modules::DarwinCamera>();
+    plant.install<ImageReactor>();
+    
     return 0;
 }
