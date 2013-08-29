ca8639d4596cbe1bb37d2ecd7e6209a3435843a5
Trent Houliston <trent@houliston.me>
2013-08-29 18:25:40 +1000

    Upgrades to the CMake system (modules no longer need to declare themselves as thoroughly and can include libraries at will)

---
+++ cmake/Includes/Compiler.cmake
@@ -0,0 +1,28 @@
+# Common C++ Flags
+SET(CMAKE_CXX_FLAGS "-std=c++11")
+SET(CMAKE_CXX_FLAGS_DEBUG "-O0 -g")
+SET(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
+
+# XCode support
+IF("${CMAKE_GENERATOR}" MATCHES "Xcode")
+    message("Enabling xcode support")
+    set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++")
+    set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++0x")
+ENDIF()
+
+IF("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
+    SET(GCC_MINIMUM_VERSION 4.7)
+    EXECUTE_PROCESS(
+        COMMAND ${CMAKE_CXX_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION)
+    IF(GCC_VERSION VERSION_LESS ${GCC_MINIMUM_VERSION})
+        message(FATAL_ERROR "${PROJECT_NAME} requires g++ 4.7.2 or greater.")
+    ENDIF()
+
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread -Wall")
+    
+ELSEIF("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wpedantic -stdlib=libc++")
+    
+ELSE()
+    MESSAGE(FATAL_ERROR "Unsupported compiler!")
+ENDIF()
