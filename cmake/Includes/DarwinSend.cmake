f9c4d9f163016e50f2ba72a0a0600a568af4c2ed
Trent Houliston <trent@houliston.me>
2013-08-30 16:59:48 +1000

    Set robocup as a dependency of the install

--- cmake/Includes/DarwinSend.cmake
+++ cmake/Includes/DarwinSend.cmake
@@ -1,4 +1,5 @@
 # Custom target to copy and install configuration files and binarys to the robot
 ADD_CUSTOM_TARGET(install 
                   COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/install.sh ${CMAKE_BINARY_DIR}/install.sh
-                  COMMAND bash install.sh \${robot} \${config})
+                  COMMAND bash install.sh \${robot} \${config}
+                  DEPENDS robocup)
