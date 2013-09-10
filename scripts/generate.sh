d146b43f65ca9392fc87f9a954f3ef529a4cedd6
Jake Woods <vodurden@gmail.com>
2013-09-10 14:46:24 +1000

    Added generate.sh

---
+++ scripts/generate.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# File:   install.sh
+# Authors: 
+#   Jake Woods <jake.f.woods@gmail.com>
+#   Trent Houliston <trent@houliston.me>
+#
+
+# Ensure we have specified a name
+if [ -z "$1" ] ; then
+    echo "You must specify a name";
+    exit 1;
+else
+    hat_name=$1;
+fi
+
+# Ensure we've got at least one module
+if [ -n "$2" ] ; then
+    hat_modules=$2;
+else
+    echo "You must specify at least one module";
+    exit 1;
+fi
+
+# Delete the old hat if it exists. We pipe to
+# /dev/null to supress a message if it doesn't
+# exist.
+rm $hat_name 2> /dev/null;
+
+# Build up our headers.
+# We always need NUClear.h
+echo "#include <NUClear.h>" >> $hat_name;
+
+# Add our module headers
+for module in ${@:2}; do
+    echo "#include \"${module}.h\"" >> $hat_name;
+done
+
+# Add our main function.
+cat >> $hat_name << EOF
+
+int main(int argc, char** argv) {
+    NUClear::PowerPlant::Configuration config;
+    config.numThreads = 4;
+
+    NUClear::PowerPlant plant(config, argc, const_cast<const char**>(argv));
+EOF
+
+# Add our installs
+for module in ${@:2}; do
+    echo -e "\tplant.install<modules::${module}>();" >> $hat_name;
+done
+
+cat >> $hat_name << EOF
+
+    plant.start();
+    return 0;
+}
+EOF
