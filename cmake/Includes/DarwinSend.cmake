660d35a3f6b4b5695ae1e7712ce507bf9e1b653f
Jake Woods <vodurden@gmail.com>
2013-09-10 15:31:27 +1000

    Fixed DarwinSend.cmake to work with the new scripts/ directory

--- cmake/Includes/DarwinSend.cmake
+++ cmake/Includes/DarwinSend.cmake
@@ -1,11 +1,9 @@
 # Custom target to copy and install configuration files and binarys to the robot
 ADD_CUSTOM_TARGET(install 
-                  COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/install.sh ${CMAKE_BINARY_DIR}/install.sh
                   COMMAND bash "${NUBOTS_SCRIPTS_DIR}/install.sh" \${robot} \${config}
-                  DEPENDS ${hats})
+                  DEPENDS ${hats} "${NUBOTS_SCRIPTS_DIR}/install.sh")
 
 # Custom target to copy and install configuration files and binarys to the robot
 ADD_CUSTOM_TARGET(run 
-                  COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/run.sh ${CMAKE_BINARY_DIR}/run.sh
                   COMMAND bash "${NUBOTS_SCRIPTS_DIR}/run.sh" \${robot} \${config}
-                  DEPENDS install)
+                  DEPENDS install "${NUBOTS_SCRIPTS_DIR}/run.sh")
