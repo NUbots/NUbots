2cc44a56efa458fabaa4d07e389c4122e976d54c
Joshua Kearns <joshau-k@hotmail.com>
2013-09-13 17:42:17 +1000

    Beat detection code that isn't working, cleaned up a bit.

--- modules/BeatDetector/src/BeatDetector.cpp
+++ modules/BeatDetector/src/BeatDetector.cpp
@@ -29,82 +29,58 @@ static const int NUM_CHUNKS = 50;
 namespace modules {
     BeatDetector::BeatDetector(NUClear::PowerPlant* plant) : Reactor(plant) {
         
-        
-        //struct SoundChunk& chunk;
-    
-        //on<Trigger<messages::SoundChunk>>([this](const messages::SoundChunk& chunk) {
         on<Trigger<Last<NUM_CHUNKS, messages::SoundChunk>>> ([this](const std::vector<std::shared_ptr<const messages::SoundChunk>>& chunks) {
-            //std::cout << "Beat Detector Called";
+            // The last NUM_CHUNKS (50) chunks of sound data that were recorded (or from a file) are received
             
-            
-            //auto chunk = std::make_unique<messages::SoundChunk>();
             std::vector<int16_t> chunk;
+            //The audio chunks are combined into a single vector
             for (int i = 0; i < NUM_CHUNKS; ++i)
             {
                 chunk.insert( chunk.end(), chunks.at(i)->data.begin(), chunks.at(i)->data.end() );
             }
             
             int numChannels = chunks.at(0)->numChannels;
+            //Calculate length of recording in milliseconds
             int numMilliseconds = (int) (chunk.size() + 0.0/ numChannels) / chunks.at(0)->sampleRate * 1000 ;
-            //std::cout << "chunk size: " << chunk.size() << " num channels: " << numChannels << " sample rate: " << chunks.at(0)->sampleRate << "\n";
-            //std::cout << "Length of recording: " << numMilliseconds << "ms\n";
             NUClear::clock::time_point endTime = chunks.at(0)->endTime;
 
-            //int NUM_CHANNELS = 2;
-            //int NUM_MILLISECONDS = 2;//(std::chrono::milliseconds) 2;
             fvec_t * out;
             fvec_t * beat_input_data;
             aubio_beattracking_t * abt;
-            //struct messages::BeatLocations * beatLocations;
             
-            //messages::BeatLocations * beatLocations = new messages::BeatLocations;
-           auto beatLocations = std::make_unique<messages::BeatLocations>();
+
             
-            //NUClear::clock::time_point firstBeatTime;
-            int beatPeriodFrames;
-            float beatPeriodMilliseconds;
-            //NUClear::clock::duration beatPeriod;
-            int numBeats;
-            //NUClear::clock::time_point firstBeatTime;
-            
-            
+            //Audio data is written to a fvec
             beat_input_data = new_fvec(chunk.size()/numChannels , numChannels);
-            //new_fvec();
-                        //data.recordedSamples;
-           //chunk.data.at(0);
-
-
-           for (int i = 0; i < (int) chunk.size()/numChannels; ++i)
+            for (int i = 0; i < (int) chunk.size()/numChannels; ++i)
             {
                for (int j = 0; j < numChannels; ++j)
                {
                    fvec_write_sample(beat_input_data,  chunk.at(2*i + j), j, i); //for channel j
                }
-                //fvec_write_sample(beat_input_data,  chunk.at(2*i), 0, i);
-                //fvec_write_sample(beat_input_data, chunk.at(2*i + 1), 1, i);
             }
  
             abt = new_aubio_beattracking(chunk.size()/numChannels, numChannels);
-            //abt = new_aubio_beattracking(sizeof(SAMPLE), NUM_CHANNELS);
-
+            
             out = new_fvec(chunk.size()/numChannels , numChannels);
 
             aubio_beattracking_do(abt, beat_input_data, out);
 
             del_aubio_beattracking(abt);
 
+            //Display the results of the beat detection algorithm
             std::cout << "found " << (out->data[0][0] -1) << " beats\n";
             
-            //beatFound = false;
-            //start = 0;
-            //end = 0;
             for (int i = 1; i <= out->data[0][0] -1; ++i)
             {
-                
                 std::cout << "Beat value " << i << " : " << (out->data[0][i])/(chunk.size()/ numChannels) * numMilliseconds/1000 << "s \n";
                 std::cout << "Beat value " << i << " : " << "Position in array: " << (out->data[0][i]) << " Size of array: " <<(chunk.size()/numChannels) << " # Seconds of recording: " << numMilliseconds/1000 << "s \n";
-
             }
+            
+            auto beatLocations = std::make_unique<messages::BeatLocations>();
+            int beatPeriodFrames;
+            float beatPeriodMilliseconds;
+            int numBeats;
 
             numBeats = out->data[0][0];
             //beatPeriod = (out->data[0][1]*4)/(chunk.data.size()/NUM_CHANNELS) * (NUM_SECONDS - out->data[0][2]*4)/(chunk.data.size()/NUM_CHANNELS) * NUM_SECONDS;
@@ -112,13 +88,9 @@ namespace modules {
             beatPeriodMilliseconds = beatPeriodFrames/chunk.size() * numMilliseconds ; //NUM_CHANNELS
             float firstBeatTimeMilliseconds = out->data[0][1]/chunk.size()/numChannels * numMilliseconds;
             
-            //std::time_t firstBeatTime = std::chrono::system_clock::to_time_t(chunk.endTime - std::chrono::milliseconds(NUM_MILLISECONDS) +  std::chrono::microseconds((int) (firstBeatTimeMilliseconds * 1000)));
-            //NUClear::clock::time_point firstBeatTimePoint = std::chrono::system_clock::from_time_t(firstBeatTime);
-            
             NUClear::clock::time_point firstBeatTime = endTime - std::chrono::milliseconds(numMilliseconds) +  std::chrono::microseconds((int) (firstBeatTimeMilliseconds * 1000));
             
             std::time_t t = std::chrono::system_clock::to_time_t(firstBeatTime);
-            //std::cout << 
             std::cout <<"First beat time: " << std::ctime(&t) << "\n";
             
             beatLocations->firstBeatTime = firstBeatTime;
@@ -126,7 +98,6 @@ namespace modules {
             beatLocations->numBeats = numBeats;
 
             powerPlant->emit(std::move(beatLocations));
-            //powerPlant->emit(std::move(chunk));
              
         });    
     
