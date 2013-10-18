3e95e98c8a80117579643d602ee6dde7f5209b11
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2013-10-18 21:40:40 +1100

    Add a gcc48 class and begin NUClear and NUClearPort classes.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,7 +2,7 @@ class vim {
   package { 'vim':
     ensure => latest
   } ->
-  package { 'vim-puppet':
+  package { 'vim-puppet':  # syntax highlighting
     ensure => latest,
   } ->
   file { ['/home/mitchell/.vim', '/home/mitchell/.vim/plugin']:
@@ -14,15 +14,53 @@ class vim {
   }
 }
 
+class gcc48 {
+  include apt
+
+  apt::ppa { 'ppa:ubuntu-toolchain-r/test': }
+
+  package { 'gcc-4.8':
+    ensure => latest,
+    require => Apt::Ppa['ppa:ubuntu-toolchain-r/test'], 
+  } ~>
+  exec { 'gcc-4.8_alternatives':
+    command => 'update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6',
+    path => $path,
+    refreshonly => true, 
+  }
+
+  package { 'g++-4.8':
+    ensure => latest,
+    require => Apt::Ppa['ppa:ubuntu-toolchain-r/test'], 
+  } ->
+  exec { 'g++-4.8_alternatives':
+    command => 'update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 80 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8',
+    path => $path,
+    refreshonly => true, 
+  }
+}
+
+class nuclear {
+  require 'gcc48'
+
+  file { '/home/mitchell/NUbots':
+    ensure => directory,
+  } ->
+  vcsrepo { '/home/mitchell/NUbots/NUClear':
+    ensure => present,
+    provider => 'git',
+    source => "https://github.com/Fastcode/NUClear.git",
+    require => Package['git'],
+  } 
+}
+
+class nuclearport {
+  require 'nuclear'
+}
 
 node default {
-  package { 'git':
-    ensure => latest,
-  }
-
-  package { 'openssh-server':
-    ensure => latest,
-  }
-
-  include vim  
+  package { 'git': ensure => latest }
+  package { 'openssh-server': ensure => latest }
+  include vim
+  include nuclearPort
 }
