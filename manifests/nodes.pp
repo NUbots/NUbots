19c012a5503e1342c95fa51d36513e39d267b113
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2013-10-19 13:54:35 +1100

    Make NUClear class correctly install NUClear.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -22,7 +22,7 @@ class gcc48 {
   package { 'gcc-4.8':
     ensure => latest,
     require => Apt::Ppa['ppa:ubuntu-toolchain-r/test'], 
-  } ~>
+  } ->
   exec { 'gcc-4.8_alternatives':
     command => 'update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6',
     path => $path,
@@ -51,14 +51,46 @@ class nuclear {
   include gcc48
   include catch
 
-  file { '/home/mitchell/NUbots':
+  $nuclear_build_dir = '/home/mitchell/NUbots/NUClear/build'
+
+  file { 'nubots_dir':
+    path => '/home/mitchell/NUbots',
     ensure => directory,
   } ->
-  vcsrepo { '/home/mitchell/NUbots/NUClear':
+  vcsrepo { 'nuclear_repo':
+    path => '/home/mitchell/NUbots/NUClear',
     ensure => present,
     provider => 'git',
     source => "https://github.com/Fastcode/NUClear.git",
     require => Package['git'],
+  } ->
+  file { 'nuclear_build_dir':
+    path => $nuclear_build_dir,
+    ensure => directory,
+    purge => true,
+    force => true,
+  } ~>
+  exec { 'nuclear_cmake':
+    require => [Package['cmake'], Package['libzmq-dev'], Package['protobuf-compiler']],
+    command => 'cmake ..',
+    cwd => $nuclear_build_dir,
+    path => $path,
+    refreshonly => true,
+    logoutput => "on_failure",
+  } ~>
+  exec { 'nuclear_make':
+    command => 'make -j',
+    cwd => $nuclear_build_dir,
+    path => $path,
+    refreshonly => true,
+    logoutput => "on_failure",
+  } ~>
+  exec { 'nuclear_install':
+    command => 'make install',
+    cwd => $nuclear_build_dir,
+    path => $path,
+    refreshonly => true,
+    logoutput => "on_failure",
   }
 }
 
@@ -67,8 +99,11 @@ class nuclearport {
 }
 
 node default {
+  package { 'cmake': ensure => latest }
   package { 'git': ensure => latest }
   package { 'openssh-server': ensure => latest }
+  package { 'libzmq-dev': ensure => latest }
+  package { ['libprotobuf-dev', 'protobuf-compiler']: ensure => latest }
   include vim
   include nuclearPort
 }
