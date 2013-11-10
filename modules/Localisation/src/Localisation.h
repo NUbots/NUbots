105652b90859f9f3eacea9dfaebb01b2bfe0bc2a
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2013-11-10 16:35:17 +1100

    LOPO-2: Create an empty 'Localisation' NUClear module

---
+++ modules/Localisation/src/Localisation.h
@@ -0,0 +1,32 @@
+/*
+ * This file is part of Localisation.
+ *
+ * Localisation is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Localisation is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Localisation.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2013 NUBots <nubots@nubots.net>
+ */
+
+#ifndef MODULES_LOCALISATION_H
+#define MODULES_LOCALISATION_H
+
+#include <NUClear.h>
+
+namespace modules {
+    class Localisation : public NUClear::Reactor {
+    public:
+        explicit Localisation(NUClear::PowerPlant* plant);
+    };
+}
+#endif
+
