a8cc2837bceec643017b8690418c9631774f1639
Trent Houliston <trent@houliston.me>
2013-11-29 13:06:12 +1100

    Started work on the script optimizer

---
+++ modules/Research/ScriptOptimizer/src/ScriptOptimizer.cpp
@@ -0,0 +1,44 @@
+/*
+ * This file is part of ScriptOptimizer.
+ *
+ * ScriptOptimizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ScriptOptimizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ScriptOptimizer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2013 NUBots <nubots@nubots.net>
+ */
+
+#include "ScriptOptimizer.h"
+#include "messages/OptimizeScript.pb.h"
+#include "messages/DarwinSensors.h"
+
+namespace modules {
+    namespace Research {
+        ScriptOptimizer::ScriptOptimizer(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+
+            on<Trigger<Network<messages::OptimizeScript>>, With<NUClear::extensions::NetworkingConfiguration>>([this]
+                    (const Network<messages::OptimizeScript>& task, const NUClear::extensions::NetworkingConfiguration config) {
+
+                // Check if this script is for us
+                if (config.deviceName == task.data->target()) {
+
+                    // Make a script from the frames
+                    for (const auto& frame : task.data->frames()) {
+                        for (const auto& target : frame.targets()) {
+
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
