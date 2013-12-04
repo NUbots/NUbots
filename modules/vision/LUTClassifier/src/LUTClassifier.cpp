63fb9a83064e3879cb3c7548e059f20ae3232322
Jake Fountain <jake.fountain@uon.edu.au>
2013-12-04 14:50:27 +1100

    Added vision config.

--- modules/vision/LUTClassifier/src/LUTClassifier.cpp
+++ modules/vision/LUTClassifier/src/LUTClassifier.cpp
@@ -23,9 +23,96 @@ namespace modules {
     namespace vision {
 
         using messages::input::Image;
-
+        using messages::support::Configuration;
         
         LUTClassifier::LUTClassifier(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+           
+            on<Trigger<Configuration<LUTClassifier>>>([this](const Configuration<LUTClassifier>& constants) {
+            	//HACK FOR RC2013
+				WHITE_SIDE_IS_BLUE = constants.config["WHITE_SIDE_IS_BLUE"];
+				NON_WHITE_SIDE_CHECK = constants.config["NON_WHITE_SIDE_CHECK"];
+				UPPER_WHITE_THRESHOLD = constants.config["UPPER_WHITE_THRESHOLD"];
+				LOWER_WHITE_THRESHOLD = constants.config["LOWER_WHITE_THRESHOLD"];
+				//! Distortion Correction
+				DO_RADIAL_CORRECTION = constants.config["DO_RADIAL_CORRECTION"];
+				RADIAL_CORRECTION_COEFFICIENT = constants.config["RADIAL_CORRECTION_COEFFICIENT"];
+				//! Goal filtering constants
+				THROWOUT_ON_ABOVE_KIN_HOR_GOALS = constants.config["THROWOUT_ON_ABOVE_KIN_HOR_GOALS"];
+				THROWOUT_ON_DISTANCE_METHOD_DISCREPENCY_GOALS = constants.config["THROWOUT_ON_DISTANCE_METHOD_DISCREPENCY_GOALS"];
+				MAX_DISTANCE_METHOD_DISCREPENCY_GOALS = constants.config["MAX_DISTANCE_METHOD_DISCREPENCY_GOALS"];
+				THROWOUT_DISTANT_GOALS = constants.config["THROWOUT_DISTANT_GOALS"];
+				MAX_GOAL_DISTANCE = constants.config["MAX_GOAL_DISTANCE"];
+				THROWOUT_INSIGNIFICANT_GOALS = constants.config["THROWOUT_INSIGNIFICANT_GOALS"];
+				MIN_TRANSITIONS_FOR_SIGNIFICANCE_GOALS = constants.config["MIN_TRANSITIONS_FOR_SIGNIFICANCE_GOALS"];
+				THROWOUT_NARROW_GOALS = constants.config["THROWOUT_NARROW_GOALS"];
+				MIN_GOAL_WIDTH = constants.config["MIN_GOAL_WIDTH"];
+				THROWOUT_SHORT_GOALS = constants.config["THROWOUT_SHORT_GOALS"];
+				MIN_GOAL_HEIGHT = constants.config["MIN_GOAL_HEIGHT"];
+				GOAL_HEIGHT_TO_WIDTH_RATIO_MIN = constants.config["GOAL_HEIGHT_TO_WIDTH_RATIO_MIN"];
+				GOAL_MAX_OBJECTS = constants.config["GOAL_MAX_OBJECTS"];
+				GOAL_BINS = constants.config["GOAL_BINS"];
+				GOAL_MIN_THRESHOLD = constants.config["GOAL_MIN_THRESHOLD"];
+				GOAL_SDEV_THRESHOLD = constants.config["GOAL_SDEV_THRESHOLD"];
+				GOAL_RANSAC_MATCHING_TOLERANCE = constants.config["GOAL_RANSAC_MATCHING_TOLERANCE"];
+				THROWOUT_ON_ABOVE_KIN_HOR_BALL = constants.config["THROWOUT_ON_ABOVE_KIN_HOR_BALL"];
+				THROWOUT_ON_DISTANCE_METHOD_DISCREPENCY_BALL = constants.config["THROWOUT_ON_DISTANCE_METHOD_DISCREPENCY_BALL"];
+				MAX_DISTANCE_METHOD_DISCREPENCY_BALL = constants.config["MAX_DISTANCE_METHOD_DISCREPENCY_BALL"];
+				THROWOUT_SMALL_BALLS = constants.config["THROWOUT_SMALL_BALLS"];
+				MIN_BALL_DIAMETER_PIXELS = constants.config["MIN_BALL_DIAMETER_PIXELS"];
+				THROWOUT_INSIGNIFICANT_BALLS = constants.config["THROWOUT_INSIGNIFICANT_BALLS"];
+				MIN_TRANSITIONS_FOR_SIGNIFICANCE_BALL = constants.config["MIN_TRANSITIONS_FOR_SIGNIFICANCE_BALL"];
+				THROWOUT_DISTANT_BALLS = constants.config["THROWOUT_DISTANT_BALLS"];
+				MAX_BALL_DISTANCE = constants.config["MAX_BALL_DISTANCE"];
+				//! Distance calculation options
+				D2P_INCLUDE_BODY_PITCH = constants.config["D2P_INCLUDE_BODY_PITCH"];
+				BALL_DISTANCE_POSITION_BOTTOM = constants.config["BALL_DISTANCE_POSITION_BOTTOM"];
+				//! Distance method options
+				BALL_DISTANCE_METHOD = constants.config["BALL_DISTANCE_METHOD"];
+				GOAL_DISTANCE_METHOD = constants.config["GOAL_DISTANCE_METHOD"];
+				//DistanceMethod VisionConstants::BEACON_DISTANCE_METHOD;
+				LINE_METHOD = constants.config["LINE_METHOD"];
+				//! Field-object detection constants
+				BALL_EDGE_THRESHOLD = constants.config["BALL_EDGE_THRESHOLD"];
+				BALL_ORANGE_TOLERANCE = constants.config["BALL_ORANGE_TOLERANCE"];
+				BALL_MIN_PERCENT_ORANGE = constants.config["BALL_MIN_PERCENT_ORANGE"];
+				GOAL_MIN_PERCENT_YELLOW = constants.config["GOAL_MIN_PERCENT_YELLOW"];
+				GOAL_MIN_PERCENT_BLUE = constants.config["GOAL_MIN_PERCENT_BLUE"];
+				//float VisionConstants::BEACON_MIN_PERCENT_YELLOW;
+				//float VisionConstants::BEACON_MIN_PERCENT_BLUE;
+				MIN_GOAL_SEPARATION = constants.config["MIN_GOAL_SEPARATION"];
+				//! Obstacle detection constants
+				MIN_DISTANCE_FROM_HORIZON = constants.config["MIN_DISTANCE_FROM_HORIZON"];
+				MIN_CONSECUTIVE_POINTS = constants.config["MIN_CONSECUTIVE_POINTS"];
+				//! Field dimension constants
+				GOAL_WIDTH = constants.config["GOAL_WIDTH"];
+				GOAL_HEIGHT = constants.config["GOAL_HEIGHT"];
+				DISTANCE_BETWEEN_POSTS = constants.config["DISTANCE_BETWEEN_POSTS"];
+				BALL_WIDTH = constants.config["BALL_WIDTH"];
+				CENTRE_CIRCLE_RADIUS = constants.config["CENTRE_CIRCLE_RADIUS"];
+				//float VisionConstants::BEACON_WIDTH;
+				//! ScanLine options
+				HORIZONTAL_SCANLINE_SPACING = constants.config["HORIZONTAL_SCANLINE_SPACING"];
+				VERTICAL_SCANLINE_SPACING = constants.config["VERTICAL_SCANLINE_SPACING"];
+				GREEN_HORIZON_SCAN_SPACING = constants.config["GREEN_HORIZON_SCAN_SPACING"];
+				GREEN_HORIZON_MIN_GREEN_PIXELS = constants.config["GREEN_HORIZON_MIN_GREEN_PIXELS"];
+				GREEN_HORIZON_UPPER_THRESHOLD_MULT = constants.config["GREEN_HORIZON_UPPER_THRESHOLD_MULT"];
+				//! Split and Merge constants
+				SAM_MAX_LINES = constants.config["SAM_MAX_LINES"];
+				SAM_SPLIT_DISTANCE = constants.config["SAM_SPLIT_DISTANCE"];
+				SAM_MIN_POINTS_OVER = constants.config["SAM_MIN_POINTS_OVER"];
+				SAM_MIN_POINTS_TO_LINE = constants.config["SAM_MIN_POINTS_TO_LINE"];
+				SAM_MAX_ANGLE_DIFF_TO_MERGE = constants.config["SAM_MAX_ANGLE_DIFF_TO_MERGE"];
+				SAM_MAX_DISTANCE_TO_MERGE = constants.config["SAM_MAX_DISTANCE_TO_MERGE"];
+				SAM_MIN_POINTS_TO_LINE_FINAL = constants.config["SAM_MIN_POINTS_TO_LINE_FINAL"];
+				SAM_MIN_LINE_R2_FIT = constants.config["SAM_MIN_LINE_R2_FIT"];
+				SAM_MAX_LINE_MSD = constants.config["SAM_MAX_LINE_MSD"];
+				SAM_CLEAR_SMALL = constants.config["SAM_CLEAR_SMALL"];
+				SAM_CLEAR_DIRTY = constants.config["SAM_CLEAR_DIRTY"];
+				//! RANSAC constants
+				RANSAC_MAX_ANGLE_DIFF_TO_MERGE = constants.config["RANSAC_MAX_ANGLE_DIFF_TO_MERGE"]; 
+				RANSAC_MAX_DISTANCE_TO_MERGE = constants.config["RANSAC_MAX_DISTANCE_TO_MERGE"]; 
+            });
+
             on<Trigger<Image>>([this](const Image& image){
 
             	std::vector<arma::vec> green_horizon_points = CalculateGreenHorizon(image);
@@ -72,13 +159,13 @@ namespace modules {
 		        //Search for green below the kinematics horizon
 		        for (int y = kin_hor_y; y < height; y++) {
 
-		            if (isPixelGreen(img, x, y)) {
+		            if (IsPixelGreen(img(x, y))) {
 		                if (green_count == 0) {
 		                    green_top = y;
 		                }
 		                green_count++;
 		                // if VER_THRESHOLD green pixels found, add point
-		                if (green_count == VisionConstants::GREEN_HORIZON_MIN_GREEN_PIXELS) {//TODO
+		                if (green_count == GREEN_HORIZON_MIN_GREEN_PIXELS) {//TODO
 		                    vec v(2);
 		                    v[0] = x;
 		                    v[1] = green_top;
@@ -126,12 +213,12 @@ namespace modules {
 		    }
 
 		    mean_y = mean(acc);
-		    std_dev_y = sqrt(variance(acc)); 
+		    std_dev_y = sqrt(variance(acc));
 		
 
 		    std::vector<arma::vec>::iterator p = horizon_points.begin();
 		    while(p < horizon_points.end()) {
-		        if (p->y < mean_y - VisionConstants::GREEN_HORIZON_UPPER_THRESHOLD_MULT*std_dev_y) {//TODO
+		        if (p->y < mean_y - GREEN_HORIZON_UPPER_THRESHOLD_MULT*std_dev_y) {//TODO
 		            thrown_points.push_back(*p);
 		            p = horizon_points.erase(p);
 		        }
@@ -147,7 +234,11 @@ namespace modules {
 		   
 		    return horizon_points;
         }
-        
+
+        std::vector<int> LUTClassifier::GenerateScanLines(const messages::input::Image& image, const std::vector<arma::vec>& green_horizon_points){
+
+        }
+          
 
         std::vector<arma::vec> LUTClassifier::upperConvexHull(const std::vector<arma::vec>& points){
         	int n = points.size(),
@@ -165,6 +256,12 @@ namespace modules {
 	        H.resize(k);
 	        return H;
 	    }
+
+
+        bool LUTClassifier::IsPixelGreen(const messages::input::Image::Pixel& p){
+        	//TODO LUT
+        	return true;
+        }
 
     }  // vision
 }  // modules
--- modules/vision/LUTClassifier/src/LUTClassifier.h
+++ modules/vision/LUTClassifier/src/LUTClassifier.h
@@ -21,6 +21,7 @@
 #define MODULES_VISION_LUTCLASSIFIER_H
 
 #include <nuclear> 
+#include <string>
 #include <armadillo>
 #include "messages/input/Image.h"
 
@@ -34,6 +35,77 @@ namespace modules {
          */
         class LUTClassifier : public NUClear::Reactor {
         private:
+            //Vision constants
+            //Should this be moved to be accessible to other classes? Perhaps partition.
+            int WHITE_SIDE_IS_BLUE;
+            bool NON_WHITE_SIDE_CHECK;
+            int UPPER_WHITE_THRESHOLD;
+            int LOWER_WHITE_THRESHOLD;
+            bool DO_RADIAL_CORRECTION;
+            float RADIAL_CORRECTION_COEFFICIENT;
+            bool THROWOUT_ON_ABOVE_KIN_HOR_GOALS;
+            bool THROWOUT_ON_DISTANCE_METHOD_DISCREPENCY_GOALS;
+            float MAX_DISTANCE_METHOD_DISCREPENCY_GOALS;
+            bool THROWOUT_DISTANT_GOALS;
+            float MAX_GOAL_DISTANCE;
+            bool THROWOUT_INSIGNIFICANT_GOALS;
+            int MIN_TRANSITIONS_FOR_SIGNIFICANCE_GOALS;
+            bool THROWOUT_NARROW_GOALS;
+            int MIN_GOAL_WIDTH;
+            bool THROWOUT_SHORT_GOALS;
+            int MIN_GOAL_HEIGHT;
+            float GOAL_HEIGHT_TO_WIDTH_RATIO_MIN;
+            int GOAL_MAX_OBJECTS;
+            int GOAL_BINS;
+            int GOAL_MIN_THRESHOLD;
+            float GOAL_SDEV_THRESHOLD;
+            float GOAL_RANSAC_MATCHING_TOLERANCE;
+            bool THROWOUT_ON_ABOVE_KIN_HOR_BALL;
+            bool THROWOUT_ON_DISTANCE_METHOD_DISCREPENCY_BALL;
+            float MAX_DISTANCE_METHOD_DISCREPENCY_BALL;
+            bool THROWOUT_SMALL_BALLS;
+            float MIN_BALL_DIAMETER_PIXELS;
+            bool THROWOUT_INSIGNIFICANT_BALLS;
+            int MIN_TRANSITIONS_FOR_SIGNIFICANCE_BALL;
+            bool THROWOUT_DISTANT_BALLS;
+            float MAX_BALL_DISTANCE;
+            bool D2P_INCLUDE_BODY_PITCH;
+            bool BALL_DISTANCE_POSITION_BOTTOM;
+            std::string BALL_DISTANCE_METHOD;
+            std::string GOAL_DISTANCE_METHOD;
+            std::string LINE_METHOD;
+            int BALL_EDGE_THRESHOLD;
+            int BALL_ORANGE_TOLERANCE;
+            float BALL_MIN_PERCENT_ORANGE;
+            float GOAL_MIN_PERCENT_YELLOW;
+            float GOAL_MIN_PERCENT_BLUE;
+            int MIN_GOAL_SEPARATION;
+            int MIN_DISTANCE_FROM_HORIZON;
+            int MIN_CONSECUTIVE_POINTS;
+            float GOAL_WIDTH;
+            float GOAL_HEIGHT;
+            float DISTANCE_BETWEEN_POSTS;
+            float BALL_WIDTH;
+            float CENTRE_CIRCLE_RADIUS;
+            unsigned int HORIZONTAL_SCANLINE_SPACING;
+            unsigned int VERTICAL_SCANLINE_SPACING;
+            unsigned int GREEN_HORIZON_SCAN_SPACING;
+            unsigned int GREEN_HORIZON_MIN_GREEN_PIXELS;
+            unsigned int SAM_MAX_LINES;
+            unsigned int SAM_MIN_POINTS_OVER;
+            unsigned int SAM_MIN_POINTS_TO_LINE;
+            unsigned int SAM_MIN_POINTS_TO_LINE_FINAL;
+            float GREEN_HORIZON_UPPER_THRESHOLD_MULT;
+            float SAM_SPLIT_DISTANCE;
+            float SAM_MAX_ANGLE_DIFF_TO_MERGE;
+            float SAM_MAX_DISTANCE_TO_MERGE;
+            float SAM_MIN_LINE_R2_FIT;
+            float SAM_MAX_LINE_MSD;
+            bool SAM_CLEAR_SMALL;
+            bool SAM_CLEAR_DIRTY;
+            float RANSAC_MAX_ANGLE_DIFF_TO_MERGE;
+            float RANSAC_MAX_DISTANCE_TO_MERGE;
+
             /*! @brief Computes the visual green horizon.
                 Note that the use of kinematics horizon has been replaced by dummmy code 
                 @param image The raw image
@@ -41,7 +113,6 @@ namespace modules {
             std::vector<arma::vec> CalculateGreenHorizon(const messages::input::Image& image);
             
             /*! @brief Generates the scan lines
-                
             */ 
             std::vector<int> GenerateScanLines(const messages::input::Image& image, const std::vector<arma::vec>& green_horizon_points);
             
@@ -61,8 +132,12 @@ namespace modules {
             {
                 return (A.[0] - O.[0]) * (B.[1] - O.[1]) - (A.[1] - O.[1]) * (B.[0] - O.[0]);
             }
+
+            bool IsPixelGreen(const messages::input::Image::Pixel& p);
         public:
             explicit LUTClassifier(std::unique_ptr<NUClear::Environment> environment);
+
+            static constexpr const char* CONFIGURATION_PATH = "VisionConstants.json";
         };
     
     }  // vision
