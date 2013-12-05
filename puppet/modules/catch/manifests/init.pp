841b796b538dc12261901ab22442f1509243b9e2
Alex Biddulph <c3124185@uon.edu.au>
2013-12-05 21:31:15 +1100

    Fixed up catch module for puppet. Should be downloading the correct file now.

--- puppet/modules/catch/manifests/init.pp
+++ puppet/modules/catch/manifests/init.pp
@@ -39,6 +39,6 @@
 class catch {
   wget::fetch { 'catch.hpp':
     destination => '/usr/local/include/catch.hpp',
-    source => 'https://github.com/philsquared/Catch/tree/5ecb72b9bb65cd8fed2aec4da23a3bc21bbccd74/single_include/catch.hpp',
+    source => 'https://raw.github.com/philsquared/Catch/5ecb72b9bb65cd8fed2aec4da23a3bc21bbccd74/single_include/catch.hpp',
   }
 }
