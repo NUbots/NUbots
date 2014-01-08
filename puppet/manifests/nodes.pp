938d85e67a0d860bbcf4dbd2553504299b640663
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2014-01-08 13:45:43 +1100

    WCI-4: Convert NUbugger Vagrant/Puppet config to use a multi-vm setup

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,21 +1,39 @@
-node npvagrant {
+class initial_apt_update {
   # Perform a single `apt-get update` before installing ANY packages.
   exec { "initial_apt_update":
     command => "/usr/bin/apt-get update"
   } -> Package <| |>
+}
+
+node nuclearportvm {
+  include initial_apt_update
 
   # define variables for this node
   $username = 'vagrant'
 
-  class { 'nubugger':
-    username => $username,
+  class { 'nuclearport::build_dep': username => $username, }
+
+  class { 'vim':  username => $username, }
+}
+
+node nubuggervm {
+  include initial_apt_update
+
+  # define variables for this node
+  $username = 'vagrant'
+
+  # these (and most packages) should be virtual resources
+  package { 'build-essential': ensure => latest }
+  package { 'git': ensure => latest }
+
+  file { 'nubots_dir':
+    path => "/home/${username}/nubots",
+    ensure => directory,
+    owner => $username,
+    group => $username,
   }
 
-  class { 'nuclearport::build_dep':
-    username => $username,
-  }
+  class { 'nubugger': username => $username, }
 
-  class { 'vim': 
-    username => $username,
-  }
+  class { 'vim': username => $username, }
 }
