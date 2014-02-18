adafecd3a86ab0c8e6aa860d7e852832536dc6b6
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2014-02-18 15:23:37 +1100

    WCI-9: Improve Puppet config for the base box

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -8,42 +8,64 @@ class initial_apt_update {
 node nuclearportvm {
   include initial_apt_update
 
-  # define variables for this node
+  # # define variables for this node
   $username = 'vagrant'
 
   class { 'nuclearport::build_dep': username => $username, }
 
+  # Non-essential developer tools:
   class { 'vim':  username => $username, }
+  package { 'screen': ensure => latest, }
+  package { 'htop': ensure => latest, }
+  package { 'gdb': ensure => latest, }
 }
 
-node nubuggervm {
-  include initial_apt_update
+# node nubuggervm {
+#   include initial_apt_update
 
-  # define variables for this node
-  $username = 'vagrant'
+#   # define variables for this node
+#   $username = 'vagrant'
 
-  # these (and most packages) should be virtual resources
-  package { 'build-essential': ensure => latest }
-  package { 'git': ensure => latest }
+#   # these (and most packages) should be virtual resources
+#   package { 'build-essential': ensure => latest }
+#   package { 'git': ensure => latest }
 
-  file { 'nubots_dir':
-    path => "/home/${username}/nubots",
-    ensure => directory,
-    owner => $username,
-    group => $username,
-  }
+#   file { 'nubots_dir':
+#     path => "/home/${username}/nubots",
+#     ensure => directory,
+#     owner => $username,
+#     group => $username,
+#   }
 
-  class { 'nubugger': username => $username, }
+#   class { 'nubugger': username => $username, }
 
-  class { 'vim': username => $username, }
-}
+#   class { 'vim': username => $username, }
+# }
 
 node packer-virtualbox-iso, packer-vmware-iso {
   include initial_apt_update
 
   $username = 'vagrant'
 
-  class { 'nuclearport::build_dep': username => $username, }
+  # nuclear::build_dep
+  include gcc48
+  include catch
+  include zmq
+
+  # nuclearport::build_dep
+  package { 'build-essential': ensure => latest }
+  package { 'cmake': ensure => latest }
+  package { 'git': ensure => latest }
+  package { 'openssh-server': ensure => latest }
+  package { ['libprotobuf-dev', 'protobuf-compiler']: ensure => latest }
+  package { 'libespeak-dev': ensure => latest }
+  package { 'librtaudio-dev': ensure => latest }
+  package { 'libncurses5-dev': ensure => latest }
+  package { 'libjpeg-turbo8-dev': ensure => latest }
+  package { 'libfftw3-dev': ensure => latest }
+  package { 'libaubio-dev': ensure => latest }
+  package { 'libsndfile-dev': ensure => latest }
+  package { 'libboost-math-dev': ensure => latest }
 
   # nubugger::build_dep
   package { 'pkg-config': ensure => latest, }
@@ -61,4 +83,5 @@ node packer-virtualbox-iso, packer-vmware-iso {
   class { 'vim':  username => $username, }
   package { 'screen': ensure => latest, }
   package { 'htop': ensure => latest, }
+  package { 'gdb': ensure => latest, }
 }
