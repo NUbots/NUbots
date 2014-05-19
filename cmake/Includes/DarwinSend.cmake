69c6f503a49d737a89b4d2ae506f1b1c2eeefa3b
Brendan Annable <brendan.annable@uon.edu.au>
2014-05-19 14:13:49 +1000

    install.sh has been replaced with install.py which now supports named parameters including the new 'username' parameter

--- cmake/Includes/DarwinSend.cmake
+++ cmake/Includes/DarwinSend.cmake
@@ -1,7 +1,7 @@
 # Custom target to copy and install configuration files and binarys to the robot
 ADD_CUSTOM_TARGET(install
-                  COMMAND bash "${CMAKE_SOURCE_DIR}/cmake/Scripts/install.sh" \${robot} \${config}
-                  DEPENDS ${roles} "${CMAKE_SOURCE_DIR}/cmake/Scripts/install.sh")
+                  COMMAND python "${CMAKE_SOURCE_DIR}/cmake/Scripts/install.py" "--robot_ip=\${robot}" "--config=\${config}" "--username=\${username}"
+                  DEPENDS ${roles} "${CMAKE_SOURCE_DIR}/cmake/Scripts/install.py")
 
 # Custom target to copy and install configuration files and binarys to the robot
 ADD_CUSTOM_TARGET(run
