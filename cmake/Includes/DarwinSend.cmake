43b1b58d15b4cd5574dff0704018da629802e5ba
Trent Houliston <trent@houliston.me>
2014-06-07 04:49:37 +1000

    Removed some old redundant code (that's never used)

--- cmake/Includes/DarwinSend.cmake
+++ cmake/Includes/DarwinSend.cmake
@@ -2,8 +2,3 @@
 ADD_CUSTOM_TARGET(install
                   COMMAND python "${CMAKE_SOURCE_DIR}/cmake/Scripts/install.py" "--robot_ip=\${robot}" "--config=\${config}" "--username=\${username}"
                   DEPENDS ${roles} "${CMAKE_SOURCE_DIR}/cmake/Scripts/install.py")
-
-# Custom target to copy and install configuration files and binarys to the robot
-ADD_CUSTOM_TARGET(run
-                  COMMAND bash "${CMAKE_SOURCE_DIR}/cmake/Scripts/run.sh" \${robot} \${config}
-                  DEPENDS install "${CMAKE_SOURCE_DIR}/cmake/Scripts/run.sh")
