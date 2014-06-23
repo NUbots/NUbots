ef56d4a031ff740e1baaab9695544db52c06c32c
Trent Houliston <trent@houliston.me>
2014-06-23 18:22:09 +1000

    Added some code for the goal detector

--- modules/vision/GoalDetector/src/GoalDetector.cpp
+++ modules/vision/GoalDetector/src/GoalDetector.cpp
@@ -18,17 +18,65 @@
  */
 
 #include "GoalDetector.h"
-#include <nuclear>
+
+#include "messages/vision/ClassifiedImage.h"
 
 namespace modules {
 namespace vision {
 
+    using messages::vision::ObjectClass;
+    using messages::vision::ClassifiedImage;
+
     GoalDetector::GoalDetector(std::unique_ptr<NUClear::Environment> environment)
         : Reactor(std::move(environment)) {
 
+        on<Trigger<ClassifiedImage<ObjectClass>>>([this](const ClassifiedImage<ObjectClass>& image) {
+
+        	// Get our left and right points
+        	std::vector<arma::uvec2> leftPoints;
+        	std::vector<arma::uvec2> rightPoints;
+
+            auto hSegments = image.horizontalSegments.equal_range(ObjectClass::GOAL);
+            auto vSegments = image.verticalSegments.equal_range(ObjectClass::GOAL);
+
+        	for(auto it = hSegments.first; it != hSegments.second; ++it) {
+                // We throw out points if they are:
+                // Less the full quality (subsampled)
+                // Do not have a transition on either side (are on an edge)
+                if(it->second.subsample == 1
+                    && it->second.previous
+                    && it->second.next) {
+
+                    leftPoints.push_back(it->second.start);
+                    rightPoints.push_back(it->second.end);
+                }
+        	}
+
+            // Fit lines to each set of points using ransac
+            // auto lines = ransac<LineModel>(leftPoints);
 
 
-        // Do the things
+            //                                                                                 CONSENSUS_THRESHOLD,
+            //                                                                                 MINIMUM_POINTS,
+            //                                                                                 MAX_ITERATIONS_PER_FITTING,
+            //                                                                                 MAX_FITTING_ATTEMPTS,
+            //                                                                                 SELECTION_METHOD
+
+            // Build quads using the sets of lines
+
+            // Throwout/merge quads
+
+            // Assign a "Left" and "Right" if we can
+
+            // The quads are the goal posts?
+
+
+
+
+        	// Use ransac to find left goalposts (left segment ends)
+
+        	// Use ransac to find right goalposts (right segment ends)
+        });
 
     }
 
