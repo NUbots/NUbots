f4285e38c73d216534425fdcb036a6361a223ca8
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2014-07-11 15:32:25 +1000

    Fix ownership issue in vm_ssh_keys

--- puppet/modules/vm_ssh_keys/manifests/init.pp
+++ puppet/modules/vm_ssh_keys/manifests/init.pp
@@ -4,11 +4,13 @@ class vm_ssh_keys {
         path => '/home/vagrant/.ssh/id_rsa',
         ensure => present,
         source => 'puppet:///modules/vm_ssh_keys/id_rsa',
+        owner => 'vagrant',
     }
 
     file { 'vm_public_key':
         path => '/home/vagrant/.ssh/id_rsa.pub',
         ensure => present,
         source => 'puppet:///modules/vm_ssh_keys/id_rsa.pub',
+        owner => 'vagrant',
     }
 }
