f71f4dc34b7c5f9bb8ff3c8ef080de9b569e0662
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2014-07-14 06:23:57 +0000

    Add error message to MMKFRobotLocalisation when no hypotheses exist

--- modules/localisation/MMKFRobotLocalisation/src/MMKFRobotLocalisation.cpp
+++ modules/localisation/MMKFRobotLocalisation/src/MMKFRobotLocalisation.cpp
@@ -84,9 +84,15 @@ namespace localisation {
            Options<Sync<MMKFRobotLocalisation>>
            >("NUbugger Output", [this](const time_t&, const Sensors& sensors) {
 
+            auto& hypotheses = engine_->robot_models_.hypotheses();
+            if (hypotheses.size() == 0) {
+                NUClear::log<NUClear::ERROR>("MMKFRobotLocalisation has no robot hypotheses.");
+                return;
+            }
+
             auto robots = std::vector<Self>();
 
-            for (auto& model : engine_->robot_models_.hypotheses()) {
+            for (auto& model : hypotheses) {
                 arma::vec::fixed<localisation::robot::RobotModel::size> model_state = model->GetEstimate();
                 auto model_cov = model->GetCovariance();
 
@@ -138,25 +144,25 @@ namespace localisation {
             // if (goals.size() < 2)
             //     return;
 
-            // std::cout << __FILE__ << ", " << __LINE__ << ": " << __func__ << std::endl;
-            for (auto& goal : goals) {
-                // std::cout << __FILE__ << ", " << __LINE__ << ":" << std::endl;
-                // std::cout << "  side:";
-                // std::cout << ((goal.side == Goal::Side::LEFT) ? "LEFT" :
-                //               (goal.side == Goal::Side::RIGHT) ? "RIGHT" : "UNKNOWN")
-                //           << std::endl;
+            // // std::cout << __FILE__ << ", " << __LINE__ << ": " << __func__ << std::endl;
+            // for (auto& goal : goals) {
+            //     // std::cout << __FILE__ << ", " << __LINE__ << ":" << std::endl;
+            //     // std::cout << "  side:";
+            //     // std::cout << ((goal.side == Goal::Side::LEFT) ? "LEFT" :
+            //     //               (goal.side == Goal::Side::RIGHT) ? "RIGHT" : "UNKNOWN")
+            //     //           << std::endl;
 
-                for(uint i = 0; i < goal.measurements.size(); ++i) {
-                    std::stringstream msg;
-                    msg << ((goal.side == Goal::Side::LEFT) ? "LGoal Pos" :
-                           (goal.side == Goal::Side::RIGHT) ? "RGoal Pos" : "UGoal Pos") <<
-                     " " << i;
-                    emit(graph(msg.str(), goal.measurements[i].position[0], goal.measurements[i].position[1], goal.measurements[i].position[2]));
-                    // std::cout << "  measurement: " << num++ << std::endl;
-                    // std::cout << "    position:" << measurement.position.t() << std::endl;
-                    // std::cout << "    error:" << measurement.error << std::endl;
-                }
-            }
+            //     for(uint i = 0; i < goal.measurements.size(); ++i) {
+            //         std::stringstream msg;
+            //         msg << ((goal.side == Goal::Side::LEFT) ? "LGoal Pos" :
+            //                (goal.side == Goal::Side::RIGHT) ? "RGoal Pos" : "UGoal Pos") <<
+            //          " " << i;
+            //         emit(graph(msg.str(), goal.measurements[i].position[0], goal.measurements[i].position[1], goal.measurements[i].position[2]));
+            //         // std::cout << "  measurement: " << num++ << std::endl;
+            //         // std::cout << "    position:" << measurement.position.t() << std::endl;
+            //         // std::cout << "    error:" << measurement.error << std::endl;
+            //     }
+            // }
 
             auto curr_time = NUClear::clock::now();
             engine_->TimeUpdate(curr_time);
