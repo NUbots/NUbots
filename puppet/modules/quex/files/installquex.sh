0ca852ae51bfca808c0c419b5cb96dc73fde9dd0
Trent Houliston <trent@houliston.me>
2014-11-07 01:11:27 +1100

    Updating quex version

--- puppet/modules/quex/files/installquex.sh
+++ puppet/modules/quex/files/installquex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /var/tmp
-wget https://downloads.sourceforge.net/project/quex/DOWNLOAD/quex-0.65.1c.tar.gz -O quex-0.65.1c.tar.gz
-tar -zxf quex-0.65.1c.tar.gz
-mv quex-0.65.1c/ /usr/local/etc/quex
+wget https://downloads.sourceforge.net/project/quex/DOWNLOAD/quex-0.65.2.tar.gz -O quex-0.65.2.tar.gz
+tar -zxf quex-0.65.2.tar.gz
+mv quex-0.65.2/ /usr/local/etc/quex
 ln -s /usr/local/etc/quex/quex/ /usr/local/include/quex
 echo '#!/bin/bash' > /usr/local/bin/quex
 echo 'QUEX_PATH=/usr/local/etc/quex python /usr/local/etc/quex/quex-exe.py "$@"' >> /usr/local/bin/quex
