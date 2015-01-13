2c6567cbe2fa60cacafb81655cdd77660044aa67
Trent Houliston <trent@houliston.me>
2015-01-13 18:48:53 +1100

    Simplified role files so that they no longer need their own name, made it possible to build selected roles (rather then all roles)

--- cmake/Includes/AddRole.cmake
+++ cmake/Includes/AddRole.cmake
@@ -1,27 +1,26 @@
 FUNCTION(ADD_ROLE)
-    SET(options, "")
-    SET(oneValueArgs "NAME")
-    SET(multiValueArgs "MODULES")
-    CMAKE_PARSE_ARGUMENTS(ROLE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    # Custom command that specifies how to generate ${ROLE_NAME}.cpp
+    # Store our role_modules in a sane variable
+    SET(role_modules ${ARGN})
+
+    # Custom command that specifies how to generate ${role}.cpp
     ADD_CUSTOM_COMMAND(
-        OUTPUT "${CMAKE_BINARY_DIR}/roles/${ROLE_NAME}.cpp"
-        COMMAND "${CMAKE_SOURCE_DIR}/cmake/Scripts/generate.py" "${CMAKE_BINARY_DIR}/roles/${ROLE_NAME}.cpp" ${ROLE_MODULES}
-        COMMENT "Generating the ${ROLE_NAME} role"
+        OUTPUT "${CMAKE_BINARY_DIR}/roles/${role}.cpp"
+        COMMAND "${CMAKE_SOURCE_DIR}/cmake/Scripts/generate.py" "${CMAKE_BINARY_DIR}/roles/${role}.cpp" ${role_modules}
+        COMMENT "Generating the ${role} role"
         DEPENDS "${CMAKE_SOURCE_DIR}/cmake/Scripts/generate.py")
 
     INCLUDE_DIRECTORIES("${CMAKE_SOURCE_DIR}")
     INCLUDE_DIRECTORIES("${CMAKE_BINARY_DIR}")
 
-    STRING(REPLACE "::" "" ROLE_MODULES_TARGETS "${ROLE_MODULES}")
+    STRING(REPLACE "::" "" role_module_targets "${role_modules}")
 
-    ADD_EXECUTABLE("${ROLE_NAME}" "${CMAKE_BINARY_DIR}/roles/${ROLE_NAME}.cpp")
-    TARGET_LINK_LIBRARIES(${ROLE_NAME} ${ROLE_MODULES_TARGETS} ${NUBOTS_SHARED_LIBRARIES})
-    SET_PROPERTY(TARGET ${ROLE_NAME} PROPERTY RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/")
+    ADD_EXECUTABLE("${role}" "${CMAKE_BINARY_DIR}/roles/${role}.cpp")
+    TARGET_LINK_LIBRARIES(${role} ${role_module_targets} ${NUBOTS_SHARED_LIBRARIES})
+    SET_PROPERTY(TARGET ${role} PROPERTY RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/")
 
-    SET_PROPERTY(TARGET ${ROLE_NAME} PROPERTY FOLDER "roles/")
+    SET_PROPERTY(TARGET ${role} PROPERTY FOLDER "roles/")
 
-    SET(roles ${roles} ${ROLE_NAME} PARENT_SCOPE)
-    SET(modules ${modules} ${ROLE_MODULES} PARENT_SCOPE)
+    SET(roles ${roles} ${role} PARENT_SCOPE)
+    SET(modules ${modules} ${role_modules} PARENT_SCOPE)
 ENDFUNCTION(ADD_ROLE)
