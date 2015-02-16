cdde16b66864dba471fbf1afd177770177b1a403
Trent Houliston <trent@houliston.me>
2015-02-16 16:36:15 +1100

    Smartly handle some datatypes (such as NUClear types and configuration types) Change our "Output" module for types that we know are coming from NUClear

--- report/graph/build_role_graph.py
+++ report/graph/build_role_graph.py
@@ -65,9 +65,19 @@ class JsonToDot:
             self.graph.add_node(node)
 
             for reaction_output in data['outputs']:
+
+                effective_module_name = module_name
+
+                # Our output may actually belong to us
+                if reaction_output['type'] and reaction_output['type'][0] == 'NUClear':
+                    effective_module_name = 'NUClear'
+                elif (reaction_output['type']) > 3 and reaction_output['type'][:3] == ['messages', 'support', 'Configuration']:
+                    effective_module_name = 'support::configuration::ConfigSystem'
+
                 output_name = self.type_to_str(reaction_output['type'])
+
                 if self.is_valid_edge(output_name):
-                    edge = self._add_output(module_name, output_name)
+                    edge = self._add_output(effective_module_name, output_name)
                     self._check_edge(output_name, edge)
 
             for reaction in data['reactions']:
@@ -79,25 +89,78 @@ class JsonToDot:
                         self._check_edge(input_name, edge)
 
                 for reaction_output in reaction['outputs']:
+                    # Our output may actually belong to us
+                    if reaction_output['type'] and reaction_output['type'][0] == 'NUClear':
+                        effective_module_name = 'NUClear'
+                    elif (reaction_output['type']) > 3 and reaction_output['type'][:3] == ['messages', 'support', 'Configuration']:
+                        effective_module_name = 'support::configuration::ConfigSystem'
+
                     output_name = self.type_to_str(reaction_output['type'])
                     if self.is_valid_edge(output_name):
-                        edge = self._add_output(module_name, output_name)
+                        edge = self._add_output(effective_module_name, output_name)
                         self._check_edge(output_name, edge)
 
     def is_valid_edge(self, name):
-        if 'configuration' in name.lower():
-            return False
-        if 'nuclear' in name.lower():
-            return False
         return True
 
     def type_to_str(self, type_list):
-        flat_list = []
-        for item in type_list:
-            if isinstance(item, basestring):
-                flat_list.append(item)
+
+        # Extract datatypes out of DataFor types
+        if len(type_list) > 2 and type_list[:2] == ['NUClear', 'DataFor']:
+            type_list = type_list[2][0]
+
+        # Remove the allocator from vectors
+        if len(type_list) > 2 and type_list[:2] == ['std', 'vector']:
+            type_list = type_list[:2] + [[type_list[2][0]]]
+
+        # Parse Every into sanity
+        if len(type_list) > 3 and type_list[:3] == ['NUClear', 'dsl', 'Every']:
+            # Get the number of ticks we are doing
+            ticks = int(type_list[3][0][0])
+
+            # Extract our period information
+            period = type_list[3][1]
+            is_per = len(type_list[3][1]) > 3 and period[:3] == ['NUClear', 'dsl', 'Per']
+            if is_per:
+                period = period[3][0]
+            period = period[3][-1][-1]
+            period = ['std', 'chrono'] + {
+                1e-9 : ['nanoseconds'],
+                1e-6 : ['microseconds'],
+                1e-3 : ['milliseconds'],
+                1    : ['seconds'],
+                60   : ['minutes'],
+                3600 : ['hours']
+            }[float(period[0][0][:-2]) / float(period[1][0][:-2])]
+
+            if is_per:
+                type_list = type_list[:3] + [[[str(ticks)], ['NUClear', 'dsl', 'Per', [period]]]]
             else:
+                type_list = type_list[:3] + [[[str(ticks)], period]]
+                pass
+
+            sys.stderr.write("T: {}\nP: {}\nP: {}\n\n".format(ticks, is_per, period))
+
+        flat_list = []
+        short_type = False
+        previous = type_list[0]
+
+        for item in type_list:
+            if isinstance(item, basestring) and (short_type or (item[0].isupper() and item != 'NUClear')):
+                short_type = True
+                flat_list.append(item)
+            elif not isinstance(item, basestring):
+                # If we didn't have a type yet we must use the last one
+                if not short_type:
+                    flat_list.append(previous)
+                    short_type = True
                 flat_list.append('<{}>'.format(','.join([self.type_to_str(subitem) for subitem in item])))
+            else:
+                previous = item
+
+        if not short_type:
+            flat_list.append(previous)
+
 
         return '::'.join(flat_list)
 
