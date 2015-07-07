e6b477927778492d6d6c12da61387296972f2ed7
Trent Houliston <trent@houliston.me>
2015-07-07 17:17:17 +1000

    Needs the dependencies listed explicitly apparently

--- puppet/manifests/nubots.pp
+++ puppet/manifests/nubots.pp
@@ -7,6 +7,15 @@ Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
 node nubotsvm {
 
+  package { 'libboost-system-dev': }
+  package { 'libboost-filesystem-dev': }
+  package { 'libboost-thread-dev': }
+  package { 'libboost-serialization-dev': }
+  package { 'libboost-program-options-dev': }
+  package { 'libboost-test-dev': }
+  package { 'libboost-chrono-dev': }
+  package { 'libboost-date-time-de': }
+
   # We need dev tools
   class {'dev_tools': }
   $toolchain_url = "https://www.dropbox.com/s/gj86fejcncri4ip/nubots-toolchain1.0.0.deb"
@@ -19,7 +28,14 @@ node nubotsvm {
   ~> package { 'nubots-toolchain':
     provider => 'dpkg',
     ensure => 'latest',
-    source => "/tmp/nubots-toolchain.deb"
+    source => "/tmp/nubots-toolchain.deb",
+    require => [Package['libboost-system-dev'],
+                Package['libboost-filesystem-dev'],
+                Package['libboost-thread-dev'],
+                Package['libboost-serialization-dev'],
+                Package['libboost-program-options-dev'],
+                Package['libboost-test-dev'],
+                Package['libboost-chrono-dev'],
+                Package['libboost-date-time-de']]
   }
-
 }
