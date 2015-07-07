c4538ed45fb0b0d6929afdf1ea214ff0ed6906dc
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2015-07-07 14:47:28 +1000

    Update subsumption protocol buffer and its handlers.

--- modules/support/NUbugger/src/Subsumption.cpp
+++ modules/support/NUbugger/src/Subsumption.cpp
@@ -21,6 +21,7 @@
 
 #include "messages/support/nubugger/proto/Message.pb.h"
 #include "messages/behaviour/Action.h"
+#include "messages/input/LimbID.h"
 
 #include "utility/time/time.h"
 
@@ -33,7 +34,25 @@ namespace support {
     using messages::behaviour::ActionKill;
     using messages::behaviour::RegisterAction;
     using messages::behaviour::proto::Subsumption;
-    using messages::behaviour::proto::ActionStateChange;
+
+    using messages::input::LimbID;
+
+    inline Subsumption::Limb getLimb(const LimbID& limb) {
+        switch (limb) {
+            case LimbID::LEFT_LEG:
+                return Subsumption::LEFT_LEG;
+            case LimbID::RIGHT_LEG:
+                return Subsumption::RIGHT_LEG;
+            case LimbID::LEFT_ARM:
+                return Subsumption::LEFT_ARM;
+            case LimbID::RIGHT_ARM:
+                return Subsumption::RIGHT_ARM;
+            case LimbID::HEAD:
+                return Subsumption::HEAD;
+            default:
+                throw std::runtime_error("Invalid Limb");
+        }
+    }
 
     void NUbugger::provideSubsumption() {
         handles["subsumption"].push_back(on<Trigger<ActionStart>>([this](const ActionStart& actionStart) {
@@ -45,10 +64,10 @@ namespace support {
             auto* subsumption = message.mutable_subsumption();
             subsumption->set_type(Subsumption::ACTION_STATE);
             auto* actionStateChange = subsumption->mutable_action_state_change();
-            actionStateChange->set_state(ActionStateChange::START);
+            actionStateChange->set_state(Subsumption::ActionStateChange::START);
             actionStateChange->set_name(actionStart.name);
             for (auto& limbID : actionStart.limbs) {
-                actionStateChange->add_limbs(static_cast<int>(limbID));
+                actionStateChange->add_limbs(getLimb(limbID));
             }
 
             send(message);
@@ -63,10 +82,10 @@ namespace support {
             auto* subsumption = message.mutable_subsumption();
             subsumption->set_type(Subsumption::ACTION_STATE);
             auto* actionStateChange = subsumption->mutable_action_state_change();
-            actionStateChange->set_state(ActionStateChange::KILL);
+            actionStateChange->set_state(Subsumption::ActionStateChange::KILL);
             actionStateChange->set_name(actionKill.name);
             for (auto& limbID : actionKill.limbs) {
-                actionStateChange->add_limbs(static_cast<int>(limbID));
+                actionStateChange->add_limbs(getLimb(limbID));
             }
 
             send(message);
@@ -87,12 +106,13 @@ namespace support {
                 auto* limbSet = actionRegister->add_limb_set();
                 limbSet->set_priority(set.first);
                 for (auto& limbID : set.second) {
-                    limbSet->add_limbs(static_cast<int>(limbID));
+                    limbSet->add_limbs(getLimb(limbID));
                 }
             }
 
             send(message);
         }));
     }
+
 }
 }
