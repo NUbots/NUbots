11ab8f1b40b6b91dd1cd419b24162c552fcf3521
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-07-18 11:09:24 +0300

    Fix YAML error in KinematicsModel

--- modules/platform/darwin/KinematicsModel/src/KinematicsModel.cpp
+++ modules/platform/darwin/KinematicsModel/src/KinematicsModel.cpp
@@ -61,24 +61,24 @@ namespace darwin {
         DarwinModel::Leg::HIP_OFFSET_Y = leg.hipOffset[1];
         DarwinModel::Leg::HIP_OFFSET_Z = leg.hipOffset[2];
 
-        DarwinModel::Leg::UPPER_LEG_LENGTH = leg.upperLegLength = objLeg["upper_leg_length"].as<Expression>();
-        DarwinModel::Leg::LOWER_LEG_LENGTH = leg.lowerLegLength = objLeg["lower_leg_length"].as<Expression>();
+        DarwinModel::Leg::UPPER_LEG_LENGTH = leg.upperLegLength = objLeg["upper_leg_length"].as<float>();
+        DarwinModel::Leg::LOWER_LEG_LENGTH = leg.lowerLegLength = objLeg["lower_leg_length"].as<float>();
 
-        DarwinModel::Leg::HEEL_LENGTH = leg.heelLength = objLeg["heel_length"].as<Expression>();
+        DarwinModel::Leg::HEEL_LENGTH = leg.heelLength = objLeg["heel_length"].as<float>();
 
         DarwinModel::Leg::LENGTH_BETWEEN_LEGS = leg.lengthBetweenLegs = leg.hipOffset[1] * 2;
-        DarwinModel::Leg::FOOT_CENTRE_TO_ANKLE_CENTRE = leg.footCentreToAnkleCentre = objLeg["foot_centre_to_ankle_centre"].as<Expression>();
+        DarwinModel::Leg::FOOT_CENTRE_TO_ANKLE_CENTRE = leg.footCentreToAnkleCentre = objLeg["foot_centre_to_ankle_centre"].as<float>();
 
         auto& foot = leg.foot;
         auto& objFoot = objLeg["foot"];
-        DarwinModel::Leg::FOOT_WIDTH = foot.width = objFoot["width"].as<Expression>();
-        DarwinModel::Leg::FOOT_HEIGHT = foot.height = objFoot["height"].as<Expression>();
-        DarwinModel::Leg::FOOT_LENGTH = foot.length = objFoot["length"].as<Expression>();
-        DarwinModel::Leg::TOE_LENGTH = foot.toeLength = objFoot["toe_length"].as<Expression>();
+        DarwinModel::Leg::FOOT_WIDTH = foot.width = objFoot["width"].as<float>();
+        DarwinModel::Leg::FOOT_HEIGHT = foot.height = objFoot["height"].as<float>();
+        DarwinModel::Leg::FOOT_LENGTH = foot.length = objFoot["length"].as<float>();
+        DarwinModel::Leg::TOE_LENGTH = foot.toeLength = objFoot["toe_length"].as<float>();
     }
 
     void KinematicsModel::configureHead (DarwinKinematicsModel::Head& head, const YAML::Node& objHead) {
-        DarwinModel::Head::CAMERA_DECLINATION_ANGLE_OFFSET = head.cameraDeclinationAngleOffset = objHead["camera_declination_angle_offset"].as<Expression>();
+        DarwinModel::Head::CAMERA_DECLINATION_ANGLE_OFFSET = head.cameraDeclinationAngleOffset = objHead["camera_declination_angle_offset"].as<float>();
         
         head.neckToCamera = objHead["neck_to_camera"].as<arma::vec3>();
         DarwinModel::Head::NECK_TO_CAMERA_X = head.neckToCamera[0];
@@ -88,7 +88,7 @@ namespace darwin {
         auto& neck = head.neck;
         auto& objNeck = objHead["neck"];
 
-        DarwinModel::Head::NECK_LENGTH = neck.length = objNeck["length"].as<Expression>();
+        DarwinModel::Head::NECK_LENGTH = neck.length = objNeck["length"].as<float>();
         
         neck.basePositionFromOrigin = objNeck["base_position_from_origin"].as<arma::vec3>();
         DarwinModel::Head::NECK_BASE_POS_FROM_ORIGIN_X = neck.basePositionFromOrigin[0];
@@ -108,42 +108,42 @@ namespace darwin {
     }
 
     void KinematicsModel::configureArm (DarwinKinematicsModel::Arm& arm, const YAML::Node& objArm) {
-        // arm.distanceBetweenShoulders = objArm["distance_between_shoulders"].as<Expression>();
+        // arm.distanceBetweenShoulders = objArm["distance_between_shoulders"].as<float>();
 
         // auto& shoulder = arm.shoulder;
         auto& objShoulder = objArm["shoulder"];
-        // shoulder.length = objShoulder["length"].as<Expression>();
-        // shoulder.width = objShoulder["width"].as<Expression>();
-        // shoulder.height = objShoulder["height"].as<Expression>();
+        // shoulder.length = objShoulder["length"].as<float>();
+        // shoulder.width = objShoulder["width"].as<float>();
+        // shoulder.height = objShoulder["height"].as<float>();
         // shoulder.offset = objShoulder["offset"].as<arma::vec2>();
 
         // auto& upperArm = arm.upperArm;
         auto& objUpperArm = objArm["upper_arm"];
-        // upperArm.length = objUpperArm["length"].as<Expression>();
+        // upperArm.length = objUpperArm["length"].as<float>();
         // upperArm.offset = objUpperArm["offset"].as<arma::vec2>();
 
         // auto& lowerArm = arm.lowerArm;
         auto& objLowerArm = objArm["lower_arm"];
-        // lowerArm.length = objLowerArm["length"].as<Expression>();
+        // lowerArm.length = objLowerArm["length"].as<float>();
         // lowerArm.offset = objLowerArm["offset"].as<arma::vec2>();
 
-        DarwinModel::Arm::DISTANCE_BETWEEN_SHOULDERS = objArm["distance_between_shoulders"].as<Expression>();
+        DarwinModel::Arm::DISTANCE_BETWEEN_SHOULDERS = objArm["distance_between_shoulders"].as<float>();
         DarwinModel::Arm::SHOULDER_Z_OFFSET = objShoulder["offset"].as<arma::vec2>()[1];
         DarwinModel::Arm::SHOULDER_X_OFFSET = objShoulder["offset"].as<arma::vec2>()[0];
-        DarwinModel::Arm::SHOULDER_LENGTH = objShoulder["length"].as<Expression>();
-        DarwinModel::Arm::SHOULDER_WIDTH = objShoulder["width"].as<Expression>();
-        DarwinModel::Arm::SHOULDER_HEIGHT = objShoulder["height"].as<Expression>();
-        DarwinModel::Arm::UPPER_ARM_LENGTH = objUpperArm["length"].as<Expression>();
+        DarwinModel::Arm::SHOULDER_LENGTH = objShoulder["length"].as<float>();
+        DarwinModel::Arm::SHOULDER_WIDTH = objShoulder["width"].as<float>();
+        DarwinModel::Arm::SHOULDER_HEIGHT = objShoulder["height"].as<float>();
+        DarwinModel::Arm::UPPER_ARM_LENGTH = objUpperArm["length"].as<float>();
         DarwinModel::Arm::UPPER_ARM_Y_OFFSET = objUpperArm["offset"].as<arma::vec2>()[0];
         DarwinModel::Arm::UPPER_ARM_Z_OFFSET = objUpperArm["offset"].as<arma::vec2>()[1];
-        DarwinModel::Arm::LOWER_ARM_LENGTH = objLowerArm["length"].as<Expression>();
+        DarwinModel::Arm::LOWER_ARM_LENGTH = objLowerArm["length"].as<float>();
         DarwinModel::Arm::LOWER_ARM_Y_OFFSET = objLowerArm["offset"].as<arma::vec2>()[0];
         DarwinModel::Arm::LOWER_ARM_Z_OFFSET = objLowerArm["offset"].as<arma::vec2>()[1];
     }
 
     void KinematicsModel::configureMassModel (DarwinKinematicsModel::MassModel& massModel, const YAML::Node& objMassModel) {
-    	// massModel.numberOfMasses = objMassModel["number_of_masses"].as<Expression>();
-     //    massModel.massRepresentationDimension = objMassModel["mass_representation_dimension"].as<Expression>();
+    	// massModel.numberOfMasses = objMassModel["number_of_masses"].as<float>();
+     //    massModel.massRepresentationDimension = objMassModel["mass_representation_dimension"].as<float>();
 
         // auto& masses = massModel.masses;
         auto& objMasses = objMassModel["masses"];
