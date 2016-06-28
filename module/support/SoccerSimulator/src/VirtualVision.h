4ceef738ebd3a5e2c3323bcb05f28b95ada67077
Josiah Walker <merlz42@gmail.com>
2016-06-28 18:41:02 +1000

    add virtual vision goal models

--- module/support/SoccerSimulator/src/VirtualVision.h
+++ module/support/SoccerSimulator/src/VirtualVision.h
@@ -67,7 +67,6 @@ class VirtualGoalPost {
 			} else {
 				result[2] = 1.0;
 				const double ndiff = norm1[0] * norm2[1] - norm1[1] * norm2[0];
-				result[2] = 1.0;
 				result[1] = (norm1[2] * norm2[0] - norm1[0] * norm2[2]) / ndiff;
 				result[0] = (-norm1[2] - norm1[1] * result[1]) / norm1[0];
 				if (result[0] < 0.0) {
@@ -104,47 +103,83 @@ class VirtualGoalPost {
 					const FieldDescription& field){
 			Goal result;
 
+
 			//push the new measurement types
 			//TODO: simulate not having values if off screen
+			//std::cout << this->position;
 			arma::mat::fixed<3,4> goalNormals = cameraSpaceGoalProjection(robotPose, this->position, field, *sensors);
-			result.measurements.push_back(
-				std::make_pair<Goal::MeasurementType, arma::vec3>(
-							Goal::MeasurementType::LEFT_NORMAL,
-							goalNormals.col(0)
-						)
-				);
-			result.measurements.push_back(
-				std::make_pair<Goal::MeasurementType, arma::vec3>(
-							Goal::MeasurementType::RIGHT_NORMAL,
-							goalNormals.col(1)
-						)
-				);
-			result.measurements.push_back(
-				std::make_pair<Goal::MeasurementType, arma::vec3>(
-							Goal::MeasurementType::TOP_NORMAL,
-							goalNormals.col(2)
-						)
-				);
-			result.measurements.push_back(
-				std::make_pair<Goal::MeasurementType, arma::vec3>(
-							Goal::MeasurementType::BASE_NORMAL,
-							goalNormals.col(3)
-						)
-				);
+			if (arma::any(arma::any(goalNormals > 0.0))) {
+				result.measurements.push_back(
+					std::make_pair<Goal::MeasurementType, arma::vec3>(
+								Goal::MeasurementType::LEFT_NORMAL,
+								goalNormals.col(0)
+							)
+					);
+				result.measurements.push_back(
+					std::make_pair<Goal::MeasurementType, arma::vec3>(
+								Goal::MeasurementType::RIGHT_NORMAL,
+								goalNormals.col(1)
+							)
+					);
+				result.measurements.push_back(
+					std::make_pair<Goal::MeasurementType, arma::vec3>(
+								Goal::MeasurementType::TOP_NORMAL,
+								goalNormals.col(2)
+							)
+					);
+				result.measurements.push_back(
+					std::make_pair<Goal::MeasurementType, arma::vec3>(
+								Goal::MeasurementType::BASE_NORMAL,
+								goalNormals.col(3)
+							)
+					);
 
-			//build the predicted quad
-            utility::math::geometry::Quad quad(
-            		getCamRay(goalNormals.col(0), goalNormals.col(3), camParams.focalLengthPixels, camParams.imageSizePixels),
-            		getCamRay(goalNormals.col(0), goalNormals.col(2), camParams.focalLengthPixels, camParams.imageSizePixels),
-            		getCamRay(goalNormals.col(1), goalNormals.col(2), camParams.focalLengthPixels, camParams.imageSizePixels),
-            		getCamRay(goalNormals.col(1), goalNormals.col(3), camParams.focalLengthPixels, camParams.imageSizePixels)
-            	);
+				//build the predicted quad
+	            utility::math::geometry::Quad quad(
+	            		getCamRay(goalNormals.col(0), goalNormals.col(3), camParams.focalLengthPixels, camParams.imageSizePixels),
+	            		getCamRay(goalNormals.col(0), goalNormals.col(2), camParams.focalLengthPixels, camParams.imageSizePixels),
+	            		getCamRay(goalNormals.col(1), goalNormals.col(2), camParams.focalLengthPixels, camParams.imageSizePixels),
+	            		getCamRay(goalNormals.col(1), goalNormals.col(3), camParams.focalLengthPixels, camParams.imageSizePixels)
+	            	);
 
+	        	//goal base visibility check
+	        	if (not (quad.getBottomRight()[1] > 0 && quad.getBottomRight()[1] < camParams.imageSizePixels[1]
+	        			&& quad.getBottomLeft()[1] > 0 && quad.getBottomLeft()[1] < camParams.imageSizePixels[1]) ) {
+
+	        		result.measurements.erase(result.measurements.begin()+3);
+	        	}
+	        	//goal top visibility check
+	        	if (not (quad.getTopRight()[1] > 0 && quad.getTopRight()[1] < camParams.imageSizePixels[1]
+	        			&& quad.getTopLeft()[1] > 0 && quad.getTopLeft()[1] < camParams.imageSizePixels[1]) ) {
+
+	        		result.measurements.erase(result.measurements.begin()+2);
+	        	}
+	        	//goal sides visibility check
+	            if ( not (((quad.getBottomLeft()[0] > 0 && quad.getBottomLeft()[0] < camParams.imageSizePixels[0] 
+	             && quad.getBottomRight()[0] > 0 && quad.getBottomRight()[0] < camParams.imageSizePixels[0])
+	             || (quad.getTopLeft()[0] > 0 && quad.getTopLeft()[0] < camParams.imageSizePixels[0] 
+	             && quad.getTopRight()[0] > 0 && quad.getTopRight()[0] < camParams.imageSizePixels[0] ))
+	             && (quad.getBottomRight()[1] < 0 && quad.getTopRight()[1] > camParams.imageSizePixels[1]
+	             || quad.getBottomLeft()[1] < 0 && quad.getTopLeft()[1] > camParams.imageSizePixels[1]))
+
+				) {
+	            	result.measurements.erase(result.measurements.begin()+1);
+	            	result.measurements.erase(result.measurements.begin());
+	            }
+	            if (!result.measurements.empty()) {
+					result.quad = quad;
+				}	
+				/*std::cout << result.measurements.size() << std::endl 
+						  << quad.getTopLeft() << std::endl 
+						  << quad.getTopRight()  << std::endl 
+						  << quad.getBottomLeft() << std::endl
+						  << quad.getBottomRight() << std::endl;*/
+			}
 			result.sensors = sensors;
 			result.timestamp = sensors->timestamp; // TODO: Eventually allow this to be different to sensors.
 			result.side = side;
 			result.team = team;
-			result.quad = quad;
+
 			//If no measurements are in the goal, then it was not observed
 			return result;
 		}
