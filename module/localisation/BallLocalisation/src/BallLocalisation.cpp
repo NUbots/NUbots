4e1ef1dab1a62d9e334bff3b2ad6327f3789fffc
ejcatt <ejcatt@gmail.com>
2016-06-29 17:03:48 +0200

    Fixed all issue with walk path planner

--- module/localisation/BallLocalisation/src/BallLocalisation.cpp
+++ module/localisation/BallLocalisation/src/BallLocalisation.cpp
@@ -1,16 +1,19 @@
 #include "BallLocalisation.h"
 
 #include "message/support/Configuration.h"
+#include "message/localisation/FieldObject.h"
 
 namespace module {
 namespace localisation {
 
     using message::support::Configuration;
+    using message::localisation::Ball;
 
     BallLocalisation::BallLocalisation(std::unique_ptr<NUClear::Environment> environment)
     : Reactor(std::move(environment)) {
 
         on<Configuration>("BallLocalisation.yaml").then([this] (const Configuration&) {
+        	emit(std::make_unique<std::vector<Ball>>());
             // Use configuration here from file BallLocalisation.yaml
         });
     }
