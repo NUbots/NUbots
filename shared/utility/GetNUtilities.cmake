0dec339d3b32fc323a859a1ad1b0d9a495e1075f
Jake Fountain <jake.fountain@uon.edu.au>
2016-07-22 15:52:41 +1000

    Need to somehow get the location of GetNUtilities.cmake from within the file. Currently hacked around this by setting variable on call

--- shared/utility/GetNUtilities.cmake
+++ shared/utility/GetNUtilities.cmake
@@ -1,9 +1,16 @@
 #GetNUtilities.cmake
+MESSAGE("-- Configuring NUtilities...")
+
+SET(NUTILITIES_SRC_FILES "")
 
 # Get source files based on configuration of libraries and project
 IF(${PROJECT_NAME} MATCHES NUbots)
-	MESSAGE("Including NUbots utilities...")
-	FILE(GLOB_RECURSE src "*/**.cpp" "*/**.c" "*/**.h")
+	MESSAGE("-- Using NUbots utilities...")
+	FILE(GLOB_RECURSE NUTILITIES_SRC_FILES 
+		"${NUTILITIES_DIR}/*/**.cpp" 
+		"${NUTILITIES_DIR}/*/**.c" 
+		"${NUTILITIES_DIR}/*/**.h")
 ENDIF()
 
-SET(NUTILITIES_SRC_FILES ${src})
+MESSAGE(CURRENT_DIRECTORY ${CURRENT_DIRECTORY})
+
