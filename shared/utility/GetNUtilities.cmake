8853b246946fa87b13dee95d78d3644521dcdad4
Jake Fountain <jake.fountain@uon.edu.au>
2016-07-25 16:33:03 +1000

    NUClear roles updated to support nutilities

--- shared/utility/GetNUtilities.cmake
+++ shared/utility/GetNUtilities.cmake
@@ -1,11 +1,11 @@
 #GetNUtilities.cmake
-MESSAGE("-- Configuring NUtilities...")
+MESSAGE("** Configuring NUtilities...")
 
 SET(NUTILITIES_SRC_FILES "")
 
 # Get source files based on configuration of libraries and project
 IF(${PROJECT_NAME} MATCHES NUbots)
-	MESSAGE("-- Using NUbots utilities...")
+	MESSAGE("** Using NUbots utilities...")
 	FILE(GLOB_RECURSE NUTILITIES_SRC_FILES 
 		"${NUTILITIES_DIR}/*/**.cpp" 
 		"${NUTILITIES_DIR}/*/**.c" 
