41ee86ff4efa6860bde34e0d9c0b101120d57421
Alex Biddulph <c3124185@uon.edu.au>
2016-11-17 12:23:45 +1100

    Fixed naming issue with generated installer targets.

--- cmake/Modules/RobotSend.cmake
+++ cmake/Modules/RobotSend.cmake
@@ -6,7 +6,7 @@ SEPARATE_ARGUMENTS(KNOWN_HOSTS UNIX_COMMAND "${HOSTS}")
 
 # Ninja code!
 FOREACH(host ${KNOWN_HOSTS})
-    FOREACH(config "" -new -update -overwrite -pull -ignore)
+    FOREACH(config "" new update overwrite pull ignore)
         IF ("${host}" MATCHES "i[0-9]+")
             SET(user "nubots")
         ELSE()
@@ -14,14 +14,14 @@ FOREACH(host ${KNOWN_HOSTS})
         ENDIF()
         
         # Make our installer
-        ADD_CUSTOM_TARGET("${host}${config}"
+        ADD_CUSTOM_TARGET("${host}-${config}"
             USES_TERMINAL
             COMMAND ${PYTHON_EXECUTABLE}
             "${CMAKE_SOURCE_DIR}/nuclear/b.py" "install" "${host}" "--config=${config}" "--user=${user}"
             DEPENDS ${NUCLEAR_ROLES} "${CMAKE_SOURCE_DIR}/tools/install.py")
 
         # Move our installer to an IDE group
-        SET_PROPERTY(TARGET "${host}${config}" PROPERTY FOLDER "installers")
+        SET_PROPERTY(TARGET "${host}-${config}" PROPERTY FOLDER "installers")
     ENDFOREACH(config)
 ENDFOREACH(host)
 
