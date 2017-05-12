a89fc556debe28ed699b33b747aaec158797209a
Brendan Annable <brendan@annable.id.au>
2017-05-13 00:32:09 +1000

    Add jest as test runner and travis config (NUbots/NUsight2#5)

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', 'src/global.d.ts'],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/tests/**',
+    '!tools/**'
+  ],
+  globals: {
+    __TS_CONFIG__: './tsconfig.test.json'
+  },
+  mapCoverage: true,
+  moduleDirectories: [
+    'node_modules',
+    '<rootDir>/src'
+  ],
+  moduleFileExtensions: [
+    'js',
+    'ts',
+    'tsx'
+  ],
+  moduleNameMapper: {
+    '\\.(css)$': 'identity-obj-proxy'
+  },
+  roots: [
+    '<rootDir>/src'
+  ],
+  modulePaths: [
+    '<rootDir>/src'
+  ],
+  testMatch: [
+    '**/tests/**/*.tests.{ts,tsx}'
+  ],
+  transform: {
+    '.(ts|tsx)': '<rootDir>/node_modules/ts-jest/preprocessor.js'
+  }
+}
+
---
+++ src/client/components/localisation/tests/presenter.tests.ts
@@ -0,0 +1,34 @@
+import { LocalisationModel } from '../model'
+import { LocalisationPresenter } from '../presenter'
+import { ViewMode } from '../model'
+
+describe('LocalisationPresenter', () => {
+  let presenter, model
+
+  beforeEach(() => {
+    model = LocalisationModel.of()
+    presenter = new LocalisationPresenter({ model })
+  })
+
+  describe('clicking the hawk eye button', () => {
+    beforeEach(() => {
+      presenter.onHawkEyeClick()
+    })
+
+    it('resets yaw to 0', () => {
+      expect(model.camera.yaw).toEqual(0)
+    })
+
+    it('resets pitch to looking vertically down', () => {
+      expect(model.camera.pitch).toEqual(-Math.PI / 2)
+    })
+
+    it('moves camera above the field', () => {
+      expect(model.camera.position).toEqual({ x: 0, y: 5, z: 0 })
+    })
+
+    it('resets viewing mode to no clip', () => {
+      expect(model.viewMode).toEqual(ViewMode.NO_CLIP)
+    })
+  })
+})
