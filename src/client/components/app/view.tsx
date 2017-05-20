3a025e109fdab1e9cbb6343c8d219cfee70ff5ed
Brendan Annable <brendan@annable.id.au>
2017-05-20 17:49:55 +1000

    Remove mobx react devtools (NUbots/NUsight2#35)
    * Use the chrome extension instead:
        https://chrome.google.com/webstore/detail/mobx-developer-tools/pfgnfdagidkfgccljigdamigbcnndkod?hl=en

--- src/client/components/app/view.tsx
+++ src/client/components/app/view.tsx
@@ -10,17 +10,9 @@ export class AppView extends React.Component<any, any> {
           <div className={style.app__container}>
             <div className={style.app__content}>
               {this.props.children}
-              {this.renderDevTool()}
             </div>
           </div>
         </div>
     )
-  }
-
-  private renderDevTool() {
-    if (process.env.NODE_ENV !== 'production') {
-      const DevTools = require('mobx-react-devtools').default
-      return <DevTools />
-    }
   }
 }
