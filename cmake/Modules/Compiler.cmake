9fb6ab8f4522001cf13ee11bc7cdc65ae160101c
Trent Houliston <trent@houliston.me>
2017-06-07 15:09:14 +1000

    Remove more references of armadillo from the codebase

--- cmake/Modules/Compiler.cmake
+++ cmake/Modules/Compiler.cmake
@@ -3,11 +3,9 @@
 ADD_COMPILE_OPTIONS(-Wall
                     -Wpedantic
                     -Wextra
-                    # -Werror
                     -fnon-call-exceptions)
 
 LIST(APPEND CMAKE_INSTALL_RPATH toolchain/)
-LIST(APPEND CMAKE_INSTALL_RPATH /home/darwin/toolchain/)
 LIST(APPEND CMAKE_INSTALL_RPATH /home/nubots/toolchain/)
 LIST(APPEND CMAKE_INSTALL_RPATH /nubots/toolchain/${PLATFORM}/lib/)
 LIST(APPEND CMAKE_INSTALL_RPATH /nubots/toolchain/lib/)
@@ -20,8 +18,4 @@ IF(CMAKE_CXX_COMPILER_ID MATCHES GNU)
     ENDIF()
 ENDIF()
 
-SET(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_RPATH} /home/darwin/toolchain/ /home/nubots/toolchain/)
-
-# Disable armadillo bounds checking in release and min size release builds
-SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -DARMA_NO_DEBUG")
-SET(CMAKE_CXX_FLAGS_MINSIZEREL "${CMAKE_CXX_FLAGS_MINSIZEREL} -DARMA_NO_DEBUG")
+SET(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_RPATH} /home/nubots/toolchain/)
