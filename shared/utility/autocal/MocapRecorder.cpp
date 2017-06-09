492bc4a939b39f19f60f9ac598cf5b867a46e56a
Alex Biddulph <c3124185@uon.edu.au>
2017-06-09 16:36:11 +1000

    Manual step
    Replace arma dynamic matrix with eigen. Correct col initialisation loop.

--- shared/utility/autocal/MocapRecorder.cpp
+++ shared/utility/autocal/MocapRecorder.cpp
@@ -4,7 +4,8 @@ using utility::math::matrix::Transform3D;
 using utility::math::matrix::Rotation3D;
 
 bool MocapRecorder::saveFrame(const std::vector<Transform3D>& poses){
-    arma::mat rigidBodies(13,poses.size());
+    Eigen::MatrixXd rigidBodies(13, poses.size());
+
 	int i = 0;
 	for (const auto& pose : poses) {
 
@@ -13,9 +14,9 @@ bool MocapRecorder::saveFrame(const std::vector<Transform3D>& poses){
         float y = pose.translation()[1];
         float z = pose.translation()[2];
         const Rotation3D& r = pose.rotation();
-        rigidBodies.col(i++) = arma::vec({double(id),x,y,z, r.row(0)[0],r.row(0)[1],r.row(0)[2],
-            										r.row(1)[0],r.row(1)[1],r.row(1)[2],
-            										r.row(2)[0],r.row(2)[1],r.row(2)[2]});
+        rigidBodies.col(i++) << double(id), x, y, z, r.row(0)[0], r.row(0)[1], r.row(0)[2],
+                                                     r.row(1)[0], r.row(1)[1], r.row(1)[2],
+                                                     r.row(2)[0], r.row(2)[1], r.row(2)[2];
     }
     auto now = std::chrono::system_clock::now();
     std::stringstream filename;
