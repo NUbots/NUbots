8ec308bae5f7d4472bf5a2762beb13ba5d70e95e
Alex Biddulph <c3124185@uon.edu.au>
2017-06-16 21:00:53 +1000

    Fixes code style

--- module/tools/FirmwareInstaller/src/FirmwareInstaller.cpp
+++ module/tools/FirmwareInstaller/src/FirmwareInstaller.cpp
@@ -13,163 +13,158 @@
 namespace module {
 namespace tools {
 
-using extension::Configuration;
+    using extension::Configuration;
 
-struct FlashCM730 {};
+    struct FlashCM730 {};
 
-FirmwareInstaller::FirmwareInstaller(
-    std::unique_ptr<NUClear::Environment> environment)
-    : Reactor(std::move(environment)), device("UNKNOWN"), ignore_inputs(false) {
-  on<Configuration>("FirmwareInstaller.yaml")
-      .then([this](const Configuration& config) {
-        device = config["device"].as<std::string>();
+    FirmwareInstaller::FirmwareInstaller(std::unique_ptr<NUClear::Environment> environment)
+        : Reactor(std::move(environment)), device("UNKNOWN"), ignore_inputs(false) {
+        on<Configuration>("FirmwareInstaller.yaml").then([this](const Configuration& config) {
+            device = config["device"].as<std::string>();
 
-        for (auto& f : config["firmware"].config) {
-          std::string name = f.first.as<std::string>();
-          std::string path = f.second.as<std::string>();
+            for (auto& f : config["firmware"].config) {
+                std::string name = f.first.as<std::string>();
+                std::string path = f.second.as<std::string>();
 
-          Firmware fw;
+                Firmware fw;
 
-          if (utility::file::exists(path)) {
-            auto ifs = std::ifstream(path, std::ios::in | std::ios::binary);
+                if (utility::file::exists(path)) {
+                    auto ifs = std::ifstream(path, std::ios::in | std::ios::binary);
 
-            fw.firmware =
-                std::vector<uint8_t>((std::istreambuf_iterator<char>(ifs)),
-                                     std::istreambuf_iterator<char>());
-            fw.checksum = std::accumulate(fw.firmware.begin(),
-                                          fw.firmware.end(), uint8_t(0));
+                    fw.firmware =
+                        std::vector<uint8_t>((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+                    fw.checksum = std::accumulate(fw.firmware.begin(), fw.firmware.end(), uint8_t(0));
 
-            firmwares.insert(std::make_pair(name, fw));
-          } else {
-            log<NUClear::WARN>("The firmware file", path, "for", name,
-                               "does not exist");
-          }
-        }
-      });
+                    firmwares.insert(std::make_pair(name, fw));
+                }
+                else {
+                    log<NUClear::WARN>("The firmware file", path, "for", name, "does not exist");
+                }
+            }
+        });
 
-  auto showMenu = [this]() -> void {
-    std::cout << "\n\n" << std::endl;
-    std::cout << "***********************************" << std::endl;
-    std::cout << "* Welcome to NUfirmware Installer *" << std::endl;
-    std::cout << "***********************************" << std::endl;
-    std::cout << "\tType \"CM730\" to flash CM730 firmware." << std::endl;
-    std::cout << "\tType \"DYNXL\" to flash Dynamixel firmware." << std::endl;
-    std::cout << "\tType \"QUIT\" to quit." << std::endl;
-  };
+        auto showMenu = [this]() -> void {
+            std::cout << "\n\n" << std::endl;
+            std::cout << "***********************************" << std::endl;
+            std::cout << "* Welcome to NUfirmware Installer *" << std::endl;
+            std::cout << "***********************************" << std::endl;
+            std::cout << "\tType \"CM730\" to flash CM730 firmware." << std::endl;
+            std::cout << "\tType \"DYNXL\" to flash Dynamixel firmware." << std::endl;
+            std::cout << "\tType \"QUIT\" to quit." << std::endl;
+        };
 
-  showMenu();
+        showMenu();
 
-  on<IO>(STDIN_FILENO, IO::READ).then([this, &showMenu] {
+        on<IO>(STDIN_FILENO, IO::READ).then([this, &showMenu] {
 
-    if (!ignore_inputs) {
-      std::string input;
-      std::cin >> input;
+            if (!ignore_inputs) {
+                std::string input;
+                std::cin >> input;
 
-      // Convert input to uppercase.
-      std::transform(input.begin(), input.end(), input.begin(),
-                     [](const char& c) -> char { return std::toupper(c); });
+                // Convert input to uppercase.
+                std::transform(
+                    input.begin(), input.end(), input.begin(), [](const char& c) -> char { return std::toupper(c); });
 
-      if (input.compare("CM730") == 0) {
-        emit(std::make_unique<FlashCM730>());
-      }
+                if (input.compare("CM730") == 0) {
+                    emit(std::make_unique<FlashCM730>());
+                }
 
-      else if (input.compare("DYNXL") == 0) {
-        std::cout << "Too bad. We haven't implemented this yet." << std::endl;
-      }
+                else if (input.compare("DYNXL") == 0) {
+                    std::cout << "Too bad. We haven't implemented this yet." << std::endl;
+                }
 
-      else if (input.compare("QUIT") == 0) {
-        std::cout << "Bye." << std::endl;
-        powerplant.shutdown();
-      }
+                else if (input.compare("QUIT") == 0) {
+                    std::cout << "Bye." << std::endl;
+                    powerplant.shutdown();
+                }
 
-      else {
-        std::cout << "Unknown input. Try again." << std::endl;
-      }
+                else {
+                    std::cout << "Unknown input. Try again." << std::endl;
+                }
 
-      showMenu();
+                showMenu();
+            }
+        });
+
+        on<Trigger<FlashCM730>, Sync<FirmwareInstaller>>().then([this] {
+
+            // Make sure the user can't quit part way through the process.
+            ignore_inputs = true;
+
+            const auto& it = firmwares.find("CM730");
+
+            if (it == firmwares.end()) {
+                log<NUClear::WARN>("Unable to find CM730 firmware. Check your config file.");
+            }
+
+            else {
+                const auto& cm730 = it->second;
+
+                // Open the UART
+                utility::io::uart uart(device, 57600);
+
+                pollfd pfd{uart.native_handle(), POLLIN, 0};
+
+                log<NUClear::INFO>("Please press the CM730 reset button to begin...");
+
+                // Wait for connection to the CM730 Bootloader
+                char send = '#';
+                char recv = '\0';
+                while (recv != '#' && powerplant.running()) {
+                    uart.write(&send, sizeof(send));
+
+                    // Wait for some action!
+                    poll(&pfd, 1, 10);
+                    uart.read(&recv, sizeof(recv));
+                }
+
+                // Check we are good to go
+                if (recv == '#') {
+                    // Enter erase block mode and erase
+                    uart.write("l\r", 2);
+
+                    // Wait for the block to be erased
+                    int read = 1;
+                    char buff[256];
+                    while (read != 0) {
+                        poll(&pfd, 1, 100);
+                        read = uart.read(buff, sizeof(buff));
+
+                        std::string text(buff, read);
+                        log<NUClear::INFO>(text);
+                    }
+
+                    log<NUClear::INFO>("Erase block complete...");
+
+                    // Give the bootloader time to catch its breath
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+                    // TODO write in such a way that you get progress
+                    uart.write(cm730.firmware.data(), cm730.firmware.size());
+
+                    // Write the checksum
+                    uart.write(&cm730.checksum, sizeof(cm730.checksum));
+
+                    // Wait for the device to finish processing the firmware
+                    while (read != 0) {
+                        poll(&pfd, 1, 10);
+                        read = uart.read(buff, sizeof(buff));
+
+                        std::string text(buff, read);
+                        log<NUClear::INFO>(text);
+                    }
+
+                    // Exit the bootloader
+                    uart.write("\rgo\r", 4);
+
+                    // Close our uart
+                    uart.close();
+                }
+            }
+
+            ignore_inputs = false;
+        });
     }
-  });
-
-  on<Trigger<FlashCM730>, Sync<FirmwareInstaller>>().then([this] {
-
-    // Make sure the user can't quit part way through the process.
-    ignore_inputs = true;
-
-    const auto& it = firmwares.find("CM730");
-
-    if (it == firmwares.end()) {
-      log<NUClear::WARN>(
-          "Unable to find CM730 firmware. Check your config file.");
-    }
-
-    else {
-      const auto& cm730 = it->second;
-
-      // Open the UART
-      utility::io::uart uart(device, 57600);
-
-      pollfd pfd{uart.native_handle(), POLLIN, 0};
-
-      log<NUClear::INFO>("Please press the CM730 reset button to begin...");
-
-      // Wait for connection to the CM730 Bootloader
-      char send = '#';
-      char recv = '\0';
-      while (recv != '#' && powerplant.running()) {
-        uart.write(&send, sizeof(send));
-
-        // Wait for some action!
-        poll(&pfd, 1, 10);
-        uart.read(&recv, sizeof(recv));
-      }
-
-      // Check we are good to go
-      if (recv == '#') {
-        // Enter erase block mode and erase
-        uart.write("l\r", 2);
-
-        // Wait for the block to be erased
-        int read = 1;
-        char buff[256];
-        while (read != 0) {
-          poll(&pfd, 1, 100);
-          read = uart.read(buff, sizeof(buff));
-
-          std::string text(buff, read);
-          log<NUClear::INFO>(text);
-        }
-
-        log<NUClear::INFO>("Erase block complete...");
-
-        // Give the bootloader time to catch its breath
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-        // TODO write in such a way that you get progress
-        uart.write(cm730.firmware.data(), cm730.firmware.size());
-
-        // Write the checksum
-        uart.write(&cm730.checksum, sizeof(cm730.checksum));
-
-        // Wait for the device to finish processing the firmware
-        while (read != 0) {
-          poll(&pfd, 1, 10);
-          read = uart.read(buff, sizeof(buff));
-
-          std::string text(buff, read);
-          log<NUClear::INFO>(text);
-        }
-
-        // Exit the bootloader
-        uart.write("\rgo\r", 4);
-
-        // Close our uart
-        uart.close();
-      }
-    }
-
-    ignore_inputs = false;
-  });
-}
 
 }  // namespace tools
 }  // namespace module
--- module/tools/FirmwareInstaller/src/FirmwareInstaller.h
+++ module/tools/FirmwareInstaller/src/FirmwareInstaller.h
@@ -8,23 +8,23 @@
 namespace module {
 namespace tools {
 
-class FirmwareInstaller : public NUClear::Reactor {
- public:
-  /// @brief Called by the powerplant to build and setup the FirmwareInstaller
-  /// reactor.
-  explicit FirmwareInstaller(std::unique_ptr<NUClear::Environment> environment);
+    class FirmwareInstaller : public NUClear::Reactor {
+    public:
+        /// @brief Called by the powerplant to build and setup the FirmwareInstaller
+        /// reactor.
+        explicit FirmwareInstaller(std::unique_ptr<NUClear::Environment> environment);
 
- private:
-  std::string device;
-  struct Firmware {
-    std::vector<uint8_t> firmware;
-    uint8_t checksum;
-  };
+    private:
+        std::string device;
+        struct Firmware {
+            std::vector<uint8_t> firmware;
+            uint8_t checksum;
+        };
 
-  std::map<std::string, Firmware> firmwares;
+        std::map<std::string, Firmware> firmwares;
 
-  bool ignore_inputs;
-};
+        bool ignore_inputs;
+    };
 
 }  // namespace tools
 }  // namespace module
