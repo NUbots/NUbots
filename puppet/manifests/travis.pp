96c7a1f0403cb8eedf8718d11ada53442f678794
Trent Houliston <trent@houliston.me>
2017-06-21 03:04:23 +1000

    Puppet fixes + building of a 14.04 deb file

--- puppet/manifests/travis.pp
+++ puppet/manifests/travis.pp
@@ -12,13 +12,13 @@ node default {
   $toolchain_version = '2.1.1'
 
   wget::fetch { 'nubots_deb':
-    destination => "/nubots/nubots-toolchain-${toolchain_version}.deb",
-    source      => "http://nubots.net/debs/nubots-toolchain-${toolchain_version}.deb",
+    destination => "/nubots/nubots-toolchain-${toolchain_version}-travis.deb",
+    source      => "http://nubots.net/debs/nubots-toolchain-${toolchain_version}-travis.deb",
     timeout     => 0,
   } ->
   package { 'nubots-toolchain':
     provider => 'dpkg',
     ensure   => 'latest',
-    source   => "/nubots/nubots-toolchain-${toolchain_version}.deb",
+    source   => "/nubots/nubots-toolchain-${toolchain_version}-travis.deb",
   }
 }
