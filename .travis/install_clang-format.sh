4f9c34f7297b59d97f34ed209a1c9b3a98014e9e
Trent Houliston <trent@houliston.me>
2017-07-07 22:42:05 +1000

    Add script to install clang-format

--- .travis/install_clang-format.sh
+++ .travis/install_clang-format.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# Add the llvm 4.0 repo to our sources and add its key
+sudo echo "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-4.0 main" | sudo tee /etc/apt/sources.list.d/llvm.list
+wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+
+# Update and install clang-format
+sudo apt-get update
+sudo apt-get install clang-format
