9dbccec482ddddcf9073a7f1df8bd5a18015ebdd
Trent Houliston <trent@houliston.me>
2017-07-08 09:17:14 +1000

    Try again

--- .travis/validate_format.sh
+++ .travis/validate_format.sh
@@ -5,17 +5,18 @@ ret=0
 # Loop through all c/cpp files
 find . -type f \( -name *.h -o -name *.c -o -name *.cc -o -name *.cxx -o -name *.cpp -o -name *.hpp -o -name *.ipp \) -print0 | while IFS= read -r -d $'\0' line; do
 
-    # Get the original and formatted code
-    src=$( cat $line )
+    # Get what our formatted code should be
     fmt=$( clang-format-4.0 -style=file $line )
 
-    # Check if our text is formatted
-    if [ "$src" != "$fmt" ]; then
+    # Check if our text is formatted incorrectly
+    if ! cmp -s $line <(echo "$fmt"); then
+
+        # Flag that it is wrong and print the difference
+        ret=1
         echo "$line is incorrectly formatted"
-        if ! colordiff <(echo "$src") <(echo "$fmt"); then
-            ret=1
-        fi
+        colordiff $line <(echo "$fmt")
     fi
 done
 
+# If we failed somewhere this will exit 1 and fail travis
 exit $ret
