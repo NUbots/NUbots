eab50558897f58b766f3c4d5e95329e2def593fb
Trent Houliston <trent@houliston.me>
2017-07-07 23:31:46 +1000

    Print file names

--- .travis/validate_format.sh
+++ .travis/validate_format.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+find . -type f -iname *.h -o -iname *.c -o -iname *.cc -o -iname *.cxx -o -iname *.cpp -o -iname *.hpp -o -iname *.ipp -print0 | while IFS= read -r -d $'\0' line; do
+    echo $line
+done
