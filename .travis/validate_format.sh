a617387c927ff766c15ef951e7420e1010a498ce
Trent Houliston <trent@houliston.me>
2017-07-08 00:03:51 +1000

    try printing again

--- .travis/validate_format.sh
+++ .travis/validate_format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find . -type f -iname *.h -o -iname *.c -o -iname *.cc -o -iname *.cxx -o -iname *.cpp -o -iname *.hpp -o -iname *.ipp -print0 | while IFS= read -r -d $'\0' line; do
+find . -type f \( -name *.h -o -name *.c -o -name *.cc -o -name *.cxx -o -name *.cpp -o -name *.hpp -o -name *.ipp \) -print0 | while IFS= read -r -d $'\0' line; do
     echo $line
 done
