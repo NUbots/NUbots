0d49d7d84d0e5692c8ea262bc841a77952803231
Monica <monica.j.olejniczak@gmail.com>
2017-07-14 23:26:07 +1000

    Add checkbox component (NUbots/NUsight2#62)

---
+++ src/client/components/checkbox/view.tsx
@@ -0,0 +1,38 @@
+import * as classNames from 'classnames'
+import * as React from 'react'
+import { ChangeEvent } from 'react'
+import { StatelessComponent } from 'react'
+import CheckIcon from './check.svg'
+import * as style from './style.css'
+
+export interface CheckboxProps {
+  checked: boolean
+  disabled?: boolean
+  onChange(event: ChangeEvent<HTMLInputElement>): void
+}
+
+export const Checkbox: StatelessComponent<CheckboxProps> = (props: CheckboxProps) => {
+  const { checked, disabled, onChange } = props
+
+  const backgroundClassName = classNames(style.background, {
+    [style.checked]: checked,
+    [style.disabled]: disabled,
+  })
+
+  const checkIconClassName = classNames(style.checkIcon, {
+    [style.checkIconChecked]: checked,
+  })
+
+  return (
+    <span className={style.checkbox}>
+      <input type='checkbox'
+             className={style.nativeControl}
+             checked={checked}
+             disabled={disabled}
+             onChange={onChange} />
+      <span className={backgroundClassName}>
+        <CheckIcon className={checkIconClassName} />
+      </span>
+    </span>
+  )
+}
