c47d4d081a43d21c4529a2e2b9735f74267aab53
Daniel Ginn <daniel.ginn@live.com.au>
2017-07-25 13:06:56 +1000

    Camera Fix Alex made (#99)

--- module/input/Camera/src/Camera.cpp
+++ module/input/Camera/src/Camera.cpp
@@ -28,39 +28,42 @@ namespace input {
         on<Configuration>("Cameras").then("Camera driver loader", [this](const Configuration& config) {
             // Monitor camera config directory for files.
             // Each file MUST define a "driver", we use this driver to load the appropriate handler for the camera.
-            auto driver   = config["driver"].as<std::string>();
-            auto deviceID = config["deviceID"].as<std::string>();
 
-            log("Searching for", driver, "camera with deviceID", deviceID);
+            if (config.config["driver"] && config.config["deviceID"]) {
+                auto driver   = config["driver"].as<std::string>();
+                auto deviceID = config["deviceID"].as<std::string>();
 
-            if (driver == "V4L2") {
-                auto cam = V4L2Cameras.find(deviceID);
+                log("Searching for", driver, "camera with deviceID", deviceID);
 
-                if (cam == V4L2Cameras.end()) {
-                    V4L2Cameras.insert(std::make_pair(deviceID, std::move(initiateV4L2Camera(config))));
-                    cameraCount++;
+                if (driver == "V4L2") {
+                    auto cam = V4L2Cameras.find(deviceID);
+
+                    if (cam == V4L2Cameras.end()) {
+                        V4L2Cameras.insert(std::make_pair(deviceID, std::move(initiateV4L2Camera(config))));
+                        cameraCount++;
+                    }
+
+                    else {
+                        cam->second.setConfig(config);
+                    }
+                }
+
+                else if (driver == "Spinnaker") {
+                    auto cam = SpinnakerCameras.find(deviceID);
+
+                    if (cam == SpinnakerCameras.end()) {
+                        initiateSpinnakerCamera(config);
+                        cameraCount++;
+                    }
+
+                    else {
+                        resetSpinnakerCamera(cam, config);
+                    }
                 }
 
                 else {
-                    cam->second.setConfig(config);
+                    log<NUClear::FATAL>("Unsupported camera driver:", driver);
                 }
-            }
-
-            else if (driver == "Spinnaker") {
-                auto cam = SpinnakerCameras.find(deviceID);
-
-                if (cam == SpinnakerCameras.end()) {
-                    initiateSpinnakerCamera(config);
-                    cameraCount++;
-                }
-
-                else {
-                    resetSpinnakerCamera(cam, config);
-                }
-            }
-
-            else {
-                log<NUClear::FATAL>("Unsupported camera driver:", driver);
             }
         });
 
