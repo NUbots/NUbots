d22608f0ecc8d8e9ee9234e77a525be3a5f1cb01
Jake Fountain <jake.fountain@uon.edu.au>
2017-07-29 11:51:38 +0900

    Adjusting middle button localisation (#130)
    * added penalty shootout reset to middle button press
    
    * changed penalty reset values

--- module/behaviour/strategy/SoccerStrategy/src/SoccerStrategy.cpp
+++ module/behaviour/strategy/SoccerStrategy/src/SoccerStrategy.cpp
@@ -169,15 +169,17 @@ namespace behaviour {
                 });
 
 
-            on<Trigger<ButtonMiddleDown>, Single>().then([this] {
+            on<Trigger<ButtonMiddleDown>, With<FieldDescription>, Single>().then(
+                [this](const FieldDescription& fieldDescription) {
 
-                if (!cfg_.forcePlaying) {
-                    NUClear::log("Force playing started.");
-                    emit(std::make_unique<Nod>(true));
-                    cfg_.forcePlaying = true;
-                }
+                    if (!cfg_.forcePlaying) {
+                        NUClear::log("Force playing started.");
+                        emit(std::make_unique<Nod>(true));
+                        cfg_.forcePlaying = true;
+                        penaltyShootoutLocalisationReset(fieldDescription);
+                    }
 
-            });
+                });
 
             // Main Loop
             // TODO: ensure a reasonable state is emitted even if gamecontroller is not running
@@ -358,8 +360,8 @@ namespace behaviour {
             auto reset = std::make_unique<ResetRobotHypotheses>();
 
             ResetRobotHypotheses::Self selfSideBaseLine;
-            selfSideBaseLine.position << 2.0, 0.0;
-            selfSideBaseLine.position_cov = Eigen::Vector2d::Constant(0.01).asDiagonal();
+            selfSideBaseLine.position << 1, 0.0;
+            selfSideBaseLine.position_cov = Eigen::Vector2d::Constant(0.1).asDiagonal();
             selfSideBaseLine.heading      = 0;
             selfSideBaseLine.heading_var  = 0.005;
             reset->hypotheses.push_back(selfSideBaseLine);
