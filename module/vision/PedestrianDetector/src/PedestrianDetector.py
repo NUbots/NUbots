f5e2281c50282f8aa047152fe5b15a5445515761
Alex Biddulph <c3124185@uon.edu.au>
2017-10-28 13:09:36 +1100

    Adds non-maximal suppression to reduce number of detections

--- module/vision/PedestrianDetector/src/PedestrianDetector.py
+++ module/vision/PedestrianDetector/src/PedestrianDetector.py
@@ -90,7 +90,8 @@ class PedestrianDetector(object):
                 print('PedestrianDetector::run_detection: Detection time: {0}'.format(detection_end - detection_start))
 
                 msg = []
-                for box in np.squeeze(boxes):
+                merged_boxes = self.non_max_suppression_fast(np.squeeze(boxes), 0.5)
+                for box in merged_boxes:
                     obj = Obstacle()
                     obj.visObject.timestamp = datetime.datetime.now()
                     obj.visObject.camera_id = image.camera_id
@@ -105,3 +106,50 @@ class PedestrianDetector(object):
 
                 self.emit(msg)
 
+    # https://stackoverflow.com/a/37850394
+    def non_max_suppression_fast(self, boxes, overlap_threshold):
+        # if there are no boxes, return an empty list
+        if len(boxes) == 0:
+            return []
+
+        # initialize the list of picked indexes
+        pick = []
+
+        # grab the coordinates of the bounding boxes
+        y1 = boxes[:, 0]
+        x1 = boxes[:, 1]
+        y2 = boxes[:, 2]
+        x2 = boxes[:, 3]
+
+        # compute the area of the bounding boxes and sort the bounding
+        # boxes by the bottom-right y-coordinate of the bounding box
+        area = (x2 - x1 + 1) * (y2 - y1 + 1)
+        idxs = np.argsort(y2)
+
+        # keep looping while some indexes still remain in the indexes
+        # list
+        while len(idxs) > 0:
+            # grab the last index in the indexes list and add the index value to the list of picked indexes
+            last = len(idxs) - 1
+            i = idxs[last]
+            pick.append(i)
+
+            # find the largest (x, y) coordinates for the start of the bounding box and the smallest (x, y) coordinates
+            # for the end of the bounding box
+            xx1 = np.maximum(x1[i], x1[idxs[:last]])
+            yy1 = np.maximum(y1[i], y1[idxs[:last]])
+            xx2 = np.minimum(x2[i], x2[idxs[:last]])
+            yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+            # compute the width and height of the bounding box
+            w = np.maximum(0, xx2 - xx1 + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+
+            # compute the ratio of overlap
+            overlap = (w * h) / area[idxs[:last]]
+
+            # delete all indexes from the index list that have
+            idxs = np.delete(idxs, np.concatenate(([last], np.where(overlap > overlap_threshold)[0])))
+
+        # return only the bounding boxes that were picked using the integer data type
+        return boxes[pick]
