7f4fcc0ed23fc27cae1f3b3f60e0eedafc94e2e3
Alex Biddulph <c3124185@uon.edu.au>
2017-11-16 14:35:18 +1100

    Optimises tensorflow loading and synchronises images and detections.
    Bakes detections into image rather than sending detections as a separate message

--- module/vision/PedestrianDetector/src/PedestrianDetector.py
+++ module/vision/PedestrianDetector/src/PedestrianDetector.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 from nuclear import Reactor, on, Trigger, Single, With, Every
-from message.vision import ReprojectedImage, Obstacle
+from message.vision import ReprojectedImage, Obstacle, BakedImage
 import numpy as np
 import tensorflow as tf
 import yaml
 import datetime
 import time
+import sys
+from skimage.draw import polygon, set_color
 
 @Reactor
 class PedestrianDetector(object):
@@ -23,8 +25,25 @@ class PedestrianDetector(object):
                 self.od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(self.od_graph_def, name='')
 
+        # Definite input and output Tensors for detection_graph
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+
+        # Each box represents a part of the image where a particular object was detected.
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+
+        self.session = tf.Session(graph=self.detection_graph)
+
         self.avg_fp_ms = 0
         self.avg_count = 0
+
+    def __del__(self):
+        self.session.close()
 
     # @on(Configuration('PedestrianDetector.yaml'))
     # def PedestrianDetector_configfuration(self, config):
@@ -34,53 +53,46 @@ class PedestrianDetector(object):
     def run_detection(self, image):
         if image.camera_id > 1:
             # Convert image to numpy array
-            image_np = np.zeros((image.dimensions[1], image.dimensions[0], 3), dtype = np.uint8)
-            img = np.array(image.data).reshape((image.dimensions[1], image.dimensions[0], 3)).astype(np.uint8)
+            #image_np = np.zeros((image.dimensions[1], image.dimensions[0], 3), dtype = np.uint8)
+            input_img = np.array(image.data).reshape((image.dimensions[1], image.dimensions[0], 3)).astype(np.uint8).copy()
 
             with self.detection_graph.as_default():
-                with tf.Session(graph=self.detection_graph) as sess:
-                    # Definite input and output Tensors for detection_graph
-                    image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_np_expanded = np.expand_dims(input_img, axis = 0)
 
-                    # Each box represents a part of the image where a particular object was detected.
-                    detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+                # Actual detection.
+                detection_start = time.time()
+                (boxes, scores, classes, num) = self.session.run(
+                        [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+                        feed_dict={self.image_tensor: image_np_expanded})
+                detection_end = time.time()
+                self.avg_fp_ms += detection_end - detection_start
+                self.avg_count += 1
+                print('PedestrianDetector::run_detection: Detection time: {0:.4f} s (avg: {1:.4f} s)'.format(
+                                                detection_end - detection_start, self.avg_fp_ms / self.avg_count))
 
-                    # Each score represent how level of confidence for each of the objects.
-                    # Score is shown on the result image, together with the class label.
-                    detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-                    detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-                    num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+                merged_boxes = self.non_max_suppression_fast(np.squeeze(boxes), 0.5)
+                for box in merged_boxes:
+                    ymin, xmin, ymax, xmax = box.tolist()
+                    xmin = int(xmin * image.dimensions[0])
+                    xmax = int(xmax * image.dimensions[0])
+                    ymin = int(ymin * image.dimensions[1])
+                    ymax = int(ymax * image.dimensions[1])
 
-                    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                    image_np_expanded = np.expand_dims(image_np, axis = 0)
+                r = ([ymin, ymin, ymax, ymax, ymin])
+                c = ([xmin, xmax, xmax, xmin, xmin])
+                rr, cc = polygon(r, c, input_img.shape)
+                set_color(input_img, (rr, cc), (255, 0, 0), 0.25)
 
-                    # Actual detection.
-                    detection_start = time.time()
-                    (boxes, scores, classes, num) = sess.run(
-                            [detection_boxes, detection_scores, detection_classes, num_detections],
-                            feed_dict={image_tensor: image_np_expanded})
-                    detection_end = time.time()
-                    self.avg_fp_ms += detection_end - detection_start
-                    self.avg_count += 1
-                    print('PedestrianDetector::run_detection: Detection time: {0:.4f} s (avg: {1:.4f} s)'.format(
-                                                    detection_end - detection_start, self.avg_fp_ms / self.avg_count))
+                img = BakedImage()
+                img.format = image.format
+                img.dimensions = image.dimensions.copy()
+                img.data = input_img.flatten().tolist().copy()
+                img.camera_id = image.camera_id
+                img.serial_number = "PedestrianDetector"
+                img.timestamp = datetime.datetime.now()
 
-                    msg = []
-                    merged_boxes = self.non_max_suppression_fast(np.squeeze(boxes), 0.5)
-                    for box in merged_boxes:
-                        obj = Obstacle()
-                        obj.visObject.timestamp = datetime.datetime.now()
-                        obj.visObject.camera_id = image.camera_id
-                        ymin, xmin, ymax, xmax = box.tolist()
-                        obj.shape.points = [[int(xmin * image.dimensions[0]), int(ymin * image.dimensions[1])],
-                                            [int(xmax * image.dimensions[0]), int(ymin * image.dimensions[1])],
-                                            [int(xmax * image.dimensions[0]), int(ymax * image.dimensions[1])],
-                                            [int(xmin * image.dimensions[0]), int(ymax * image.dimensions[1])]]
-                        obj.team = Obstacle.Team.UNKNOWN_TEAM
-
-                        msg.append(obj)
-
-                    self.emit(msg)
+                self.emit(img)
 
     # https://stackoverflow.com/a/37850394
     def non_max_suppression_fast(self, boxes, overlap_threshold):
