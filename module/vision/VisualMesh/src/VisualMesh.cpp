813bc4c0ebd7cb5c645a9d3f7f3b803e782c1099
Trent Houliston <trent@houliston.me>
2018-01-11 13:12:00 +1100

    Add missing VisualMesh.cpp file

--- module/vision/VisualMesh/src/VisualMesh.cpp
+++ module/vision/VisualMesh/src/VisualMesh.cpp
@@ -6,6 +6,7 @@
 #include "mesh/Sphere.hpp"
 #include "message/input/Image.h"
 #include "message/input/Sensors.h"
+#include "message/vision/VisualMesh.h"
 #include "utility/nubugger/NUhelpers.h"
 #include "utility/support/Timer.hpp"
 
@@ -15,6 +16,7 @@ namespace vision {
     using extension::Configuration;
     using message::input::Image;
     using message::input::Sensors;
+    using VisualMeshMsg = message::vision::VisualMesh;
 
     VisualMesh::VisualMesh(std::unique_ptr<NUClear::Environment> environment)
         : Reactor(std::move(environment)), mesh(mesh::Sphere<float>(0, 0.075, 4, 10), 0.5, 1.0, 50, M_PI / 1280.0) {
@@ -62,14 +64,14 @@ namespace vision {
             log("Finished loading visual mesh");
         });
 
-        on<Trigger<Image>, Buffer<2>>().then([this](const Image& img) {
+        on<Trigger<Image>, Buffer<4>>().then([this](const Image& img) {
 
+            // Get our camera to world matrix
             Eigen::Affine3f Hcw(img.Hcw.cast<float>());
 
-            // Transpose Hcw into Hoc
+            // Transpose and store in our row major array
             std::array<std::array<float, 4>, 4> Hoc;
-
-            Eigen::Map<Eigen::Matrix4f>(Hoc[0].data()) = Hcw.inverse().matrix();
+            Eigen::Map<Eigen::Matrix<float, 4, 4, Eigen::RowMajor>>(Hoc[0].data()) = Hcw.inverse().matrix();
 
             mesh::VisualMesh<float>::Lens lens;
             lens.projection   = mesh::VisualMesh<float>::Lens::EQUIDISTANT;
@@ -78,33 +80,69 @@ namespace vision {
             lens.focal_length = (1.0 / 0.0026997136600899543);
 
             Timer t;
-            auto projected = mesh.project(Hoc, lens);
-            auto results   = classifier(img.data.data(), mesh::VisualMesh<float>::FOURCC(img.format), Hoc, lens);
+            auto results = classifier(img.data.data(), mesh::VisualMesh<float>::FOURCC(img.format), Hoc, lens);
+            t.measure("Classified");
 
-            std::vector<std::tuple<Eigen::Vector2i, Eigen::Vector2i, Eigen::Vector4d>,
-                        Eigen::aligned_allocator<std::tuple<Eigen::Vector2i, Eigen::Vector2i, Eigen::Vector4d>>>
-                lines;
+            // Get the mesh that was used so we can make our message
+            const auto& m = mesh.height(Hoc[2][3]);
 
+            auto msg = std::make_unique<VisualMeshMsg>();
 
-            std::vector<std::array<int, 2>> pixel_coordinates = projected.pixel_coordinates;
+            // Pass through our camera id
+            msg->camera_id = img.camera_id;
 
-            for (uint i = 0; i < pixel_coordinates.size(); ++i) {
-
-                Eigen::Vector2i p1(pixel_coordinates[i][0], pixel_coordinates[i][1]);
-
-                Eigen::Vector4d colour(results[i][1], 0, results[i][0], 1);
-
-                for (const auto& n : projected.neighbourhood[i]) {
-                    if (n < pixel_coordinates.size()) {
-                        Eigen::Vector2i p2(pixel_coordinates[n][0], pixel_coordinates[n][1]);
-                        Eigen::Vector2i p2x = p1 + ((p2 - p1) / 2);
-                        lines.emplace_back(p1, p2x, colour);
-                    }
-                }
+            // Add our description
+            for (const auto& r : m.rows) {
+                msg->rows.emplace_back(r.phi, r.end - r.begin);
             }
-            emit(utility::nubugger::drawVisionLines(lines));
 
-            t.measure("Finished processing");
+            // Add our indices
+            msg->indices = std::move(results.global_indices);
+
+            // Add our coordinates
+            msg->coordinates = results.pixel_coordinates.as<Eigen::Matrix<int, 2, 1, Eigen::DontAlign>>();
+
+            // Add our classifications
+            // Add our first (the image) and last (the results) to our list
+            for (const auto& c : results.classifications) {
+                msg->classifications.emplace_back(c.first, c.second);
+            }
+
+            emit(msg);
+            // msg->classifications.emplace_back(results.classifications.front().first,
+            //                                   results.classifications.front().second);
+            // msg->classifications.emplace_back(results.classifications.back().first,
+            //                                   results.classifications.back().second);
+
+
+            // std::vector<std::tuple<Eigen::Vector2i, Eigen::Vector2i, Eigen::Vector4d>,
+            //             Eigen::aligned_allocator<std::tuple<Eigen::Vector2i, Eigen::Vector2i, Eigen::Vector4d>>>
+            //     lines;
+
+
+            // std::vector<std::array<int, 2>> pixel_coordinates = results.pixel_coordinates;
+            // std::vector<float> classification                 = results.classifications.front().second;
+
+            // for (uint i = 0; i < pixel_coordinates.size(); ++i) {
+
+            //     Eigen::Vector2i p1(pixel_coordinates[i][0], pixel_coordinates[i][1]);
+
+            //     // Eigen::Vector4d colour(results.second[i][1], 0, results.second[i][0], 1);
+            //     // Eigen::Vector4d colour(classification[i * 2 + 1] > 0.5, 0, classification[i * 2 + 0] > 0.5, 1);
+            //     Eigen::Vector4d colour(
+            //         classification[i * 4 + 0], classification[i * 4 + 1], classification[i * 4 + 2], 1);
+
+            //     for (const auto& n : results.neighbourhood[i]) {
+            //         if (n < pixel_coordinates.size()) {
+            //             Eigen::Vector2i p2(pixel_coordinates[n][0], pixel_coordinates[n][1]);
+            //             Eigen::Vector2i p2x = p1 + ((p2 - p1) / 2);
+            //             lines.emplace_back(p1, p2x, colour);
+            //         }
+            //     }
+            // }
+            // emit(utility::nubugger::drawVisionLines(lines));
+
+            t.measure("Saved in message");
         });
 
         on<Shutdown>().then([this] {
