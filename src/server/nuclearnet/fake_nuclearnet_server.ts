6d42b632a4f7dda2bf0307345a2c67f6a1572bb7
Trent Houliston <trent@houliston.me>
2018-01-13 13:08:10 +1100

    Fix the hash function when the result isn't 16 characters (NUbots/NUsight2#148)
    * Fix the hash function when the result isn't 16 characters
    
    * Add test and change to using repeat
    
    * Add test snapshot

--- src/server/nuclearnet/fake_nuclearnet_server.ts
+++ src/server/nuclearnet/fake_nuclearnet_server.ts
@@ -86,6 +86,9 @@ export class FakeNUClearNetServer {
 export function hashType(type: string): Buffer {
   // Matches hashing implementation from NUClearNet
   // See https://goo.gl/6NDPo2
-  const hashString: string = XXH.h64(type, 0x4e55436c).toString(16)
+  let hashString: string = XXH.h64(type, 0x4e55436c).toString(16)
+  // The hash string may truncate if it's smaller than 16 characters so we pad it with 0s
+  hashString = ('0'.repeat(16) + hashString).slice(-16)
+
   return Buffer.from((hashString.match(/../g) as string[]).reverse().join(''), 'hex')
 }
