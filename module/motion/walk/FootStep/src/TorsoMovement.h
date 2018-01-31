575ba4cf4870852a10e25647db0bc713c3e80c95
Ysobel Sims <y_star_light_y@live.com.au>
2018-01-31 16:04:51 +1100

    Begin torso movement for walk

---
+++ module/motion/walk/FootStep/src/TorsoMovement.h
@@ -0,0 +1,24 @@
+#ifndef MODULE_MOTION_WALK_TORSOMOVEMENT_H
+#define MODULE_MOTION_WALK_TORSOMOVEMENT_H
+
+#include <Eigen/Core>
+#include <nuclear>
+
+namespace module {
+namespace motion {
+    namespace walk {
+
+        class TorsoMovement : public NUClear::Reactor {
+        private:
+            size_t subsumptionId;
+
+
+        public:
+            /// @brief Called by the powerplant to build and setup the FootStep reactor.
+            explicit TorsoMovement(std::unique_ptr<NUClear::Environment> environment);
+        };
+    }
+}
+}
+
+#endif  // MODULE_MOTION_WALK_TORSOMOVEMENT_H
