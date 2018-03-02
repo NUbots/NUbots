ae9310e41f74b62763515bd60ddb6a49d1015e77
Ysobel Sims <y_star_light_y@live.com.au>
2018-03-02 13:20:42 +1100

    Changes to names (f from f_s), comments

--- module/motion/walk/TorsoMovement/src/TorsoMovement.cpp
+++ module/motion/walk/TorsoMovement/src/TorsoMovement.cpp
@@ -43,6 +43,7 @@ namespace motion {
                 double y = config["test"]["y"].as<double>();
                 double z = config["test"]["z"].as<double>();
                 int foot = config["foot"].as<int>();
+
                 Eigen::Affine3d Haf_s;
                 Haf_s.linear()      = Eigen::Matrix3d::Identity();
                 Haf_s.translation() = -Eigen::Vector3d(x, y, z);
@@ -53,48 +54,49 @@ namespace motion {
 
             on<Trigger<Sensors>, With<KinematicsModel>, With<TorsoTarget>>().then(
                 [this](const Sensors& sensors, const KinematicsModel& model, const TorsoTarget& target) {
-                    // Get support foot and swing foot coordinate systems
+                    // Get support foot coordinate system
 
-                    Eigen::Affine3d Htf_s;  // support foot
+                    Eigen::Affine3d Htf;  // support foot
 
-                    // Right foot is the swing foot
+                    // Right foot is the support foot
                     if (target.isRightFootSupport) {
-                        // Transform of left foot to torso
-                        Htf_s = Eigen::Affine3d(sensors.forwardKinematics[ServoID::R_ANKLE_ROLL]);
-                    }
-                    // Left foot is the swing foot
-                    else {
                         // Transform of right foot to torso
-                        Htf_s = Eigen::Affine3d(sensors.forwardKinematics[ServoID::L_ANKLE_ROLL]);
+                        Htf = Eigen::Affine3d(sensors.forwardKinematics[ServoID::R_ANKLE_ROLL]);
+                    }
+                    // Left foot is the support foot
+                    else {
+                        // Transform of left foot to torso
+                        Htf = Eigen::Affine3d(sensors.forwardKinematics[ServoID::L_ANKLE_ROLL]);
                     }
 
-                    Eigen::Affine3d Haf_s;
-                    Haf_s = target.Haf_s;
+                    // Target given in support foot space
+                    Eigen::Affine3d Haf;
+                    Haf = target.Haf;
 
-                    // Target position is in foot space
-                    Eigen::Vector3d rAF_sf_s = -Haf_s.translation();
+                    // Create vector to target from support foot
+                    Eigen::Vector3d rAFf = -Haf.translation();
                     // Position of target in torso space
-                    Eigen::Vector3d rATt = Htf_s * rAF_sf_s;
-                    // Next torso target in torso space
+                    Eigen::Vector3d rATt = Htf * rAFf;
+                    // Create next torso target in torso space
                     Eigen::Vector3d rT_tTt = rATt * 0.01;
                     // Create vector from torso to foot target
-                    Eigen::Vector3d rF_tF_st = -rT_tTt;
-                    Eigen::Vector3d rF_tTt   = Htf_s * rF_tF_st;
+                    Eigen::Vector3d rF_tFt = -rT_tTt;
+                    Eigen::Vector3d rF_tTt = Htf * rF_tFt;
 
                     // Target rotation from torso space
                     Eigen::Affine3d Hat;
-                    Hat = Haf_s * Htf_s.inverse();
+                    Hat = Haf * Htf.inverse();
                     // Get support foot rotation from torso as quaternion
-                    Eigen::Quaterniond Rf_st;
-                    Rf_st = Htf_s.inverse().linear();
+                    Eigen::Quaterniond Rft;
+                    Rft = Htf.inverse().linear();
                     // Get target rotation from torso as quaternion
                     Eigen::Quaterniond Rat;
                     Rat = Hat.linear();
                     // Create rotation of torso to torso target
                     Eigen::Matrix3d Rf_tt;
                     // Slerp the above two Quaternions and switch to rotation matrix to get the rotation
-                    // TODO: determine t
-                    Rf_tt = Rf_st.slerp(0.1, Rat).toRotationMatrix();
+                    // TODO: determine slerp first arg (t)
+                    Rf_tt = Rft.slerp(0.1, Rat).toRotationMatrix();
                     Eigen::Affine3d Htf_t;
                     Htf_t.linear()      = Rf_tt.inverse();  // Rotation as above from slerp
                     Htf_t.translation() = rF_tTt;           // Translation to foot target
