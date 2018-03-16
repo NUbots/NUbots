152ae8858470588c87bdda44193a1eb5f299d18e
Alex Biddulph <c3124185@uon.edu.au>
2018-03-17 09:41:54 +1100

    Adds toolchain environment to travis build

--- .travis/build_code.sh
+++ .travis/build_code.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-export PATH=/usr/lib/ccache:$PATH
-/usr/bin/python3 nuclear/b platform select $PLATFORM
+# Define environment variables
+PREFIX=/nubots/toolchain
+
+export LD_LIBRARY_PATH="$PREFIX/lib"
+export PATH="$PREFIX/bin:/usr/lib/ccache:$PATH"
+export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+export CMAKE_PREFIX_PATH="$PREFIX"
+
+/nubots/toolchain/bin/python3 nuclear/b platform select $PLATFORM
 cd build
 ninja
