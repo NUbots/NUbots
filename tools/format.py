3a0fc3347a12fc0908fe0d28be155f2182441512
Alex Biddulph <c3124185@uon.edu.au>
2018-06-03 15:05:20 +1000

    Revert "Changes formatting script to search all directories"
    This reverts commit 0d165540282c800614bd7911ceeb42d30e1c4b7b.

--- tools/format.py
+++ tools/format.py
@@ -14,12 +14,13 @@ def register(command):
 
 def run(**kwargs):
     # Get our relevant directories
-    for dirpath, dnames, fnames in os.walk(b.project_dir):
-        for f in fnames:
-            if f.endswith(('.h', '.c', '.cc', '.cxx', '.cpp', '.hpp', '.ipp', '.proto')):
-                print('Formatting', f)
-                call(['clang-format-6.0', '-i', '-style=file', os.path.join(dirpath, f)])
+    for d in [os.path.join(b.project_dir, p) for p in ['module', 'shared', 'tests', 'tools']]:
+        for dirpath, dnames, fnames in os.walk(d):
+            for f in fnames:
+                if f.endswith(('.h', '.c', '.cc', '.cxx', '.cpp', '.hpp', '.ipp', '.proto')):
+                    print('Formatting', f)
+                    call(['clang-format-6.0', '-i', '-style=file', os.path.join(dirpath, f)])
 
-            if f.endswith(('.py')):
-                print('Formatting', f)
-                call(['yapf', '-i', os.path.join(dirpath, f)])
+                if f.endswith(('.py')):
+                    print('Formatting', f)
+                    call(['yapf', '-i', os.path.join(dirpath, f)])
