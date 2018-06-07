ad0bf962d020ee64c09877224d6a01f56ffa4f69
Alex Biddulph <c3124185@uon.edu.au>
2018-06-08 09:51:25 +1000

    Python3-ifies NN training code

--- module/support/logging/LegLoadsLogger/train_nn.py
+++ module/support/logging/LegLoadsLogger/train_nn.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python
-import numpy.random
-def loadfile(path, label=None, max_heigh_delta=0.005):
+#!/usr/bin/env python3
+
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+
+
+def loadfile(path, label=None, max_height_delta=0.005):
     with open(path, 'r') as f:
         # Get our lines from our csv
         lines = f.readlines()
@@ -12,98 +16,91 @@ def loadfile(path, label=None, max_heigh_delta=0.005):
         for point in zip(data, data[1:]):
 
             # Left
-            output.append((int(label if label != None else point[1][1] < point[1][0] + max_heigh_delta)
-                           , point[1][ 4]                 # L_HIP_PITCH_PRESENT_VELOCITY
-                           , point[1][ 4] - point[0][ 4]  # L_HIP_PITCH_ACCELERATION
-                           , point[1][ 5]                 # L_HIP_PITCH_LOAD
-                           , point[1][ 8]                 # L_KNEE_PRESENT_VELOCITY
-                           , point[1][ 8] - point[0][ 8]  # L_KNEE_ACCELERATION
-                           , point[1][ 9]                 # L_KNEE_LOAD
-                           , point[1][12]                 # L_ANKLE_PITCH_PRESENT_VELOCITY
-                           , point[1][12] - point[0][12]  # L_ANKLE_PITCH_ACCELERATION
-                           , point[1][13]                 # L_ANKLE_PITCH_LOAD
-                           ))
+            output.append((
+                int(label if label != None else point[1][1] < point[1][0] + max_height_delta),
+                point[1][4],  # L_HIP_PITCH_PRESENT_VELOCITY
+                point[1][4] - point[0][4],  # L_HIP_PITCH_ACCELERATION
+                point[1][5],  # L_HIP_PITCH_LOAD
+                point[1][8],  # L_KNEE_PRESENT_VELOCITY
+                point[1][8] - point[0][8],  # L_KNEE_ACCELERATION
+                point[1][9],  # L_KNEE_LOAD
+                point[1][12],  # L_ANKLE_PITCH_PRESENT_VELOCITY
+                point[1][12] - point[0][12],  # L_ANKLE_PITCH_ACCELERATION
+                point[1][13]  # L_ANKLE_PITCH_LOAD
+            ))
 
-            output.append((int(label if label != None else point[1][0] < point[1][1] +max_heigh_delta)
-                          , point[1][ 2]                 # R_HIP_PITCH_PRESENT_VELOCITY
-                          , point[1][ 2] - point[0][ 2]  # R_HIP_PITCH_ACCELERATION
-                          , point[1][ 3]                 # R_HIP_PITCH_LOAD
-                          , point[1][ 6]                 # R_KNEE_PRESENT_VELOCITY
-                          , point[1][ 6] - point[0][ 6]  # R_KNEE_ACCELERATION
-                          , point[1][ 7]                 # R_KNEE_LOAD
-                          , point[1][10]                 # R_ANKLE_PITCH_PRESENT_VELOCITY
-                          , point[1][10] - point[0][10]  # R_ANKLE_PITCH_ACCELERATION
-                          , point[1][11]                 # R_ANKLE_PITCH_LOAD
-                          ))
+            # Right
+            output.append((
+                int(label if label != None else point[1][0] < point[1][1] + max_height_delta),
+                point[1][2],  # R_HIP_PITCH_PRESENT_VELOCITY
+                point[1][2] - point[0][2],  # R_HIP_PITCH_ACCELERATION
+                point[1][3],  # R_HIP_PITCH_LOAD
+                point[1][6],  # R_KNEE_PRESENT_VELOCITY
+                point[1][6] - point[0][6],  # R_KNEE_ACCELERATION
+                point[1][7],  # R_KNEE_LOAD
+                point[1][10],  # R_ANKLE_PITCH_PRESENT_VELOCITY
+                point[1][10] - point[0][10],  # R_ANKLE_PITCH_ACCELERATION
+                point[1][11]  # R_ANKLE_PITCH_LOAD
+            ))
 
     return output
 
+
+###
+### Load all our files
+###
 # Files that have the feet up
-negfiles   = ['walk_load_datasets/airstand.csv'
-              , 'walk_load_datasets/airstand_old.csv'
-              , 'walk_load_datasets/airwalk.csv'
-              , 'walk_load_datasets/airwalkforward.csv'
-              , 'walk_load_datasets/evenmoreairwalk_old.csv'
-              , 'walk_load_datasets/moreairwalk_old.csv'
-              , 'walk_load_datasets/back_turtle.csv'
-              , 'walk_load_datasets/lyingback.csv']
+negative_files = [
+    loadfile(f, 0) for f in [
+        'laying_back.csv',
+        'laying_front.csv',
+        'picked_up.csv',
+        'walking_picked_up.csv',
+        'walking_laying_back.csv',
+        'walking_laying_front.csv',
+    ]
+]
 
 # Files that have the feet down
-posfiles   = ['walk_load_datasets/standing.csv'
-              , 'walk_load_datasets/standing_old.csv']
+positive_files = [loadfile(f, 1) for f in ['standing.csv']]
 
 # Files that have the feet changing based on the walk
-mixedfiles = ['walk_load_datasets/morewalking_old.csv'
-              , 'walk_load_datasets/walkforward.csv'
-              , 'walk_load_datasets/walking_old.csv'
-              , 'walk_load_datasets/walkonspot.csv'
-              , 'walk_load_datasets/walkspin.csv']
+mixed_files = [loadfile(f) for f in ['walking.csv', 'walking2.csv']]
 
 # The file we will use for validation
-testfiles = ['walk_load_datasets/bigwalk.csv']
-
-# Load all our files
-negfiles   = [loadfile(f, 0) for f in negfiles]
-posfiles   = [loadfile(f, 1) for f in posfiles]
-mixedfiles = [loadfile(f)    for f in mixedfiles]
-testfiles  = [loadfile(f)    for f in testfiles]
+test_files = [loadfile(f) for f in ['long_walk.csv']]
 
 # Flatten the data
-trainingdata = [item for sublist in (negfiles + posfiles + mixedfiles) for item in sublist]
-testdata = [item for sublist in testfiles for item in sublist]
+training_data = np.array([item for sub_list in (negative_files + positive_files + mixed_files) for item in sub_list])
+test_data = np.array([item for sub_list in test_files for item in sub_list])
 
-from sklearn.neural_network import MLPClassifier
-import numpy
-trainingdata = numpy.array(trainingdata)
-testdata = numpy.array(testdata)
-clf = MLPClassifier(alpha=1e-3, hidden_layer_sizes=(5), max_iter=5000,verbose=True,random_state=numpy.random.RandomState(1))
-clf.fit(trainingdata[:,1:],trainingdata[:,0])
+clf = MLPClassifier(
+    alpha=1e-3, hidden_layer_sizes=(5), max_iter=5000, verbose=True, random_state=np.random.RandomState(1)
+)
+clf.fit(training_data[:, 1:], training_data[:, 0])
 
 #matrix
-print "Hidden Weights:"
+print('Hidden Weights:')
 for l in clf.coefs_[0].T:
-    print "-",l.tolist()
-print "Hidden Bias"
-print clf.intercepts_[0].tolist()
+    print('-', l.tolist())
+print('Hidden Bias')
+print(clf.intercepts_[0].tolist())
 
-print "Output Weights:"
+print('Output Weights:')
 for l in clf.coefs_[1].T[0]:
-    print "-",str([l])
-print "Output Bias"
-print clf.intercepts_[1]
+    print('-', str([l]))
+print('Output Bias')
+print(clf.intercepts_[1])
 
-probs = clf.predict_proba(testdata[:,1:])
-out = numpy.hstack([(probs[:,1] > .5).reshape(-1,1),probs])
+probs = clf.predict_proba(test_data[:, 1:])
+out = np.hstack([(probs[:, 1] > .5).reshape(-1, 1), probs])
 
 # Write out our validation output
 with open('validation_prediction', 'w') as f:
-  f.write("labels 0 1\n")
-  for d in out:
-      f.write(str(list(d)).strip('[]').replace(',', '') + '\n')
+    f.write('labels 0 1\n')
+    for d in out:
+        f.write(str(list(d)).strip('[]').replace(',', '') + '\n')
 
 with open('validation_ground_truth', 'w') as f:
-  for d in testdata:
-    f.write('{}\n'.format(d[0]))
-
-
-
+    for d in test_data:
+        f.write('{}\n'.format(d[0]))
--- module/support/logging/LegLoadsLogger/validate.py
+++ module/support/logging/LegLoadsLogger/validate.py
@@ -1,13 +1,53 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-import numpy
 from matplotlib import pyplot
 
-left_certainty = 0.5
-left_uncertainty = 0.3
 
-right_certainty = 0.5
-right_uncertainty = 0.3
+def bayesian_filter(probability, uncertainty=0.3, certainty=0.5):
+    np = 0.000001
+    s = 0.5
+    n = 0.5
+    predicted_state = 1
+    prediction = []
+    state = []
+
+    for v in probability:
+        k = n / (n + np)  #2 * v)
+        s = s + k * (v - s)
+        n = (1 - k) * n + 1
+
+        # Store our raw probability prediction
+        prediction.append(s)
+
+        # Apply our hysteresis and store our state prediction
+        if s < uncertainty:
+            predicted_state = 0
+        elif s > certainty:
+            predicted_state = 1
+
+        state.append(predicted_state)
+
+    return prediction, state
+
+
+def calculate_confusion_matrix(truth, prediction):
+    fp = 0
+    fn = 0
+    tp = 0
+    tn = 0
+
+    for v in zip(truth, prediction):
+        if v[0] == 0 and v[1] == 0:
+            tn += 1
+        elif v[0] == 0 and v[1] == 1:
+            fp += 1
+        elif v[0] == 1 and v[1] == 0:
+            fn += 1
+        elif v[0] == 1 and v[1] == 1:
+            tp += 1
+
+    return tp, fp, tn, fn
+
 
 # Load our ground truth validation data
 with open('validation_ground_truth', 'r') as f:
@@ -22,96 +62,52 @@ with open('validation_prediction', 'r') as f:
     right_probability = [float(d.strip().split(' ')[2]) for d in data[2::2]]
 
 # Apply our bayesian filter
-np = .000001
-s = 0.5
-n = 0.5
-state = 1
-left_predict = []
-left_state = []
-for v in left_probability:
-    k = n / (n + np) #2 * v)
-    s = s + k * (v - s)
-    n = (1 - k) * n + 1
-
-    # Store our raw probability prediction
-    left_predict.append(s)
-
-    # Apply our hysteresis
-    if s < left_uncertainty:
-        state = 0
-    elif s > left_certainty:
-        state = 1
-
-    # Store our state prediction
-    left_state.append(state)
+left_predict, left_state = bayesian_filter(left_probability)
 
 # Apply our bayesian filter
-np = .000001
-s = 0.5
-n = 0.5
-state = 1
-right_predict = []
-right_state = []
-for v in right_probability:
-    k = n / (n + np) #2 * v)
-    s = s + k * (v - s)
-    n = (1 - k) * n+1
+right_predict, right_state = bayesian_filter(right_probability)
 
-    right_predict.append(s)
+# Calculate our confusion matrices
+left_tp, left_fp, left_tn, left_fn = calculate_confusion_matrix(left_truth, left_state)
+right_tp, right_fp, right_tn, right_fn = calculate_confusion_matrix(right_truth, right_state)
 
-    # Apply our hysteresis
-    if s < right_uncertainty:
-        state = 0
-    elif s > right_certainty:
-        state = 1
+print('Left False Positive.: {:5.2f}%'.format(100.0 * float(left_fp) / float(left_fp + left_fn + left_tp + left_tn)))
+print('Left False Negative.: {:5.2f}%'.format(100.0 * float(left_fn) / float(left_fp + left_fn + left_tp + left_tn)))
+print('Left True Positive..: {:5.2f}%'.format(100.0 * float(left_tp) / float(left_fp + left_fn + left_tp + left_tn)))
+print('Left True Negative..: {:5.2f}%'.format(100.0 * float(left_tn) / float(left_fp + left_fn + left_tp + left_tn)))
+print(
+    'Left Accuracy.......: {:5.2f}%'.format(
+        100.0 * float(left_tp + left_tn) / float(left_fp + left_fn + left_tp + left_tn)
+    )
+)
 
-    # Store our state prediction
-    right_state.append(state)
+print()
 
-left_fp = 0
-left_fn = 0
-left_tp = 0
-left_tn = 0
-for v in zip(left_truth, left_state):
-    if   v[0] == 0 and v[1] == 0:
-        left_tn += 1
-    elif v[0] == 0 and v[1] == 1:
-        left_fp += 1
-    elif v[0] == 1 and v[1] == 0:
-        left_fn += 1
-    elif v[0] == 1 and v[1] == 1:
-        left_tp += 1
+print(
+    'Right False Positive: {:5.2f}%'.format(100.0 * float(right_fp) / float(right_fp + right_fn + right_tp + right_tn))
+)
+print(
+    'Right False Negative: {:5.2f}%'.format(100.0 * float(right_fn) / float(right_fp + right_fn + right_tp + right_tn))
+)
+print(
+    'Right True Positive.: {:5.2f}%'.format(100.0 * float(right_tp) / float(right_fp + right_fn + right_tp + right_tn))
+)
+print(
+    'Right True Negative.: {:5.2f}%'.format(100.0 * float(right_tn) / float(right_fp + right_fn + right_tp + right_tn))
+)
+print(
+    'Right Accuracy......: {:5.2f}%'.format(
+        100.0 * float(right_tp + right_tn) / float(right_fp + right_fn + right_tp + right_tn)
+    )
+)
 
-right_fp = 0
-right_fn = 0
-right_tp = 0
-right_tn = 0
-for v in zip(right_truth, right_state):
-    if   v[0] == 0 and v[1] == 0:
-        right_tn += 1
-    elif v[0] == 0 and v[1] == 1:
-        right_fp += 1
-    elif v[0] == 1 and v[1] == 0:
-        right_fn += 1
-    elif v[0] == 1 and v[1] == 1:
-        right_tp += 1
-
-print 'Left False Positive  {:5.2f}%'.format(100.0 * float(left_fp) / float(left_fp + left_fn + left_tp + left_tn))
-print 'Left False Negative  {:5.2f}%'.format(100.0 * float(left_fn) / float(left_fp + left_fn + left_tp + left_tn))
-print 'Left True Positive   {:5.2f}%'.format(100.0 * float(left_tp) / float(left_fp + left_fn + left_tp + left_tn))
-print 'Left True Negative   {:5.2f}%'.format(100.0 * float(left_tn) / float(left_fp + left_fn + left_tp + left_tn))
-print 'Left Accuracy        {:5.2f}%'.format(100.0 * float(left_tp + left_tn) / float(left_fp + left_fn + left_tp + left_tn))
-
-print
-
-print 'Right False Positive {:5.2f}%'.format(100.0 * float(right_fp) / float(right_fp + right_fn + right_tp + right_tn))
-print 'Right False Negative {:5.2f}%'.format(100.0 * float(right_fn) / float(right_fp + right_fn + right_tp + right_tn))
-print 'Right True Positive  {:5.2f}%'.format(100.0 * float(right_tp) / float(right_fp + right_fn + right_tp + right_tn))
-print 'Right True Negative  {:5.2f}%'.format(100.0 * float(right_tn) / float(right_fp + right_fn + right_tp + right_tn))
-print 'Right Accuracy       {:5.2f}%'.format(100.0 * float(right_tp + right_tn) / float(right_fp + right_fn + right_tp + right_tn))
-
-print
-print 'Total Accuracy       {:5.2f}%'.format(100.0 * float(right_tp + right_tn + left_tp + left_tn) / float(left_fp + left_fn + left_tp + left_tn + right_fp + right_fn + right_tp + right_tn))
+print()
+print(
+    'Total Accuracy......: {:5.2f}%'.format(
+        100.0 * float(right_tp + right_tn + left_tp + left_tn) /
+        float(left_fp + left_fn + left_tp + left_tn + right_fp + right_fn + right_tp + right_tn)
+    )
+)
 
 # Plot our predicted state with some offset to make it easy to distinguish from the ground truth
 pyplot.plot([s * 0.8 + 0.1 for s in right_state], marker='^')
@@ -126,8 +122,7 @@ pyplot.plot(right_predict, marker='x')
 pyplot.plot(right_truth, marker='o')
 
 # Extend a little above 0,1
-pyplot.ylim((-0.05,1.05))
+pyplot.ylim((-0.05, 1.05))
 
 # Show our graph
 pyplot.show()
-
