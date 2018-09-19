c03a4a5349cf9a1ffcfdaeb4ab0685a2fce2ff56
Brendan Annable <brendan.a@canva.com>
2018-09-19 23:58:40 +1000

    Simplify createSingletonFactory and add tests (NUbots/NUsight2#202)

--- src/shared/base/create_singleton_factory.ts
+++ src/shared/base/create_singleton_factory.ts
@@ -1,18 +1,10 @@
-/**
- * Creates a function which will cache the value of the given factory function.
- * e.g.
- *
- * const fn = createSingleton(() => new MyClass())
- * fn() === fn() // true
- */
+/** Creates a function which will cache the value of the given factory function. */
 export const createSingletonFactory = <T>(factory: () => T) => {
-  return (() => {
-    let instance: T
-    return () => {
-      if (!instance) {
-        instance = factory()
-      }
-      return instance
+  let instance: T
+  return () => {
+    if (!instance) {
+      instance = factory()
     }
-  })()
+    return instance
+  }
 }
