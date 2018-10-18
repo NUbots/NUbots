e0864fff9736b74e0b8de1fe9c111ea78dc10310
Alex Biddulph <c3124185@uon.edu.au>
2018-10-18 11:38:58 +1100

    Travis debugging

--- .travis/build_code.sh
+++ .travis/build_code.sh
@@ -3,10 +3,13 @@
 # Define environment variables
 PREFIX=/nubots/toolchain
 
-export LD_LIBRARY_PATH="$PREFIX/lib"
-export PATH="$PREFIX/bin:/usr/lib/ccache:$PATH"
-export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+export LD_LIBRARY_PATH="$PREFIX/$PLATFORM/lib:$PREFIX/lib"
+export PATH="$PREFIX/$PLATFORM/bin:$PREFIX/bin:/usr/lib/ccache:$PATH"
+export PKG_CONFIG_PATH="$PREFIX/$PLATFORM/lib/pkgconfig:$PREFIX/lib/pkgconfig"
 export CMAKE_PREFIX_PATH="$PREFIX"
+
+which cmake
+cmake --version
 
 /nubots/toolchain/bin/python3 nuclear/b platform select $PLATFORM
 cd build
