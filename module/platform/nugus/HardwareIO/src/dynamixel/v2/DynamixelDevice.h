37c442b9cd4df58ba7de110d93f39d2ecd3fe1f2
Alex Biddulph <c3124185@uon.edu.au>
2018-10-24 15:30:33 +1100

    Adds RAM table for dynamixel servos

---
+++ module/platform/nugus/HardwareIO/src/dynamixel/v2/DynamixelDevice.h
@@ -0,0 +1,12 @@
+#ifndef MODULE_PLATFORM_NUGUS_DYNAMIXELDEVICE_H
+#define MODULE_PLATFORM_NUGUS_DYNAMIXELDEVICE_H
+
+namespace module {
+namespace platform {
+    namespace nugus {
+        class DynamixelDevice {};
+    }  // namespace nugus
+}  // namespace platform
+}  // namespace module
+
+#endif  // MODULE_PLATFORM_NUGUS_DYNAMIXELDEVICE_H
