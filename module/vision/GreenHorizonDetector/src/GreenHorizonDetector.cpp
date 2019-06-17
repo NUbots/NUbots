6c36fbcc2771e3badefd747340988fa7d5d3db55
Alex Biddulph <c3124185@uon.edu.au>
2019-06-17 16:34:45 +1000

    Fixes bugs in GreenHorizonDetector

--- module/vision/GreenHorizonDetector/src/GreenHorizonDetector.cpp
+++ module/vision/GreenHorizonDetector/src/GreenHorizonDetector.cpp
@@ -1,22 +1,21 @@
 #include "GreenHorizonDetector.h"
 
+#include <fmt/format.h>
 #include <set>
 
 #include "extension/Configuration.h"
 
 #include "message/vision/GreenHorizon.h"
-#include "message/vision/Line.h"
 #include "message/vision/VisualMesh.h"
 
 #include "utility/math/geometry/ConvexHull.h"
+#include "utility/math/vision.h"
 
 namespace module {
 namespace vision {
 
     using extension::Configuration;
 
-    using message::vision::Line;
-    using message::vision::Lines;
     using message::vision::VisualMesh;
     using GreenHorizonMsg = message::vision::GreenHorizon;
 
@@ -28,13 +27,13 @@ namespace vision {
 
         on<Configuration>("GreenHorizonDetector.yaml").then([this](const Configuration& cfg) {
             // Use configuration here from file GreenHorizonDetector.yaml
-            config.seed_confidence  = cfg["seed_confidence"].as<float>();
-            config.end_confidence   = cfg["end_confidence"].as<float>();
-            config.cluster_points   = cfg["cluster_points"].as<int>();
-            config.draw_convex_hull = cfg["debug"]["draw_convex_hull"].as<bool>();
+            config.seed_confidence = cfg["seed_confidence"].as<float>();
+            config.end_confidence  = cfg["end_confidence"].as<float>();
+            config.cluster_points  = cfg["cluster_points"].as<int>();
+            config.debug           = cfg["debug"].as<bool>();
         });
 
-        on<Trigger<VisualMesh>>().then("Green Horizon", [this](const VisualMesh& mesh) {
+        on<Trigger<VisualMesh>, Buffer<2>>().then("Green Horizon", [this](const VisualMesh& mesh) {
             // Convinence variables
             const auto& cls        = mesh.classifications;
             const auto& neighbours = mesh.neighbourhood;
@@ -44,19 +43,15 @@ namespace vision {
             std::vector<std::vector<int>> clusters;
 
             // To keep track of all visited indices
-            std::set<int> visited;
-
-            // Threshold all field and field line detections
-            auto cls_seed_thresholded = (cls.col(LINE_INDEX) + cls.col(FIELD_INDEX)).array() > config.seed_confidence;
-            auto cls_end_thresholded  = (cls.col(LINE_INDEX) + cls.col(FIELD_INDEX)).array() > config.end_confidence;
+            std::vector<uint8_t> visited(mesh.indices.size(), 0);
 
             for (size_t i = 0; i < mesh.indices.size(); ++i) {
-                if (!visited.count(i)) {
+                if (visited[i] == 0) {
                     // Visiting this point
-                    visited.insert(i);
+                    visited[i] = 1;
 
-                    // If this point is confident enough to be a seed point breadth first search from here
-                    if (cls_seed_thresholded(i)) {
+                    // If this point is confident enough to be a seed point depth first search from here
+                    if ((cls(i, LINE_INDEX) + cls(i, FIELD_INDEX)) >= config.seed_confidence) {
                         // Start a new cluster
                         std::vector<int> cluster;
                         std::vector<int> stack;
@@ -66,42 +61,45 @@ namespace vision {
 
                         while (!stack.empty()) {
                             // Grab the next point
-                            int idx = stack.back();
+                            int search_idx = stack.back();
                             stack.pop_back();
 
                             // If it's good enough continue our search
-                            if (cls_end_thresholded(i)) {
-                                cluster.push_back(idx);
+                            if ((cls(search_idx, LINE_INDEX) + cls(search_idx, FIELD_INDEX)) >= config.end_confidence) {
+                                cluster.push_back(search_idx);
 
                                 // Skip the first point as it's always just us
-                                for (int j = 1; j < 7; ++j) {
-                                    int idx = neighbours(i, j);
-                                    if (!visited.count(idx)) {
-                                        visited.insert(idx);
-                                        stack.push_back(idx);
+                                for (int j = 0; j < 6; ++j) {
+                                    int neighbour_idx = neighbours(search_idx, j);
+                                    if ((visited[neighbour_idx] == 0) && (neighbour_idx < mesh.indices.size())) {
+                                        visited[neighbour_idx] = 1;
+                                        stack.push_back(neighbour_idx);
                                     }
                                 }
                             }
                         }
 
-                        clusters.emplace_back(std::move(cluster));
+                        // Discard clusters smaller than a given threshold
+                        if (cluster.size() >= config.cluster_points) {
+                            clusters.emplace_back(std::move(cluster));
+                        }
+                        else {
+                            if (config.debug) {
+                                log<NUClear::DEBUG>(fmt::format("Discarding cluster with size {}", cluster.size()));
+                            }
+                        }
                     }
                 }
             }
-
-            // Discard clusters smaller than a given threshold
-            clusters.erase(std::remove_if(clusters.begin(),
-                                          clusters.end(),
-                                          [this](const std::vector<int>& cluster) {
-                                              return cluster.size() < config.cluster_points;
-                                          }),
-                           clusters.end());
 
             // Sort clusters by size
             std::sort(
                 clusters.begin(), clusters.end(), [](const auto& a, const auto& b) { return a.size() < b.size(); });
 
             // Merge or discard clusters until only 1 remains
+            if (config.debug) {
+                log<NUClear::DEBUG>(fmt::format("Merging {} clusters", clusters.size()));
+            }
             while (clusters.size() > 1) {
                 for (auto it = clusters.begin(); it != clusters.end(); it = std::next(it)) {
                     // Get the largest and smallest X values
@@ -109,56 +107,47 @@ namespace vision {
                         return coords(a, 0) < coords(b, 0);
                     });
 
-                    for (auto it2 = std::next(it); it != clusters.end();) {
+                    const int& min_a = *range_a.first;
+                    const int& max_a = *range_a.second;
+
+                    for (auto it2 = std::next(it); it2 != clusters.end();) {
                         // Get the largest and smallest X values
                         auto range_b =
-                            std::minmax_element(it->begin(), it->end(), [&coords](const auto& a, const auto& b) {
+                            std::minmax_element(it2->begin(), it2->end(), [&coords](const auto& a, const auto& b) {
                                 return coords(a, 0) < coords(b, 0);
                             });
 
+                        const int& min_b = *range_b.first;
+                        const int& max_b = *range_b.second;
+
                         // Ranges do not overlap
                         // Merge the clusters
-                        if (range_a.second < range_b.first) {
+                        if ((max_a < min_b) || (max_b < min_a)) {
                             // Append the second cluster on to the first
                             it->insert(it->end(), it2->begin(), it2->end());
                             // Delete the second cluster
                             it2 = clusters.erase(it2);
-                            // Step over first cluster
-                            it = std::next(it);
                         }
                         // Second cluster is overlapping first cluster either on the left or the right
                         // Keep the largest cluster, or merge if the clusters are the same size
-                        else if (((range_a.first < range_b.first) && (range_b.first < range_a.second))
-                                 || ((range_b.first < range_a.first) && (range_a.first < range_b.second))) {
+                        else if (((min_a < min_b) && (min_b < max_a)) || ((min_b < min_a) && (min_a < max_b))) {
                             if (it->size() > it2->size()) {
                                 // Delete the second cluster
                                 it2 = clusters.erase(it2);
-                                // Step over first cluster
-                                it = std::next(it);
-                            }
-                            else if (it->size() < it2->size()) {
-                                // Delete the first cluster
-                                it = clusters.erase(it);
-                                // Step over second cluster
-                                it2 = std::next(it2);
                             }
                             else {
                                 // Clusters are the same size
-                                // If we have distinct min and max elements for both clusters, merge them
-                                // Otherwise, discard the second cluster
-                                if ((range_a.first != range_b.first) && (range_a.second != range_b.second)) {
+                                // If we have distinct min and max elements for both clusters, merge
+                                // them Otherwise, discard the second cluster
+                                if ((min_a != min_b) && (max_a != max_b)) {
                                     // Append the second cluster on to the first
                                     it->insert(it->end(), it2->begin(), it2->end());
                                     // Delete the second cluster
                                     it2 = clusters.erase(it2);
-                                    // Step over first cluster
-                                    it = std::next(it);
                                 }
                                 else {
                                     // Delete the second cluster
                                     it2 = clusters.erase(it2);
-                                    // Step over first cluster
-                                    it = std::next(it);
                                 }
                             }
                         }
@@ -166,47 +155,45 @@ namespace vision {
                 }
             }
 
-            // Sort remaining cluster from left to right
-            std::vector<int>& cluster = clusters.front();
-            std::sort(cluster.begin(), cluster.end(), [&](const int& a, const int& b) {
-                return coords(a, 0) < coords(b, 0);
-            });
-
-            auto msg = std::make_unique<GreenHorizonMsg>();
-
-            // Preserve mesh so that anyone using the GreenHorizon can access the original data
-            msg->mesh = mesh;
-
-            // Find the convex hull of the cluster
-            msg->cluster_indices = cluster;
-            msg->horizon_indices = utility::math::geometry::graham_scan(cluster, coords);
-
-            // Create vision lines for debugging purposes
-            if (config.draw_convex_hull && (msg->horizon_indices.size() > 1)) {
-                auto lines_msg = std::make_unique<Lines>();
-                // Make a line from each pair of points
-                for (auto it = std::next(msg->horizon_indices.begin()); it != msg->horizon_indices.end();
-                     it      = std::next(it)) {
-                    lines_msg->lines.emplace_back(mesh.camera_id,
-                                                  NUClear::clock::now(),
-                                                  coords.row(*std::prev(it)).cast<int>(),
-                                                  coords.row(*it).cast<int>(),
-                                                  Eigen::Vector4d{0.0, 1.0, 0.0, 1.0});
+            if (clusters.empty()) {
+                if (config.debug) {
+                    log<NUClear::DEBUG>("Failed to find green horizon");
                 }
-                // Add a line closing the convex hull
-                // Only add it if we have more than 2 points in the convex hull
-                if (msg->horizon_indices.size() > 2) {
-                    lines_msg->lines.emplace_back(mesh.camera_id,
-                                                  NUClear::clock::now(),
-                                                  coords.row(msg->horizon_indices.back()).cast<int>(),
-                                                  coords.row(msg->horizon_indices.front()).cast<int>(),
-                                                  Eigen::Vector4d{0.0, 1.0, 0.0, 1.0});
-                }
-                emit(std::move(lines_msg));
             }
+            else {
+                // Sort remaining cluster from left to right
+                std::sort(clusters.front().begin(), clusters.front().end(), [&](const int& a, const int& b) {
+                    return coords(a, 0) < coords(b, 0);
+                });
 
-            emit(std::move(msg));
+                auto msg = std::make_unique<GreenHorizonMsg>();
+
+                // Preserve mesh so that anyone using the GreenHorizon can access the original data
+                msg->mesh = const_cast<VisualMesh*>(&mesh)->shared_from_this();
+
+                msg->camera_id = mesh.camera_id;
+                msg->Hcw       = mesh.Hcw;
+
+                // Find the convex hull of the cluster
+                std::vector<int> horizon_indices(utility::math::geometry::graham_scan(clusters.front(), coords));
+
+                msg->horizon.reserve(horizon_indices.size());
+                for (const auto& index : horizon_indices) {
+                    auto unit = utility::math::vision::getCamFromImage(convert<float, 2>(coords.row(index)),
+                                                                       convert<unsigned int, 2>(mesh.image->dimensions),
+                                                                       mesh.image->lens);
+                    msg->horizon.emplace_back(convert<double, 3>(unit).cast<float>());
+                }
+
+                if (config.debug) {
+                    log<NUClear::DEBUG>(fmt::format("Calculated convex hull with {} points from cluster with {} points",
+                                                    msg->horizon.size(),
+                                                    clusters.front().size()));
+                }
+
+                emit(std::move(msg));
+            }
         });
-    }
+    }  // namespace vision
 }  // namespace vision
 }  // namespace module
