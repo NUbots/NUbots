ba108a4274d93ce9c0e4c31105057bd6c46943a2
tayloryoung6396 <c3206230@uon.edu.au>
2019-06-19 17:14:02 +1000

    Add an image compressor to the NUbots codebase (#205)
    * Add an image compressor that uses libjpeg
    
    * Adds de-bayer library from libdc1394
    
    * Updates fourcc definition to work outside of Vision.cpp
    
    * Updates Image Compressor module
    
    * Updates NUsight config to include compressed images
    
    * Fixes typo
    
    * Includes compressed images in role
    
    * Fixes formatting
    
    * Stylistic changes

---
+++ module/output/ImageCompressor/src/ImageCompressor.h
@@ -0,0 +1,30 @@
+#ifndef MODULE_OUTPUT_IMAGECOMPRESSOR_H
+#define MODULE_OUTPUT_IMAGECOMPRESSOR_H
+
+#include <turbojpeg.h>
+#include <cstdint>
+#include <nuclear>
+#include "bayer.h"
+#include "message/input/Image.h"
+
+namespace module {
+namespace output {
+
+    class ImageCompressor : public NUClear::Reactor {
+    private:
+        // Quality of compressed image
+        int quality;
+        // Method of which to perform debayering
+        DEBAYER_METHOD method;
+
+        void compress(const message::input::Image& image, const TJPF& format);
+
+    public:
+        /// @brief Called by the powerplant to build and setup the ImageCompressor reactor.
+        explicit ImageCompressor(std::unique_ptr<NUClear::Environment> environment);
+    };
+
+}  // namespace output
+}  // namespace module
+
+#endif  // MODULE_OUTPUT_IMAGECOMPRESSOR_H
