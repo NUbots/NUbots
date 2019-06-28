a22ba6a3cab145aef178a6484a6abe69cfa9b0b4
vagrant <vagrant@nubotsvm.nubots.net>
2019-06-28 14:16:48 +1000

    updates

--- tools/footdown.py
+++ tools/footdown.py
@@ -32,21 +32,32 @@ SERVO_ID = {
 
 
 def displacement(fk):
-    return -(
-        np.linalg.inv(
-            np.array(
-                [
-                    [fk.x.x, fk.y.x, fk.z.x, fk.t.x],
-                    [fk.x.y, fk.y.y, fk.z.y, fk.t.y],
-                    [fk.x.z, fk.y.z, fk.z.z, fk.t.z],
-                    [fk.x.t, fk.y.t, fk.z.t, fk.t.t],
-                ]
-            )
-        )[2, 3]
+
+    # Sometimes the matrix is transposed so if x, y and z translation are 0, it's the bottom rather than the translation
+    Htf = (
+        np.array(
+            [
+                [fk.x.x, fk.x.y, fk.x.z, fk.x.t],
+                [fk.y.x, fk.y.y, fk.y.z, fk.y.t],
+                [fk.z.x, fk.z.y, fk.z.z, fk.z.t],
+                [fk.t.x, fk.t.y, fk.t.z, fk.t.t],
+            ]
+        )
+        if fk.t.x == 0 and fk.t.y == 0 and fk.t.z == 0
+        else np.array(
+            [
+                [fk.x.x, fk.y.x, fk.z.x, fk.t.x],
+                [fk.x.y, fk.y.y, fk.z.y, fk.t.y],
+                [fk.x.z, fk.y.z, fk.z.z, fk.t.z],
+                [fk.x.t, fk.y.t, fk.z.t, fk.t.t],
+            ]
+        )
     )
 
+    return -(np.linalg.inv(Htf)[2, 3])
 
-def dataset(path, state):
+
+def dataset(path, state, foot_delta):
     for type_name, timestamp, msg in tqdm(
         nbs_decoder.decode(path), dynamic_ncols=True, unit="packets"
     ):
@@ -54,20 +65,33 @@ def dataset(path, state):
 
             # Work out the class
             if state == "WALK":
+                # Work out how far the foot is from the torso
                 l_height = displacement(
                     msg.forward_kinematics[SERVO_ID["L_ANKLE_ROLL"]]
                 )
                 r_height = displacement(
                     msg.forward_kinematics[SERVO_ID["R_ANKLE_ROLL"]]
                 )
-                print(path, l_height, r_height)
+                delta = abs(l_height - r_height)
+
+                # Get the load on the knee
+                l_load = msg.servo[SERVO_ID["R_KNEE"]].load
+                r_load = msg.servo[SERVO_ID["L_KNEE"]].load
+
+                y = (
+                    1 if l_height < r_height or delta < foot_delta else 0,
+                    1 if r_height < l_height or delta < foot_delta else 0,
+                )
+
+                tqdm.write("{},{},{},{}".format(l_height, r_height, l_load, r_load))
+
             elif state == "UP":
-                y = 0
+                y = (0, 0)
             elif state == "DOWN":
-                y = 1
+                y = (1, 1)
             else:
                 raise RuntimeError("The state must be UP DOWN or WALK")
-            data = (msg.servo[0].present_position, msg.servo[0].present_velocity)
+    exit(0)
 
 
 def register(command):
@@ -90,7 +114,9 @@ def run(config, **kwargs):
     with open(config, "r") as f:
         config = yaml.load(f)
 
+    foot_delta = config["config"]["foot_delta"]
+
     print("Loading data from NBS files")
     for group in tqdm(config["data"], dynamic_ncols=True, unit="group"):
         for f in tqdm(group["files"], dynamic_ncols=True, unit="file"):
-            data = dataset(os.path.join(data_path, f), group["state"])
+            data = dataset(os.path.join(data_path, f), group["state"], foot_delta)
