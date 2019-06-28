00ea2ec67d7eaa715ed069a751699544afea1e2f
Trent Houliston <trent@houliston.me>
2019-06-28 12:20:47 +1000

    Add displacement calculations

--- tools/footdown.py
+++ tools/footdown.py
@@ -5,6 +5,45 @@ import tensorflow as tf
 import yaml
 from tqdm import tqdm
 from util import nbs_decoder
+import numpy as np
+
+SERVO_ID = {
+    "R_SHOULDER_PITCH": 0,
+    "L_SHOULDER_PITCH": 1,
+    "R_SHOULDER_ROLL": 2,
+    "L_SHOULDER_ROLL": 3,
+    "R_ELBOW": 4,
+    "L_ELBOW": 5,
+    "R_HIP_YAW": 6,
+    "L_HIP_YAW": 7,
+    "R_HIP_ROLL": 8,
+    "L_HIP_ROLL": 9,
+    "R_HIP_PITCH": 10,
+    "L_HIP_PITCH": 11,
+    "R_KNEE": 12,
+    "L_KNEE": 13,
+    "R_ANKLE_PITCH": 14,
+    "L_ANKLE_PITCH": 15,
+    "R_ANKLE_ROLL": 16,
+    "L_ANKLE_ROLL": 17,
+    "HEAD_YAW": 18,
+    "HEAD_PITCH": 19,
+}
+
+
+def displacement(fk):
+    return -(
+        np.linalg.inv(
+            np.array(
+                [
+                    [fk.x.x, fk.y.x, fk.z.x, fk.t.x],
+                    [fk.x.y, fk.y.y, fk.z.y, fk.t.y],
+                    [fk.x.z, fk.y.z, fk.z.z, fk.t.z],
+                    [fk.x.t, fk.y.t, fk.z.t, fk.t.t],
+                ]
+            )
+        )[2, 3]
+    )
 
 
 def dataset(path, state):
@@ -12,7 +51,23 @@ def dataset(path, state):
         nbs_decoder.decode(path), dynamic_ncols=True, unit="packets"
     ):
         if type_name == "message.input.Sensors":
-            pass
+
+            # Work out the class
+            if state == "WALK":
+                l_height = displacement(
+                    msg.forward_kinematics[SERVO_ID["L_ANKLE_ROLL"]]
+                )
+                r_height = displacement(
+                    msg.forward_kinematics[SERVO_ID["R_ANKLE_ROLL"]]
+                )
+                print(path, l_height, r_height)
+            elif state == "UP":
+                y = 0
+            elif state == "DOWN":
+                y = 1
+            else:
+                raise RuntimeError("The state must be UP DOWN or WALK")
+            data = (msg.servo[0].present_position, msg.servo[0].present_velocity)
 
 
 def register(command):
