7564f6101e8db4dc4eebd43d3bf11d3eab80e6c3
Trent Houliston <trent@houliston.me>
2019-06-29 20:32:40 +1000

    Start upgrading the c++ code

--- module/platform/darwin/SensorFilter/src/VirtualLoadSensor.h
+++ module/platform/darwin/SensorFilter/src/VirtualLoadSensor.h
@@ -20,49 +20,82 @@
 #ifndef MODULES_PLATFORM_DARWIN_VIRTUALLOADSENSOR_H
 #define MODULES_PLATFORM_DARWIN_VIRTUALLOADSENSOR_H
 
-#include <armadillo>
-#include <array>
+#include <Eigen/Core>
 
 #include <extension/Configuration.h>
+#include <message/input/Sensors.h>
 
 namespace module {
 namespace platform {
     namespace darwin {
 
+        template <typename Scalar>
         class VirtualLoadSensor {
-            // this implements a linear model (trained by logistic regression) with a bayes filter on the output
-        private:
-            float noise_factor;
-            float current_noise;
-            float certainty_threshold;
-            float uncertainty_threshold;
-
-            arma::fmat::fixed<9, 8> W1;
-            arma::frowvec::fixed<8> b1;
-            arma::fmat::fixed<8, 2> W2;
-            arma::frowvec::fixed<2> b2;
-
-            arma::frowvec::fixed<2> state;
-            std::array<bool, 2> output_state;
-
-            arma::frowvec::fixed<2> softmax(const arma::frowvec::fixed<4>& x);
-
         public:
             VirtualLoadSensor()
                 : noise_factor(0.0f)
                 , current_noise(0.0f)
                 , certainty_threshold(0.0f)
                 , uncertainty_threshold(0.0f)
-                , W1(arma::fill::zeros)
-                , b1(arma::fill::zeros)
-                , W2(arma::fill::zeros)
-                , b2(arma::fill::zeros)
-                , state(arma::fill::zeros)
-                , output_state({true, true}) {}
-            VirtualLoadSensor(const ::extension::Configuration& network);
+                ) {}
+            VirtualLoadSensor(const ::extension::Configuration& network) {
+                // TODO load the weights and biases into the layers object
+            }
 
-            std::array<bool, 2> updateFeet(const arma::frowvec::fixed<9>& features);
-            arma::frowvec::fixed<2> sigmoid(const arma::frowvec::fixed<2>& x);
+            static constexpr Scalar sigmoid(const Scalar& value) {
+                return 1.0 / (1.0 + std::exp(-value));
+            }
+
+            std::array<bool, 2> updateFeet(const ::message::input::Sensors& input) {
+
+                // TODO based on configuration grab the sensor readings we are using
+                Eigen::Matrix<Scalar, Eigen::Dynamic, 1> logits;
+
+                // Run the neural network
+                for (int i = 0; i < layers.size(); ++i) {
+
+                    // Weights and bias
+                    logits = logits * l.first + l.second;
+
+                    // RELU except last layer
+                    if (i < layers.size() - 1) {
+                        logits = (logits > 0).select(logits, 0.0);
+                    }
+                    // Sigmoid final layer
+                    else {
+                        logits = logits.unaryExp(sigmoid);
+                    }
+                }
+
+                // Do the bayes update (1D kalman filter thing)
+                Scalar k = current_noise / (current_noise + noise_factor);
+                state += k * (logits - state);
+                current_noise *= 1.0f - k;
+                current_noise += 1.0f;
+
+                for (size_t leg = 0; leg < 2; leg++) {
+                    // We have some certainty in our measurement
+                    if (state[leg] > certainty_threshold) {
+                        output_state[leg] = true;
+                    }
+                    if (state[leg] < uncertainty_threshold) {
+                        output_state[leg] = false;
+                    }
+                }
+            }
+
+        private:
+            Scalar noise_factor;
+            Scalar current_noise;
+            Scalar certainty_threshold;
+            Scalar uncertainty_threshold;
+
+            std::vector<std::pair<Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>,
+                                  Eigen::Matrix<Scalar, Eigen::Dynamic, 1>>>
+                layers;
+
+            Eigen::Matrix<Scalar, 2, 1, Eigen::DontAlign> state;
+            std::array<bool, 2> output_state;
         };
     }  // namespace darwin
 }  // namespace platform
