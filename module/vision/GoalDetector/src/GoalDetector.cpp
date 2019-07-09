d6b9725fbdf31ddd3dbc8fcbc88be436fecf0f18
Trent Houliston <trent@houliston.me>
2019-07-09 17:14:53 +1000

    Another one down

--- module/vision/GoalDetector/src/GoalDetector.cpp
+++ module/vision/GoalDetector/src/GoalDetector.cpp
@@ -31,7 +31,6 @@
 
 #include "utility/math/coordinates.h"
 #include "utility/math/geometry/ConvexHull.h"
-#include "utility/math/vision.h"
 #include "utility/support/eigen_armadillo.h"
 #include "utility/support/yaml_armadillo.h"
 #include "utility/vision/Vision.h"
