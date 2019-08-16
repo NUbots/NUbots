64eae1f90bd498bd28b61e60570c6759c5712d5d
Alex Biddulph <c3124185@uon.edu.au>
2019-08-16 15:10:40 +1000

    Simplify code validation

--- .buildkite/scripts/validate_format.sh
+++ .buildkite/scripts/validate_format.sh
@@ -2,34 +2,18 @@
 
 ret=0
 
-find . -type f \( -name *.h \
-                  -o -name *.c \
-                  -o -name *.cc \
-                  -o -name *.cxx \
-                  -o -name *.cpp \
-                  -o -name *.hpp \
-                  -o -name *.ipp \
-                  -o -name *.proto \) > files.txt
+# Loop through all c/cpp/proto files and check validation
+for src in $(find . -regex '.*\.\(c\|cc\|cpp\|cxx\|hpp\|ipp\|proto\)')
+do
+    echo "Validating formatting for ${src}"
+    clang-format -style=file "${src}" | colordiff -u "${src}" -
+    ret=$((ret + $?))
+done
 
-# Loop through all c/cpp files
-while IFS= read -r in_file; do
-
-    # Print a status message so we know what it's doing
-    echo "Validating formatting for $in_file"
-
-    # Get what our formatted code should be
-    clang-format -style=file $in_file > correctly_formatted
-
-    # Check if our text is formatted incorrectly
-    if ! cmp -s $in_file correctly_formatted; then
-
-        # Flag that it is wrong and print the difference
-        ret=1
-        echo "$in_file is incorrectly formatted"
-        colordiff -u $in_file correctly_formatted
-    fi
-# This must be at the bottom since otherwise piping into the while will make a subshell
-done < files.txt
-
-# If we failed somewhere this will exit 1 and fail the build
-exit $ret
+echo "$ret files are not formatted correctly"
+if [ $ret -eq 0 ]
+then
+    exit 0
+else
+    exit 1
+fi
