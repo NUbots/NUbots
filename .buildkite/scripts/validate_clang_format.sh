5fe11b30eaa7b03074fc879e273de7b98017614b
Trent Houliston <trent@houliston.me>
2019-08-17 18:16:59 +1000

    Split the formatting jobs into multiple buildkite jobs

---
+++ .buildkite/scripts/validate_clang_format.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+check_formatting() {
+    echo "Validating formatting for $1"
+    clang-format -style=file "$1" | colordiff --color=yes -u "$1" -
+    return $?
+}
+export -f check_formatting
+
+# Loop through all c/cpp/proto files and check validation
+find . -regex '.*\.\(c\|cc\|cpp\|cxx\|hpp\|ipp\|proto\)$' \
+     | parallel --joblog formatting.log -j$(nproc) check_formatting
+# Count how many returned a non zero exist status
+ret=$(tail -n +2 formatting.log | awk '{ sum += $7; } END {print sum}')
+
+echo "$ret files are not formatted correctly"
+if [ $ret -eq 0 ]
+then
+    exit 0
+else
+    exit 1
+fi
---
+++ .buildkite/scripts/validate_python_format.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+check_formatting() {
+    echo "Validating formatting for $1"
+    cat "$1" | black -q - | colordiff --color=yes -u "$1" -
+    return $?
+}
+export -f check_formatting
+
+# Loop through all python files and check validation
+find . -regex '.*\.py$' \
+     | parallel --joblog formatting.log -j$(nproc) check_formatting
+# Count how many returned a non zero exist status
+ret=$(tail -n +2 formatting.log | awk '{ sum += $7; } END {print sum}')
+
+echo "$ret files are not formatted correctly"
+if [ $ret -eq 0 ]
+then
+    exit 0
+else
+    exit 1
+fi
