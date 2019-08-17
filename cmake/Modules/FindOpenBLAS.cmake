4dab497f9c9b23728aa8a3477275f8382309213c
Trent Houliston <trent@houliston.me>
2019-08-17 15:23:45 +1000

    Update where openblas is located

--- cmake/Modules/FindOpenBLAS.cmake
+++ cmake/Modules/FindOpenBLAS.cmake
@@ -1,9 +1,15 @@
-INCLUDE(ToolchainLibraryFinder)
-ToolchainLibraryFinder(NAME OpenBLAS
-                       HEADER cblas.h
-                       LIBRARY openblas
-                       VERSION_FILE openblas_config.h
-                       VERSION_REGEX "OPENBLAS_VERSION \" OpenBLAS (([0-9]+\\.?)+) \""
-)
+include(ToolchainLibraryFinder)
+toolchainlibraryfinder(NAME
+                       OpenBLAS
+                       HEADER
+                       cblas.h
+                       LIBRARY
+                       openblas
+                       PATH_SUFFIX
+                       openblas
+                       VERSION_FILE
+                       openblas_config.h
+                       VERSION_REGEX
+                       "OPENBLAS_VERSION \" ?OpenBLAS (([0-9]+\\.?)+) ?\"")
 
 # TODO also find LAPACK if needed
