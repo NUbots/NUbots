7b2c7bc91de2fe352e3870ffb12b1f64836c284e
Alex Biddulph <c3124185@uon.edu.au>
2019-08-20 16:00:27 +1000

    Set compilers in the toolchain files

--- docker/toolchain/nuc7i7bnh.sh
+++ docker/toolchain/nuc7i7bnh.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Set our compilers
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+
 # Set our package config so it finds things in the toolchain
 export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
 
