2ddc691122bbec769a28386add26218a12d91037
Trent Houliston <trent@houliston.me>
2019-08-27 17:42:15 +1000

    Allow shell to run anything but default to bash

--- tools/shell.py
+++ tools/shell.py
@@ -10,9 +10,16 @@ def register(command):
     # Install help
     command.help = "Open an interactive shell in a docker container"
 
+    command.add_argument(
+        "args", nargs="...", help="the command to run on the docker image. If empty will default to /bin/bash"
+    )
+
 
 @run_on_docker
-def run(**kwargs):
+def run(args, **kwargs):
 
-    # Run bash and exit with its exit code
-    exit(pty.spawn("/bin/bash") >> 8)
+    # If no arguments run bash and return its exit code
+    if len(args) == 0:
+        exit(pty.spawn("/bin/bash") >> 8)
+    else:
+        exit(pty.spawn(args) >> 8)
