58df2fe4fe09e20b60b773dd39529ad73d38b414
Alex Biddulph <c3124185@uon.edu.au>
2019-09-01 14:18:45 +1000

    Add install scripts to get Arch Linux set up on a robot

---
+++ doc/ArchInstall/arch-chroot_install.sh
@@ -0,0 +1,240 @@
+#! /bin/bash
+
+# Setup timezone information
+ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
+hwclock --systohc
+
+# Setup locale
+echo "en_AU.UTF-8 UTF-8"  > /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+
+echo "LANG=en_AU.UTF-8" > /etc/locale.conf
+
+# Setup hostname
+echo "igus${ROBOT_NUMBER}" > /etc/hostname
+echo "127.0.0.1       localhost" >> /etc/hosts
+echo "::1             localhost" >> /etc/hosts
+echo "127.0.1.1       igus${ROBOT_NUMBER}.localdomain igus${ROBOT_NUMBER}" >> /etc/hosts
+
+#########
+# USERS #
+#########
+
+# Install sudo
+pacman -S sudo
+
+# Setup users of the wheel group to be able to execute sudo commands
+sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+ALL\)/\1/' /etc/sudoers
+groupadd pgrimaging
+useradd -m -G wheel,lp,pgrimaging nubots
+
+# Set the root password
+echo "root: " | chpasswd
+
+# Set the user password
+echo "nubots: " | chpasswd
+
+# Install the intel microcode
+pacman -S --noconfirm --needed intel-ucode
+
+# Install grub
+pacman -S --noconfirm --needed grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Install system utilities
+pacman -S --noconfirm --needed openssh vim nano screen htop gdb linux-headers-generic bluez bluez-utils
+
+##############
+# NETWORKING #
+##############
+
+# Enable the ssh server
+systemctl enable sshd.socket
+systemctl enable sshd@.service
+
+# Setup the ssh issue file
+echo "Banner /etc/nubots_issue" >> /etc/ssh/sshd_config
+pacman -S python-pillow
+mkdir banner
+wget https://raw.githubusercontent.com/NUbots/NUbots/master/nuclear/roles/banner/__init__.py -O banner/__init__.py
+wget https://raw.githubusercontent.com/NUbots/NUbots/master/nuclear/roles/banner/ampscii.py -O banner/ampscii.py
+wget https://raw.githubusercontent.com/NUbots/NUbots/master/nuclear/roles/banner/bigtext.py -O banner/bigtext.py
+wget https://raw.githubusercontent.com/NUbots/NUbots/master/cmake/banner.png -O banner.png
+cat << EOF > banner.py
+from banner import ampscii
+from banner import bigtext
+
+banner = ampscii("banner.png")
+banner_lines = banner.replace("\x1b", "\\x1b").split("\n")[:-1]
+role_banner_lines = bigtext(os.path.splitext(os.path.basename(role_name))[0]).split("\n")[:-1]
+
+with open("/etc/nubots_issue", "w") as f:
+    for l in banner_lines:
+        role_file.write('{}\n'.format(l))
+    for l in role_banner_lines:
+        role_file.write('{}\n'.format(l))
+EOF
+python ./banner.py
+rm -rf banner*
+pacman -R python-pillow
+pacman -Qdt
+
+# Setup the fallback ethernet DHCP connection
+cat << EOF > /etc/systemd/network/99-eno1-dhcp.network
+[Match]
+Name=eno1
+
+[Network]
+DHCP=yes
+EOF
+
+# Setup the fallback ethernet static connection
+cat << EOF > /etc/systemd/network/90-eno1-static.network
+[Match]
+Name=eno1
+
+[Network]
+Address=10.1.1.${ROBOT_NUMBER}/16
+Gateway=10.1.3.1
+DNS=10.1.3.1
+DNS=8.8.8.8
+EOF
+
+# Setup the fallback wireless static connection
+cat << EOF > /etc/systemd/network/90-wlp58s0-static.network
+[Match]
+Name=wlp58s0
+
+[Network]
+Address=10.1.1.${ROBOT_NUMBER}/16
+Gateway=10.1.3.1
+DNS=10.1.3.1
+DNS=8.8.8.8
+EOF
+
+# Setup wpa_supplicant networks
+cat << EOF > /etc/wpa_supplicant/wpa_supplicant-wlp58s0.conf
+ctrl_interface=/var/run/wpa_supplicant
+ctrl_interface_group=wheel
+update_config=1
+fast_reauth=1
+ap_scan=1
+
+network={
+	ssid="epsilon-z"
+	#psk="9181918191"
+	psk=cf34016f6c385ae99246e5213cca8194f4b0aad498326ee1fb4f787271b84b8f
+    priority=2
+}
+network={
+	ssid="epsilon-x"
+	#psk="9181918191"
+	psk=cf34016f6c385ae99246e5213cca8194f4b0aad498326ee1fb4f787271b84b8f
+    priority=1
+}
+EOF
+
+# Create the bond interface
+cat << EOF > /etc/systemd/network/10-bond0.netdev
+[NetDev]
+Name=bond0
+Kind=bond
+
+[Bond]
+Mode=active-backup
+MIIMonitorSec=1s
+EOF
+
+# Configure the bond
+cat << EOF > /etc/systemd/network/10-bond0.network
+[Match]
+Name=bond0
+
+[Network]
+Address=10.1.1.${ROBOT_NUMBER}/16
+Gateway=10.1.3.1
+DNS=10.1.3.1
+DNS=8.8.8.8
+EOF
+
+# Setup the bond ethernet connection
+cat << EOF > /etc/systemd/network/20-eno1.network
+[Match]
+Name=eno1
+
+[Network]
+Bond=bond0
+PrimarySlave=true
+EOF
+
+# Setup the bond wireless connection
+cat << EOF > /etc/systemd/network/30-wlp58s0.network
+[Match]
+Name=wlp58s0
+
+[Network]
+Bond=bond0
+EOF
+
+# Enable and start both networking and dns services
+systemctl enable systemd-networkd.service
+systemctl enable systemd-resolved.service
+systemctl enable wpa_supplicant
+systemctl enable wpa_supplicant@wlp58s0
+
+# Link to the resolve.conf from system.d
+ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
+# Update pacman now that we have internet
+pacman -Syu
+
+# Populate udev rules.
+cat << EOF > /etc/udev/rules.d/100-nubots.rules
+# Set permissions for ttyUSB0 (CM730) and video* (webcam) devices
+KERNEL=="ttyUSB*", MODE="0666"
+KERNEL=="video*", MODE="0666"
+
+# Symlink the CM740 device to /dev/CM740
+KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE="0666", SYMLINK+="CM740"
+
+# Make sure the USB devices end up in the pgrimaging group.
+SUBSYSTEM=="usb", GROUP="pgrimaging"
+EOF
+
+# Install robocup.service to allow robocup to autostart
+cat << EOF > /etc/systemd/system/robocup.service
+[Unit]
+Description="RoboCup auto-start unit"
+Wants=network.target
+After=network.target
+RequiresMountsFor=/home/nubots
+
+[Service]
+Type=simple
+Restart=always
+WorkingDirectory=/home/nubots
+User=nubots
+Environment=HOME="/home/nubots/"
+PassEnvironment=HOME
+ExecStart=/home/nubots/robocup
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Make sure the system checks /usr/local for libraries
+echo -e "/usr/local/lib\n/usr/local/lib64" > /etc/ld.so.conf.d/usrlocal.conf
+
+# Make sure python checks /usr/local for packages
+sed "s/^\(PREFIXES\s=\s\)\[\([^]]*\)\]/\1[\2, '\/usr\/local']/" -i /usr/lib/python3.7/site.py \
+
+# Change ownership on /usr/local
+chown -R nubots:nubots /usr/local
+
+echo ""
+echo "######################################################################################"
+echo "# New system is now configured. Run the following commands then unplug the USB drive #"
+echo "# umount /mnt/boot/efi && umount /mnt && reboot                                      #"
+echo "######################################################################################"
---
+++ doc/ArchInstall/arch_install.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+# Ensure that the time is accurate
+timedatectl set-ntp true
+
+################
+# PARTITIONING #
+################
+DRIVE="/dev/nvme0n1"
+BOOT="${DRIVE}p1"
+ROOT="${DRIVE}p2"
+
+# Remove all existing partitions
+for v_partition in $(parted -s ${DRIVE} print|awk '/^ / {print $1}')
+do
+    parted -s ${DRIVE} rm ${v_partition}
+done
+
+# Setup partitions
+parted ${DRIVE} mklabel gpt
+parted ${DRIVE} mkpart primary fat32 1MiB 261MiB
+parted ${DRIVE} mkpart primary ext4 261MiB 100%
+
+# Format partitions
+mkfs.fat ${BOOT}
+mkfs.ext4 ${ROOT}
+
+#################
+# BOOTSTRAPPING #
+#################
+
+# Mount the partitions
+mount ${ROOT} /mnt
+mkdir -p /mnt/boot/efi
+mount ${BOOT} /mnt/boot/efi
+
+# Bootstrap Pacman
+pacstrap /mnt base
+
+# Update fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Download install script into chroot drive
+wget https://raw.githubusercontent.com/NUbots/NUbots/master/doc/ArchInstall/arch-chroot_install.sh \
+    -O /mnt/arch-chroot_install.sh
+chmod +x /mnt/arch-chroot_install.sh
+
+echo ""
+echo "####################################################################################"
+echo "# Time to configure the new system. Run the following commands to achieve glory!!! #"
+echo "# ROBOT_NUMBER=4 arch-chroot /mnt ./arch-chroot_install.sh                         #"
+echo "####################################################################################"
