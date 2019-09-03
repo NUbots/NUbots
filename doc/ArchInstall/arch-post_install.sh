e338311a65632e58bd78527d2d395387c77db5d9
Alex Biddulph <c3124185@uon.edu.au>
2019-09-04 08:55:24 +1000

    Fix spelling

--- doc/ArchInstall/arch-post_install.sh
+++ doc/ArchInstall/arch-post_install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Unable to make this symlink in the chroot environment as /etc/resolv.conf is a mounted volume
-ln -sf /run/systemd/resolve/stb-resolv.conf /mnt/etc/resolv.conf
+ln -sf /run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
 
 # Unmount the partitions
 umount /mnt/boot/efi
