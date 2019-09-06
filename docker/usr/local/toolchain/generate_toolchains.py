db48d7ecc7ba936bec6cea001d0e1801695cef15
Alex Biddulph <c3124185@uon.edu.au>
2019-09-06 15:54:56 +1000

    Set system processor in cmake toolchain

--- docker/usr/local/toolchain/generate_toolchains.py
+++ docker/usr/local/toolchain/generate_toolchains.py
@@ -82,6 +82,7 @@ def generate_cmake_toolchain(target, output_path):
     template = dedent(
         """\
         set(CMAKE_SYSTEM_NAME Linux)
+        set(CMAKE_SYSTEM_PROCESSOR x86_64)
         set(CMAKE_C_COMPILER gcc)
         set(CMAKE_CXX_COMPILER g++)
         set(CMAKE_FIND_ROOT_PATH
