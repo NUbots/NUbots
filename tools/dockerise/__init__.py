5c3572fcee33572826d1dc2559d02059eb09ed4b
Alex Biddulph <c3124185@uon.edu.au>
2019-10-15 19:19:30 +1100

    Add a wrapper around pty to handle window resizing

--- tools/dockerise/__init__.py
+++ tools/dockerise/__init__.py
@@ -6,6 +6,12 @@ import re
 import sys
 import shutil
 import subprocess
+import array
+import fcntl
+import pty
+import tty
+import signal
+import termios
 from tqdm import tqdm
 
 # Try to import docker, if we are already in docker this will fail
@@ -20,6 +26,53 @@ except:
 repository = "nubots"
 user = "nubots"
 directory = "NUbots"
+
+
+class WrapPty:
+    def __init__(self):
+        self.fd = None
+
+    def _set_pty_size(self):
+        buf = array.array("h", [0, 0, 0, 0])
+        fcntl.ioctl(pty.STDOUT_FILENO, termios.TIOCGWINSZ, buf, True)
+        fcntl.ioctl(self.fd, termios.TIOCSWINSZ, buf)
+
+    def _signal_winch(self, signum, frame):
+        """
+        Signal handler for SIGWINCH - window size has changed.
+        """
+        self._set_pty_size()
+
+    def spawn(self, args):
+        pid, self.fd = pty.fork()
+
+        if len(args) == 0:
+            args = ["/bin/bash"]
+
+        if pid == pty.CHILD:
+            # If no arguments run bash
+            os.execlp(args[0], *args)
+
+        old_handler = signal.signal(signal.SIGWINCH, self._signal_winch)
+        try:
+            mode = tty.tcgetattr(pty.STDIN_FILENO)
+            tty.setraw(pty.STDIN_FILENO)
+            restore = True
+        except tty.error:  # This is the same as termios.error
+            restore = False
+
+        self._set_pty_size()
+
+        try:
+            pty._copy(self.fd)
+        except (IOError, OSError):
+            if restore:
+                tty.tcsetattr(pty.STDIN_FILENO, tty.TCSAFLUSH, mode)
+
+        os.close(self.fd)
+        signal.signal(signal.SIGWINCH, old_handler)
+        self.fd = None
+        return os.waitpid(pid, 0)[1] >> 8
 
 
 def is_docker():
