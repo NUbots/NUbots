3e5654df83a78acb21910f4a5f57c1412b71403d
Alex Biddulph <c3124185@uon.edu.au>
2019-11-07 09:26:49 +1100

    Fix commented code in Neutron.cmake

--- nuclear/message/Neutron.cmake
+++ nuclear/message/Neutron.cmake
@@ -227,10 +227,13 @@ if(pybind11_FOUND)
 endif()
 
 if(src)
-  # Build a library from these files if(NUCLEAR_SHARED_BUILD) add_library(nuclear_message SHARED ${protobufs} ${src})
-  # set_property(TARGET nuclear_message PROPERTY LIBRARY_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/bin/lib") else()
-  # add_library(nuclear_message STATIC ${protobufs} ${src}) endif()
-  add_library(nuclear_message STATIC ${protobufs} ${src})
+  # Build a library from these files
+  if(NUCLEAR_SHARED_BUILD)
+    add_library(nuclear_message SHARED ${protobufs} ${src})
+    set_property(TARGET nuclear_message PROPERTY LIBRARY_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/bin/lib")
+  else()
+    add_library(nuclear_message STATIC ${protobufs} ${src})
+  endif()
 
   # The library uses protocol buffers
   target_link_libraries(nuclear_message ${PROTOBUF_LIBRARIES})
