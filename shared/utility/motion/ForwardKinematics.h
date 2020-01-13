5a22f12f34f9da46910ec5270ff813473c191cee
Ysobel <ysobelsims@gmail.com>
2020-01-13 12:09:43 +1100

    Removes comparison.h

--- shared/utility/motion/ForwardKinematics.h
+++ shared/utility/motion/ForwardKinematics.h
@@ -31,7 +31,6 @@
 #include "utility/input/LimbID.h"
 #include "utility/input/ServoID.h"
 #include "utility/math/angle.h"
-#include "utility/math/comparison.h"
 #include "utility/math/matrix/Rotation3D.h"
 #include "utility/math/matrix/Transform3D.h"
 #include "utility/support/eigen_armadillo.h"
