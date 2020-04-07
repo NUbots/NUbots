eccfc14f048392f0d564b7a5f8cc9f154619f055
Alex Biddulph <c3124185@uon.edu.au>
2020-04-07 13:14:39 +1000

    Make it possible to run roles inside of gdb and valgrind

--- tools/run.py
+++ tools/run.py
@@ -14,11 +14,24 @@ def register(command):
     command.add_argument(
         "--asan", dest="use_asan", action="store_true", default=False, help="Add ASAN environment variables"
     )
+    command.add_argument(
+        "--gdb", dest="use_gdb", action="store_true", default=False, help="Run the specified program using gdb"
+    )
+    command.add_argument(
+        "--valgrind",
+        dest="use_valgrind",
+        action="store_true",
+        default=False,
+        help="Run the specified program using valgrind",
+    )
     command.add_argument("args", nargs="+", help="the command and any arguments that should be used for the execution")
 
 
 @run_on_docker
-def run(args, use_asan, **kwargs):
+def run(args, use_asan, use_gdb, use_valgrind, **kwargs):
+
+    if use_gdb and use_valgrind:
+        raise Exception("Cannot run with both gdb and valgrind")
 
     # Change into the build directory
     os.chdir(os.path.join(b.project_dir, "..", "build"))
@@ -33,6 +46,27 @@ def run(args, use_asan, **kwargs):
     if use_asan:
         env.update({"ASAN_OPTIONS": "log_path=/home/nubots/NUbots/asan.log"})
 
+    if use_gdb:
+        cmd = cmd + [
+            "gdb",
+            "-ex",
+            "'set logging redirect on'",
+            "-ex",
+            "'set logging file /home/nubots/build/gdb.log'",
+            "-ex",
+            "r",
+            "--args",
+        ]
+    elif use_valgrind:
+        cmd = cmd + [
+            "valgrind",
+            "--log-file=/home/nubots/build/valgrind.log",
+            "--show-error-list=yes",
+            "--leak-check=full",
+            "--show-leak-kinds=all",
+        ]
+    else:
+        cmd = []
 
     # Run the command
     pty = WrapPty()
