a93ef53b289e6e45828baf888886c066dda52209
Alex Biddulph <c3124185@uon.edu.au>
2020-04-17 15:00:12 +1000

    Adjust PATH environment variable
    Apparently python programs are now installed in the users home directory

--- tools/format.py
+++ tools/format.py
@@ -18,16 +18,20 @@ def run(**kwargs):
     # Change into the project directory
     os.chdir(b.project_dir)
 
+    # Prepend the users local bin directory to the path
+    env = os.environ
+    env["PATH"] = os.pathsep.join([os.path.join(env["HOME"], ".local", "bin"), env["PATH"]])
+
     # Use git to get all of the files that are committed to the repository
     files = check_output(["git", "ls-files"]).decode("utf-8")
     for f in files.splitlines():
         if f.endswith((".h", ".c", ".cc", ".cxx", ".cpp", ".hpp", ".ipp", ".proto")):
             print("Formatting {} with clang-format".format(f))
-            call(["clang-format", "-i", "-style=file", f])
+            call(["clang-format", "-i", "-style=file", f], env=env)
         elif f.endswith(("CMakeLists.txt", ".cmake", ".role")):
             print("Formatting {} with cmake-format".format(f))
-            call(["cmake-format", "-i", f])
+            call(["cmake-format", "-i", f], env=env)
         elif f.endswith((".py")):
             print("Formatting {} with isort and black".format(f))
-            call(["isort", f])
-            call(["black", f])
+            call(["isort", f], env=env)
+            call(["black", f], env=env)
