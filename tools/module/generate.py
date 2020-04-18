09ef23c2a334f1ee96884cfd896b3aa4c655730b
Trent Houliston <trent@houliston.me>
2020-04-18 14:38:44 +1000

    Make test conform to style guide

--- tools/module/generate.py
+++ tools/module/generate.py
@@ -179,7 +179,9 @@ def generate_test(parts):
         //#include <catch.hpp>
 
         // Remove this line when test files are added
-        int main() {{ return 0; }}
+        int main() {{
+            return 0;
+        }}
         """
     )
 
