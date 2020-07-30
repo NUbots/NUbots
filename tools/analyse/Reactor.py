1195fde9c0e95b8cc03c134ce387648c4ab0f5f4
ElderNoSpace <cameron.murtagh00@gmail.com>
2020-07-30 19:37:54 +1000

    can find emit and on nodes in methods

--- tools/analyse/Reactor.py
+++ tools/analyse/Reactor.py
@@ -1,21 +1,52 @@
+import clang.cindex
+
+
+class Method:
+    def __init__(self, node):
+        self.node = node
+        self.on = self._findOnNodes(node)
+        self.emit = self._findEmitNodes(node)
+
+    def __repr__(self):
+        representation = self.node.spelling
+        for on in self.on:
+            representation += " on:{}:{}:{}".format(on.location.file, on.location.line, on.location.column)
+        for emit in self.emit:
+            representation += " emit:{}:{}:{}".format(emit.location.file, emit.location.line, emit.location.column)
+        return representation
+
+    def _findOnNodes(self, node):
+        ons = []
+        for child in node.walk_preorder():
+            if (
+                child.kind == clang.cindex.CursorKind.CALL_EXPR
+                and child.type.spelling == "NUClear::threading::ReactionHandle"
+                and child.spelling == "then"
+            ):
+                ons.append(child)
+        return ons
+
+    # Find emit statement
+    def _findEmitNodes(self, node):
+        emits = []
+        for child in node.walk_preorder():
+            if child.kind == clang.cindex.CursorKind.CALL_EXPR and child.spelling == "emit":
+                emits.append(child)
+        return emits
+
+
+class on:
+    def __init__(self, node):
+        self.node = node
+
+
 class Reactor:
     def __init__(self, node, methods):
         self.node = node
-        self.methods = methods
+        self.methods = [Method(method) for method in methods]
 
     def __repr__(self):
         reperesentation = self.node.spelling
         for method in self.methods:
-            reperesentation += " {}".format(method.spelling)
+            reperesentation += " {}".format(method)
         return reperesentation
-
-    # def _findOnNodes(self, node):
-    #    for child in node.walk_preorder():
-    #        if child.kind == clang.cindex.CursorKind.CALL_EXPR and child.spelling == "emit":
-    #            yield child
-
-    # Find emit statement
-    # def _findEmitNodes(self, node):
-    #    for child in node.walk_preorder():
-    #        if child.kind == clang.cindex.CursorKind.CALL_EXPR and child.spelling == "emit":
-    #            yield child
