0dd2b7a3c081793a8f05db480ff9a2486d6683f0
ElderNoSpace <cameron.murtagh00@gmail.com>
2020-08-01 20:18:39 +1000

    Fixed edge case with nusight emits

--- tools/analyse/__init__.py
+++ tools/analyse/__init__.py
@@ -3,7 +3,7 @@ from .TranslationUnit import TranslationUnit
 
 import clang.cindex
 
-# TODO put in cofig file
+# TODO put in config file
 libraryFile = "/usr/local/lib"  # llvm-config --libdir
 clang.cindex.Config.set_library_path(libraryFile)
 
@@ -18,25 +18,18 @@ def translate(index, path):
 
 # Prints out one node
 def printNode(node, tab=0):
-    print(
-        "  " * tab
-        + "{} {}#{} {}:{}:{} {}".format(
-            node.kind.name,
-            node.type.spelling,
-            node.spelling,
-            node.location.file,
-            node.location.line,
-            node.location.column,
-            node.raw_comment,
-        )
+    return "  " * tab + "{} {}#{} {}:{}:{}\n".format(
+        node.kind.name, node.type.spelling, node.spelling, node.location.file, node.location.line, node.location.column
     )
 
 
 # Print out a kinda readable tree
 def printTree(node, tab=0):
-    printNode(node, tab)
+    out = ""
+    out += printNode(node, tab)
     for child in node.get_children():
-        printTree(child, tab + 1)
+        out += printTree(child, tab + 1)
+    return out
 
 
 def printReactorAst(reactor):
