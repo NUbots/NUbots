eb48dffea82ff84731c37ebe63e21c04a44c443e
Kip Hamiltons <c3255012@uon.edu.au>
2021-01-13 14:08:10 +1100

    Adds some comments to the LLVM script with context.

--- docker/usr/local/package/llvm.sh
+++ docker/usr/local/package/llvm.sh
@@ -15,17 +15,23 @@ BUILD_FOLDER="/var/tmp/build"
 mkdir -p "${BUILD_FOLDER}"
 cd "${BUILD_FOLDER}"
 
+# This in-tree build process has been taken from the IGC readme, almost verbatim
+# The checkout of the llvm repo is the fix they suggest for patching issues
+
 git clone https://github.com/intel/vc-intrinsics vc-intrinsics
 
+# Needs to be cloned so that the tag can be checked out, so that the patches apply correctly
 # download-and-extract https://github.com/llvm/llvm-project/archive/llvmorg-10.0.0.tar.gz && mv llvm-project-llvmorg-10.0.0 llvm-project
 
 git clone -b release/10.x https://github.com/llvm/llvm-project llvm-project
 cd llvm-project
+# Checkout this specific tag so that the patches apply automatically during the build process
 git checkout -b tag llvmorg-10.0.0
 cd ..
 
 download-and-extract https://github.com/intel/opencl-clang/archive/v10.0.0-2.tar.gz && mv opencl-clang-10.0.0-2 llvm-project/llvm/projects/opencl-clang
 
+# Needs to be cloned because during the build process, something is done with the .git directory.
 # download-and-extract https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/v10.0.0.tar.gz && mv SPIRV-LLVM-Translator-10.0.0 llvm-project/llvm/projects/llvm-spirv
 
 git clone -b llvm_release_100 https://github.com/KhronosGroup/SPIRV-LLVM-Translator llvm-project/llvm/projects/llvm-spirv
@@ -35,6 +41,7 @@ git clone https://github.com/intel/llvm-patches llvm_patches
 
 mkdir build
 cd build
+# Does not build with the toolchain.cmake flag. TODO: fix that
 cmake ../igc/IGC
 #     -DCMAKE_BUILD_TYPE="Release" \
 #     -DCMAKE_TOOLCHAIN_FILE="/usr/local/toolchain.cmake" \
