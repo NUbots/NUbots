ca8cb124b96d0e51aef1c4f77a3294a786986005
Trent Houliston <trent@houliston.me>
2021-02-05 21:35:22 +1100

    Update the formatting script to handle typescript files

--- tools/format.py
+++ tools/format.py
@@ -3,25 +3,35 @@
 import multiprocessing
 import os
 import sys
+from functools import partial
 from subprocess import STDOUT, CalledProcessError, check_output
 
 import b
 from dockerise import run_on_docker
+from utility.yarn import find_eslint, find_package_json
 
 
-def _do_format(path):
+def _do_format(path, eslint_path, package_path):
     text = ""
     try:
+        # Use the absolute path to file
+        abs_path = os.path.abspath(path)
+
         if path.endswith((".h", ".c", ".cc", ".cxx", ".cpp", ".hpp", ".ipp", ".proto", ".frag", ".glsl", ".vert")):
             text = "Formatting {} with clang-format\n".format(path)
-            text = text + check_output(["clang-format", "-i", "-style=file", path], stderr=STDOUT).decode("utf-8")
+            text = text + check_output(["clang-format", "-i", "-style=file", abs_path], stderr=STDOUT).decode("utf-8")
         elif path.endswith(("CMakeLists.txt", ".cmake", ".role")):
             text = "Formatting {} with cmake-format\n".format(path)
-            text = text + check_output(["cmake-format", "-i", path], stderr=STDOUT).decode("utf-8")
+            text = text + check_output(["cmake-format", "-i", abs_path], stderr=STDOUT).decode("utf-8")
         elif path.endswith((".py")):
             text = "Formatting {} with isort and black\n".format(path)
-            text = text + check_output(["isort", path], stderr=STDOUT).decode("utf-8")
-            text = text + check_output(["black", path], stderr=STDOUT).decode("utf-8")
+            text = text + check_output(["isort", abs_path], stderr=STDOUT).decode("utf-8")
+            text = text + check_output(["black", abs_path], stderr=STDOUT).decode("utf-8")
+        elif eslint_path is not None and (path.endswith((".ts")) or path.endswith((".tsx"))):
+            text = "Formatting {} with eslint and prettier\n".format(path)
+            text = text + check_output(
+                [eslint_path, "--color", "--fix", abs_path], cwd=package_path, stderr=STDOUT
+            ).decode("utf-8")
     except CalledProcessError as e:
         text = text + e.output.decode("utf-8")
 
@@ -35,6 +45,12 @@ def register(command):
 
 @run_on_docker
 def run(**kwargs):
+    # Check for eslint in node_modules folder
+    eslint_path = find_eslint(os.path.join(b.project_dir, "nusight2", "node_modules"))
+
+    # Find path to package.json
+    # We need to run the eslint command from this directory
+    package_path = find_package_json(project_root=b.project_dir, package_name="NUsight")
 
     # Change into the project directory
     os.chdir(b.project_dir)
@@ -43,5 +59,7 @@ def run(**kwargs):
     files = check_output(["git", "ls-files"]).decode("utf-8")
 
     with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
-        for r in pool.imap_unordered(_do_format, files.splitlines()):
+        for r in pool.imap_unordered(
+            partial(_do_format, eslint_path=eslint_path, package_path=package_path), files.splitlines()
+        ):
             sys.stdout.write(r)
