cb37131ad44a1d7fed3ce5cf7cd0fdf4dd6a72d5
Trent Houliston <trent@houliston.me>
2021-02-22 16:32:17 +1100

    Update b.py

--- nuclear/b.py
+++ nuclear/b.py
@@ -75,7 +75,7 @@ except:
 
 if __name__ == "__main__":
 
-    # Add our builtin tools to the path and user tools
+    # Prepend nuclear and user tools to the path, so we prefer our packages
     sys.path.insert(0, nuclear_tools_path)
     sys.path.insert(0, user_tools_path)
 
