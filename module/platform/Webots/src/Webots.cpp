bc9a665dc32d3152ee7524bf7afd24c0d1556efc
Cameron Murtagh <43051455+CMurtagh-LGTM@users.noreply.github.com>
2021-04-02 04:25:19 +0000

    Fixed order of operations.

--- module/platform/Webots/src/Webots.cpp
+++ module/platform/Webots/src/Webots.cpp
@@ -68,7 +68,9 @@ int Webots::tcpip_connect(const std::string& server_name, const char& port) {
 
     // Store the ip address information that we will connect to
     addrinfo *address;
-	if(int error = getaddrinfo(server_name.c_str(), &port, &hints, &address) != 0){
+	
+	int error;
+	if((error = getaddrinfo(server_name.c_str(), &port, &hints, &address)) != 0){
     	log<NUClear::ERROR>(fmt::format("Cannot resolve server name: {}. Error {}. Error code {}", server_name, gai_strerror(error), error));
 		return -1;
 	}    
