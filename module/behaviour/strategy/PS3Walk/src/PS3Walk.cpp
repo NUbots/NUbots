bca5915be8715c688221c2753cee6ca5d40b452e
Kip <p0nage@live.com.au>
2021-04-26 15:35:52 +1000

    Formatting

--- module/behaviour/strategy/PS3Walk/src/PS3Walk.cpp
+++ module/behaviour/strategy/PS3Walk/src/PS3Walk.cpp
@@ -133,7 +133,7 @@ namespace module::behaviour::strategy {
         on<Every<20, Per<std::chrono::seconds>>>().then([this] {
             if (!headLocked) {
                 auto headCommand         = std::make_unique<HeadCommand>();
-                headCommand->yaw        = headYaw / std::numeric_limits<short>::max() * 1.5f;
+                headCommand->yaw         = headYaw / std::numeric_limits<short>::max() * 1.5f;
                 headCommand->pitch       = headPitch / std::numeric_limits<short>::max();
                 headCommand->robot_space = true;
                 emit(std::move(headCommand));
