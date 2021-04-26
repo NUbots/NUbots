d4f7068f8345e43bfe66967a0731d054564fff48
Kip Hamiltons <p0nage@live.com.au>
2021-04-26 12:12:26 +1000

    Removes struct keyword in struct type instantiation

--- module/support/SignalCatcher/src/SignalCatcher.cpp
+++ module/support/SignalCatcher/src/SignalCatcher.cpp
@@ -64,7 +64,7 @@ namespace module::support {
 
         // Store our powerplant in the static variable
         POWER_PLANT = &powerplant;
-        struct sigaction action {};
+        sigaction action{};
 
         // Setup our segmentation fault signal handler/converter
         std::memset(&action, 0, sizeof(action));
