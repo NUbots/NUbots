66d7b001d681ae359b42bd1427916313575b6d07
Kip Hamiltons <p0nage@live.com.au>
2021-05-22 16:00:54 +1000

    clock fixes

--- shared/clock/clock.hpp
+++ shared/clock/clock.hpp
@@ -4,7 +4,9 @@
 #include <nuclear>
 
 namespace utility::clock {
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
     extern double custom_rtf;  // real time factor
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
     extern std::chrono::steady_clock::time_point last_update;
 }  // namespace utility::clock
 
