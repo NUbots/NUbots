08fd10531fabd2dac115148adced739d8c3642b0
Alex Biddulph <c3124185@uon.edu.au>
2021-05-23 14:53:46 +1000

    Remove unused variable

--- module/behaviour/skills/Getup/src/Getup.hpp
+++ module/behaviour/skills/Getup/src/Getup.hpp
@@ -32,9 +32,6 @@ namespace module::behaviour::skills {
      */
     class Getup : public NUClear::Reactor {
     private:
-        // If accelerometer z value faals below this threshold then we have fallen over
-        static constexpr double FALLEN_THRESHOLD = 8.0;
-
         const size_t id;
 
         bool isFront;
