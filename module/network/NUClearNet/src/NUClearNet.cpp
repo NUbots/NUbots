9ca98000db9ccbbe1bdb622e3c89758257c7b400
Josephus Paye II <j.paye96@gmail.com>
2021-06-01 19:31:32 +1000

    Update module/network/NUClearNet/src/NUClearNet.cpp
    Co-authored-by: Alex Biddulph <c3124185@uon.edu.au>

--- module/network/NUClearNet/src/NUClearNet.cpp
+++ module/network/NUClearNet/src/NUClearNet.cpp
@@ -30,7 +30,7 @@ namespace module::network {
         on<Configuration>("NUClearNet.yaml").then([this](const Configuration& config) {
             auto netConfig              = std::make_unique<NUClear::message::NetworkConfiguration>();
             std::string name            = config["name"].as<std::string>();
-            netConfig->name             = name.length() == 0 ? utility::support::getHostname() : name;
+            netConfig->name             = name.empty() ? utility::support::getHostname() : name;
             netConfig->announce_address = config["address"].as<std::string>();
             netConfig->announce_port    = config["port"].as<uint16_t>();
             emit<Scope::DIRECT>(netConfig);
