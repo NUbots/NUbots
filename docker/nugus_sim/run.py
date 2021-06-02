acf3356875e622b652d049349031175387d0c22c
Josephus Paye II <j.paye96@gmail.com>
2021-06-02 16:02:13 +1000

    Add final suggestions

--- docker/nugus_sim/run.py
+++ docker/nugus_sim/run.py
@@ -7,8 +7,8 @@ import sys
 
 import ruamel.yaml
 
-DEFAULT_BINARIES_DIR = "/home/nubots/NUbots/binaries"
-DEFAULT_CONFIG_DIR = DEFAULT_BINARIES_DIR + "/config"
+DEFAULT_BINARIES_DIR = os.path.abspath(os.path.join(os.sep, "home", "nubots", "NUbots", "binaries"))
+DEFAULT_CONFIG_DIR = os.path.join(DEFAULT_BINARIES_DIR, "config")
 
 REQUIRED_ENV_VARS = ("ROBOCUP_ROBOT_ID", "ROBOCUP_TEAM_COLOR", "ROBOCUP_SIMULATOR_ADDR")
 OPTIONAL_ENV_VARS = (
@@ -19,11 +19,23 @@ OPTIONAL_ENV_VARS = (
 )
 
 
+yaml = ruamel.yaml.YAML()  # Can't use `typ="safe"` since we want round-tripping to preserve comments
+
+
+def read_config(file_path):
+    with open(file_path, "r") as file:
+        return yaml.load(file)
+
+
+def write_config(file_path, config):
+    with open(file_path, "w") as file:
+        yaml.dump(config, file)
+
+
 def read_args() -> dict:
     parser = argparse.ArgumentParser(description="Run a role for webots RoboCup")
 
     parser.add_argument("role", help="The role to run")
-    parser.add_argument("--goalie", action="store_true", default=False, help="Run the role with goalie behaviour")
     parser.add_argument(
         "--binaries-dir", dest="binaries_dir", default=DEFAULT_BINARIES_DIR, help="The path to built role binaries"
     )
@@ -59,52 +71,34 @@ def read_args() -> dict:
         "binaries_dir": args.binaries_dir,
         "config_dir": args.config_dir,
         "env_vars": env_vars,
-        "is_goalie": args.goalie,
     }
 
 
-def update_config(args: dict) -> None:
+def update_config_files(args: dict) -> None:
     env_vars = args["env_vars"]
-    is_goalie = args["is_goalie"]
-
-    yaml = ruamel.yaml.YAML()  # Can't use `typ="safe"` since we want round-tripping to preserve comments
 
     # Change into the config directory
     os.chdir(args["config_dir"])
 
     # Set `player_id` in GlobalConfig.yaml from ROBOCUP_ROBOT_ID
-    with open("GlobalConfig.yaml", "r+") as file:
-        global_config = yaml.load(file)
-        global_config["player_id"] = int(env_vars["ROBOCUP_ROBOT_ID"])
-        file.seek(0)
-        yaml.dump(global_config, file)
+    global_config = read_config("GlobalConfig.yaml")
+    global_config["player_id"] = int(env_vars["ROBOCUP_ROBOT_ID"])
+    write_config("GlobalConfig.yaml", global_config)
 
     # Set `player_id` in GameController.yaml from ROBOCUP_ROBOT_ID
-    with open("GameController.yaml", "r+") as file:
-        game_controller_config = yaml.load(file)
-        game_controller_config["player_id"] = int(env_vars["ROBOCUP_ROBOT_ID"])
-        file.seek(0)
-        yaml.dump(game_controller_config, file)
+    game_controller_config = read_config("GameController.yaml")
+    game_controller_config["player_id"] = int(env_vars["ROBOCUP_ROBOT_ID"])
+    write_config("GameController.yaml", game_controller_config)
 
     # ROBOCUP_TEAM_COLOR
     # (not set as it's not used by our code)
 
     # Set `server_address` and `port` in webots.yaml from ROBOCUP_SIMULATOR_ADDR
-    with open("webots.yaml", "r+") as file:
-        webots_config = yaml.load(file)
-        address, port = env_vars["ROBOCUP_SIMULATOR_ADDR"].split(":", 2)
-        webots_config["server_address"] = address
-        webots_config["port"] = int(port)
-        file.seek(0)
-        yaml.dump(webots_config, file)
-
-    # Set `goalie` in SoccerSimulator.yaml from the --goalie argument
-    if is_goalie:
-        with open("SoccerStrategy.yaml", "r+") as file:
-            soccer_simulator_config = yaml.load(file)
-            soccer_simulator_config["goalie"] = is_goalie
-            file.seek(0)
-            yaml.dump(soccer_simulator_config, file)
+    webots_config = read_config("webots.yaml")
+    address, port = env_vars["ROBOCUP_SIMULATOR_ADDR"].split(":", 2)
+    webots_config["server_address"] = address
+    webots_config["port"] = int(port)
+    write_config("webots.yaml", webots_config)
 
 
 def run_role(role: str, binaries_dir: str, env_vars: dict) -> None:
@@ -126,5 +120,5 @@ def run_role(role: str, binaries_dir: str, env_vars: dict) -> None:
 
 if __name__ == "__main__":
     args = read_args()
-    update_config(args)
+    update_config_files(args)
     run_role(args["role"], args["binaries_dir"], args["env_vars"])
