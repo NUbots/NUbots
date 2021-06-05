e5c00989b1508e79ff9190f58a6237ef067ac703
Ysobel <ysobelsims@gmail.com>
2021-06-05 20:10:44 +1000

    treat bool properly

--- module/input/GameController/src/GameControllerData.hpp
+++ module/input/GameController/src/GameControllerData.hpp
@@ -130,7 +130,7 @@ namespace module::input::gamecontroller {
            << "\t\tNumber of warnings: " << uint(robot.numberOfWarnings) << std::endl
            << "\t\tYellow Card Count..: " << uint(robot.yellowCardCount) << std::endl
            << "\t\tRed Card Count.....: " << uint(robot.redCardCount) << std::endl
-           << "\t\tGoalkeeper: " << uint(robot.goalKeeper) << std::endl;
+           << "\t\tGoalkeeper: " << std::boolalpha << robot.goalKeeper << std::endl;
         return os;
     }
 
