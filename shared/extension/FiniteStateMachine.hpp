a9279317d1e7a0166d4ad1b1cfdf97373c31d08b
cameron <cameron.murtagh00@gmail.com>
2021-06-22 23:41:19 +1000

    a rethink

--- shared/extension/FiniteStateMachine.hpp
+++ shared/extension/FiniteStateMachine.hpp
@@ -2,94 +2,64 @@
 #define EXTENSION_FINITE_STATE_MACHINE
 
 #include <memory>
+#include <tuple>
 #include <nuclear>
-#include <string>
-#include <vector>
-#include <atomic>
 #include <typeinfo>
+#include <tuple>
 
 namespace extension::finite_state {
 
     template <typename T>
-    std::string type_name(){
-        return NUClear::demangle(typeid(T).name());
-    }
+    struct has_leaving {
+        //TODO(cameron)
+    };
 
-    template <typename T>
-    void recursive_demangle(std::vector<std::string>& destination){
-        destination.emplace_back(type_name<T>());
-    }
+    class State {
+        public:
+            virtual void entering(){}
 
-    template <typename T, typename... U>
-    void recursive_demangle(std::vector<std::string>& destination){
-        destination.emplace_back(type_name<T>());
-        recursive_demangle<U...>(destination);
-    }
+            virtual void running() = 0;
 
-    template <int I, typename T>
-    bool recursive_check(const std::vector<std::string>& current_state){
-        return destination[I] == type_name<T>();
-    }
+            virtual void leaving(){}
 
-    template <int I, typename T, typename... U>
-    bool recursive_check(const std::vector<std::string>& current_state){
-        return recursive_check<I + 1, U...>(current_state) && destination[I] == type_name<T>();
-    }
+            template <typename... Triggers>
+            struct Transition {
+                using triggers = std::tuple<Triggers...>;
 
-    struct initial{};
+                virtual State get_new_state() = 0;
 
-    std::vector<std::string> current_state = initial();
+                virtual bool transition_gaurd(){
+                    return true;
+                }
+            };
+    };
 
-    std::atomic_bool transitioning = false;
-
-    template <typename Transition, typename... State>
-    struct Entering : NUClear::dsl::word::Trigger<Transition> {
-        template <typename DSL>
-        static inline bool precondition(NUClear::threading::reaction& reaction){
-            if(transitioning.load || !recursive_check<0, State...>(current_state)){
-                return false;
+    template <typename Name>
+    class FiniteStateMachineRunner : NUClear::Reactor {
+        private:
+            /*template <int I, typename Tuple, typename DSL>
+            void call_bind(const std::shared_ptr<NUClear::threading::reaction>& reaction){
+                NUClear::dsl::operation::TypeBind<std::tuple_element<I, Tuple>>::bind<DSL>(reaction);
+                if constexpr (I < std::tuple_size_v<Tuple>){
+                    call_bind<I+1, Tuple, DSL>(reaction);
+                }
             }
 
-            transitioning.store(true);
-            reaction.reactor.powerplant.emit<NUClear::dsl::word::emit::Direct>(std::make_unique<run_leaving<Transition, State...>>());
+            template <typename... Triggers>
+            struct TriggersHandler {
+                template <typename DSL>
+                static inline void bind(const std::shared_ptr<NUClear::threading::reaction>& reaction){
+                    call_bind<0, State, 
+                }
+            }*/
+
+        public:
+        FiniteStateMachineRunner(std::unique_ptr<NUClear::Environment> environment): NUClear::Reactor(environment){}
+
+        static void install(NUClear::PowerPlant power_plant){
+            power_plant.install<FiniteStateMachineRunner<Name>>();
         }
-
-        template <typename DSL>
-        static inline void postcondition(NUClear::threading::ReactionTask& task){
-            transitioning.store(false);
-            task.parent.reactor.powerplant.emit(std::make_unique<run_state<Transition, State...>>());
-        }
-    }
-
-    template <typename T, typename... S>
-    struct run_state {};
-
-    template <typename Transition, typename... State>
-    struct Running : NUClear::dsl::operation::TypeBind<run_state<Transition, State...>>, NUClear::dsl::operation::CacheGet<Transition> {
-        template <typename DSL>
-        static inline void bind(const std::shared_ptr<threading::Reaction>& reaction){
-            // We do want to run bind from Trigger
-            NUClear::dsl::word::Trigger<Transition>::bind();
-
-            //TODO(cameron) Somehow store the templated types, so we can check 
-        }
-
-        template <typename DSL>
-        static inline void postcondition(NUClear::threading::ReactionTask& task){
-            // We will wait in the queue, so we don't use up a whole thread.
-            task.parent.reactor.powerplant.emit(std::make_unique<run_state<Transition, State...>>());
-        }
-    }
-
-    template <typename T, typename... S>
-    struct run_leaving {};
-
-    template <typename Transition, typename... State>
-    struct Leaving :  NUClear::dsl::operation::TypeBind<run_state<Transition, State...>>, NUClear::dsl::operation::CacheGet<Transition>{}
-
-    namespace trait {
-        // TODO(cameron) Work out transience
-    }
+    };
 }
 
 #endif //EXTENSION_FINITE_STATE_MACHINE
