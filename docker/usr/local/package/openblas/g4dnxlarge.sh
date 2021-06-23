92b63e257aafddc34b205cf2bc516718be7373d1
Yosiah de Koeyer <yosiahdekoeyer@gmail.com>
2021-06-23 13:51:11 +1000

    correct openblas target

--- docker/usr/local/package/openblas/g4dnxlarge.sh
+++ docker/usr/local/package/openblas/g4dnxlarge.sh
@@ -5,7 +5,7 @@ install-make-from-source $1 \
     BINARY=64 \
     SMP=1 \
     NUM_THREADS=8 \
-    TARGET=CASCADELAKE \
+    TARGET=HASWELL \
     USE_THREAD=1 \
     NO_SHARED=0 \
     NO_STATIC=0
