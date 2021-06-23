427da58d72229c7c30341a58be72646e35039e5b
Yosiah de Koeyer <yosiahdekoeyer@gmail.com>
2021-06-23 22:47:08 +1000

    We doing things

--- docker/usr/local/package/llvm.sh
+++ docker/usr/local/package/llvm.sh
@@ -9,21 +9,20 @@ shift
 
 if [ "${PLATFORM}" == "g4dnxlarge" ]; then
     # Build llvm with cross compile args for aws
-    sh /usr/local/bin/install-cmake-from-source "${URL}" \
+    BUILD_FILE_DIR='llvm' install-cmake-from-source "${URL}" "$@"\
         -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
         -DLLVM_ENABLE_SPHINX=OFF \
         -DLLVM_BUILD_LLVM_DYLIB=ON \
         -DLLVM_LINK_LLVM_DYLIB=ON \
         -DCMAKE_CROSSCOMPILING=True \
-        # -DCMAKE_INSTALL_PREFIX=<install-dir> \ ??
         -DLLVM_TABLEGEN=/usr/local/bin/llvm-tblgen \
         -DCLANG_TABLEGEN=/usr/local/bin/clang-tblgen \
         -DLLVM_DEFAULT_TARGET_TRIPLE=x86_64-pc-linux-gnu \
-        -DLLVM_TARGET_ARCH=x86_64 \
-        -DLLVM_TARGETS_TO_BUILD=x86_64 \
+        -DLLVM_TARGET_ARCH=x86_64
+        # -DLLVM_TARGETS_TO_BUILD=X86
 else
     # Build llvm normally :)
-    sh /usr/local/bin/install-cmake-from-source "${URL}" \
+    install-cmake-from-source "${URL}" "$@"\
         -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
         -DLLVM_ENABLE_SPHINX=OFF \
         -DLLVM_BUILD_LLVM_DYLIB=ON \
