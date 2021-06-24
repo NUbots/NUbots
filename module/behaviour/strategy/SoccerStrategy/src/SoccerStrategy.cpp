42bdb59844b8ccfee43de00f2daf43dc7801fbe6
Josephus Paye II <j.paye96@gmail.com>
2021-06-25 02:58:08 +1000

    Fix formatting

--- module/behaviour/strategy/SoccerStrategy/src/SoccerStrategy.cpp
+++ module/behaviour/strategy/SoccerStrategy/src/SoccerStrategy.cpp
@@ -200,7 +200,8 @@ namespace module::behaviour::strategy {
                         case GameMode::PENALTY_SHOOTOUT:
                             penaltyShootout(gameState, phase, fieldDescription, field, ball);
                             break;
-                        // We handle NORMAL and OVERTIME the same at the moment because we don't have any special behavior for overtime.
+                        // We handle NORMAL and OVERTIME the same at the moment because we don't have any special
+                        // behavior for overtime.
                         case GameMode::NORMAL: normal(gameState, phase, fieldDescription, field, ball, mode); break;
                         case GameMode::OVERTIME: normal(gameState, phase, fieldDescription, field, ball, mode); break;
                         default: log<NUClear::WARN>("Game mode unknown.");
