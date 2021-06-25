7fe26f494faff05c7730d19cc50afd55c64d0013
Taylor <c3206230@uon.edu.au>
2021-06-25 21:53:48 +1000

    Fixes NUsight messages

--- nusight2/src/client/components/vision/network.ts
+++ nusight2/src/client/components/vision/network.ts
@@ -100,7 +100,7 @@ export class VisionNetwork {
         axis: Vector3.from(ball.cone?.axis),
         radius: ball.cone?.radius!,
       },
-      distance: Math.abs(ball.measurements?.[0].rBCc?.x!),
+      distance: Math.abs(ball.measurements?.[0].srBCc?.x!),
       colour: Vector4.from(ball.colour),
     }))
   }
@@ -120,8 +120,8 @@ export class VisionNetwork {
         goal.side === message.vision.Goal.Side.LEFT
           ? 'left'
           : goal.side === message.vision.Goal.Side.RIGHT
-          ? 'right'
-          : 'unknown',
+            ? 'right'
+            : 'unknown',
       post: {
         top: Vector3.from(goal.post?.top),
         bottom: Vector3.from(goal.post?.bottom),
