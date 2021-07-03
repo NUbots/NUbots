f0051f76f6d7cfb905e1a6ba4c9beb013ce7f047
Trent Houliston <trent@houliston.me>
2021-07-03 14:55:37 +1000

    Update module/extension/Director/src/Director.cpp
    Co-authored-by: Alex Biddulph <c3124185@uon.edu.au>

--- module/extension/Director/src/Director.cpp
+++ module/extension/Director/src/Director.cpp
@@ -131,9 +131,9 @@ namespace module::extension {
             auto provider = it->second;
 
             // Add a reaction that will listen for changes to this state and notify the director
-            auto id               = when.reaction->id;
-            auto type             = when.type;
-            auto validator        = when.validator;
+            const auto id               = when.reaction->id;
+            const auto type             = when.type;
+            const auto validator        = when.validator;
             ReactionHandle handle = when.binder(*this, [this, id, type, validator](const int& state) {  //
                 if (validator(state)) {
                     emit(std::make_unique<StateUpdate>(id, type, state));
