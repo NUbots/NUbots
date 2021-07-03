24d049d6efe067f7a93a7479de8040a1a2249a2e
Kip Hamiltons <p0nage@live.com.au>
2021-07-03 18:44:39 +1000

    This is heading down the road of return the resulting emissions. That becomes too dirty though I think.
    Going to continue by making you set up your own test reactor which catches the emissions

--- shared/extension/ModuleTest.hpp
+++ shared/extension/ModuleTest.hpp
@@ -20,7 +20,9 @@
 #ifndef EXTENSION_MODULETEST_HPP
 #define EXTENSION_MODULETEST_HPP
 
+#include <catch.hpp>
 #include <nuclear>
+#include <tuple>
 #include <type_traits>
 
 namespace extension {
@@ -30,28 +32,61 @@ namespace extension {
     public:
         ModuleTest() = delete;
 
-        explicit ModuleTest(Module m) : module(m), NUClear::PowerPlant(getSingleThreadConfig()) {
+        // TODO: somehow disable the reactions which aren't on<Trigger<..>>, such as on<Every<..>> or on<Always>
+
+        explicit ModuleTest(const bool start_powerplant_automatically = true)
+            : NUClear::PowerPlant(get_single_thread_config()) {
             // This assert could be worth playing with
             // static_assert(std::isconvertible<Module*, NUClear::Reactor*>);
             install<Module>();
-            start();
+            if (start_powerplant_automatically) {
+                startup_manually();
+            }
         }
         ~ModuleTest() {
+            started = false;
             shutdown();
         }
+        // TODO(KipHamiltons): add varargs to this
+        void startup_manually() {
+            INFO("Starting up ModuleTest power plant.");
+            started = true;
+            start();
+        }
+        void shutdown_manually() {
+            INFO("Shutting down ModuleTest power plant. Further use will require a restart.");
+            started = false;
+            shutdown();
+        }
+
+        template <typename EmitType, typename... ResponseEmissionTypes>
+        [[nodiscard]] auto run_reaction(const EmitType& emission) {
+            std::vector<NUClear::threading::ReactionHandle> response_catchers =
+                create_response_catchers<ResponseEmissionTypes...>();
+            for (auto& response_catcher : response_catchers) {
+                response_catcher.enable();
+            }
+            powerplant.emit<EmitType>(std::move(std::make_unique(emission)));
+
+            for (auto& response_catcher : response_catchers) {
+                response_catcher.disable();
+            }
+            return std::tuple_cat
+        }
+
         ModuleTest(ModuleTest& other)  = delete;
         ModuleTest(ModuleTest&& other) = delete;
         ModuleTest& operator=(ModuleTest& other) = delete;
         ModuleTest&& operator=(ModuleTest&& other) = delete;
 
     private:
-        static const NUClear::PowerPlant::Configuration& getSingleThreadConfig() {
+        static const NUClear::PowerPlant::Configuration& get_single_thread_config() {
             NUClear::PowerPlant::Configuration cfg;
             cfg.thread_count = 1;
             return cfg;
         }
 
-        Module module;
+        bool started = false;
     };
 
 }  // namespace extension
--- shared/tests/extension/TestModuleTest.cpp
+++ shared/tests/extension/TestModuleTest.cpp
@@ -18,17 +18,57 @@
  */
 
 #include <catch.hpp>
+#include <nuclear>
 #include <utility>
 
 #include "extension/ModuleTest.hpp"
 
 // Anon namespace so that this reactor is local to this file
 namespace {
+    // Usually you would use ModuleTest to test an existing reactor with existing messages, but testing ModuleTest
+    // itself requires a reactor and a message, so we define them here
+    template <int id>
+    struct Message {
+    }
 
+    // The reactor which is taking the place of the module being tested. When testing a real module, the real module
+    // would take the place of this reactor
     class TestReactor : public NUClear::Reactor {
-        // Pass the test reactor as the ModuleTest's reactor.
+        explicit TestReactor(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+            on<Trigger<Message<1>>>().then([this](Message<1> /*msg*/) {  //
+                emit(std::make_unique<Message<11>>());
+            });
+            on<Trigger<Message<2>>>().then([this](Message<2> /*msg*/) {
+                emit(std::make_unique<Message<21>>());
+                emit(std::make_unique<Message<22>>());
+            });
+        }
     }
 
 }  // namespace
 
-TEST_CASE()
+TEST_CASE("Testing the module test", "[extension][ModuleTest]") {
+    const Message<0> expected_startup_output{};
+    const Message<11> expected_single_output{};
+    const auto [expected_double_output_1, expected_double_output_2] = std::tuple<Message<21>, Message<22>>();
+
+    // We need to test on<Startup> for this reactor (actually for the ModuleTest), so we disable automatic startup
+    static constexpr bool STARTUP_REACTOR_AUTOMATICALLY = false;
+    auto module_test                                    = ModuleTest<TestReactor>(STARTUP_REACTOR_AUTOMATICALLY);
+
+    // TODO(KipHamiltons): make a varargs startup test.
+
+    const Message<11> actual_single_output = module_test.run_reaction<Message<1>, Message<11>>(Message<1>());
+
+    // After we have got the output, then we compare it to what we expected
+    // We require that the output is the same as the expected (or within acceptable error), which is trivially true here
+    REQUIRE(expected_single_output == actual_single_output);
+
+    const auto [actual_double_output_1, actual_double_output_2] =
+        module_test.run_reaction<Message<2>, Message<21>, Message<22>>(Message<2>());
+
+    REQUIRE(expected_double_output_1 == actual_double_output_1);
+    REQUIRE(expected_double_output_2 == actual_double_output_2);
+
+    // TODO: call the function and compare the emissions to the expected emissions
+}
