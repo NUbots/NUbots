55d53a8cc42de22960f552460b796d2bf05fbbc2
Kip Hamiltons <p0nage@live.com.au>
2021-08-26 16:24:21 +1000

    First draft. Not sure if I am keen on these. I think the API needs improving

---
+++ shared/tests/utility/math/TestTransform.cpp
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the NUbots Codebase.
+ *
+ * The NUbots Codebase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The NUbots Codebase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the NUbots Codebase.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2021 NUbots <nubots@nubots.net>
+ */
+
+#include <catch.hpp>
+#include <type_traits>
+
+#include "utility/math/Transform.hpp"
+
+using utility::math::Transform;
+using namespace utility::math::space;
+
+namespace {
+    template <typename Scalar, int LTo, int LFrom, int RTo, int RFrom>
+    [[nodiscard]] bool spaces_are_compatible(Transform<Scalar, LTo, LFrom> /*lTransform*/,
+                                             Transform<Scalar, RTo, RFrom> /*rTransform*/) {
+        return LFrom == RTo;
+    }
+
+    template <typename Scalar, int To, int From, int RequiredTo>
+    [[nodiscard]] bool to_space_correct(Transform<Scalar, To, From> /*transform*/) {
+        return To == RequiredTo;
+    }
+
+    template <typename Scalar, int To, int From, int RequiredFrom>
+    [[nodiscard]] bool from_space_correct(Transform<Scalar, To, From> /*transform*/) {
+        return From == RequiredFrom;
+    }
+}  // namespace
+
+
+SCENARIO("Transforms can be multiplied") {
+
+    GIVEN("Two transforms with compatible spaces") {
+
+        auto Htc = Transform<double, TORSO, CAMERA>();
+        auto Hcf = Transform<double, CAMERA, FIELD>();
+
+        // Validate our GIVEN clause assumption
+        THEN("Spaces are compatible") {
+            REQUIRE(spaces_are_compatible(Htc, Hcf));
+        }
+
+        WHEN("The Transforms are multiplied") {
+            Transform<double, TORSO, FIELD> Htf = Htc * Hcf;
+
+            THEN("The result has the correct spaces") {
+                REQUIRE(from_space_correct<double, TORSO, FIELD, FIELD>(Htf));
+                REQUIRE(to_space_correct<double, TORSO, FIELD, TORSO>(Htf));
+            }
+        }
+    }
+}
