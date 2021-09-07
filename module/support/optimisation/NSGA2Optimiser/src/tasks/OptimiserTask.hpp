35528b1c22aef34df744602164a805fa1a4de506
Joel Wong <wongjoel@noreply.users.github.com>
2021-09-07 20:50:56 +1000

    Remove resolved TODO

--- module/support/optimisation/NSGA2Optimiser/src/tasks/OptimiserTask.hpp
+++ module/support/optimisation/NSGA2Optimiser/src/tasks/OptimiserTask.hpp
@@ -16,7 +16,6 @@ namespace module {
         namespace optimisation {
             using message::support::optimisation::NSGA2EvaluationRequest;
 
-            /// TODO: Joel Wong 2021-09-01: I designed this as an "interface", but I can't work out how to make the methods virtual and still have everything compile
             class OptimiserTask {
             public:
                 virtual void SetupNSGA2(const ::extension::Configuration& config, nsga2::NSGA2& nsga2Algorithm) = 0;
