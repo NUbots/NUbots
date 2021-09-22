195dfc5d38fcfb33947142ddbd06009b76061612
Kip Hamiltons <p0nage@live.com.au>
2021-09-22 19:22:59 +1000

    Removes Unused and Unnecessary PortAudio Dep

--- cmake/Modules/FindeSpeak.cmake
+++ cmake/Modules/FindeSpeak.cmake
@@ -3,9 +3,6 @@ include(ToolchainLibraryFinder)
 # Find ALSA for eSpeak
 find_package(ALSA REQUIRED)
 
-# Find PortAudio for eSpeak
-find_package(PortAudio REQUIRED)
-
 ToolchainLibraryFinder(
   NAME eSpeak
   HEADER espeak/speak_lib.h
