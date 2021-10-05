c94a45819a7837ed1199c960c951aaf555b7487a
Andrew Thompson <6337878+Mrwerdo@users.noreply.github.com>
2021-10-06 10:05:28 +1100

    Patches for building phonetisaurus

--- docker/PKGBUILD/phonetisaurus.sh
+++ docker/PKGBUILD/phonetisaurus.sh
@@ -1,6 +1,6 @@
 # Maintainer: Jingbei Li <i@jingbei.li>
-pkgname=phonetisaurus-git
-pkgver=r171.23f182f
+pkgname=phonetisaurus
+pkgver=0.9.1
 pkgrel=1
 pkgdesc="WFST-driven grapheme-to-phoneme (g2p) framework suitable for rapid development of high quality g2p or p2g systems."
 arch=('i686' 'x86_64' 'arm' 'armv6h' 'armv7h')
@@ -13,19 +13,14 @@ sha256sums=('SKIP')
 provides=('phonetisaurus')
 conflicts=('phonetisaurus')
 
-pkgver() {
-        cd "$srcdir/Phonetisaurus"
-        printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
-}
-
 prepare() {
-        cd "$srcdir/Phonetisaurus"
+        cd "$srcdir/Phonetisaurus-$pkgver"
         sed '41ausing namespace std;' -i src/include/util.h
         #sed '/MapToken/s/string&/std::string&/g' -i src/include/util.h
 }
 
 build() {
-        cd "$srcdir/Phonetisaurus"
+        cd "$srcdir/Phonetisaurus-$pkgver"
         PYTHON=/usr/bin/python ./configure \
                 --prefix=/usr \
                 --with-openfst-includes=/usr/include/fst \
@@ -35,11 +30,11 @@ build() {
 }
 
 package() {
-        cd "$srcdir/Phonetisaurus/python"
+        cd "$srcdir/Phonetisaurus-$pkgver/python"
         cp ../.libs/Phonetisaurus.so .
         python setup.py install --root="$pkgdir"/ --optimize=1
 
-        cd "$srcdir/Phonetisaurus"
+        cd "$srcdir/Phonetisaurus-$pkgver"
         make DESTDIR=${pkgdir} install
 
         install -Dm644 LICENSE "${pkgdir}/usr/share/licenses/${pkgname}/LICENSE"
