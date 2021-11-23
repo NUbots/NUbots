b6a56a9a10f880e935df19ada83c44399ea17e37
Ysobel <ysobelsims@gmail.com>
2021-11-23 20:33:36 +1100

    Update the python vector field file
    to reflect changes to the vector field in the code

--- module/motion/walk/MovementController/docs/vectorfield.py
+++ module/motion/walk/MovementController/docs/vectorfield.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-
-import sys
+import math
 
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.patches import Circle
 
 xmin = -10
 xmax = 10
@@ -19,22 +17,28 @@ ys = np.linspace(ymin, ymax, ny)
 dx = np.zeros((ny, nx))
 dy = np.zeros((ny, nx))
 
-step_steep = 2
-well_width = 0.02
+# step_steep = 2
+# well_width = 0.02
+# step_height = 1
+
+scaling_factor = 2
 step_height = 1
 
-c = (
-    step_steep ** (2 / step_steep)
-    * step_height ** (1 / step_steep)
-    * (step_steep * step_height + step_steep ** 2 * step_height) ** (-1 / step_steep)
-    / well_width
-)
+
+# c = (
+#     step_steep ** (2 / step_steep)
+#     * step_height ** (1 / step_steep)
+#     * (step_steep * step_height + step_steep ** 2 * step_height) ** (-1 / step_steep)
+#     / well_width
+# )
 
 # Fills xs and ys with values from dx and dy function
 for xi, x in enumerate(xs):
     for yi, y in enumerate(ys):
-        dx[yi, xi] = -x / abs(x) * np.exp(-abs(c * x ** -step_steep))
-        dy[yi, xi] = np.exp(-abs(c * x ** -step_steep)) - y / step_height
+        dx[yi, xi] = -math.tanh(x * scaling_factor)
+        dy[yi, xi] = (step_height * (math.tanh(x * scaling_factor) ** 2) - y) * scaling_factor
+        # dx[yi, xi] = -x / abs(x) * np.exp(-abs(c * x ** -step_steep))
+        # dy[yi, xi] = np.exp(-abs(c * x ** -step_steep)) - y / step_height
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
