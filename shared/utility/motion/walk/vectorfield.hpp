ee705fb089f48cb28cb63b44e5fbebfdad457fd0
Ysobel <ysobelsims@gmail.com>
2021-11-23 20:24:51 +1100

    Templatise the vector field utilities

--- shared/utility/motion/walk/vectorfield.hpp
+++ shared/utility/motion/walk/vectorfield.hpp
@@ -1,38 +1,45 @@
 #include <Eigen/Core>
 
 namespace utility::motion::walk {
-    // Returns x-position of vector field. See vectorfield.py for graphical representation of the vector field
-    double f_x(const Eigen::Vector3d& pos, float scaling_factor) {
+    // Returns x-position of vector field. See module/motion/walk/docs/vectorfield.py for graphical representation of
+    // the vector field
+    template <typename Scalar>
+    Scalar f_x(const Eigen::Matrix<Scalar, 3, 1>& pos, Scalar scaling_factor) {
         // Prevent divide by zero error with 0 position
         return tanh(pos.x() * scaling_factor);
     }
 
     // Returns z-position of vector field. See vectorfield.py for graphical representation of the vector field
-    double f_z(const Eigen::Vector3d& pos, const float step_height, const float scaling_factor) {
-        return -(step_height * pow(tanh(pos.x() * scaling_factor), 2) - pos.z())
-                * scaling_factor;
+    template <typename Scalar>
+    Scalar f_z(const Eigen::Matrix<Scalar, 3, 1>& pos, const Scalar step_height, const Scalar scaling_factor) {
+        return -(step_height * pow(tanh(pos.x() * scaling_factor), 2) - pos.z()) * scaling_factor;
     }
 
-    double integral(double x, double h, double c) {
+    // Integral for the vector field used in calculating the path length
+    template <typename Scalar>
+    Scalar integral(Scalar x, Scalar h, Scalar c) {
         return sqrt(1 + pow(cosh(x) * (c + h * atan(1 / sinh(x))) - h * tanh(x), 2));
     }
 
-    // path length to origin; x,y=foots starting location; h=step height; -1<x<1 0<y<1 0<h<1
-    double pathlength(const Eigen::Vector3d& pos, const int num_steps, const float scaling_factor, const float step_height) {
-        double x      = scaling_factor * pos.x();
-        double y      = scaling_factor * pos.z();
-        double h      = scaling_factor * step_height;
-        double c      = y / sinh(x) - h * atan(1 / sinh(x));
+    // Calculates the length of the path of the vector field from the current foot position to the origin.
+    template <typename Scalar>
+    Scalar pathlength(const Eigen::Matrix<Scalar, 3, 1>& pos,
+                      const int num_steps,
+                      const Scalar scaling_factor,
+                      const Scalar step_height) {
+        Scalar x = scaling_factor * pos.x();
+        Scalar y = scaling_factor * pos.z();
+        Scalar h = scaling_factor * step_height;
+        Scalar c = y / sinh(x) - h * atan(1 / sinh(x));
 
-        double x2   = x / num_steps;
-        double x1   = 0;
-        double path = 0;
+        Scalar x2   = x / num_steps;
+        Scalar x1   = 0;
+        Scalar path = 0;
 
         int i = 0;
         while (i < num_steps) {
             path = path
-                    + ((x2 - x1) / 6)
-                            * (integral(x1, h, c) + 4 * (integral((x1 + x2) / 2, h, c)) + integral(x2, h, c));
+                   + ((x2 - x1) / 6) * (integral(x1, h, c) + 4 * (integral((x1 + x2) / 2, h, c)) + integral(x2, h, c));
             x1 = x2;
             x2 = x2 + x / num_steps;
             i++;
@@ -41,4 +48,4 @@ namespace utility::motion::walk {
         return path / scaling_factor;
     }
 
-}
+}  // namespace utility::motion::walk
