eaa1d59ad0882a06a23b0270695e7985a317d707
Ysobel <ysobelsims@gmail.com>
2022-02-26 16:31:11 +1100

    Update the clock code to compile

--- shared/clock/clock.cpp
+++ shared/clock/clock.cpp
@@ -13,14 +13,16 @@ namespace utility::clock {
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
     double rate = 1.0;  // real time factor
 
-    void update_rtf(const double& rft) {
+    void update_rtf(const double& rtf) {
         auto now = NUClear::base_clock::now();
 
         // Since we are updating our rtf we need to advance our state so the new deltas will be calculated properly
         // We set the variables in this specific order to mimise the error that will occur if the threading reads as we
         // are writing By reducing the delta between state and now any changes in rtf will have a minimal change on
         // delta
-        epoch += delta(now - last_update) * rate;  // set before we update the variables
+        epoch = epoch
+                + std::chrono::duration_cast<std::chrono::steady_clock::duration>(
+                    (now - last_update) * rate);  // set before we update the variables
         last_update = now;
         rate        = rtf;
     }
@@ -29,8 +31,10 @@ namespace utility::clock {
 
 namespace NUClear {
     clock::time_point clock::now() {
-        // Move along our time
-        return epoch + delta(NUClear::base_clock::now() - utility::clock::last_update) * utility::clock::custom_rtf;
+        // Move along the time
+        return utility::clock::epoch
+               + std::chrono::duration_cast<std::chrono::steady_clock::duration>(
+                   (NUClear::base_clock::now() - utility::clock::last_update) * utility::clock::rate);
     }
 
 }  // namespace NUClear
