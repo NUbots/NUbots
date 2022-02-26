f0dae99680b51f68fe9b5626a820a6bd284f6866
Trent Houliston <trent@houliston.me>
2022-02-26 14:17:26 +1100

    Update the clock to help reduce multithreading issues

--- shared/clock/clock.cpp
+++ shared/clock/clock.cpp
@@ -7,26 +7,30 @@
 namespace utility::clock {
 
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
-    double custom_rtf = 1.0;  // real time factor
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
     NUClear::base_clock::time_point last_update = NUClear::base_clock::now();
-    NUClear::base_clock::time_point state       = last_update;
-    std::mutex mutex;
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+    NUClear::base_clock::time_point epoch = last_update;
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+    double rate = 1.0;  // real time factor
+
+    void update_rtf(const double& rft) {
+        auto now = NUClear::base_clock::now();
+
+        // Since we are updating our rtf we need to advance our state so the new deltas will be calculated properly
+        // We set the variables in this specific order to mimise the error that will occur if the threading reads as we
+        // are writing By reducing the delta between state and now any changes in rtf will have a minimal change on
+        // delta
+        epoch += delta(now - last_update) * rate;  // set before we update the variables
+        last_update = now;
+        rate        = rtf;
+    }
+
 }  // namespace utility::clock
 
 namespace NUClear {
     clock::time_point clock::now() {
-        // Prevent another instance of this function running at the same time
-        std::lock_guard<std::mutex> lock(utility::clock::mutex);
-
-        // Now is multiplied by the real time factor to sync
-        // NUClear with the simulation time
-        auto now   = NUClear::base_clock::now();
-        auto delta = std::chrono::duration_cast<std::chrono::steady_clock::duration>((now - utility::clock::last_update)
-                                                                                     * utility::clock::custom_rtf);
-        utility::clock::state       = clock::time_point(utility::clock::state + delta);
-        utility::clock::last_update = now;
-        return utility::clock::state;
+        // Move along our time
+        return epoch + delta(NUClear::base_clock::now() - utility::clock::last_update) * utility::clock::custom_rtf;
     }
 
 }  // namespace NUClear
--- shared/clock/clock.hpp
+++ shared/clock/clock.hpp
@@ -5,7 +5,7 @@
 
 namespace utility::clock {
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
-    extern double custom_rtf;  // real time factor
+    void update_rtf(const double& rtf);
 }  // namespace utility::clock
 
 #endif  // UTILITY_CLOCK_HPP
