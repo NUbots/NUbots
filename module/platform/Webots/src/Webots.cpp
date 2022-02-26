8a5c9acab2b908bcdb4655aee1c9850bcb537691
Ysobel <ysobelsims@gmail.com>
2022-02-26 16:31:24 +1100

    Use the function to update the rtf in webots

--- module/platform/Webots/src/Webots.cpp
+++ module/platform/Webots/src/Webots.cpp
@@ -614,7 +614,7 @@ namespace module::platform {
             static_cast<double>(sim_delta + prev_sim_delta) / static_cast<double>(real_delta + prev_real_delta);
 
         // Exponential filter to do the smoothing
-        utility::clock::custom_rtf = utility::clock::custom_rtf * clock_smoothing + (1.0 - clock_smoothing) * ratio;
+        utility::clock::update_rtf(utility::clock::custom_rtf * clock_smoothing + (1.0 - clock_smoothing) * ratio);
 
         // Update our current times
         current_sim_time  = sensor_measurements.time;
