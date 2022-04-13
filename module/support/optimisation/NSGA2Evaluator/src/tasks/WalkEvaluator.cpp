a8da32a329a02723e871e1f2e9ddb919245ff15d
Jason <Jason.Dsiher@uon.edu.au>
2022-04-13 16:43:13 +1000

    Comment out

--- module/support/optimisation/NSGA2Evaluator/src/tasks/WalkEvaluator.cpp
+++ module/support/optimisation/NSGA2Evaluator/src/tasks/WalkEvaluator.cpp
@@ -208,7 +208,7 @@ namespace module {
                 bool offCourse         = false;
                 auto distanceOffCourse = std::fabs(robotPosition.y() - initialRobotPosition.y());
 
-                if (distanceOffCourse > 10)
+                if (distanceOffCourse > 1.0)
                 {
                     NUClear::log<NUClear::DEBUG>("OffCourse!");
                     NUClear::log<NUClear::DEBUG>("orination on robot (x y z): ", robotPosition.x(),
