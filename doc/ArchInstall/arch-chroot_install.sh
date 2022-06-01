f0247a77dcfc5c60995494ad61c178101c07ec68
Ysobel Sims <ysobelsims@gmail.com>
2022-06-01 14:22:28 +1000

    Maybe a better name

--- doc/ArchInstall/arch-chroot_install.sh
+++ doc/ArchInstall/arch-chroot_install.sh
@@ -39,8 +39,8 @@ EOF
 pacman -S --noconfirm --needed sudo
 
 # Setup users of the wheel group to be able to execute sudo commands with no password
-echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/nubots_sudo
-chmod 440 /etc/sudoers.d/nubots_sudo
+echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel_sudo
+chmod 440 /etc/sudoers.d/wheel_sudo
 
 # Get sudo to insult users when they type a password wrong
 echo "Defaults insults" > /etc/sudoers.d/insults
