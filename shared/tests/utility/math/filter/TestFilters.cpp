c9fc0112ea773b35e0a9788bf40a0bff0d1a312b
Andrew Thompson <6337878+Mrwerdo@users.noreply.github.com>
2022-06-03 06:46:57 +0000

    Add comments to the UKF filter test.

--- shared/tests/utility/math/filter/TestFilters.cpp
+++ shared/tests/utility/math/filter/TestFilters.cpp
@@ -57,20 +57,53 @@ TEST_CASE("Test the UKF", "[utility][math][filter][UKF]") {
     INFO("    Process Noise.....: " << process_noise.transpose());
     INFO("    Initial State.....: " << initial_state.transpose());
     INFO("    Initial Covariance: \n" << initial_covariance);
+
+    // The aim of the game is to verify that the unscented Kalman filter is filtering a noisy signal. Another way
+    // of saying this is that given a vector of measurements that relate non-linearly to a random variable, say,
+    // true_state, that we care about, is checking whether the unscented Kalman filter is able to, after seeing a list
+    // of measurements, give a close enough measurement to the true_state.
+    //
+    // The non-linear model being used is the VanDerPol model. https://en.wikipedia.org/wiki/Van_der_Pol_oscillator
+    // In particular, the equations being used are: x1 <- x1 + x2 * dt, and x2 <- x2 + ((1 - x1*x1)*x2 - x1) * dt.
+    // (i.e. "another commonly used transformation" according to the wikipedia section)...
+    //
+    // The aim is to verify that after seeing a sequence of points, the unscented Kalman filter has calculated a mean
+    // and variance which indicate with reasonably accuracy where [x1, x2] are located in the real world model being
+    // tracked.
+
+    // Process noise is a random variable which is present in the dynamic system when updating the state.
     model_filter.model.process_noise = process_noise;
+
+    // Assume a reasonable initial state for the non-linear model.
     model_filter.set_state(initial_state, initial_covariance);
 
     INFO("Feeding noisy measurements into the filter");
     std::vector<double> innovations;
+
+    // Records the mean and covariance calculated by the model filter.
     std::vector<std::pair<utility::math::filter::UKF<double, shared::tests::VanDerPolModel>::StateVec,
                           utility::math::filter::UKF<double, shared::tests::VanDerPolModel>::StateMat>>
         actual_state;
+
     innovations.reserve(true_state.size());
     actual_state.reserve(true_state.size());
+
+    // Observe the measurements...
+    // A measurement is the first component from the VanDerPol model, plus some measurement noise. The measurement noise
+    // passed is a parameter to the UKF (according to the theory/researchers, that is we'd need to verify it for our
+    // models). The data should already include the noise.
     for (const auto& measurement : measurements) {
+        // Given a measurement, perform bayesian inference and update the model_filter's belief of the random variable.
         model_filter.measure(measurement, measurement_noise);
+        // Over time, update the bayesian model based on our prior knowledge of the process governing the random
+        // variable.
         model_filter.time(deltaT);
+        // Adds the difference between the current measurement and the variable predicted by the model.
         innovations.push_back(measurement.x() - model_filter.get().x());
+        // Records the predicted variables and covariance for later...
+        // How does the covariance relate to variance? variance is identical to covariance for the same random variable.
+        // Basically, this is adding the mean of the predicted variable, and the variance of the predicted variable
+        // to the actual_state list.
         actual_state.push_back(std::make_pair(model_filter.get(), model_filter.getCovariance()));
     }
 
@@ -83,12 +116,21 @@ TEST_CASE("Test the UKF", "[utility][math][filter][UKF]") {
     double mean_x2_boundary          = 0.0;
     Eigen::Vector2d mean_state_error = Eigen::Vector2d::Zero();
     for (size_t i = 0; i < actual_state.size(); ++i) {
+        // Calculate the difference between the mean of the estimated state and the true state of the VanDerPol process.
         Eigen::Vector2d state_error = Eigen::Vector2d(true_state[i]) - actual_state[i].first;
+        // The .second(0, 0) and .second(1, 1) return the variance for the first and second state variable.
+        // i.e. Variance of the x1, x2 or x(t) and y(t) from the wikipedia article.
         double covariance_bounds_x1 = std::sqrt(actual_state[i].second(0, 0));
         double covariance_bounds_x2 = std::sqrt(actual_state[i].second(1, 1));
+
         mean_x1_boundary += covariance_bounds_x1;
         mean_x2_boundary += covariance_bounds_x2;
 
+        // If the error is larger than the variance for a given component, record it as an error, because the kalman
+        // filter should be giving close approximations for this data.
+        // When will this happen? If the filter is lagging behind the true state too much, which means that either not
+        // enough measurements have been recorded, or the model is not accurately representing the process being
+        // measured.
         count_x1 += (std::abs(state_error.x()) - covariance_bounds_x1) > 0.0 ? 1.0 : 0.0;
         count_x2 += (std::abs(state_error.y()) - covariance_bounds_x2) > 0.0 ? 1.0 : 0.0;
 
@@ -96,11 +138,13 @@ TEST_CASE("Test the UKF", "[utility][math][filter][UKF]") {
         mean_state_error += state_error;
     }
 
+    // Divide by the number of elements to calculate the mean...
     mean_innovations /= innovations.size();
     mean_state_error /= actual_state.size();
     mean_x1_boundary /= actual_state.size();
     mean_x2_boundary /= actual_state.size();
 
+    // Display using percentages for convenience.
     double percentage_x1 = 100.0 * count_x1 / actual_state.size();
     double percentage_x2 = 100.0 * count_x2 / actual_state.size();
 
@@ -111,6 +155,8 @@ TEST_CASE("Test the UKF", "[utility][math][filter][UKF]") {
     INFO("The mean 1\u03C3 boundary for state 1 is [" << -mean_x1_boundary << ", " << mean_x1_boundary << "]");
     INFO("The mean 1\u03C3 boundary for state 2 is [" << -mean_x2_boundary << ", " << mean_x2_boundary << "]");
 
+    // The test passess if 70 percent or more of the filter's estimates were within variance distance of the
+    // unknown variables true state.
     REQUIRE(percentage_x1 <= 30.0);
 }
 
