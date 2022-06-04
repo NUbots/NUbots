bb36e0ebbc3093631dadfc0840d1f411fe028698
tom <thomas16441@gmail.com>
2022-06-04 19:34:35 +1000

    Remove comment

--- module/behaviour/skills/HeadBehaviourSoccer/src/HeadBehaviourSoccer.cpp
+++ module/behaviour/skills/HeadBehaviourSoccer/src/HeadBehaviourSoccer.cpp
@@ -227,9 +227,7 @@ namespace module::behaviour::skills {
                           orientation        = Eigen::Affine3d(sensors.Htw).rotation().transpose();
                       }
 
-                      //   Eigen::Matrix3d headToIMUSpace = orientation * headToBodyRotation;
                       Eigen::Matrix3d headToIMUSpace = orientation * headToBodyRotation;
-
 
                       // If objects visible, check current centroid to see if it moved
                       if (!lost) {
