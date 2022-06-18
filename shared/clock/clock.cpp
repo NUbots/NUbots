24bccc2de7eb73ff90fa141a896e1526e22e8d5d
Trent Houliston <trent@houliston.me>
2022-06-18 11:42:17 +0000

    Fix the clock

--- shared/clock/clock.cpp
+++ shared/clock/clock.cpp
@@ -1,55 +1,63 @@
 
 #include "clock.hpp"
 
+#include <array>
 #include <chrono>
 
 namespace utility::clock {
 
-    struct clockStruct {
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+    struct ClockData {
+        /// When the clock was last updated under the true time
         NUClear::base_clock::time_point last_update = NUClear::base_clock::now();
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+        /// Our calculated time when the clock was last updated in simulated time
         NUClear::base_clock::time_point epoch = last_update;
-        // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
-        double rtf = 1.0;  // real time factor
+        /// The real time factor of the simulated clock
+        double rtf = 1.0;
     };
 
-    static clockStruct clockArray[2];
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+    static std::array<ClockData, 3> data;
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+    static std::atomic_int active = 0;
 
-    static std::atomic_int clockArrayIndex  = 0;
+    /// Convenience function to cast durations to the base clock duration type so the functions fit on one line
+    template <typename T>
+    NUClear::base_clock::duration dc(T&& t) {
+        return std::chrono::duration_cast<NUClear::base_clock::duration>(t);
+    }
 
-    void update_rtf(const double& new_rtf) {
-        auto now = NUClear::clock::now();  // Changed from base_clock
+    void update_rtf(const double& rtf) {
+        using namespace std::chrono;  // Using namespace is fine in a function scope
 
-        static std::atomic_int updateClockIndex = clockArrayIndex ^ 1;  // Switch to other clock in array
+        auto now = NUClear::base_clock::now();
 
-        // Since we are updating our rtf we need to advance our state so the new deltas will be calculated properly
-        // We set the variables in this specific order to mimise the error that will occur if the threading reads as we
-        // are writing By reducing the delta between state and now any changes in rtf will have a minimal change on
-        // delta. The old rtf is used since it is assume that since the last update the old rtf was in effect and
-        // only now with future calculations the new rtf will be used
+        // Set up a circular buffer index for the clock data
+        int c = active;
+        int n = (c + 1) % data.size();
 
-        clockArray[updateClockIndex].epoch =
-            clockArray[updateClockIndex].epoch
-            + std::chrono::duration_cast<std::chrono::steady_clock::duration>(
-                (now - clockArray[updateClockIndex].last_update)
-                * clockArray[updateClockIndex].rtf);  // set before we update the variables
-        clockArray[updateClockIndex].last_update = now;
-        clockArray[updateClockIndex].rtf         = new_rtf;
+        // Update the next clock element in the list before we set it for reading so the whole struct is complete before
+        // we let others read it
+        data[n].epoch       = data[c].epoch + dc((now - data[c].last_update) * data[c].rtf);
+        data[n].last_update = now;
+        data[n].rtf         = rtf;
 
-        clockArrayIndex ^= 1;  // Set read
+        // Set active to the clock element we just updated
+        active = n;
     }
 
 }  // namespace utility::clock
 
 namespace NUClear {
     clock::time_point clock::now() {
-        // Move along the time
-        return utility::clock::clockArray[utility::clock::clockArrayIndex].epoch
-               + std::chrono::duration_cast<std::chrono::steady_clock::duration>(
-                   (NUClear::base_clock::now()
-                    - utility::clock::clockArray[utility::clock::clockArrayIndex].last_update)
-                   * utility::clock::clockArray[utility::clock::clockArrayIndex].rtf);
+
+        using namespace utility::clock;
+        using namespace std::chrono;  // Using namespace is fine in a function scope
+
+        // Get the current index in a variable in case it changes while we use it
+        int c = active;
+
+        // Calculate the time
+        return data[active].epoch + dc((NUClear::base_clock::now() - data[c].last_update) * data[c].rtf);
     }
 
 }  // namespace NUClear
