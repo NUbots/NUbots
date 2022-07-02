829927cd4149135eeb7eee03e22c3e361142c3f7
Jason Disher <Jason.Disher@uon.edu.au>
2022-07-02 12:49:15 +1000

    Fall relax best as can be

--- module/behaviour/skills/FallingRelax/src/FallingRelax.cpp
+++ module/behaviour/skills/FallingRelax/src/FallingRelax.cpp
@@ -17,137 +17,6 @@
 //  * Copyright 2013 NUbots <nubots@nubots.net>
 //  */
 
-// #include "FallingRelax.hpp"
-
-// #include <cmath>
-
-// #include "extension/Configuration.hpp"
-// #include "extension/Script.hpp"
-
-// #include "message/behaviour/ServoCommand.hpp"
-// #include "message/input/Sensors.hpp"
-
-// #include "utility/behaviour/Action.hpp"
-// #include "utility/input/LimbID.hpp"
-// #include "utility/input/ServoID.hpp"
-
-// namespace module::behaviour::skills {
-
-//     // internal only callback messages to start and stop our action
-//     struct Falling {};
-//     struct KillFalling {};
-
-//     using extension::Configuration;
-//     using extension::ExecuteScriptByName;
-
-//     using message::input::Sensors;
-
-//     using utility::behaviour::ActionPriorities;
-//     using utility::behaviour::RegisterAction;
-//     using utility::input::LimbID;
-//     using utility::input::ServoID;
-
-//     FallingRelax::FallingRelax(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
-
-//         // do a little configurating
-//         on<Configuration>("FallingRelax.yaml").then([this](const Configuration& config) {
-//             log_level          = config["log_level"].as<NUClear::LogLevel>();
-//             // Store falling angle as a cosine so we can compare it directly to the z axis value
-//             const auto fallingAngle = config["FALLING_ANGLE"].as<float>();
-//             FALLING_ANGLE           = std::cos(fallingAngle);
-
-//             // When falling the acceleration should drop below this value
-//             FALLING_ACCELERATION = config["FALLING_ACCELERATION"].as<float>();
-
-//             // Once the acceleration has stabalized, we are no longer falling
-//             RECOVERY_ACCELERATION = config["RECOVERY_ACCELERATION"].as<std::vector<float>>();
-
-//             PRIORITY = config["PRIORITY"].as<float>();
-//             log<NUClear::INFO>("FallingRelax - Config");
-//         });
-
-// //         on<Trigger<Sensors>>([this](const Sensors& sensors) {
-//             log<NUClear::INFO>("FallingRelax - H1");
-//             if (!falling && !sensors.empty() && fabs(sensors.back()->Htw(2, 2)) < FALLING_ANGLE) {
-
-//                 // We might be falling, check the accelerometer
-//                 double magnitude = 0;
-
-//                 for (const auto& sensor : sensors) {
-//                     magnitude += sensor->accelerometer.norm();
-//                 }
-
-//                 magnitude /= sensors.size();
-
-//                 if (magnitude < FALLING_ACCELERATION) {
-//                     falling = true;
-//                     updatePriority(PRIORITY);
-//                 }
-//             }
-//             else if (falling) {
-//                 // We might be recovered, check the accelerometer
-//                 double magnitude = 0;
-
-//                 for (const auto& sensor : sensors) {
-//                     magnitude += sensor->accelerometer.norm();
-//                 }
-
-//                 magnitude /= sensors.size();
-
-//                 // See if we recover
-//                 if (magnitude > RECOVERY_ACCELERATION[0] && magnitude < RECOVERY_ACCELERATION[1]) {
-//                     falling = false;
-//                     updatePriority(0);
-//                 }
-//             }
-//         });
-
-//         on<Trigger<Falling>>().then([this] {
-//             NUClear::log<NUClear::INFO>("FallingRelax - H2");
-//             emit(std::make_unique<ExecuteScriptByName>(id, "Relax.yaml"));
-//         });
-
-//         on<Trigger<KillFalling>>().then([this] {
-//             falling = false;
-//             updatePriority(0);
-//         });
-
-//         emit<Scope::INITIALIZE>(std::make_unique<RegisterAction>(RegisterAction{
-//             id,
-//             "Falling Relax",
-//             {std::pair<float, std::set<LimbID>>(
-//                 0.0f,
-//                 {LimbID::LEFT_LEG, LimbID::RIGHT_LEG, LimbID::LEFT_ARM, LimbID::RIGHT_ARM, LimbID::HEAD})},
-//             [this](const std::set<LimbID>& /*unused*/) { emit(std::make_unique<Falling>()); },
-//             [this](const std::set<LimbID>& /*unused*/) { emit(std::make_unique<KillFalling>()); },
-//             [](const std::set<ServoID>& /*unused*/) {
-//                 // Ignore
-//             }}));
-//     }
-
-//     void FallingRelax::updatePcfg.fallen_angle   = config["fallen_angle"].as<float>();
-//             cfg.getup_priority = config["getup_priority"].as<float>();
-
-
-// /*
-//  * This file is part of the NUbots Codebase.
-//  *
-//  * The NUbots Codebase is free software: you can redistribute it and/or modify
-//  * it under the terms of the GNU General Public License as published by
-//  * the Free Software Foundation, either version 3 of the License, or
-//  * (at your option) any later version.
-//  *
-//  * The NUbots Codebase is distributed in the hope that it will be useful,
-//  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  * GNU General Public License for more details.
-//  *
-//  * You should have received a copy of the GNU General Public License
-//  * along with the NUbots Codebase.  If not, see <http://www.gnu.org/licenses/>.
-//  *
-//  * Copyright 2013 NUbots <nubots@nubots.net>
-//  */
-
 
 // #include "FallingRelax.hpp"
 
@@ -331,7 +200,7 @@ namespace module::behaviour::skills {
             NUClear::log<NUClear::DEBUG>("Gravity is ", magnitude);
             NUClear::log<NUClear::DEBUG>("Z angle is ", Eigen::Vector3d::UnitZ().dot(uZTw));
 
-            //if (!relaxed && !sensors.empty() && fabs(sensors.back()->Htw(2, 2)) < cfg.falling_angle) {
+
             if (!relaxed && !sensors.empty() && acos(Eigen::Vector3d::UnitZ().dot(uZTw)) > cfg.falling_angle) {
                 NUClear::log<NUClear::DEBUG>("Inside");
 
