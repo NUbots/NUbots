063f08b5888a548711b5629bc6a4e2729d1bed28
tom <thomas16441@gmail.com>
2022-07-12 00:16:03 +1000

    Add functionality to VisionBallLocalisation

--- module/localisation/VisionBallLocalisation/src/VisionBallLocalisation.cpp
+++ module/localisation/VisionBallLocalisation/src/VisionBallLocalisation.cpp
@@ -1,10 +1,12 @@
 #include "VisionBallLocalisation.hpp"
 
+#include <Eigen/Geometry>
 #include <chrono>
 
 #include "extension/Configuration.hpp"
 
-#include "message/localisation/VisionBall.hpp"
+#include "message/input/Sensors.hpp"
+#include "message/localisation/SimpleBall.hpp"
 #include "message/vision/Ball.hpp"
 
 #include "utility/input/ServoID.hpp"
@@ -15,24 +17,63 @@
 namespace module::localisation {
 
     using extension::Configuration;
-    using message::localisation::VisionBall;
+    using SimpleBall  = message::localisation::SimpleBall;
+    using VisionBalls = message::vision::Balls;
+    using VisionBall  = message::vision::Ball;
+    using message::input::Sensors;
 
+    using utility::math::coordinates::reciprocalSphericalToCartesian;
     using utility::nusight::graph;
     using utility::support::Expression;
 
     VisionBallLocalisation::VisionBallLocalisation(std::unique_ptr<NUClear::Environment> environment)
         : Reactor(std::move(environment)) {
 
+        using message::localisation::SimpleBall;
+        using utility::math::coordinates::reciprocalSphericalToCartesian;
 
         on<Configuration>("VisionBallLocalisation.yaml")
-            .then("VisionBallLocalisation Config",
-                  [this](const Configuration& config) { log_level = config["log_level"].as<NUClear::LogLevel>(); });
+            .then("VisionBallLocalisation Config", [this](const Configuration& config) {
+                log_level = config["log_level"].as<NUClear::LogLevel>();
+                // Emit on config to ensure other modules relying on VisionBall run (CHECK IF ACTUALLY NEEDED)
+                emit(std::make_unique<VisionBall>());
+            });
 
         /* To run whenever a ball has been detected */
-        on<Trigger<message::vision::Balls>>().then([this](const message::vision::Balls& balls) {
-            // If there are no balls, do nothing
-            if (!balls.balls.empty()) {
-                // Find the ball
+        on<Trigger<VisionBalls>, With<Sensors>>().then([this](const VisionBalls& balls, const Sensors& sensors) {
+            if (balls.balls.size() > 0) {
+                // Get the first vision ball measurement and treat as the closest measurement to our estimate
+                Eigen::Vector3f rBCc =
+                    reciprocalSphericalToCartesian(balls.balls[0].measurements[0].srBCc.cast<float>());
+                Eigen::Affine3f Htc(sensors.Htw.cast<float>() * balls.Hcw.inverse().cast<float>());
+                Eigen::Vector3f rBTt     = Htc * rBCc;
+                auto raw_lowest_distance = std::sqrt(std::pow(rBTt.x(), 2) + std::pow(rBTt.y(), 2));
+
+                // Loop through all our ball measurements and find the closest measurement to our current estimate
+                for (const auto& ball : balls.balls) {
+                    rBCc           = reciprocalSphericalToCartesian(ball.measurements[0].srBCc.cast<float>());
+                    auto temp_rBTt = Htc * rBCc;
+                    if (std::sqrt(std::pow(temp_rBTt.x(), 2) + std::pow(temp_rBTt.y(), 2)) < raw_lowest_distance) {
+                        raw_lowest_distance = std::sqrt(std::pow(temp_rBTt.x(), 2) + std::pow(temp_rBTt.y(), 2));
+                        rBTt                = temp_rBTt;
+                    }
+                }
+                // TODO: Apply filter to rBTt
+
+                // Generate message and emit
+                auto time_of_measurment = NUClear::clock::now();
+                float distance_to_ball  = std::sqrt(std::pow(rBTt.x(), 2) + std::pow(rBTt.y(), 2));
+                float angle_to_ball     = std::abs(std::atan2(rBTt.y(), rBTt.x()));
+                auto ball               = std::make_unique<SimpleBall>();
+                ball->rBTt              = rBTt;
+                ball->distance          = distance_to_ball;
+                ball->angle             = angle_to_ball;
+                emit(ball);
+
+                // Logging
+                log<NUClear::DEBUG>("rBTt: ", rBTt.transpose());
+                log<NUClear::DEBUG>("Distance: ", distance_to_ball);
+                log<NUClear::DEBUG>("Angle: ", angle_to_ball);
             }
         });
     }
--- module/localisation/VisionBallLocalisation/src/VisionBallLocalisation.hpp
+++ module/localisation/VisionBallLocalisation/src/VisionBallLocalisation.hpp
@@ -1,12 +1,24 @@
-#ifndef MODULE_LOCALISATION_BALLLOCALISATION_HPP
-#define MODULE_LOCALISATION_BALLLOCALISATION_HPP
+#ifndef MODULE_LOCALISATION_VISIONBALLLOCALISATION_HPP
+#define MODULE_LOCALISATION_VISIONBALLLOCALISATION_HPP
 
 #include <nuclear>
 
+#include "message/input/Sensors.hpp"
+#include "message/vision/Ball.hpp"
+
+
 namespace module::localisation {
+    using VisionBalls = message::vision::Balls;
+    using VisionBall  = message::vision::Ball;
+    using message::input::Sensors;
 
     class VisionBallLocalisation : public NUClear::Reactor {
     private:
+        struct Config {
+            Config() = default;
+
+        } cfg;
+
     public:
         /// @brief Called by the powerplant to build and setup the VisionBallLocalisation reactor.
         explicit VisionBallLocalisation(std::unique_ptr<NUClear::Environment> environment);
