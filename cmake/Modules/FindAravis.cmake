0210bd7d6aa6e3b82ec3d165174efa6769ec3bc2
cameron <cameron.murtagh00@gmail.com>
2022-07-15 16:09:49 +1000

    remove commented out

--- cmake/Modules/FindAravis.cmake
+++ cmake/Modules/FindAravis.cmake
@@ -2,7 +2,6 @@ include(ToolchainLibraryFinder)
 
 find_package(glib2 REQUIRED)
 find_package(libusb1 REQUIRED)
-# find_package(zlib REQUIRED)
 find_package(ffi REQUIRED)
 find_package(libxml2 REQUIRED)
 
@@ -15,6 +14,5 @@ ToolchainLibraryFinder(
 
 target_link_libraries(Aravis::Aravis INTERFACE glib2::glib2)
 target_link_libraries(Aravis::Aravis INTERFACE libusb1::libusb1)
-# target_link_libraries(Aravis::Aravis INTERFACE zlib::zlib)
 target_link_libraries(Aravis::Aravis INTERFACE ffi::ffi)
 target_link_libraries(Aravis::Aravis INTERFACE libxml2::libxml2)
