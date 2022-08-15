6217e5e1b50073fc6d27bcb16ba4d5ca8e84a7f4
LachlanCourt <lachlan.m.court@gmail.com>
2022-08-15 20:06:00 +1000

    .

--- nusight2/src/client/components/reactions/odometry_visualizer/stories/test.stories.tsx
+++ nusight2/src/client/components/reactions/odometry_visualizer/stories/test.stories.tsx
@@ -1,66 +1,60 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
-import { RobotModel } from '../../../robot/model'
 import { fullscreen } from '../../../storybook/fullscreen'
-import { ReactionStats } from '../../model'
-import { ReactionVisualizerModel } from '../model'
-import { ReactionVisualizer } from '../view'
 
 storiesOf('components.reactions.reaction_visualizer', module)
   .addDecorator(fullscreen)
   .add('Renders statically', () => <ReactionVisualizerHarness />)
 
 class ReactionVisualizerHarness extends React.Component<{ animate?: boolean }> {
-  const reaction : ReactionStats = getReaction();
-  private model = <ReactionVisualizerModel reaction={this.reaction/>
+  // const reaction : ReactionStats = getReaction();
+  // private model = <ReactionVisualizerModel reaction={this.reaction} />
 
   render() {
-      return <ReactionVisualizer model={this.model} />
-    }
+    return <div /> //<ReactionVisualizer model={this.model} />
   }
-
-}
-function getReaction(): ReactionStats {
-  return {
-    name: 'a',
-    triggerName: 'string',
-    functionName: 'string',
-    reactionId: 1,
-    taskId: 1,
-    causeReactionId:1,
-    causeTaskId: 1,
-    emitted: 1,
-    started: 1,
-    finished: 1,
-  }
-
 }
 
-function getRobots(): RobotModel[] {
-  return [
-    {
-      id: '1',
-      name: 'Virtual Robot 1',
-      connected: true,
-      enabled: true,
-      address: '10.222.10.14',
-      port: 57619,
-    },
-    {
-      id: '2',
-      name: 'Virtual Robot 2',
-      connected: true,
-      enabled: true,
-      address: '10.222.10.19',
-      port: 36261,
-    },
-    {
-      id: '3',
-      name: 'Virtual Robot 3',
-      connected: false,
-      enabled: true,
-      address: '10.222.10.20',
-      port: 67341,
-    },
-  ]
-}
+// function getReaction(): ReactionStats {
+//   return {
+//     name: 'a',
+//     triggerName: 'string',
+//     functionName: 'string',
+//     reactionId: 1,
+//     taskId: 1,
+//     causeReactionId: 1,
+//     causeTaskId: 1,
+//     emitted: 1,
+//     started: 1,
+//     finished: 1,
+//   }
+// }
+
+// function getRobots(): RobotModel[] {
+//   return [
+//     {
+//       id: '1',
+//       name: 'Virtual Robot 1',
+//       connected: true,
+//       enabled: true,
+//       address: '10.222.10.14',
+//       port: 57619,
+//     },
+//     {
+//       id: '2',
+//       name: 'Virtual Robot 2',
+//       connected: true,
+//       enabled: true,
+//       address: '10.222.10.19',
+//       port: 36261,
+//     },
+//     {
+//       id: '3',
+//       name: 'Virtual Robot 3',
+//       connected: false,
+//       enabled: true,
+//       address: '10.222.10.20',
+//       port: 67341,
+//     },
+//   ]
+// }
--- nusight2/src/client/components/reactions/odometry_visualizer/view.tsx
+++ nusight2/src/client/components/reactions/odometry_visualizer/view.tsx
@@ -1,23 +1,22 @@
 import React from 'react'
-import { ReactionStats } from '../model'
-import styles from './styles.css'
 
-export const ReactionVisualizer = ({ reaction }: { reaction: ReactionStats }) => {
+export const ReactionVisualizer = () => {
+  //({ reaction }: { reaction: ReactionStats }) => {
   //const emittedReaction = reaction.emitted
-  const emitted = reaction.emitted //new Date( emittedReaction ).toISOString().slice(11, -1)
+  // const emitted = reaction.emitted //new Date( emittedReaction ).toISOString().slice(11, -1)
   return <div />
-  return (
-    <div className={styles.visualizer}>
-      <div>{stats.name}</div>
-      <div>{stats.triggerName}</div>
-      <div>{stats.functionName}</div>
-      <div>{stats.reactionId}</div>
-      <div>{stats.taskId}</div>
-      <div>{stats.causeReactionId}</div>
-      <div>{stats.causeTaskId}</div>
-      <div>{stats.emitted}</div>
-      <div>{stats.started}</div>
-      <div>{stats.finished}</div>
-    </div>
-  )
+  // return (
+  //   <div className={styles.visualizer}>
+  //     <div>{stats.name}</div>
+  //     <div>{stats.triggerName}</div>
+  //     <div>{stats.functionName}</div>
+  //     <div>{stats.reactionId}</div>
+  //     <div>{stats.taskId}</div>
+  //     <div>{stats.causeReactionId}</div>
+  //     <div>{stats.causeTaskId}</div>
+  //     <div>{stats.emitted}</div>
+  //     <div>{stats.started}</div>
+  //     <div>{stats.finished}</div>
+  //   </div>
+  // )
 }
