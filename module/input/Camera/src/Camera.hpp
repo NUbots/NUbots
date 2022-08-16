f2cd5f72ad3c745d88c06dd1d3e16bdcfda69c63
Trent Houliston <trent@houliston.me>
2022-08-16 17:18:26 +1000

    Update the Aravis Camera module to have better robustness and work with newer aravis versions

--- module/input/Camera/src/Camera.hpp
+++ module/input/Camera/src/Camera.hpp
@@ -1,12 +1,10 @@
 #ifndef MODULE_INPUT_CAMERA_HPP
 #define MODULE_INPUT_CAMERA_HPP
 
-extern "C" {
-#include <aravis-0.8/arv.h>
-}
-
 #include <Eigen/Core>
 #include <Eigen/Geometry>
+#include <aravis-0.8/arv.h>
+#include <map>
 #include <mutex>
 
 #include "CameraContext.hpp"
@@ -21,15 +19,17 @@ namespace module::input {
         /// @brief Called by the powerplant to build and setup the Camera reactor.
         explicit Camera(std::unique_ptr<NUClear::Environment> environment);
 
-    private:
         static void emit_image(ArvStream* stream, CameraContext* context);
         static void control_lost(ArvGvDevice* device, CameraContext* context);
 
+    private:
         std::mutex sensors_mutex;
-        std::vector<std::pair<NUClear::clock::time_point, Eigen::Affine3d>> Hwps;
+        std::vector<std::pair<NUClear::clock::time_point, Eigen::Transform<double, 3, Eigen::Affine, Eigen::DontAlign>>>
+            Hwps;
+
         std::map<std::string, CameraContext> cameras;
-        uint32_t num_cameras = 0;
     };
+
 }  // namespace module::input
 
 #endif  // MODULE_INPUT_CAMERA_HPP
--- module/input/Camera/src/CameraContext.hpp
+++ module/input/Camera/src/CameraContext.hpp
@@ -2,16 +2,17 @@
 #define MODULE_INPUT_CAMERA_CONTEXT_HPP
 
 #include <Eigen/Geometry>
+#include <aravis-0.8/arv.h>
+#include <atomic>
 #include <cstdint>
 #include <memory>
 #include <nuclear>
 #include <string>
 
+#include "extension/Configuration.hpp"
+
 #include "message/input/Image.hpp"
 
-extern "C" {
-#include <aravis-0.8/arv.h>
-}
 
 namespace module::input {
     // Forward declare camera
@@ -20,22 +21,24 @@ namespace module::input {
     // Camera contextual information for Aravis new-buffer callback function.
     struct CameraContext {
         Camera& reactor;
+        ::extension::Configuration config;
         std::string name;
         uint32_t fourcc;
         uint32_t id;
         message::input::Image::Lens lens;
-        // Homogenous transform from platform (p) to camera where platform is the rigid body the camera is attached
-        // to
-        Eigen::Affine3d Hpc;
+        // Homogenous transform from platform (p) to camera where platform is the rigid body the camera is attached to
+        Eigen::Transform<double, 3, Eigen::Affine, Eigen::DontAlign> Hpc;
         std::shared_ptr<ArvCamera> camera;
         std::shared_ptr<ArvStream> stream;
+        std::shared_ptr<ArvDevice> device;
 
         // Values used to correct the time between the clock on the device and the local clock
         struct TimeCorrection {
+            // The type of method we are using to sync with the camera
             // The amount to add to camera timestamps
             std::int64_t offset;
             union {
-                struct {
+                struct {  // NOLINT(clang-diagnostic-nested-anon-types)
                     /// The time on the camera at calibration time
                     std::int64_t cam_at_calibration;
                     /// The time on the local machine at calibration time
@@ -46,7 +49,7 @@ namespace module::input {
                     /// If this exceeds a threshold we assume the clock is out of sync
                     std::int64_t over_time_count;
                 } drift;
-                struct {
+                struct {  // NOLINT(clang-diagnostic-nested-anon-types)
                     // Estimation error covariance
                     double p;
                     // Measurement noise covariance
@@ -55,9 +58,10 @@ namespace module::input {
                     double q;
                 } kf;
             };
-            bool live;
+            enum { LIVE, LATCHED, PTP } type;
         } time;
     };
+
 }  // namespace module::input
 
 #endif  // MODULE_INPUT_CAMERA_CONTEXT_HPP
