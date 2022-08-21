07ec2bdb0e42f93232994712b3f6e597b2469806
Kip Hamiltons <p0nage@live.com.au>
2022-08-21 17:52:46 +1000

    Adds test for install order
    Would prefer to check the log, but this is simpler than using a harness

--- shared/tests/utility/module_test_utils/TestModuleTester.cpp
+++ shared/tests/utility/module_test_utils/TestModuleTester.cpp
@@ -48,6 +48,7 @@ TEST_CASE("Testing that tests don't timeout if they complete in time",
 
     static constexpr int NUM_THREADS = 1;
 
+    // Give the test 5 seconds before it times out
     ModuleTester<DummyReactor, 5, std::chrono::seconds> tester(NUM_THREADS);
 
     tester.run();
@@ -56,10 +57,49 @@ TEST_CASE("Testing that tests don't timeout if they complete in time",
 TEST_CASE("Testing that tests fail if the timeout finishes",
           "[shared][utility][module_test_utils][ModuleTester][TimeoutModule][!shouldfail]") {
 
-
     static constexpr int NUM_THREADS = 1;
 
+    // Set the timeout to be 1 nanosecond - should fail
     ModuleTester<DummyReactor, 1, std::chrono::nanoseconds> tester(NUM_THREADS);
 
     tester.run();
+}
+
+namespace {
+// It's a global, but DO NOT USE IT IN OTHER TEST CASES
+std::vector<std::string> install_log;
+}
+
+TEST_CASE("Modules install in the right order", "[shared][utility][module_test_utils][ModuleTester]") {
+    // When we have modules which are dependencies of the test, we need to install them before calling tester.run().
+    // This test makes sure that these dependencies are installed properly and in the right order
+    
+    class FirstToInstall : public NUClear::Reactor {
+    public:
+        explicit FirstToInstall(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+            on<Startup>().then([install_log] { install_log.push_back("FirstToInstall"); });
+        }
+    };
+
+    class SecondToInstall : public NUClear::Reactor {
+    public:
+        explicit SecondToInstall(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+            on<Startup>().then([install_log, this] { 
+                install_log.push_back("SecondToInstall"); 
+                powerplant.shutdown();
+            });
+        }
+    };
+
+    static constexpr int NUM_THREADS = 1;
+
+    ModuleTester<DummyReactor> tester(NUM_THREADS);
+
+    tester.install<FirstToInstall>("FirstToInstall");
+    tester.install<SecondToInstall>("SecondToInstall");
+
+    tester.run();
+
+    REQUIRE(install_log.at(0) == "FirstToInstall");
+    REQUIRE(install_log.at(1) == "SecondToInstall");
 }
