7a748928e5674338421a712dc2a55deae9253f00
thomaslegge98 <72440840+thomaslegge98@users.noreply.github.com>
2022-08-25 12:21:04 +1000

    Added wav_path and comments

--- module/input/WavReader/src/WavReader.cpp
+++ module/input/WavReader/src/WavReader.cpp
@@ -5,23 +5,20 @@
 #include <iostream>
 #include <string>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::fstream;
-using std::string;
-
 #include "extension/Configuration.hpp"
 namespace module::input {
 
     using extension::Configuration;
-
+    
+    //function to read wav file - See example of functions in
+    
     WavReader::WavReader(std::unique_ptr<NUClear::Environment> environment)
         : Reactor(std::move(environment)), config{} {
 
         on<Configuration>("WavReader.yaml").then([this](const Configuration& cfg) {
             // Use configuration here from file WavReader.yaml
             this->log_level = cfg["log_level"].as<NUClear::LogLevel>();
+            this->log_level = cfg["wav_path"].as<std::string>>();
         });
     }
 
