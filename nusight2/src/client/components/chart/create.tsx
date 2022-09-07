f7ba1f48cd2b695b7cf858c1254d95f3cf556a83
Josephus Paye II <j.paye96@gmail.com>
2022-09-07 19:41:14 +1000

    Remove need for import alias

--- nusight2/src/client/components/chart/create.tsx
+++ nusight2/src/client/components/chart/create.tsx
@@ -3,7 +3,7 @@ import { ComponentType } from 'react'
 import { NUsightNetwork } from '../../network/nusight_network'
 import { AppModel } from '../app/model'
 
-import { LineChart as LineChartImpl } from './line_chart/view'
+import { LineChart } from './line_chart/view'
 import { ChartModel } from './model'
 import { ChartView } from './view'
 
@@ -17,6 +17,5 @@ export function createChartView({
   Menu: ComponentType
 }) {
   const model = ChartModel.of({ robotModels: appModel.robots })
-  const LineChart = LineChartImpl.of(model)
-  return ChartView.of({ model, menu: Menu, nusightNetwork, LineChart })
+  return ChartView.of({ model, menu: Menu, nusightNetwork, LineChart: LineChart.of(model) })
 }
