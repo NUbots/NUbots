c18073860cb2671933481c7ad5a68277d2767527
thomaslegge98 <72440840+thomaslegge98@users.noreply.github.com>
2022-09-08 13:51:29 +1000

    Update WavReader.cpp

--- module/input/WavReader/src/WavReader.cpp
+++ module/input/WavReader/src/WavReader.cpp
@@ -12,29 +12,9 @@ namespace module::input {
  
     //function to read wav file - See example of functions in
  
-    
+	
     WavReader::WavReader(std::unique_ptr<NUClear::Environment> environment)
         : Reactor(std::move(environment)), config{} {
-    
-	FILE *wavin;
-	char buf[3200];
-	int nread = 1, final = 0; 
-	
-	//Wasn't complaing when there was no test.wav, where is this coming from?
-	wavin = fopen("test.wav", "rb");
-	fseek(wavin, 44, SEEK_SET);
-	while (!feof(wavin)) {
-	 nread = fread(buf, 1, sizeof(buf), wavin);
-
-	 if (final) {
-	     printf("test1\n");
-	 } else {
-	     printf("test2\n");
-	 }
-	}
-
-	fclose(wavin);
-
     
         on<Configuration>("WavReader.yaml").then([this](const Configuration& cfg) {
             // Use configuration here from file WavReader.yaml
@@ -42,7 +22,33 @@ namespace module::input {
             this->config.wav_path = cfg["wav_path"].as<std::string>(); //config is the name of the struct
             std::cout << config.wav_path << std::endl;
             log<NUClear::DEBUG>(config.wav_path);
+		read();
         });
+		
+	WavReader::read(){
+		FILE *wavin;
+		char* buf;
+		int nread = 1, var = 0; //What is var, need to understand what these two variables are doing
+
+		//Wasn't complaing when there was no test.wav, where is this coming from?
+		wavin = fopen(config.wav_path, "rb");
+		fseek(wavin, 44, SEEK_SET);
+		
+		fessk(wavin, 0, SEEK_END);
+		lSize = ftell (wavin);
+		rewind(pFile);
+		while (!feof(wavin)) {
+		 nread = fread(buf, 1, sizeof(buf), wavin);
+
+		 if (var) {
+		     printf("test1\n");
+		 } else {
+		     printf(wavin->);
+		 }
+		}
+
+		fclose(wavin);
+	}
     }
 
 }  // namespace module::input
