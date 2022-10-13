25e0f48c9cd6c4f2087ffe676707b8cc78aa05cb
Thomas O'Brien <41043317+TheCoolKidsTable@users.noreply.github.com>
2022-10-13 23:41:57 +1100

    1
    1

--- module/sound/SpeechRecognition/src/SpeechRecognition.cpp
+++ module/sound/SpeechRecognition/src/SpeechRecognition.cpp
@@ -1,17 +1,26 @@
 #include "SpeechRecognition.hpp"
 
+#include <vosk_api.h>
+
 #include "extension/Configuration.hpp"
 
 namespace module {
 
-using extension::Configuration;
+    using extension::Configuration;
+		
 
-SpeechRecognition::SpeechRecognition(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)), config{} {
+    SpeechRecognition::SpeechRecognition(std::unique_ptr<NUClear::Environment> environment)
+        : Reactor(std::move(environment)), config{} {
 
-    on<Configuration>("SpeechRecognition.yaml").then([this](const Configuration& cfg) {
-        // Use configuration here from file SpeechRecognition.yaml
-        this->log_level = cfg["log_level"].as<NUClear::LogLevel>();
-    });
-}
+        on<Configuration>("SpeechRecognition.yaml").then([this](const Configuration& cfg) {
+            // Use configuration here from file SpeechRecognition.yaml
+            this->log_level = cfg["log_level"].as<NUClear::LogLevel>();
+        });
+        
+        //This reactor receives audiodata from emission
+	on<Trigger<AudioData>>().then([this] (AudioData& audioData ) {
+	});	
+    }
+
 
 }  // namespace module
