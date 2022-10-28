6ff1b7af98db787b567eaf4b9815c017104d82a3
Ysobel Sims <35280100+ysims@users.noreply.github.com>
2022-10-28 13:41:29 +1100

    Always and only emit on new task
    Co-authored-by: Trent Houliston <trent@houliston.me>

--- module/motion/Servos/src/Servos.hpp
+++ module/motion/Servos/src/Servos.hpp
@@ -23,17 +23,18 @@ namespace module::motion {
         // TODO(ysims): add capability to be Done when the servo reaches the target position
         template <typename Servo, ServoID::Value ID>
         void add_servo_provider() {
-            on<Provide<Servo>, Trigger<Sensors>>().then([this](const Servo& servo) {
-                // If the time to reach the position is over, then stop requesting the position
-                if (NUClear::clock::now() >= servo.command.time) {
-                    emit<Task>(std::make_unique<Done>());
-                    return;
-                }
-                emit(std::make_unique<ServoTarget>(servo.command.time,
+            on<Provide<Servo>, Trigger<Sensors>>().then([this](const Servo& servo, const RunInfo& info) {
+                if (info.run_reason = NEW_TASK) {
+                    emit(std::make_unique<ServoTarget>(servo.command.time,
                                                    ID,
                                                    servo.command.position,
                                                    servo.command.state.gain,
                                                    servo.command.state.torque));
+                }
+                // If the time to reach the position is over, then stop requesting the position
+                else if (NUClear::clock::now() >= servo.command.time) {
+                    emit<Task>(std::make_unique<Done>());
+                }
             });
         }
 
