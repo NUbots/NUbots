21d2d7a9ea56ab33c553d36c7e1587a5b66483cc
tom <thomas16441@gmail.com>
2022-11-06 12:11:12 +1100

    Add test for KalmanFilter

--- shared/tests/utility/math/filter/TestFilters.cpp
+++ shared/tests/utility/math/filter/TestFilters.cpp
@@ -20,10 +20,12 @@
 #include <Eigen/Core>
 #include <array>
 #include <catch.hpp>
+#include <iostream>
 #include <utility>
 
 #include "VanDerPolModel.hpp"
 
+#include "utility/math/filter/KalmanFilter.hpp"
 #include "utility/math/filter/ParticleFilter.hpp"
 #include "utility/math/filter/UKF.hpp"
 #include "utility/support/yaml_expression.hpp"
@@ -198,4 +200,76 @@ TEST_CASE("Test the ParticleFilter", "[utility][math][filter][ParticleFilter]") 
     INFO("The mean 1\u03C3 boundary for state 2 is [" << -mean_x2_boundary << ", " << mean_x2_boundary << "]");
 
     REQUIRE(percentage_x1 <= 30.0);
+}
+
+TEST_CASE("Test the KalmanFilter", "[utility][math][filter][KalmanFilter]") {
+
+    // Load in the test data
+    const YAML::Node config                         = YAML::LoadFile("tests/TestKalman.yaml");
+    const std::vector<double> true_angle            = resolve_expression<double>(config["true_angle"]);
+    const std::vector<Eigen::Vector2d> measurements = resolve_expression<Eigen::Vector2d>(config["measurements"]);
+
+    // Define the model
+    const size_t n_states  = 3;
+    const size_t n_inputs  = 0;
+    const size_t n_outputs = 2;
+
+    // Define the state transition model
+    Eigen::MatrixXd A = Eigen::MatrixXd::Zero(n_states, n_states);
+    A << 0, 0, 0, 1, 0, 0, 0, 0, 0;
+
+    // Define the control model
+    Eigen::MatrixXd B = Eigen::MatrixXd::Zero(n_states, n_inputs);
+
+    // Define the measurement model
+    Eigen::MatrixXd C = Eigen::MatrixXd::Zero(n_outputs, n_states);
+    C << 0, 1, 0, 1, 0, 1;
+
+    // Define the process noise covariance TODO: Move to yaml
+    Eigen::Matrix3d Q;
+    Q << 3.336564728582061e-04, 1.210220716654972e-05, -1.708092671728623e-04, 1.210220716654972e-05,
+        4.680081577954583e-06, 6.268022135995622e-06, -1.708092671728623e-04, 6.268022135995622e-06,
+        1.320010202073407e-04;
+
+    // Define the measurement noise covariance TODO: Move to yaml
+    Eigen::Matrix2d R;
+    R << 0.0502, 0.0, 0.0, 3.4546e-06;
+
+    // Create a Kalman filter
+    utility::math::filter::KalmanFilter<double, n_states, n_inputs, n_outputs> kalman_filter(A, B, C, Q, R);
+
+    // Define the initial state
+    Eigen::Matrix<double, n_states, 1> x0 = Eigen::Matrix<double, n_states, 1>::Zero();
+    // Define the initial covariance
+    Eigen::Matrix<double, n_states, n_states> P0 = Eigen::Matrix<double, n_states, n_states>::Identity();
+
+    // Initialize the filter
+    kalman_filter.init(x0, P0);
+
+    // Define the time step
+    const double dt = 0.005;
+    int N           = 1000;
+
+    // Run the filter
+    double total_error = 0.0;
+    for (int i = 0; i < N; ++i) {
+        // No control input
+        Eigen::Matrix<double, n_inputs, 1> u  = Eigen::Matrix<double, n_inputs, 1>::Zero();
+        Eigen::Matrix<double, n_outputs, 1> y = measurements[i];
+        kalman_filter.run(u, y, dt);
+
+        // Get the state
+        Eigen::Matrix<double, n_states, 1> x_i = kalman_filter.get_state();
+
+        // Compute the error between ground truth and the filter
+        double error = std::abs(true_angle[i] - x_i(1));
+        total_error += error;
+    }
+    double average_error = total_error / N;
+
+    std::cout << "The total error is: " << total_error << std::endl;
+    std::cout << "The average error is: " << average_error << std::endl;
+    std::cout << "The final state is: " << kalman_filter.get_state().transpose() << std::endl;
+
+    REQUIRE(average_error < 1e-2);
 }
