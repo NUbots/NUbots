4e250b6cbf34ae7db9c0497d100104cf08cba329
Ysobel <ysobelsims@gmail.com>
2022-12-15 21:05:25 +1100

    compile errors

--- module/skills/KickScript/src/KickScript.cpp
+++ module/skills/KickScript/src/KickScript.cpp
@@ -2,15 +2,22 @@
 
 #include "extension/Behaviour.hpp"
 #include "extension/Configuration.hpp"
+#include "extension/behaviour/Script.hpp"
 
-#include "message/motion/Kick.hpp"
 #include "message/motion/Limbs.hpp"
+#include "message/skills/Kick.hpp"
+
+#include "utility/input/LimbID.hpp"
 
 namespace module::skills {
 
     using extension::Configuration;
-    using message::motion::Kick;
+    using extension::behaviour::Script;
+    using extension::behaviour::ScriptRequest;
     using message::motion::LimbsSequence;
+    using message::skills::Kick;
+    using message::skills::KickType;
+    using utility::input::LimbID;
 
     KickScript::KickScript(std::unique_ptr<NUClear::Environment> environment)
         : BehaviourReactor(std::move(environment)) {
@@ -27,7 +34,8 @@ namespace module::skills {
                 return;
             }
             // If it isn't a new task, don't do anything
-            if (info.run_reason == RunInfo::RunReason::IDLE) {
+            if (info.run_reason != RunInfo::RunReason::NEW_TASK) {
+                emit<Task>(std::make_unique<Idle>());
                 return;
             }
 
@@ -42,7 +50,7 @@ namespace module::skills {
             // Otherwise do a normal kick
             if (kick.leg == LimbID::RIGHT_LEG) {
                 emit<Script>(std::make_unique<LimbsSequence>(),
-                             std::vector<ScriptRequest>{{"Stand.yaml"}, {"KickLeft.yaml"}, {"Stand.yaml"}});
+                             std::vector<ScriptRequest>{{"Stand.yaml"}, {"KickRight.yaml"}, {"Stand.yaml"}});
             }
             else {  // LEFT_LEG
                 emit<Script>(std::make_unique<LimbsSequence>(),
