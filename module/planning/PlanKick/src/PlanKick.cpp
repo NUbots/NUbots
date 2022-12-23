603b2f28dfd11b32290b3672f6e84d430d4c3864
Ysobel <ysobelsims@gmail.com>
2022-12-23 18:44:21 +1100

    make the message, add in checks, config, compiles

--- module/planning/PlanKick/src/PlanKick.cpp
+++ module/planning/PlanKick/src/PlanKick.cpp
@@ -1,60 +1,78 @@
 #include "PlanKick.hpp"
 
+#include <string>
+
+#include "extension/Behaviour.hpp"
 #include "extension/Configuration.hpp"
+
+#include "message/localisation/FilteredBall.hpp"
+#include "message/planning/KickTo.hpp"
+#include "message/skill/Kick.hpp"
+
+#include "utility/input/LimbID.hpp"
 
 namespace module::planning {
 
     using extension::Configuration;
+    using message::localisation::FilteredBall;
+    using message::planning::KickTo;
+    using message::skill::Kick;
+    using utility::input::LimbID;
 
-    PlanKick::PlanKick(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+    PlanKick::PlanKick(std::unique_ptr<NUClear::Environment> environment) : BehaviourReactor(std::move(environment)) {
 
         on<Configuration>("PlanKick.yaml").then([this](const Configuration& config) {
             // Use configuration here from file PlanKick.yaml
-            this->log_level        = config["log_level"].as<NUClear::LogLevel>();
-            cfg.distance_threshold = config["distance_threshold"].as<float>();
-            cfg.angle_threshold    = config["distance_threshold"].as<float>();
+            this->log_level             = config["log_level"].as<NUClear::LogLevel>();
+            cfg.ball_distance_threshold = config["ball_distance_threshold"].as<float>();
+            cfg.ball_angle_threshold    = config["ball_angle_threshold"].as<float>();
+            cfg.align                   = config["align"].as<bool>();
+            cfg.target_angle_threshold  = config["target_angle_threshold"].as<float>();
+            cfg.kick_leg                = config["kick_leg"].as<std::string>();
+        });
+
+        on<Provide<KickTo>, Trigger<FilteredBall>>().then([this](const KickTo& kick, const FilteredBall& ball) {
+            // CHECK IF CLOSE TO BALL
+            // Get the angle and distance to the ball
+            float ball_angle    = std::abs(std::atan2(ball.rBTt.y(), ball.rBTt.x()));
+            float ball_distance = ball.rBTt.head(2).norm();
+
+            // Need to be near the ball to consider kicking it
+            if (ball_distance > cfg.ball_distance_threshold || ball_angle > cfg.ball_angle_threshold) {
+                emit(std::make_unique<Idle>());
+                return;
+            }
+
+            // CHECK IF FACING POINT TO KICK TO
+            float align_angle = std::abs(std::atan2(kick.rPTt.y(), kick.rPTt.x()));
+
+            // Don't kick if we should align but we're not aligned to the target
+            if (cfg.align && align_angle > cfg.target_angle_threshold) {
+                emit(std::make_unique<Idle>());
+                return;
+            }
+
+            // ALL CHECKS PASSED, KICK!
+            // Check if config specifies a leg
+            if (cfg.kick_leg == "left") {  // can only kick with left
+                emit(std::make_unique<Kick>(LimbID::LEFT_LEG));
+                return;
+            }
+            else if (cfg.kick_leg == "right") {  // can only kick with right
+                emit(std::make_unique<Kick>(LimbID::RIGHT_LEG));
+                return;
+            }
+
+            // Else determine leg based on ball position
+            // If the ball is more to the left, then kick with the left leg
+            if (ball.rBTt.y() > 0.0) {
+                emit(std::make_unique<Kick>(LimbID::LEFT_LEG));
+            }
+            else {  // ball is more to the right
+                emit(std::make_unique<Kick>(LimbID::RIGHT_LEG));
+            }
         });
     }
 
-
-    on<Provide<CheckCloseToBall>, Trigger<FilteredBall>>().then([this](const RunInfo& info, const FilteredBall& ball) {
-        // Get the angle and distance to the ball
-        float absolute_yaw_angle = std::abs(std::atan2(ball.rBTt.y(), ball.rBTt.x()));
-        float distance_to_ball   = ball.rBTt.head(2).norm();
-
-        // If the ball is within parameters, then we are Done checking
-        if (distance_to_ball < cfg.distance_threshold && absolute_yaw_angle < cfg.angle_threshold) {
-            // Save the position of the ball for when kicking, if no further ball detections exist
-            rBTt = ball.rBTt;
-            emit<Task>(std::make_unique<Done>());
-            return;
-        }
-
-        // Otherwise, we wait for the ball to be closer
-        emit<Task>(std::make_unique<Idle>());
-    });
-
-    on<Provide<KickBall>, With<FilteredBall>>().then([this](const RunInfo& info,
-                                                            const std::shared_ptr<const FilteredBall>& ball) {
-        // If the Kick Task is Done, then KickBall is also Done
-        if (info.run_reason == RunInfo::RunReason::SUBTASK_DONE) {
-            emit<Task>(std::make_unique<Done>());
-            return;
-        }
-
-        // If there are no balls, something is wrong! We can't kick the ball if there are no balls.
-        if (!ball) {
-            emit<Task>(std::make_unique<Done>());
-            return;
-        }
-
-        // If the ball is more to the left, then kick with the left leg
-        if (ball.rBTt.y() > 0.0) {
-            emit(std::make_unique<Kick>(LimbID::LEFT_LEG));
-        }
-        else {  // ball is more to the right
-            emit(std::make_unique<Kick>(LimbID::RIGHT_LEG));
-        }
-    });
 
 }  // namespace module::planning
--- module/planning/PlanKick/src/PlanKick.hpp
+++ module/planning/PlanKick/src/PlanKick.hpp
@@ -1,20 +1,27 @@
 #ifndef MODULE_PLANNING_PLANKICK_HPP
 #define MODULE_PLANNING_PLANKICK_HPP
 
+#include <extension/Behaviour.hpp>
 #include <nuclear>
+#include <string>
 
 namespace module::planning {
 
-class PlanKick : public NUClear::Reactor {
-private:
-    /// @brief Stores configuration values
-    struct Config {
-    } cfg;
+    class PlanKick : public ::extension::behaviour::BehaviourReactor {
+    private:
+        /// @brief Stores configuration values
+        struct Config {
+            float ball_distance_threshold = 0.0;
+            float ball_angle_threshold    = 0.0;
+            bool align                    = false;
+            float target_angle_threshold  = 0.0;
+            std::string kick_leg          = "";
+        } cfg;
 
-public:
-    /// @brief Called by the powerplant to build and setup the PlanKick reactor.
-    explicit PlanKick(std::unique_ptr<NUClear::Environment> environment);
-};
+    public:
+        /// @brief Called by the powerplant to build and setup the PlanKick reactor.
+        explicit PlanKick(std::unique_ptr<NUClear::Environment> environment);
+    };
 
 }  // namespace module::planning
 
