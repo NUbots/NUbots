24c867193eced8bea8eac4f55bfb0e3a8261fe8f
dex <dex-k@users.noreply.github.com>
2023-01-30 16:35:35 +1100

    whoops. add decl for constructSensors

--- module/platform/openCR/HardwareIO/src/HardwareIO.hpp
+++ module/platform/openCR/HardwareIO/src/HardwareIO.hpp
@@ -116,6 +116,7 @@ namespace module::platform::openCR {
         void processModelInformation(const message::platform::StatusReturn& packet);
         void processOpenCRData(const message::platform::StatusReturn& packet);
         void processServoData(const message::platform::StatusReturn& packet);
+        RawSensors constructSensors();
     };
 
 }  // namespace module::platform::openCR
