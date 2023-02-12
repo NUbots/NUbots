a797131d001e64b4c9699af71a8521bde40be04e
Trent Houliston <trent@houliston.me>
2023-02-13 08:56:14 +1100

    Add missing includes

--- module/strategy/FallRecovery/src/FallRecovery.cpp
+++ module/strategy/FallRecovery/src/FallRecovery.cpp
@@ -2,10 +2,14 @@
 
 #include "extension/Behaviour.hpp"
 
+#include "message/planning/GetUpWhenFallen.hpp"
+#include "message/planning/RelaxWhenFalling.hpp"
 #include "message/strategy/FallRecovery.hpp"
 
 namespace module::strategy {
 
+    using message::planning::GetUpWhenFallen;
+    using message::strategy::RelaxWhenFalling;
     using FallRecoveryTask = message::strategy::FallRecovery;
 
     FallRecovery::FallRecovery(std::unique_ptr<NUClear::Environment> environment)
@@ -15,8 +19,8 @@ namespace module::strategy {
             // Plan to relax when falling and get up when on the ground
             // We set the priority of PlanGetUp higher than PlanFallingRelax so that relax won't take over while we are
             // getting up
-            emit<Task>(std::make_unique<PlanGetUp>(), 2);
-            emit<Task>(std::make_unique<PlanFallingRelax>(), 1);
+            emit<Task>(std::make_unique<GetUpWhenFallen>(), 2);
+            emit<Task>(std::make_unique<RelaxWhenFalling>(), 1);
         });
     }
 
