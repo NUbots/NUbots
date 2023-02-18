4ebdd885f03c76edaef0c7827a5c9ac42d4396e9
Ysobel <ysobelsims@gmail.com>
2023-02-19 10:02:49 +1100

    fix

--- shared/utility/libraries.cmake
+++ shared/utility/libraries.cmake
@@ -35,4 +35,4 @@ target_sources(nuclear_utility PRIVATE "${CMAKE_BINARY_DIR}/recordings")
 target_compile_features(nuclear_utility PUBLIC cxx_std_17)
 
 # Add the scripts directory to the build directory
-file(COPY "${nuclear_utility}/skill/scripts" DESTINATION ${PROJECT_BINARY_DIR})
+file(COPY "${PROJECT_SOURCE_DIR}/shared/utility/skill/scripts" DESTINATION ${PROJECT_BINARY_DIR})
