f8eac13b9aebcdddc91c2c9bd0f67db3441d7e28
cameron <cameron.murtagh00@gmail.com>
2023-03-01 11:31:42 +1100

    ability to set/unset groups

--- tools/configure.py
+++ tools/configure.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 
 import b
 from utility.dockerise import run_on_docker
-from utility.roles import all_role_names
+from utility.roles import all_role_names, role_folders, dir_role_names
 from utility.shell import WrapPty
 
+ROLE_GROUPS = {
+    "all" : all_role_names(),
+    "cool" : ["robocup", "test/camera"],
+    "nice" : ["scripttuner", "natnet"],
+}
+
+for r in role_folders():
+    ROLE_GROUPS[r] = dir_role_names(r)
+
+class ExtendConstAction(argparse._AppendConstAction):
+    def __call__(self, parser, namespace, values, option_string=None):
+        items = getattr(namespace, self.dest, None)
+        items = argparse._copy_items(items)
+        items.extend(self.const)
+        setattr(namespace, self.dest, items)
 
 @run_on_docker
 def register(command):
@@ -22,21 +38,37 @@ def register(command):
         help="perform an interactive configuration using ccmake",
     )
     command.add_argument(
-        "--set_roles",
-        dest="set_roles",
-        default=[],
-        choices=all_role_names(),
-        nargs="*",
-        help="Set roles via command line"
-    )
-    command.add_argument(
         "--unset_roles",
         dest="unset_roles",
+        action="extend",
         default=[],
         choices=all_role_names(),
         nargs="*",
         help="Unset roles via command line"
     )
+    for group in ROLE_GROUPS.keys():
+        command.add_argument(
+            f"--unset_{group}_roles",
+            dest="unset_roles",
+            action=ExtendConstAction,
+            const=ROLE_GROUPS[group],
+        )
+    command.add_argument(
+        "--set_roles",
+        dest="set_roles",
+        action="extend",
+        default=[],
+        choices=all_role_names(),
+        nargs="+",
+        help="Set roles via command line"
+    )
+    for group in ROLE_GROUPS.keys():
+        command.add_argument(
+            f"--set_{group}_roles",
+            dest="set_roles",
+            action=ExtendConstAction,
+            const=ROLE_GROUPS[group],
+        )
     command.add_argument("args", nargs="...", help="the arguments to pass through to cmake")
 
 
@@ -52,12 +84,12 @@ def run(interactive, set_roles, unset_roles, args, **kwargs):
     if "--" in args:
         args.remove("--")
 
-    for role in set_roles:
-        role = role.replace("/","-")
-        args.append(f"-DROLE_{role}:BOOL=ON")
     for role in unset_roles:
         role = role.replace("/","-")
         args.append(f"-DROLE_{role}:BOOL=OFF")
+    for role in set_roles:
+        role = role.replace("/","-")
+        args.append(f"-DROLE_{role}:BOOL=ON")
 
     if interactive:
         exit(
