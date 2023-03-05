7cb55a5abcb5eb955cfc55fea2a297f2f2126bfb
Ysobel Sims <35280100+ysims@users.noreply.github.com>
2023-03-05 16:45:16 +1100

    Update year

--- module/extension/Director/tests/uses_state.cpp
+++ module/extension/Director/tests/uses_state.cpp
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with the NUbots Codebase.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright 2022 NUbots <nubots@nubots.net>
+ * Copyright 2023 NUbots <nubots@nubots.net>
  */
 
 #include <catch.hpp>
