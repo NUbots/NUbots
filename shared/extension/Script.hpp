d40b5e2de6e8b4f6e6b2809e48473df2f56126df
Ysobel <ysobelsims@gmail.com>
2023-03-12 15:20:12 +1100

    formatting

--- shared/extension/Script.hpp
+++ shared/extension/Script.hpp
@@ -50,7 +50,7 @@ namespace extension {
                 Target(Target&& other) noexcept
                     : id(other.id), position(other.position), gain(other.gain), torque(other.torque) {}
                 Target& operator=(const Target& other) = default;
-                Target& operator=(Target&& other) noexcept {
+                Target& operator                       =(Target&& other) noexcept {
                     id       = other.id;
                     position = other.position;
                     gain     = other.gain;
