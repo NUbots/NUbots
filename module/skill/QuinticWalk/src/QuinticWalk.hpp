9d4031cc9d3d74328e62ce6b0d304ab2dbf4cb79
Liam Craft <62984007+Crafty15@users.noreply.github.com>
2023-03-12 19:41:49 +1100

    Adds module for the director style QuinticWalk (#1029)
    Co-authored-by: Ysobel <ysobelsims@gmail.com>
    Co-authored-by: Ysobel Sims <35280100+ysims@users.noreply.github.com>
    Co-authored-by: Trent Houliston <trent@houliston.me>

--- module/skill/QuinticWalk/src/QuinticWalk.hpp
+++ module/skill/QuinticWalk/src/QuinticWalk.hpp
@@ -1,22 +1,24 @@
-#ifndef MODULE_MOTION_QUINTICWALK_HPP
-#define MODULE_MOTION_QUINTICWALK_HPP
+#ifndef MODULE_SKILL_QUINTICWALK_HPP
+#define MODULE_SKILL_QUINTICWALK_HPP
 
 #include <map>
 #include <memory>
 #include <nuclear>
 #include <vector>
 
+#include "extension/Behaviour.hpp"
 #include "extension/Configuration.hpp"
 
 #include "message/actuation/KinematicsModel.hpp"
-#include "message/behaviour/ServoCommand.hpp"
+#include "message/actuation/ServoCommand.hpp"
+#include "message/behaviour/state/Stability.hpp"
 
 #include "utility/input/ServoID.hpp"
 #include "utility/skill/WalkEngine.hpp"
 
-namespace module::motion {
+namespace module::skill {
 
-    class QuinticWalk : public NUClear::Reactor {
+    class QuinticWalk : public ::extension::behaviour::BehaviourReactor {
 
     public:
         /// @brief Called by the powerplant to build and setup the QuinticWalk reactor.
@@ -25,17 +27,11 @@ namespace module::motion {
         static constexpr int UPDATE_FREQUENCY = 200;
 
     private:
-        /// Current subsumption ID key to access motors.
-        size_t subsumption_id = 1;
-
-        // Reaction handle for the main update loop, disabling when not moving will save unnecessary CPU
-        ReactionHandle update_handle{};
+        // Reaction handle for the imu reaction, disabling when not moving will save unnecessary CPU
         ReactionHandle imu_reaction{};
 
         void calculate_joint_goals();
         [[nodiscard]] float get_time_delta();
-        [[nodiscard]] std::unique_ptr<message::behaviour::ServoCommands> motion(
-            const std::vector<std::pair<utility::input::ServoID, float>>& joints);
 
         struct Config {
             Eigen::Vector3f max_step = Eigen::Vector3f::Zero();
@@ -47,18 +43,20 @@ namespace module::motion {
 
             utility::skill::WalkingParameter params{};
 
-            std::map<utility::input::ServoID, float> jointGains{};
+            std::map<utility::input::ServoID, message::actuation::ServoState> servo_states{};
+
             std::vector<std::pair<utility::input::ServoID, float>> arm_positions{};
-        } normal_config{}, goalie_config{};
+        } normal_cfg{}, goalie_cfg{};
 
         static void load_quintic_walk(const ::extension::Configuration& cfg, Config& config);
 
-        Config& current_config = normal_config;
-        bool first_config      = true;
+        /// @brief Stores the walking config
+        Config& current_cfg = normal_cfg;
+
+        bool first_cfg = true;
 
         Eigen::Vector3f current_orders = Eigen::Vector3f::Zero();
         bool is_left_support           = true;
-        bool falling                   = false;
         bool first_run                 = true;
 
         NUClear::clock::time_point last_update_time{};
@@ -67,6 +65,6 @@ namespace module::motion {
 
         message::actuation::KinematicsModel kinematicsModel{};
     };
-}  // namespace module::motion
+}  // namespace module::skill
 
-#endif  // MODULE_MOTION_QUINTICWALK_HPP
+#endif  // MODULE_SKILL_QUINTICWALK_HPP
