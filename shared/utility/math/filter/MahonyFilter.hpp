dc24c460190a2886dbab7146faa5be21a7648f67
tom <thomas16441@gmail.com>
2023-03-29 19:57:43 +1100

    Clean up mahony filter

--- shared/utility/math/filter/MahonyFilter.hpp
+++ shared/utility/math/filter/MahonyFilter.hpp
@@ -27,8 +27,20 @@ namespace utility {
         namespace filter {
 
             /**
-             * @brief The Mahony update uses the IMU data to compute the attitude
-             * INPUT
+             * @brief Compute skew of a vector
+             * @param v: input vector
+             * @return skew of v
+             */
+            template <typename T>
+            Eigen::Matrix<T, 3, 3> skew(const Eigen::Matrix<T, 3, 1>& v) {
+                Eigen::Matrix<T, 3, 3> skew;
+                skew << 0, -v(2), v(1), v(2), 0, -v(0), -v(1), v(0), 0;
+                return skew;
+            }
+
+            /**
+             * @brief The Mahony update uses the IMU data to compute the attitude. To see report on this, go to
+             * "public/Projects/Final Year Projects/NUgus Pose Estimation" folder on the NAS.
              * @param acc:     accelerometer reading as a column vector, [a_x, a_y, a_z]'. g force
              * @param gyro:    gyroscope reading as  column vector, [w_x, w_y, w_z]'. angular velocities in rad/s.
              * @param ts:      Sample rate of this update.
@@ -36,8 +48,6 @@ namespace utility {
              * @param Kp:      proportional gain
              * @param quat:    quaternion representing the rotation of the torso. Hwt.
              * @param bias:    gyroscope bias
-             * To see report on this, go to "public/Projects/Final Year Projects/NUgus Pose Estimation" folder on the
-             * NAS.
              */
             void MahonyUpdate(Eigen::Vector3d acc,
                               Eigen::Vector3d gyro,
@@ -48,70 +58,43 @@ namespace utility {
                               Eigen::Vector3d& bias) {
                 // Normalise the acceleration vector
                 acc.normalize();
+                Eigen::Vector3d rGTt = acc;
 
-                // Compute the 3 by 3 attitude matrix from the quaternion
-                Eigen::Vector3d rho(quat.x(), quat.y(), quat.z());
-                double q4           = quat.w();
-                double rho_norm_squ = std::pow(rho.norm(), 2);
-                // clang-format off
-                Eigen::Matrix3d rho_x;
-                rho_x << 0, -rho.z(),  rho.y(),
-                rho.z(),       0 , -rho.x(),
-                -rho.y(),  rho.x(),        0;
-                // clang-format on
+                // Convert the quaternion to a rotation matrix
+                Eigen::Matrix3d Rtw = quat.toRotationMatrix().inverse();
 
-                Eigen::Matrix3d attitude = ((std::pow(q4, 2) - rho_norm_squ) * Eigen::Matrix3d::Identity())
-                                           - (2 * q4 * rho_x) + (2 * rho * rho.transpose());
-
-                // Calculate the sensor error
-                // Reference vector for gravity (gravity is positive)
-                Eigen::Vector3d r_acc(0, 0, 1);
+                // Calculate the error between the accelerometer gravity and state estimated gravity vector
+                // World vector for gravity (gravity is positive)
+                Eigen::Vector3d rGWw(0, 0, 1);
                 // Calculate estimated accelerometer reading
-                Eigen::Vector3d est_acc = attitude * r_acc;
+                Eigen::Vector3d est_rGTt = Rtw * rGWw;
                 // Calculate error between estimate and real
-                Eigen::Matrix3d a_corr = acc * est_acc.transpose() - est_acc * acc.transpose();
+                Eigen::Matrix3d a_corr = rGTt * est_rGTt.transpose() - est_rGTt * rGTt.transpose();
                 // Vex (inverse function of skew-symmetric function) the error
-                Eigen::Vector3d omega_mes(a_corr(2, 1), a_corr(0, 2), a_corr(1, 0));
-                omega_mes = -omega_mes;
+                Eigen::Vector3d omega_mes = -1 * Eigen::Vector3d(a_corr(2, 1), a_corr(0, 2), a_corr(1, 0));
 
                 // Estimate the bias
-                if (Ki > 0) {
-                    // Calculate bias
-                    bias += Ki * omega_mes * ts;
-                }
-                else {
-                    bias = Eigen::Vector3d::Zero();
-                }
+                bias += Ki * omega_mes * ts;
 
-                // Calculate attitude using quaternion dynamics
                 // Depolarizing the gyroscope bias
                 Eigen::Vector3d l_omega = gyro + Kp * omega_mes + bias;
 
                 // Find the quaternions rate of change
-                // clang-format off
-                Eigen::Matrix3d omega_x;
-                omega_x <<         0, -l_omega.z(),  l_omega.y(),
-                     l_omega.z(),            0, -l_omega.x(),
-                    -l_omega.y(),  l_omega.x(),            0;
-                // clang-format on
-                Eigen::Matrix4d ome;
-                ome.block<3, 3>(0, 0) = -omega_x;
-                ome.block<1, 3>(3, 0) = -l_omega.transpose();
-                ome.block<3, 1>(0, 3) = l_omega;
-                ome(3, 3)             = 0;
+                Eigen::Matrix3d omega_x = skew(l_omega);
+                Eigen::Matrix4d ome     = Eigen::Matrix4d::Zero();
+                ome.block<3, 3>(0, 0)   = -omega_x;
+                ome.block<1, 3>(3, 0)   = -l_omega.transpose();
+                ome.block<3, 1>(0, 3)   = l_omega;
+                ome(3, 3)               = 0;
 
-                ome *= 0.5;
                 Eigen::Vector4d quat_vec(quat.x(), quat.y(), quat.z(), quat.w());
-                Eigen::Vector4d q_d(ome * quat_vec);
+                Eigen::Vector4d q_d(0.5 * ome * quat_vec);
 
                 // Calculate integral to find the attitude quaternion
                 quat_vec += ts * q_d;
 
                 // Set quat (Rwt) and normalise
-                quat.x() = quat_vec(0);
-                quat.y() = quat_vec(1);
-                quat.z() = quat_vec(2);
-                quat.w() = quat_vec(3);
+                quat = Eigen::Quaterniond(quat_vec(3), quat_vec(0), quat_vec(1), quat_vec(2));
                 quat.normalize();
             }
 
