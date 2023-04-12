beb7e85d07f8c86c9d8eecfd28a31bf2d77083e2
tom <thomas16441@gmail.com>
2023-04-12 15:34:10 +1000

    Move ready to striker

--- module/strategy/WalkToFieldPosition/src/WalkToFieldPosition.cpp
+++ module/strategy/WalkToFieldPosition/src/WalkToFieldPosition.cpp
@@ -27,16 +27,15 @@ namespace module::strategy {
         on<Provide<WalkToFieldPositionTask>, With<Field>, With<Sensors>, Every<30, Per<std::chrono::seconds>>>().then(
             [this](const WalkToFieldPositionTask& walk_to_field_position, const Field& field, const Sensors& sensors) {
                 // Transform the field position into the robot's torso frame
-                const Eigen::Isometry3f Hfw = Eigen::Isometry3d(field.Hfw).cast<float>();
+                const Eigen::Isometry3f Hfw = Eigen::Isometry3f(field.Hfw.cast<float>());
                 const Eigen::Isometry3f Htw = Eigen::Isometry3f(sensors.Htw.cast<float>());
                 const Eigen::Isometry3f Htf = Htw * Hfw.inverse();
-                log<NUClear::DEBUG>("rWFf :", Hfw.translation().transpose());
 
                 const Eigen::Vector3f rPFf(walk_to_field_position.rPFf.x(), walk_to_field_position.rPFf.y(), 0.0f);
                 const Eigen::Vector3f rPTt(Htf * rPFf);
 
                 // Emit a task to walk to the field position
-                // emit<Task>(std::make_unique<WalkTo>(rPTt));
+                emit<Task>(std::make_unique<WalkTo>(rPTt));
             });
     }
 
