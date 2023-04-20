a9df2cbbf16ee515b40b4883490f42dab3dc693d
tom <thomas16441@gmail.com>
2023-04-20 19:58:44 +1000

    Add NLopt library

--- shared/utility/libraries.cmake
+++ shared/utility/libraries.cmake
@@ -25,6 +25,9 @@ target_link_libraries(nuclear_utility PUBLIC tinyxml2::tinyxml2)
 find_package(tinyrobotics REQUIRED)
 target_link_libraries(nuclear_utility PUBLIC tinyrobotics::tinyrobotics)
 
+find_package(NLopt REQUIRED)
+target_link_libraries(nuclear_utility PUBLIC NLopt::nlopt)
+
 if(CMAKE_BUILD_TYPE STREQUAL "Debug")
   find_package(libbacktrace REQUIRED)
   target_link_libraries(nuclear_utility PUBLIC libbacktrace::libbacktrace ${CMAKE_DL_LIBS})
