a192da2549bc0948e2690684dbe48654aee5478c
Jason Disher <Jason.Disher@uon.edu.au>
2023-05-02 15:59:18 +1000

    .

---
+++ shared/utility/voronoi/Voronoi.hpp
@@ -0,0 +1,57 @@
+
+#ifndef UTILITY_VORONOI_VORONOI_H
+#define UTILITY_VORONOI_VORONOI_H
+
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <vector>
+
+ namespace utility::voronoi {
+
+    struct Site {
+        double x, y;
+    };
+
+    struct CircleEvent {
+        double x, y, r;
+        bool valid;
+    };
+
+    struct Event {
+        double x, y;
+        Site* site;
+        CircleEvent* circleEvent;
+
+        bool operator<(const Event& other) const {
+            if (y != other.y) {
+                return y > other.y;
+            }
+
+            return x > other.x;
+        }
+    };
+
+    struct Edge {
+        Site* left, *right;
+        Site* start, *end;
+        double m, b;
+    };
+
+    class Voronoi {
+
+        private:
+            void computeEdges(std::vector<Site*>& sites);
+
+            std::vector<Site*> sites;
+            std::vector<Edge> edges;
+            std::vector<CircleEvent*> circleEvents;
+
+        public:
+            void computeVoronoi(std::vector<Site*>& sites);
+            std::vector<Edge> get_edges();
+
+    };
+}   // namespace utility::voronoi
+
+#endif  // UTILITY_VORONOI_VORONOI_H
