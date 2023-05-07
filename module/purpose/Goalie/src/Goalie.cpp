2595dc6490011ad80ba63e4a3cd090f260983a7d
Jesse Perrin <30261678+Jesse27@users.noreply.github.com>
2023-05-07 22:19:13 +1000

    fix typo on WalkToFieldPosition

--- module/purpose/Goalie/src/Goalie.cpp
+++ module/purpose/Goalie/src/Goalie.cpp
@@ -89,7 +89,7 @@ namespace module::purpose {
         // Stop the ball!
         // Second argument is priority - higher number means higher priority
         Eigen::Vector3f position(cfg.ready_position.x(), cfg.ready_position.y(), 0);
-        emit<Task>(std::make_unique<WalkToFieldPosition>(position), cfg.ready_position.z()), 1);
+        emit<Task>(std::make_unique<WalkToFieldPosition>(position), cfg.ready_position.z(), 1);
         emit<Task>(std::make_unique<LookAround>(), 2);  // if the look at ball task is not running, find the ball
         emit<Task>(std::make_unique<LookAtBall>(), 3);  // try to track the ball
         emit<Task>(std::make_unique<DiveToBall>(), 4);  // dive to the ball
