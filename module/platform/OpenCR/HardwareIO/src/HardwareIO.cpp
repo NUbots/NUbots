b3aaf9af80cdb3a331ac63e14ef4aa5fed57e4c7
Ysobel <ysobelsims@gmail.com>
2023-05-20 14:23:20 +1000

    shouldn't really have clashes anymore so rename

--- module/platform/OpenCR/HardwareIO/src/HardwareIO.cpp
+++ module/platform/OpenCR/HardwareIO/src/HardwareIO.cpp
@@ -29,7 +29,7 @@ namespace module::platform::OpenCR {
     HardwareIO::HardwareIO(std::unique_ptr<NUClear::Environment> environment)
         : Reactor(std::move(environment)), opencr(), nugus(), byte_wait(0), packet_wait(0), packet_queue() {
 
-        on<Configuration>("HardwareIO_OpenCR.yaml").then([this](const Configuration& config) {
+        on<Configuration>("HardwareIO.yaml").then([this](const Configuration& config) {
             this->log_level = config["log_level"].as<NUClear::LogLevel>();
 
             // Make sure OpenCR is operating at the correct baud rate (based on config params)
