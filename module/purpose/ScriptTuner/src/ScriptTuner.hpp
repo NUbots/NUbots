6822ddcf462e089a90cbcbf70a012afaf1f7ea45
tom <thomas16441@gmail.com>
2023-06-05 11:11:06 +1000

    Add director script tuner

---
+++ module/purpose/ScriptTuner/src/ScriptTuner.hpp
@@ -0,0 +1,83 @@
+/*
+ * This file is part of the NUbots Codebase.
+ *
+ * The NUbots Codebase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The NUbots Codebase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the NUbots Codebase.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2023 NUbots <nubots@nubots.net>
+ */
+
+#ifndef MODULES_BEHAVIOUR_TOOLS_SCRIPTTUNER_HPP
+#define MODULES_BEHAVIOUR_TOOLS_SCRIPTTUNER_HPP
+
+#include <nuclear>
+
+#include "message/actuation/Limbs.hpp"
+
+#include "utility/skill/Script.hpp"
+
+namespace module::purpose {
+
+    using message::actuation::LimbsSequence;
+    using utility::skill::Script;
+
+    /**
+     * Provides a Curses interface to let the user customize scripts
+     *
+     * @author Trent Houliston
+     */
+    class ScriptTuner : public NUClear::Reactor {
+    private:
+        const size_t id;
+        /// The path to the script we are editing
+        std::string scriptPath;
+        /// The script object we are editing
+        Script<LimbsSequence> script;
+        /// The index of the frame we are currently editing
+        size_t frame;
+        /// The index of the item we are selecting
+        size_t selection;
+        /// If we are selecting the angle or gain for this item
+        bool angleOrGain;
+        const size_t defaultGain     = 10;
+        const size_t defaultDuration = 1000;
+
+        static std::string userInput();
+
+        void refreshView();
+        void loadScript(const std::string& path);
+        void saveScript();
+        void editDuration();
+        void editSelection();
+        void activateFrame(int frame);
+        void toggleLockMotor();
+        void newFrame();
+        void deleteFrame();
+        void playScript();
+        void jumpToFrame();
+        void help();
+        void editGainInput();
+        void mirrorScript();
+        void saveScriptAs();
+        void editGain();
+        void userInputToFrame();
+        static float userInputToGain();
+
+        volatile bool running;
+
+    public:
+        explicit ScriptTuner(std::unique_ptr<NUClear::Environment> environment);
+    };
+}  // namespace module::purpose
+
+#endif  // MODULE_PURPOSE_SCRIPTTUNER_HPP
