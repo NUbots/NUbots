cfdccd37bd0c4be4d4723aa5a9be84200690fc86
Thomas O'Brien <41043317+Tom0Brien@users.noreply.github.com>
2023-06-17 13:38:50 +1000

    Localisation tweaks (#1089)

--- module/localisation/FieldLocalisation/src/FieldLocalisation.hpp
+++ module/localisation/FieldLocalisation/src/FieldLocalisation.hpp
@@ -1,5 +1,5 @@
-#ifndef MODULE_LOCALISATION_ROBOTLOCALISATION_HPP
-#define MODULE_LOCALISATION_ROBOTLOCALISATION_HPP
+#ifndef MODULE_LOCALISATION_FIELDLOCALISATION_HPP
+#define MODULE_LOCALISATION_FIELDLOCALISATION_HPP
 
 #include <nuclear>
 
@@ -20,43 +20,46 @@ namespace module::localisation {
 
     // Particle struct
     struct Particle {
-        Eigen::Matrix<double, 3, 1> state =
-            Eigen::Matrix<double, 3, 1>::Zero();  // (x, y, theta) of world space in field space
-        double weight = 1.0;
+        Eigen::Vector3f state = Eigen::Vector3f::Zero();  // (x, y, theta) of world space in field space
+        float weight          = 1.0;
     };
 
-    class RobotLocalisation : public NUClear::Reactor {
+    class FieldLocalisation : public NUClear::Reactor {
     private:
         /// @brief Stores configuration values
         struct Config {
             /// @brief Size of the grid cells in the occupancy grid [m]
-            double grid_size = 0.0;
+            float grid_size = 0.0;
             /// @brief Number of particles to use in the particle filter
             int n_particles = 0;
             /// @brief Uncertainty in the process model
-            Eigen::Matrix<double, 3, 3> process_noise = Eigen::Matrix<double, 3, 3>::Zero();
+            Eigen::Matrix3f process_noise = Eigen::Matrix3f::Zero();
             /// @brief Uncertainty in the measurement model
-            double measurement_noise = 0;
+            float measurement_noise = 0;
             /// @brief Maximum distance a field line can be from a particle to be considered an observation [m]
-            double max_range = 0;
+            float max_range = 0;
             /// @brief Initial state (x,y,theta) of the robot, saved for resetting
-            std::vector<Eigen::Matrix<double, 3, 1>> initial_state{};
+            std::vector<Eigen::Vector3f> initial_state{};
             /// @brief Initial covariance matrix of the robot's state, saved for resetting
-            Eigen::Matrix<double, 3, 3> initial_covariance = Eigen::Matrix<double, 3, 3>::Identity();
+            Eigen::Matrix3f initial_covariance = Eigen::Matrix3f::Identity();
             /// @brief Bool to enable/disable saving the generated map as a csv file
             bool save_map = false;
+            /// @brief Minimum number of field line points for a measurement update
+            size_t min_observations = 0;
+            /// @brief Penalty factor for observations being outside map
+            float outside_map_penalty_factor = 0.0;
         } cfg;
 
         NUClear::clock::time_point last_time_update_time;
 
         /// @brief Occupancy grid map of the field lines
-        OccupancyMap fieldline_map;
+        OccupancyMap<float> fieldline_map;
 
         /// @brief State (x,y,theta) of the robot
-        Eigen::Matrix<double, 3, 1> state = Eigen::Matrix<double, 3, 1>::Zero();
+        Eigen::Vector3f state = Eigen::Vector3f::Zero();
 
         /// @brief Covariance matrix of the robot's state
-        Eigen::Matrix<double, 3, 3> covariance = Eigen::Matrix<double, 3, 3>::Identity();
+        Eigen::Matrix3f covariance = Eigen::Matrix3f::Identity();
 
         /// @brief Status of if the robot is falling
         bool falling = false;
@@ -66,36 +69,28 @@ namespace module::localisation {
 
 
     public:
-        /// @brief Called by the powerplant to build and setup the RobotLocalisation reactor.
-        explicit RobotLocalisation(std::unique_ptr<NUClear::Environment> environment);
-
-        /// @brief Converts a unit vector of point from the camera in robot space to a (x,y) point relative to the robot
-        /// on the field plane
-        /// @param uPCw unit vector of point from the camera in world space
-        /// @param Hcw the world from camera transform
-        /// @return the field point measurement (x,y) relative to the robot
-        Eigen::Vector2d ray_to_field_plane(Eigen::Vector3d uPCw, Eigen::Isometry3d Hcw);
+        /// @brief Called by the powerplant to build and setup the FieldLocalisation reactor.
+        explicit FieldLocalisation(std::unique_ptr<NUClear::Environment> environment);
 
         /// @brief Transform a point in the robot's coordinate frame into an index in the map
         /// @param particle The state of the particle (x,y,theta)
         /// @param rPRw The field point (x, y) in world space {w} [m]
         /// @return The observation location (x, y) in the map
-        Eigen::Vector2i position_in_map(const Eigen::Matrix<double, 3, 1> particle, const Eigen::Vector2d rPRw);
+        Eigen::Vector2i position_in_map(const Eigen::Vector3f particle, const Eigen::Vector3f rPRw);
 
         /// @brief Get the weight of a particle given a set of observations
         /// @param particle The state of the particle (x,y,theta)
         /// @param observations The observations (x, y) in the robot's coordinate frame [m]
         /// @return The weight of the particle
-        double calculate_weight(const Eigen::Matrix<double, 3, 1> particle,
-                                const std::vector<Eigen::Vector2d>& observations);
+        float calculate_weight(const Eigen::Vector3f particle, const std::vector<Eigen::Vector3f>& observations);
 
         /// @brief Get the current mean (state) of the robot
         // @return The current mean (state) of the robot
-        Eigen::Matrix<double, 3, 1> compute_mean();
+        Eigen::Vector3f compute_mean();
 
         /// @brief Get the current covariance matrix of the robot's state
         // @return The current covariance matrix of the robot's state
-        Eigen::Matrix<double, 3, 3> compute_covariance();
+        Eigen::Matrix3f compute_covariance();
 
         /// @brief Perform a time update on the particles
         /// @param walk_command The walk command (dx, dy, dtheta)
@@ -114,4 +109,4 @@ namespace module::localisation {
     };
 }  // namespace module::localisation
 
-#endif  // MODULE_LOCALISATION_ROBOTLOCALISATION_HPP
+#endif  // MODULE_LOCALISATION_FIELDLOCALISATION_HPP
