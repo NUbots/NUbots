684f34464e9f61335446980f8097cda2a9f7298d
Brendan Annable <brendan@annable.id.au>
2023-06-18 19:01:52 +1000

    .

--- nusight2/src/client/components/localisation/view_model.tsx
+++ nusight2/src/client/components/localisation/view_model.tsx
@@ -9,7 +9,6 @@ import { Object3D } from 'three'
 import { Vector3 } from '../../../shared/math/vector3'
 import { meshBasicMaterial } from '../three/builders'
 import { circleBufferGeometry } from '../three/builders'
-import { scene } from '../three/builders'
 import { PerspectiveCamera } from '../three/three_fiber'
 
 import { FieldView } from './field/view'
@@ -17,62 +16,36 @@ import { LocalisationModel } from './model'
 import { NUgusViewModel } from './nugus_robot/view_model'
 import { SkyboxViewModel } from './skybox/view_model'
 
-@observer
-export class LocalisationViewModel extends React.Component<{
-  model: LocalisationModel
-}> {
-  render() {
-    return <object3D>
-      <PerspectiveCamera
-        args={[75, 1, 0.01, 100]}
-        position={this.model.camera.position.toArray()}
-        rotation={[Math.PI / 2 + this.model.camera.pitch, 0, -Math.PI / 2 + this.model.camera.yaw, 'ZXY']}
-        up={[0, 0, 1]}
-      >
-        <pointLight color="white"/>
-      </PerspectiveCamera>
-      <FieldView model={this.model.field}/>
-      <SkyboxViewModel model={this.model.skybox}/>
-      <hemisphereLight args={['#fff', '#fff', 0.6]}/>
+export const LocalisationViewModel = observer(({ model }: { model: LocalisationModel }) => {
+  return <object3D>
+    <PerspectiveCamera
+      args={[75, 1, 0.01, 100]}
+      position={model.camera.position.toArray()}
+      rotation={[Math.PI / 2 + model.camera.pitch, 0, -Math.PI / 2 + model.camera.yaw, 'ZXY']}
+      up={[0, 0, 1]}
+    >
+      <pointLight color="white"/>
+    </PerspectiveCamera>
+    <FieldView model={model.field}/>
+    <SkyboxViewModel model={model.skybox}/>
+    <hemisphereLight args={['#fff', '#fff', 0.6]}/>
+    {model.robots.map((robotModel) => {
+      return robotModel.visible && <NUgusViewModel key={robotModel.id} model={robotModel}/>
+    })}
+    <FieldLineDots model={model}/>
+  </object3D>
+})
 
-      {this.model.robots.map((robotModel) => {
-        return robotModel.visible && <NUgusViewModel key={robotModel.id} model={robotModel} />
-      })}
-    </object3D>
-  }
-
-  private get model() {
-    return this.props.model
-  }
-
-  private readonly scene = scene(() => ({
-    children: [...this.robots, this.fieldLineDots],
-  }))
-
-  @computed
-  private get robots(): Object3D[] {
-    return this.model.robots
-      .filter((robotModel) => robotModel.visible)
-      .map((robotModel) => NUgusViewModel.of(robotModel).robot)
-  }
-
-  @computed
-  private get fieldLineDots() {
-    const group = new Object3D()
-    this.model.robots.forEach((robot) =>
-      robot.fieldLinesDots.rPWw.forEach((d) => {
-        const mesh = new Mesh(
-          LocalisationViewModel.fieldLineDotGeometry(),
-          LocalisationViewModel.fieldLineDotMaterial(),
-        )
-        mesh.position.copy(d.add(new Vector3(0, 0, 0.005)).toThree())
-        group.add(mesh)
+const FieldLineDots = ({ model }: { model: LocalisationModel }) => (
+  <object3D>
+    {model.robots.flatMap((robot) =>
+      robot.fieldLinesDots.rPWw.map((d, i) => {
+        return <mesh key={`${robot.id}-${i}`} position={d.add(new Vector3(0, 0, 0.005)).toArray()}>
+          <circleBufferGeometry args={[0.02, 20]}/>
+          <meshBasicMaterial color="blue"/>
+        </mesh>
       }),
     )
-    return group
-  }
-
-  private static readonly fieldLineDotGeometry = circleBufferGeometry(() => ({ radius: 0.02, segments: 20 }))
-
-  private static readonly fieldLineDotMaterial = meshBasicMaterial(() => ({ color: new Color('blue') }))
-}
+    }
+  </object3D>
+)
