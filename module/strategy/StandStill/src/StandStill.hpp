18596756653757cdf580b49c5caed43d7d469407
Ysobel <ysobelsims@gmail.com>
2023-06-20 15:07:18 +1000

    remove unused var

--- module/strategy/StandStill/src/StandStill.hpp
+++ module/strategy/StandStill/src/StandStill.hpp
@@ -9,12 +9,6 @@ namespace module::strategy {
 
     class StandStill : public ::extension::behaviour::BehaviourReactor {
 
-    private:
-        /// @brief Whether we are currently standing
-        /// This is used to prevent the robot continually calling the stand script, even when it is already in the
-        /// standing position
-        bool standing = false;
-
     public:
         /// @brief Called by the powerplant to build and setup the StandStill reactor.
         explicit StandStill(std::unique_ptr<NUClear::Environment> environment);
