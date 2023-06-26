3ba9a5284e8e1c4e7db1bffa28f6a01894bd39f9
Ysobel Sims <35280100+ysims@users.noreply.github.com>
2023-06-26 11:31:08 +1000

    Separate model and packet watchdogs to prevent dropped packets stopping the robot's motions (#1151)
    Co-authored-by: dex-k <dexter.konijn@gmail.com>

--- module/platform/OpenCR/HardwareIO/src/HardwareIO.hpp
+++ module/platform/OpenCR/HardwareIO/src/HardwareIO.hpp
@@ -214,6 +214,15 @@ namespace module::platform::OpenCR {
         /// @brief clear all packet queues
         /// @returns the number of packets cleared
         int queue_clear_all();
+
+        struct PacketWatchdog {};
+        struct ModelWatchdog {};
+
+        /// @brief Handle for the watchdog timer for the model information
+        ReactionHandle model_watchdog;
+
+        /// @brief Handle for our watchdog timer for packet handling
+        ReactionHandle packet_watchdog;
     };
 
 }  // namespace module::platform::OpenCR
