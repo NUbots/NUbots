08558ea90fb8fa590d270b18493ec3cf362076bb
tom <thomas16441@gmail.com>
2023-06-28 13:13:24 +1000

    Implement

--- module/purpose/Tester/src/Tester.cpp
+++ module/purpose/Tester/src/Tester.cpp
@@ -1,17 +1,95 @@
 #include "Tester.hpp"
 
+#include "extension/Behaviour.hpp"
 #include "extension/Configuration.hpp"
+
+#include "message/planning/KickTo.hpp"
+#include "message/planning/LookAround.hpp"
+#include "message/strategy/AlignBallToGoal.hpp"
+#include "message/strategy/FindFeature.hpp"
+#include "message/strategy/KickToGoal.hpp"
+#include "message/strategy/LookAtFeature.hpp"
+#include "message/strategy/Ready.hpp"
+#include "message/strategy/StandStill.hpp"
+#include "message/strategy/WalkToBall.hpp"
+#include "message/strategy/WalkToFieldPosition.hpp"
+
+#include "utility/support/yaml_expression.hpp"
 
 namespace module::purpose {
 
-using extension::Configuration;
+    using extension::Configuration;
 
-Tester::Tester(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+    using message::strategy::AlignBallToGoal;
+    using message::strategy::FindBall;
+    using message::strategy::KickToGoal;
+    using message::strategy::LookAtBall;
+    using message::strategy::Ready;
+    using message::strategy::StandStill;
+    using message::strategy::WalkToBall;
+    using message::strategy::WalkToFieldPosition;
 
-    on<Configuration>("Tester.yaml").then([this](const Configuration& config) {
-        // Use configuration here from file Tester.yaml
-        this->log_level = config["log_level"].as<NUClear::LogLevel>();
-    });
-}
+    using message::planning::KickTo;
+    using message::planning::LookAround;
+
+    using utility::support::Expression;
+
+    Tester::Tester(std::unique_ptr<NUClear::Environment> environment) : BehaviourReactor(std::move(environment)) {
+
+        on<Configuration>("Tester.yaml").then([this](const Configuration& config) {
+            // Use configuration here from file Tester.yaml
+            this->log_level                     = config["log_level"].as<NUClear::LogLevel>();
+            cfg.find_ball                       = config["tasks"]["find_ball"].as<bool>();
+            cfg.find_ball_priority              = config["tasks"]["find_ball_priority"].as<int>();
+            cfg.look_at_ball                    = config["tasks"]["look_at_ball"].as<bool>();
+            cfg.look_at_ball_priority           = config["tasks"]["look_at_ball_priority"].as<int>();
+            cfg.walk_to_ball                    = config["tasks"]["walk_to_ball"].as<bool>();
+            cfg.walk_to_ball_priority           = config["tasks"]["walk_to_ball_priority"].as<int>();
+            cfg.align_ball_to_goal              = config["tasks"]["align_ball_to_goal"].as<bool>();
+            cfg.align_ball_to_goal_priority     = config["tasks"]["align_ball_to_goal_priority"].as<int>();
+            cfg.kick_to_goal                    = config["tasks"]["kick_to_goal"].as<bool>();
+            cfg.kick_to_goal_priority           = config["tasks"]["kick_to_goal_priority"].as<int>();
+            cfg.walk_to_field_position          = config["tasks"]["walk_to_field_position"].as<bool>();
+            cfg.walk_to_field_position_priority = config["tasks"]["walk_to_field_position_priority"].as<int>();
+            cfg.kick_to                         = config["tasks"]["kick_to"].as<bool>();
+            cfg.kick_to_priority                = config["tasks"]["kick_to_priority"].as<int>();
+            cfg.look_around                     = config["tasks"]["look_around"].as<bool>();
+            cfg.look_around_priority            = config["tasks"]["look_around_priority"].as<int>();
+            cfg.walk_to_field_position_position = config["walk_to_field_position_position"].as<Expression>();
+        });
+
+        on<Startup>().then([this] {
+            // Emit all the tasks
+            if (cfg.find_ball) {
+                emit<Task>(std::make_unique<FindBall>(), cfg.find_ball_priority);
+            }
+            if (cfg.look_at_ball) {
+                emit<Task>(std::make_unique<LookAtBall>(), cfg.look_at_ball_priority);
+            }
+            if (cfg.walk_to_ball) {
+                emit<Task>(std::make_unique<WalkToBall>(), cfg.walk_to_ball_priority);
+            }
+            if (cfg.align_ball_to_goal) {
+                emit<Task>(std::make_unique<AlignBallToGoal>(), cfg.align_ball_to_goal_priority);
+            }
+            if (cfg.kick_to_goal) {
+                emit<Task>(std::make_unique<KickToGoal>(), cfg.kick_to_goal_priority);
+            }
+            if (cfg.walk_to_field_position) {
+                emit<Task>(
+                    std::make_unique<WalkToFieldPosition>(Eigen::Vector3f(cfg.walk_to_field_position_position.x(),
+                                                                          cfg.walk_to_field_position_position.y(),
+                                                                          0),
+                                                          cfg.walk_to_field_position_position.z()),
+                    cfg.walk_to_field_position_priority);
+            }
+            if (cfg.kick_to) {
+                emit<Task>(std::make_unique<KickTo>(), cfg.kick_to_priority);
+            }
+            if (cfg.look_around) {
+                emit<Task>(std::make_unique<LookAround>(), cfg.look_around_priority);
+            }
+        });
+    }
 
 }  // namespace module::purpose
--- module/purpose/Tester/src/Tester.hpp
+++ module/purpose/Tester/src/Tester.hpp
@@ -1,20 +1,58 @@
 #ifndef MODULE_PURPOSE_TESTER_HPP
 #define MODULE_PURPOSE_TESTER_HPP
 
+#include <Eigen/Core>
 #include <nuclear>
+
+#include "extension/Behaviour.hpp"
 
 namespace module::purpose {
 
-class Tester : public NUClear::Reactor {
-private:
-    /// @brief Stores configuration values
-    struct Config {
-    } cfg;
+    class Tester : public ::extension::behaviour::BehaviourReactor {
+    private:
+        /// @brief Stores configuration values
+        struct Config {
+            /// @brief Whether to emit FindBall task
+            bool find_ball = false;
+            /// @brief Priority of FindBall task
+            int find_ball_priority = 1;
+            /// @brief Whether to emit LookAtBall task
+            bool look_at_ball = false;
+            /// @brief Priority of LookAtBall task
+            int look_at_ball_priority = 1;
+            /// @brief Whether to emit StandStill task
+            bool walk_to_ball = false;
+            /// @brief Priority of StandStill task
+            int walk_to_ball_priority = 1;
+            /// @brief Whether to emit AlignBallToGoal task
+            bool align_ball_to_goal = false;
+            /// @brief Priority of AlignBallToGoal task
+            int align_ball_to_goal_priority = 1;
+            /// @brief Whether to emit KickToGoal task
+            bool kick_to_goal = false;
+            /// @brief Priority of KickToGoal task
+            int kick_to_goal_priority = 1;
+            /// @brief Whether to emit WalkToFieldPosition task
+            bool walk_to_field_position = false;
+            /// @brief Priority of WalkToFieldPosition task
+            int walk_to_field_position_priority = 1;
+            /// @brief Whether to emit KickTo task
+            bool kick_to = false;
+            /// @brief Priority of KickTo task
+            int kick_to_priority = 1;
+            /// @brief Whether to emit LookAround task
+            bool look_around = false;
+            /// @brief Priority of LookAround task
+            int look_around_priority = 1;
 
-public:
-    /// @brief Called by the powerplant to build and setup the Tester reactor.
-    explicit Tester(std::unique_ptr<NUClear::Environment> environment);
-};
+            /// @brief Position to walk to when emitting WalkToFieldPosition task
+            Eigen::Vector3f walk_to_field_position_position = Eigen::Vector3f::Zero();
+        } cfg;
+
+    public:
+        /// @brief Called by the powerplant to build and setup the Tester reactor.
+        explicit Tester(std::unique_ptr<NUClear::Environment> environment);
+    };
 
 }  // namespace module::purpose
 
