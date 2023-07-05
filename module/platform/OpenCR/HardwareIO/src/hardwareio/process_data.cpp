a2efd44dec9cbe310e6151675d28131ab763bcf6
jpptm <99300632+jpptm@users.noreply.github.com>
2023-07-05 20:08:43 +1000

    Add buzzer functionality for the OpenCR board (#1180)
    Co-authored-by: Josephus Paye II <j.paye96@gmail.com>

--- module/platform/OpenCR/HardwareIO/src/hardwareio/process_data.cpp
+++ module/platform/OpenCR/HardwareIO/src/hardwareio/process_data.cpp
@@ -4,11 +4,13 @@
 #include "HardwareIO.hpp"
 
 #include "utility/math/comparison.hpp"
+#include "message/output/Buzzer.hpp"
 
 namespace module::platform::OpenCR {
 
     using message::platform::RawSensors;
     using message::platform::StatusReturn;
+    using message::output::Buzzer;
 
     /*
         Process the status return packet data
@@ -127,6 +129,13 @@ namespace module::platform::OpenCR {
             convert::position(servo_index, data.present_position, nugus.servo_direction, nugus.servo_offset);
         servo_states[servo_index].voltage     = convert::voltage(data.present_voltage);
         servo_states[servo_index].temperature = convert::temperature(data.present_temperature);
+
+        for(const auto& servo : servo_states ) {
+            if (servo.temperature > cfg.max_tol_temp){
+                emit(std::make_unique<Buzzer>());
+                break;
+            }
+        }
 
         // If this servo has not been initialised yet, set the goal states to the current states
         if (!servo_states[servo_index].initialised) {
