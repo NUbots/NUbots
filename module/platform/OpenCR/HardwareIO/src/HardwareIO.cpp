722bc041f521534e0b0e55c3097f74dd0be64b02
jpptm <99300632+jpptm@users.noreply.github.com>
2023-07-19 09:01:54 +1000

    Add the ability to control the Buzzer on OpenCR and activate it on servo temperature warnings (#1212)

--- module/platform/OpenCR/HardwareIO/src/HardwareIO.cpp
+++ module/platform/OpenCR/HardwareIO/src/HardwareIO.cpp
@@ -8,6 +8,7 @@
 #include "extension/Configuration.hpp"
 
 #include "message/actuation/ServoTarget.hpp"
+#include "message/output/Buzzer.hpp"
 
 #include "utility/math/angle.hpp"
 #include "utility/math/comparison.hpp"
@@ -22,7 +23,7 @@ namespace module::platform::OpenCR {
     using message::platform::StatusReturn;
     using utility::support::Expression;
 
-    using message::platform::ButtonMiddleDown;
+    using message::output::Buzzer;
 
     HardwareIO::HardwareIO(std::unique_ptr<NUClear::Environment> environment)
         : Reactor(std::move(environment)), opencr(), nugus(), byte_wait(0), packet_wait(0), packet_queue() {
@@ -105,6 +106,9 @@ namespace module::platform::OpenCR {
                 nugus.servo_direction[i]  = config["servos"][i]["direction"].as<Expression>();
                 servo_states[i].simulated = config["servos"][i]["simulated"].as<bool>();
             }
+
+            cfg.alarms.temperature.level            = config["alarms"]["temperature"]["level"].as<float>();
+            cfg.alarms.temperature.buzzer_frequency = config["alarms"]["temperature"]["buzzer_frequency"].as<float>();
         });
 
         on<Startup>().then("HardwareIO Startup", [this] {
@@ -286,6 +290,11 @@ namespace module::platform::OpenCR {
             opencr_state.led_panel.led3 = led.led3;
             opencr_state.led_panel.led4 = led.led4;
             opencr_state.dirty          = true;
+        });
+
+        on<Trigger<Buzzer>>().then([this](const Buzzer& buzzer_msg) {
+            // Fill the necessary field within the opencr_state struct
+            opencr_state.buzzer = buzzer_msg.frequency;
         });
     }
 
