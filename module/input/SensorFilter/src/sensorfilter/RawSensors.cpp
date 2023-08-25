5860fac99fb4f2784566311f00161dfc434f71ff
dex <dexter.konijn@gmail.com>
2023-08-25 14:22:22 +1000

    Fix gyro warnings

--- module/input/SensorFilter/src/sensorfilter/RawSensors.cpp
+++ module/input/SensorFilter/src/sensorfilter/RawSensors.cpp
@@ -85,23 +85,22 @@ namespace module::input {
         // accelerometer
         if ((raw_sensors.subcontroller_error != RawSensors::PacketError::PACKET_OK) && previous_sensors) {
             sensors->accelerometer = previous_sensors->accelerometer;
+            sensors->gyroscope     = previous_sensors->gyroscope;
         }
         else {
             sensors->accelerometer = raw_sensors.accelerometer.cast<double>();
+            sensors->gyroscope     = raw_sensors.gyroscope.cast<double>();
         }
 
         // If we have a previous Sensors message AND (our platform has errors OR the gyro is spinning too fast) then
         // reuse our last sensor value of the gyroscope. Note: One of the gyros would occasionally
         // throw massive numbers without an error flag and if our hardware is working as intended, it should never
         // read that we're spinning at 2 revs/s
-        if (previous_sensors
-            && ((raw_sensors.subcontroller_error != RawSensors::PacketError::PACKET_OK)
-                || raw_sensors.gyroscope.norm() > 4.0 * M_PI)) {
+        if (raw_sensors.gyroscope.norm() > 4.0 * M_PI) {
             NUClear::log<NUClear::WARN>("Bad gyroscope value", raw_sensors.gyroscope.norm());
-            sensors->gyroscope = previous_sensors->gyroscope;
-        }
-        else {
-            sensors->gyroscope = raw_sensors.gyroscope.cast<double>();
+            if (previous_sensors) {
+                sensors->gyroscope = previous_sensors->gyroscope;
+            }
         }
 
         // **************** Timestamp ****************
