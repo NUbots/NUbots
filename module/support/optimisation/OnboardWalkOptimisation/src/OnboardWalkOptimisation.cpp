63b7cddb546c56904292b85bc9b669f0176f080d
AnExParrot <c3356128@uon.edu.au>
2023-08-31 17:09:58 +1000

    Fixed stability check

--- module/support/optimisation/OnboardWalkOptimisation/src/OnboardWalkOptimisation.cpp
+++ module/support/optimisation/OnboardWalkOptimisation/src/OnboardWalkOptimisation.cpp
@@ -1,81 +1,88 @@
 #include "OnboardWalkOptimisation.hpp"
 
 #include "extension/Configuration.hpp"
-#include "message/support/optimisation/OptimisationResetDone.hpp"
-#include "message/support/optimisation/OptimisationTimeUpdate.hpp"
+
+#include "message/input/Sensors.hpp"
 #include "message/platform/RawSensors.hpp"
 #include "message/platform/webots/messages.hpp"
-#include "message/input/Sensors.hpp"
 #include "message/support/optimisation/NSGA2Evaluator.hpp"
+#include "message/support/optimisation/OptimisationResetDone.hpp"
+#include "message/support/optimisation/OptimisationTimeUpdate.hpp"
 
 namespace module::support::optimisation {
 
-using extension::Configuration;
-    using message::support::optimisation::OptimisationResetDone;
-    using message::support::optimisation::OptimisationTimeUpdate;
-    using message::support::optimisation::NSGA2Evaluating;
+    using extension::Configuration;
+    using message::input::Sensors;
+    using message::platform::ButtonLeftDown;
+    using message::platform::ButtonMiddleDown;
     using message::platform::RawSensors;
     using message::platform::webots::OptimisationCommand;
-    using message::input::Sensors;
-    using message::platform::ButtonMiddleDown;
-    using message::platform::ButtonLeftDown;
+    using message::support::optimisation::NSGA2Evaluating;
+    using message::support::optimisation::OptimisationResetDone;
+    using message::support::optimisation::OptimisationTimeUpdate;
 
-OnboardWalkOptimisation::OnboardWalkOptimisation(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+    OnboardWalkOptimisation::OnboardWalkOptimisation(std::unique_ptr<NUClear::Environment> environment)
+        : Reactor(std::move(environment)) {
 
-    on<Configuration>("OnboardWalkOptimisation.yaml").then([this](const Configuration& config) {
-        // Use configuration here from file OnboardWalkOptimisation.yaml
-        this->log_level = config["log_level"].as<NUClear::LogLevel>();
+        on<Configuration>("OnboardWalkOptimisation.yaml").then([this](const Configuration& config) {
+            // Use configuration here from file OnboardWalkOptimisation.yaml
+            this->log_level = config["log_level"].as<NUClear::LogLevel>();
 
-        cfg.standing_angle = config["standing_angle"].as<float>();
+            cfg.standing_angle = config["standing_angle"].as<float>();
 
-        cfg.wait_time = config["wait_time"].as<float>();
-    });
+            cfg.wait_time = config["wait_time"].as<float>();
+        });
 
-    on<Trigger<OptimisationCommand>>().then([this](const OptimisationCommand& msg) {
-        if (msg.command == OptimisationCommand::CommandType::RESET_WORLD) {
-            NUClear::log<NUClear::DEBUG>("Onboard Resetting!");
-            resetting = true;
-        }
-    });
+        on<Trigger<OptimisationCommand>>().then([this](const OptimisationCommand& msg) {
+            if (msg.command == OptimisationCommand::CommandType::RESET_WORLD) {
+                NUClear::log<NUClear::DEBUG>("Onboard Resetting!");
+                resetting = true;
+            }
+        });
 
-    on<Trigger<Sensors>>().then("Stability Reset Check", [this](const Sensors& sensors) {
-        // Length of time from first standing upright
-        const auto time_waited =
-            std::chrono::duration_cast<std::chrono::duration<float>>(NUClear::clock::now() - start_time).count();
-        // Trigger the next individual after standing for the config value cfg.wait_time
-        if (resetting && is_upright && (time_waited > cfg.wait_time)) {
-            NUClear::log<NUClear::DEBUG>("Stood upright for {} seconds. Reset started.", cfg.wait_time);
-            resetting = false;
-            emit(std::make_unique<OptimisationResetDone>());
-        }
+        on<Trigger<Sensors>>().then("Stability Reset Check", [this](const Sensors& sensors) {
+            if (resetting) {
+                // Length of time from first standing upright
+                const auto time_waited =
+                    std::chrono::duration_cast<std::chrono::duration<float>>(NUClear::clock::now() - start_time)
+                        .count();
+                // Trigger the next individual after standing for the config value cfg.wait_time
+                if (is_upright && (time_waited > cfg.wait_time)) {
+                    NUClear::log<NUClear::DEBUG>(
+                        fmt::format("Stood upright for {:.1f} seconds. Reset done.", time_waited));
+                    resetting  = false;
+                    is_upright = false;
+                    emit(std::make_unique<OptimisationResetDone>());
+                }
 
-        // Transform to torso {t} from world {w} space
-        Eigen::Matrix4d Hwt = sensors.Htw.inverse().matrix();
-        // Basis Z vector of torso {t} in world {w} space
-        Eigen::Vector3d uZTw = Hwt.block(0, 2, 3, 1);
+                // Transform to torso {t} from world {w} space
+                Eigen::Matrix4d Hwt = sensors.Htw.inverse().matrix();
+                // Basis Z vector of torso {t} in world {w} space
+                Eigen::Vector3d uZTw = Hwt.block(0, 2, 3, 1);
 
-        // Check if angle between torso z axis and world z axis is greater than config value cfg.fallen_angle
-        if (resetting && !is_upright && std::acos(Eigen::Vector3d::UnitZ().dot(uZTw)) < cfg.standing_angle) {
-            is_upright = true;
-            // Start timer
-            start_time = NUClear::clock::now();
-            NUClear::log<NUClear::DEBUG>("Starting to stand upright at time", start_time.time_since_epoch());
-        }
-        else if (resetting) {
-            is_upright = false;
-        }
-    });
+                // Check if angle between torso z axis and world z axis is greater than config value cfg.fallen_angle
+                if (!is_upright && std::acos(Eigen::Vector3d::UnitZ().dot(uZTw)) < cfg.standing_angle) {
+                    is_upright = true;
+                    // Start timer
+                    start_time = NUClear::clock::now();
+                    NUClear::log<NUClear::DEBUG>(fmt::format("Standing for {} seconds.", cfg.wait_time));
+                }
+                else if (is_upright && !(std::acos(Eigen::Vector3d::UnitZ().dot(uZTw)) < cfg.standing_angle)) {
+                    is_upright = false;
+                    NUClear::log<NUClear::DEBUG>("No longer standing.");
+                }
+            }
+        });
 
-    // Reset for next run or terminate when paused
-    on<Trigger<ButtonLeftDown>, Single>().then([this] {
-        NUClear::log<NUClear::DEBUG>("Left Button Pressed");
-        if (false) {
-            emit(std::make_unique<OptimisationResetDone>());
-        }
-    });
+        // Reset for next run or terminate when paused
+        on<Trigger<ButtonLeftDown>, Single>().then([this] {
+            NUClear::log<NUClear::DEBUG>("Left Button Pressed");
+            if (false) {
+                emit(std::make_unique<OptimisationResetDone>());
+            }
+        });
 
-    // Pause optimisation
-    on<Trigger<ButtonMiddleDown>, Single>().then([this] {
-    });
-}
+        // Pause optimisation
+        on<Trigger<ButtonMiddleDown>, Single>().then([this] {});
+    }
 }  // namespace module::support::optimisation
