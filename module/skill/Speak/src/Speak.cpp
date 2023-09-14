923b626bf950d32646c2f59bef454e3f3162a560
tom <thomas16441@gmail.com>
2023-09-14 21:36:10 +1000

    Move calling of command line tool to seperate thread

--- module/skill/Speak/src/Speak.cpp
+++ module/skill/Speak/src/Speak.cpp
@@ -2,14 +2,20 @@
 
 #include <cstdlib>
 #include <string>
+#include <thread>
 
 #include "extension/Configuration.hpp"
 
+#include "message/actuation/Limbs.hpp"
 #include "message/skill/Speak.hpp"
+
+#include "utility/skill/Script.hpp"
 
 namespace module::skill {
 
     using extension::Configuration;
+    using message::actuation::LimbsSequence;
+    using utility::skill::load_script;
     using SpeakTask = message::skill::Speak;
 
     Speak::Speak(std::unique_ptr<NUClear::Environment> environment) : BehaviourReactor(std::move(environment)) {
@@ -20,9 +26,15 @@ namespace module::skill {
             emit<Task>(std::make_unique<SpeakTask>(std::string("Speaking is enabled.")));
         });
 
-        on<Provide<SpeakTask>>().then([this](const SpeakTask& speak) {
-            // Play the requested audio using python command-line tool mimic3 and aplay
-            system(std::string("mimic3 '" + speak.text + "' | aplay").c_str());
+        on<Provide<SpeakTask>>().then([this](const SpeakTask& speak, const RunInfo& info) {
+            // If it is a new task, then speak
+            if (info.run_reason == RunInfo::NEW_TASK) {
+                // Play the requested audio using python command-line tool mimic3 and aplay
+                std::thread([speak]() { system(std::string("mimic3 '" + speak.text + "' | aplay").c_str()); }).detach();
+            }
+
+            // Nod head to indicate that the robot is "talking"
+            emit<Task>(load_script<LimbsSequence>("NodYes.yaml"));
         });
     }
 
