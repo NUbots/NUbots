c95cff8a3ca23d7b0bca509fa2da2085089e222e
Crafty15 <crafty0410@gmail.com>
2023-09-17 21:02:35 +1000

    normalise script WIP

---
+++ rnn-odometry/tf-normalise.py
@@ -0,0 +1,20 @@
+import numpy as np
+import tensorflow as tf
+
+# list of numpy arrays - NOTE: Change to loop through a directory given as an argument
+arrays = [np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([7, 8, 9])]
+
+# Convert the list of arrays to tensors
+tensors = [tf.convert_to_tensor(array, dtype=tf.float32) for array in arrays]
+
+# Concatenate all tensors into one
+concatenated = tf.concat(tensors, axis=0)
+
+# Calculate the min and max of the concatenated tensor
+min_val = tf.reduce_min(concatenated)
+max_val = tf.reduce_max(concatenated)
+
+# Normalize each tensor
+normalized_tensors = [(tensor - min_val) / (max_val - min_val) for tensor in tensors]
+
+print(normalized_tensors)
