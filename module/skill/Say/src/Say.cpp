50c999c73f7cd5f602aff2b44ae994a3a6232cae
tom <thomas16441@gmail.com>
2023-10-19 20:34:52 +1100

    Add say skill

---
+++ module/skill/Say/src/Say.cpp
@@ -0,0 +1,42 @@
+#include "Say.hpp"
+
+#include <cstdlib>
+#include <string>
+#include <thread>
+
+#include "extension/Configuration.hpp"
+
+#include "message/actuation/Limbs.hpp"
+#include "message/skill/Say.hpp"
+
+#include "utility/skill/Script.hpp"
+
+namespace module::skill {
+
+    using extension::Configuration;
+    using message::actuation::LimbsSequence;
+    using utility::skill::load_script;
+    using SayTask = message::skill::Say;
+
+    Say::Say(std::unique_ptr<NUClear::Environment> environment) : BehaviourReactor(std::move(environment)) {
+
+        on<Configuration>("Say.yaml").then([this](const Configuration& config) {
+            // Use configuration here from file Say.yaml
+            this->log_level = config["log_level"].as<NUClear::LogLevel>();
+        });
+
+        on<Provide<SayTask>>().then([this](const SayTask& say, const RunInfo& info) {
+            // Only say text if it is a new task
+            if (info.run_reason == RunInfo::NEW_TASK) {
+                // Play the requested audio using python command-line tool mimic3 and aplay
+                system(std::string("mimic3 '" + say.text + "' | aplay").c_str());
+            }
+
+            // Nod head to indicate that the robot is "talking"
+            if (say.nod) {
+                emit<Task>(load_script<LimbsSequence>("NodYes.yaml"));
+            }
+        });
+    }
+
+}  // namespace module::skill
---
+++ module/skill/Say/src/Say.hpp
@@ -0,0 +1,24 @@
+#ifndef MODULE_SKILL_SAY_HPP
+#define MODULE_SKILL_SAY_HPP
+
+#include <nuclear>
+
+#include "extension/Behaviour.hpp"
+#include "extension/Configuration.hpp"
+
+namespace module::skill {
+
+    class Say : public ::extension::behaviour::BehaviourReactor {
+    private:
+        /// @brief Stores configuration values
+        struct Config {
+        } cfg;
+
+    public:
+        /// @brief Called by the powerplant to build and setup the Say reactor.
+        explicit Say(std::unique_ptr<NUClear::Environment> environment);
+    };
+
+}  // namespace module::skill
+
+#endif  // MODULE_SKILL_SAY_HPP
