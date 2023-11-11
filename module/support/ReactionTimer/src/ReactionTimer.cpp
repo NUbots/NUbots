81d4f899e5039e26dede24b4d84292318d2520d2
tom <thomas16441@gmail.com>
2023-11-11 12:03:54 +1100

    Add initial code

--- module/support/ReactionTimer/src/ReactionTimer.cpp
+++ module/support/ReactionTimer/src/ReactionTimer.cpp
@@ -2,10 +2,14 @@
 
 #include "extension/Configuration.hpp"
 
+#include "message/support/nuclear/ReactionProfile.hpp"
+
 namespace module::support {
 
     using extension::Configuration;
     using NUClear::message::ReactionStatistics;
+
+    using ReactionProfileMsg = message::support::nuclear::ReactionProfile;
 
     ReactionTimer::ReactionTimer(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
 
@@ -19,6 +23,36 @@ namespace module::support {
                     * (double((stats.finished - stats.started).count())
                        / double(NUClear::clock::duration::period::den)),
                 "ms");
+
+            // Check if we have a profile for this reaction
+            if (reaction_profiles.find(stats.identifier.name) == reaction_profiles.end()) {
+                // Create a new profile
+                reaction_profiles[stats.identifier.name]            = ReactionProfile();
+                reaction_profiles[stats.identifier.name].identifier = stats.identifier.name;
+            }
+
+            // Update the profile
+            reaction_profiles[stats.identifier.name].count++;
+            reaction_profiles[stats.identifier.name].total_time += (stats.finished - stats.started).count();
+            reaction_profiles[stats.identifier.name].avg_time =
+                reaction_profiles[stats.identifier.name].total_time / reaction_profiles[stats.identifier.name].count;
+            reaction_profiles[stats.identifier.name].max_time =
+                std::max(reaction_profiles[stats.identifier.name].max_time, (stats.finished - stats.started).count());
+            reaction_profiles[stats.identifier.name].min_time =
+                std::min(reaction_profiles[stats.identifier.name].min_time, (stats.finished - stats.started).count());
+            reaction_profiles[stats.identifier.name].avg_time =
+                reaction_profiles[stats.identifier.name].total_time / reaction_profiles[stats.identifier.name].count;
+
+
+            // Emit the profile
+            auto profile        = std::make_unique<ReactionProfileMsg>();
+            profile->identifier = stats.identifier;
+            profile->total_time = reaction_profiles[stats.identifier.name].total_time;
+            profile->count      = reaction_profiles[stats.identifier.name].count;
+            profile->max_time   = reaction_profiles[stats.identifier.name].max_time;
+            profile->min_time   = reaction_profiles[stats.identifier.name].min_time;
+            profile->avg_time   = reaction_profiles[stats.identifier.name].avg_time;
+            emit(profile);
         });
     }
 }  // namespace module::support
--- module/support/ReactionTimer/src/ReactionTimer.hpp
+++ module/support/ReactionTimer/src/ReactionTimer.hpp
@@ -5,11 +5,23 @@
 
 namespace module::support {
 
+    struct ReactionProfile {
+        std::string identifier = "";
+        uint64_t total_time    = 0;
+        uint64_t count         = 0;
+        uint64_t max_time      = std::numeric_limits<uint64_t>::min();
+        uint64_t min_time      = std::numeric_limits<uint64_t>::max();
+        uint64_t avg_time      = 0;
+    };
+
     class ReactionTimer : public NUClear::Reactor {
 
     public:
         /// @brief Called by the powerplant to build and setup the ReactionTimer reactor.
         explicit ReactionTimer(std::unique_ptr<NUClear::Environment> environment);
+
+        /// @brief Map to store the reaction profiles
+        std::map<std::string, ReactionProfile> reaction_profiles;
     };
 }  // namespace module::support
 
