1f8b28ddb671e3e7d1a1b4fc77236bc981fe035d
tom <thomas16441@gmail.com>
2023-11-14 16:00:32 +1100

    Formatting and webots role

--- module/onboarding/Judge/src/Judge.cpp
+++ module/onboarding/Judge/src/Judge.cpp
@@ -2,36 +2,35 @@
 
 #include "extension/Configuration.hpp"
 
-#include "message/onboarding/Judge.hpp"
 #include "message/actuation/Limbs.hpp"
+#include "message/onboarding/Judge.hpp"
 
 #include "utility/skill/Script.hpp"
 
 namespace module::onboarding {
 
-using extension::Configuration;
-using extension::behaviour::Task;
-using message::actuation::LimbsSequence;
-using utility::skill::load_script;
+    using extension::Configuration;
+    using extension::behaviour::Task;
+    using message::actuation::LimbsSequence;
+    using utility::skill::load_script;
 
-Judge::Judge(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
-    using message::onboarding::Judge;
+    Judge::Judge(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+        using message::onboarding::Judge;
 
-    on<Configuration>("Judge.yaml").then([this](const Configuration& config) {
-        // Use configuration here from file Judge.yaml
-        this->log_level = config["log_level"].as<NUClear::LogLevel>();
-        cfg.answer = config["answer"].as<int>();
-    });
+        on<Configuration>("Judge.yaml").then([this](const Configuration& config) {
+            // Use configuration here from file Judge.yaml
+            this->log_level = config["log_level"].as<NUClear::LogLevel>();
+            cfg.answer      = config["answer"].as<int>();
+        });
 
-    on<Trigger<Judge>>().then([this](const Judge& judge_msg){
-        log<NUClear::INFO>(judge_msg.val);
+        on<Trigger<Judge>>().then([this](const Judge& judge_msg) {
+            log<NUClear::INFO>(judge_msg.val);
 
-        if (judge_msg.val == cfg.answer){
-            emit<Task>(load_script<LimbsSequence>("Stand.yaml"));
-            log<NUClear::INFO>("Emit success");
-        }
-    });
-
-}
+            if (judge_msg.val == cfg.answer) {
+                emit<Task>(load_script<LimbsSequence>("KickRight.yaml"));
+                log<NUClear::INFO>("Emit success");
+            }
+        });
+    }
 
 }  // namespace module::onboarding
--- module/onboarding/Pong/src/Pong.cpp
+++ module/onboarding/Pong/src/Pong.cpp
@@ -2,47 +2,47 @@
 
 #include "extension/Configuration.hpp"
 
+#include "message/onboarding/Judge.hpp"
 #include "message/onboarding/Ping.hpp"
 #include "message/onboarding/Pong.hpp"
-#include "message/onboarding/Judge.hpp"
 
 
 namespace module::onboarding {
 
-using extension::Configuration;
+    using extension::Configuration;
 
 
-Pong::Pong(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+    Pong::Pong(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
 
-    using message::onboarding::Ping;
-    using message::onboarding::Pong;
-    using message::onboarding::Judge;
+        using message::onboarding::Judge;
+        using message::onboarding::Ping;
+        using message::onboarding::Pong;
 
-    on<Configuration>("Pong.yaml").then([this](const Configuration& config) {
-        // Use configuration here from file Pong.yaml
-        this->log_level = config["log_level"].as<NUClear::LogLevel>();
-        cfg.max_iter = config["max_iter"].as<int>();
-    });
+        on<Configuration>("Pong.yaml").then([this](const Configuration& config) {
+            // Use configuration here from file Pong.yaml
+            this->log_level = config["log_level"].as<NUClear::LogLevel>();
+            cfg.max_iter    = config["max_iter"].as<int>();
+        });
 
-    on<Trigger<Ping>>().then([this](const Ping& ping_msg) {
-        auto pong_msg = std::make_unique<Pong>();
-        pong_msg -> val = ping_msg.val + ping_msg.iter_count + 1;
+        on<Trigger<Ping>>().then([this](const Ping& ping_msg) {
+            auto pong_msg = std::make_unique<Pong>();
+            pong_msg->val = ping_msg.val + ping_msg.iter_count + 1;
 
-        int curr_iter = ping_msg.iter_count + 1;
-        pong_msg -> iter_count = curr_iter;
+            int curr_iter        = ping_msg.iter_count + 1;
+            pong_msg->iter_count = curr_iter;
 
-        log<NUClear::INFO>(pong_msg->val);
-        log<NUClear::INFO>(pong_msg->iter_count);
+            log<NUClear::INFO>(pong_msg->val);
+            log<NUClear::INFO>(pong_msg->iter_count);
 
-        if (curr_iter >= cfg.max_iter) {
-            auto judge_msg = std::make_unique<Judge>();
-            judge_msg -> val = pong_msg -> val;
-            emit(judge_msg);
-        }
-        else {
-            emit(pong_msg);
-        }
-    });
-}
+            if (curr_iter >= cfg.max_iter) {
+                auto judge_msg = std::make_unique<Judge>();
+                judge_msg->val = pong_msg->val;
+                emit(judge_msg);
+            }
+            else {
+                emit(pong_msg);
+            }
+        });
+    }
 
 }  // namespace module::onboarding
