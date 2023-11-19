f9788c5b77de9848918fd8e50ba1f9f0e041abe2
tom <thomas16441@gmail.com>
2023-11-19 16:50:06 +1100

    Create Mujoco platform

--- module/platform/Mujoco/src/Mujoco.cpp
+++ module/platform/Mujoco/src/Mujoco.cpp
@@ -4,14 +4,25 @@
 
 namespace module::platform {
 
-using extension::Configuration;
+    using extension::Configuration;
 
-Mujoco::Mujoco(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+    Mujoco::Mujoco(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
 
-    on<Configuration>("Mujoco.yaml").then([this](const Configuration& config) {
-        // Use configuration here from file Mujoco.yaml
-        this->log_level = config["log_level"].as<NUClear::LogLevel>();
-    });
-}
+        on<Configuration>("Mujoco.yaml").then([this](const Configuration& config) {
+            // Use configuration here from file Mujoco.yaml
+            this->log_level = config["log_level"].as<NUClear::LogLevel>();
+        });
+
+        on<Startup>().then("Start Mujoco", [this] {
+            // Start Mujoco world
+            log<NUClear::INFO>("Mujoco started");
+        });
+
+
+        on<Trigger<Sensors>>().then("Update Mujoco", [this](const Sensors& sensors) {
+            // TODO: Update Mujoco world with sensor data, object positions, etc.
+            log<NUClear::INFO>("Mujoco updated");
+        });
+    }
 
 }  // namespace module::platform
