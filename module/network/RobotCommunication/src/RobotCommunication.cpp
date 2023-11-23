3faf7c6a44aad649c98c50adc2ddaa5dae3f75f4
nubots <nubots@pop-os.localdomain>
2023-11-23 17:07:11 +1100

    finish outgoing message

--- module/network/RobotCommunication/src/RobotCommunication.cpp
+++ module/network/RobotCommunication/src/RobotCommunication.cpp
@@ -1,4 +1,6 @@
 #include "RobotCommunication.hpp"
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 
 #include "extension/Configuration.hpp"
 #include "message/skill/Kick.hpp"
@@ -62,16 +64,14 @@ namespace module::network {
         // determine way to do this where we don't require a filtered loc_ball trigger (optional triggers, every...)
         on<Every<2, Per<std::chrono::seconds>>, Optional<With<Ball>>, Optional<With<WalkState>>, Optional<With<Kick>>, Optional<With<Sensors>>, Optional<With<Field>>, Optional<With<GameState>>, Optional<With<GlobalConfig>>>()
             .then([this](const std::shared_ptr<const  Ball>& loc_ball,
-                         const std::shared_ptr<const  WalkState>& walk_state,
-                         const std::shared_ptr<const  Kick>& kick,
-                         const std::shared_ptr<const  Sensors>& sensors,
-                         const std::shared_ptr<const  Field>& field,
-                         const std::shared_ptr<const  GameState>& game_state,
-                         const std::shared_ptr<const  GlobalConfig>& config) {
+                        const std::shared_ptr<const  WalkState>& walk_state,
+                        const std::shared_ptr<const  Kick>& kick,
+                        const std::shared_ptr<const  Sensors>& sensors,
+                        const std::shared_ptr<const  Field>& field,
+                        const std::shared_ptr<const  GameState>& game_state,
+                        const std::shared_ptr<const  GlobalConfig>& config) {
 
                 log<NUClear::DEBUG>("Send robocup msg!");
-
-
 
                 // log<NUClear::DEBUG>("game_state->data.self.penalty_reason: ", game_state->data.self.penalty_reason);
                 int penaltyReason = game_state->data.self.penalty_reason;
@@ -79,9 +79,7 @@ namespace module::network {
                 int phase = game_state->data.phase;
                 // log<NUClear::DEBUG>("game_state->data.phase: ", game_state->data.phase);
 
-
                 auto msg = std::make_unique<RoboCup>();
-
 
                 // Timestamp
                 msg->timestamp = NUClear::clock::now();
@@ -97,7 +95,7 @@ namespace module::network {
 
                 Eigen::Isometry3d Hfw;
 
-                // Current pose
+                // Current pose (Position, orientation, and covariance of the player on the field)
                 if (config) {
                     msg->current_pose.player_id = config->player_id;
 
@@ -119,24 +117,33 @@ namespace module::network {
                             msg->current_pose.position.y = rTFf.y();
                             msg->current_pose.position.z = Hft.rotation().matrix().eulerAngles(0, 1, 2).z();
 
+                            Eigen::Matrix<float, 3, 3> covariance = field->covariance.cast<float>();
 
-                            // msg->Robot->covariance = field->covariance;
+                            msg->current_pose.covariance = {
+                                { covariance(0, 0), covariance(1, 0), covariance(2, 0) },
+                                { covariance(0, 1), covariance(1, 1), covariance(2, 1) },
+                                { covariance(0, 2), covariance(1, 2), covariance(2, 2) }
+                            };
+
                             msg->current_pose.team = config->team_id;
+
+                            // Debug to deletePENALTY_REASON
+                            log<NUClear::DEBUG>("current pose z ", msg->current_pose.position.z);
+                            // TODO: maybe test covariance here
                         }
                     }
                 }
 
-                // Walk_state command
+                // Walk command
                 if (walk_state != nullptr) {
-                    msg->walk_command = {static_cast<float>(walk_state->velocity_target.x()), static_cast<float>(walk_state->velocity_target.y()), static_cast<float>(walk_state->velocity_target.z())};
-                    log<NUClear::DEBUG>("walk_state command x", msg->walk_command.x);
-                    log<NUClear::DEBUG>("walk_state command y", msg->walk_command.y);
+                    msg->walk_command = {
+                        static_cast<float>(walk_state->velocity_target.x()),
+                        static_cast<float>(walk_state->velocity_target.y()),
+                        static_cast<float>(walk_state->velocity_target.z())
+                    };
                 }
 
-                // Target pose
-
-
-
+                // TODO: target pose
 
                 // Kick target
                 if (kick) {
@@ -147,12 +154,6 @@ namespace module::network {
                     log<NUClear::DEBUG>("kick_target ", kick_target.x);
                 }
 
-                // Robots
-
-                // where the robot thinks the other robots are
-
-
-
                 // Ball
                 if (loc_ball) {
                     // convert position of ball from world to field space
@@ -160,36 +161,28 @@ namespace module::network {
                     Eigen::Vector3d rBFf = Hfw * rBWw;
 
                     // Store our position from field to ball
-                    // TODO: This compiles but isn't quite the format expected
                     msg->ball.position.x = rBFf.x();
                     msg->ball.position.y = rBFf.y();
                     msg->ball.position.z = rBFf.z();
 
-                    //TODO: convert from a mat4 (Ball) to fmat3 (Robocup)
-                    //TODO: Eigen::Matrix<double, 4, 4> to Eigen::Matrix<float, 3, 3>
-                    // msg->ball.covariance = loc_ball->covariance.block(0, 0, 2, 2).cast<float>();
+                    //TODO: Check if correct, copied from Overview
+                    Eigen::Matrix<float, 3, 3> covariance = loc_ball->covariance.block(0, 0, 3, 3).cast<float>();
+                    msg->ball.covariance = {
+                        { covariance(0, 0), covariance(1, 0), covariance(2, 0) },
+                        { covariance(0, 1), covariance(1, 1), covariance(2, 1) },
+                        { covariance(0, 2), covariance(1, 2), covariance(2, 2) }
+                    };
 
                     msg->ball.velocity = {static_cast<float>(loc_ball->vBw.x()), static_cast<float>(loc_ball->vBw.y()), static_cast<float>(loc_ball->vBw.z())};
-
-
-
                     log<NUClear::DEBUG>("Ball velocity vBw: ", loc_ball->vBw);
-                    // N5Eigen6MatrixIdLi4ELi4ELi0ELi4ELi4EEE lol
-                    // log<NUClear::DEBUG>("Ball covariance: ", typeid(loc_ball->covariance).name());
                 }
 
-                if (walk_state) {
-                    // TODO: try to make shorter?
-                    msg->walk_command.x = static_cast<float>(walk_state->velocity_target(0));
-                    msg->walk_command.y = static_cast<float>(walk_state->velocity_target(1));
-                    msg->walk_command.z = static_cast<float>(walk_state->velocity_target(2));
-
-                    // log<NUClear::DEBUG>("walk command: ", walk->velocity_target);mand;
-                }
-
+                // TODO: Robots. Where the robot thinks the other robots are. This doesn't exist yet.
 
                 emit<Scope::UDP>(msg, cfg.broadcast_ip, cfg.send_port);
-            });
+            }
+        );
     }
+
 
 }  // namespace module::network
