dc8e058eb2fff5abeae3577283a909c06e856f04
Trent Houliston <trent@houliston.me>
2023-12-02 15:48:51 +1100

    Add some basic files

--- module/platform/NUSense/firmware/src/main.cpp
+++ module/platform/NUSense/firmware/src/main.cpp
@@ -1,5 +1,20 @@
 #include <Arduino.h>
 
-void setup() {}
+#include "battery/battery.hpp"
+#include "comms/comms.hpp"
+#include "dynamixel/dynamixel.hpp"
+#include "imu/imu.hpp"
 
-void loop() {}
+void setup() {
+    comms::setup();
+    dynamixel::setup();
+    imu::setup();
+    battery::setup();
+}
+
+void loop() {
+    comms::loop();
+    dynamixel::loop();
+    imu::loop();
+    battery::loop();
+}
