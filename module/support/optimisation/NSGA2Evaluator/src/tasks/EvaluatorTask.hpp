b78df733c14f94715dd779a43d035ac423288721
Ysobel <ysobelsims@gmail.com>
2024-01-23 18:16:14 +1100

    cleaning

--- module/support/optimisation/NSGA2Evaluator/src/tasks/EvaluatorTask.hpp
+++ module/support/optimisation/NSGA2Evaluator/src/tasks/EvaluatorTask.hpp
@@ -35,12 +35,25 @@ namespace module::support::optimisation {
         /// @brief Stores the position of the robot for use in the optimisation
         /// @param position The position of the robot in the world
         virtual void process_optimisation_robot_position(const OptimisationRobotPosition& position) = 0;
-        virtual void set_up_trial(const NSGA2EvaluationRequest& request)                            = 0;
-        virtual void reset_trial()                                                                  = 0;
-        virtual void evaluating_state(NSGA2Evaluator* evaluator)                                    = 0;
+
+        /// @brief Set up the trial for the given optimisation problem and request
+        /// @param request Specifics of the trial to run
+        virtual void set_up_trial(const NSGA2EvaluationRequest& request) = 0;
+
+        /// @brief Reset the trial for the given optimisation problem and request
+        virtual void reset_trial() = 0;
+
+        /// @brief Called when the trial is done to set up the robot for evaluation
+        virtual void evaluating_state(NSGA2Evaluator* evaluator) = 0;
+
+        /// @brief Calculates how well the individual performed
+        /// @param early_termination If the individual stopped before the time had elapsed, eg the robot fell
+        /// @param generation The generation this individual is from
+        /// @param individual The individual's index within the generation
+        /// @return The fitness scores for the individual
         virtual std::unique_ptr<NSGA2FitnessScores> calculate_fitness_scores(bool early_termination,
                                                                              int generation,
-                                                                             int individual)        = 0;
+                                                                             int individual) = 0;
     };
 
 }  // namespace module::support::optimisation
