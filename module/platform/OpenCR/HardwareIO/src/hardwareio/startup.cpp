23459de3ed2f48a3cc219170c9be78b1b76cbe7e
Liam Craft <62984007+Crafty15@users.noreply.github.com>
2024-03-15 23:08:46 +0000

    include convert

--- module/platform/OpenCR/HardwareIO/src/hardwareio/startup.cpp
+++ module/platform/OpenCR/HardwareIO/src/hardwareio/startup.cpp
@@ -24,6 +24,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+#include "Convert.hpp"
 #include "HardwareIO.hpp"
 
 namespace module::platform::OpenCR {
