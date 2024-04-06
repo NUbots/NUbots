597127b3d045623679a90ae0cd09797693ba31e8
Thomas O'Brien <41043317+Tom0Brien@users.noreply.github.com>
2024-04-07 09:32:58 +1000

    Add profiling tool with NUsight visualisation (#1267)

---
+++ nusight2/src/client/components/profiler/controller.ts
@@ -0,0 +1,50 @@
+import { action } from "mobx";
+
+import { RobotModel } from "../robot/model";
+
+import { ProfilerRobotModel } from "./model";
+import { ProfilerModel } from "./model";
+import { ProfileSort } from "./model";
+
+export class ProfilerController {
+  static of() {
+    return new ProfilerController();
+  }
+
+  onSelectRobot(model: ProfilerModel, robot?: RobotModel) {
+    model.selectedRobot = robot && ProfilerRobotModel.of(robot);
+  }
+
+  @action.bound
+  setSort(model: ProfilerRobotModel, column: ProfileSort["column"]) {
+    if (model.sortProfile.column === column) {
+      model.sortProfile.direction = model.sortProfile.direction === "asc" ? "desc" : "asc";
+    } else {
+      model.sortProfile.column = column;
+      model.sortProfile.direction = "asc";
+    }
+  }
+
+  @action.bound
+  setSearch(model: ProfilerRobotModel, search: string) {
+    model.search = search;
+  }
+
+  getSortIcon(model: ProfilerRobotModel, column: string) {
+    if (model.sortProfile.column === column) {
+      return model.sortProfile.direction === "asc" ? "↑" : "↓";
+    }
+    return "";
+  }
+
+  getPercentageStyle(percentage: number) {
+    // Clamp the percentage between 0 and 100
+    const clampedPercentage = Math.min(Math.max(percentage, 0), 100);
+    const hue = (clampedPercentage / 100) * 360;
+    return {
+      width: `${clampedPercentage}%`,
+      backgroundColor: `hsl(${100 - hue}, 100%, 50%)`, // Full saturation and 50% lightness for bright colors
+      height: "100%",
+    };
+  }
+}
