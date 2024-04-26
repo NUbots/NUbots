81d68c52c8630ab94cde3c3a1ec9f0e41cc07a7d
Johanne Montano <jmontano@4tel.com.au>
2024-04-26 13:51:52 +1000

    Add sanity checkss and tests

--- module/platform/NUsense/HardwareIO/src/HardwareIO.cpp
+++ module/platform/NUsense/HardwareIO/src/HardwareIO.cpp
@@ -1,5 +1,6 @@
 #include "HardwareIO.hpp"
 
+#include <fmt/format.h>
 #include <string>
 
 #include "NUSenseParser.hpp"
@@ -7,6 +8,7 @@
 #include "extension/Configuration.hpp"
 
 #include "message/actuation/ServoTarget.hpp"
+#include "message/platform/NUSenseData.hpp"
 #include "message/reflection.hpp"
 
 namespace module::platform::NUsense {
@@ -14,6 +16,7 @@ namespace module::platform::NUsense {
     using extension::Configuration;
     using message::actuation::ServoTarget;
     using message::actuation::ServoTargets;
+    using message::platform::NUSense;
 
     /**
      * Message reflector class that can be used to emit messages provided as NUSenseFrames to the rest of the system
@@ -53,7 +56,9 @@ namespace module::platform::NUsense {
         });
 
         on<PostConnect>().then("Apply Configuration", [this](const PostConnect& apply) {
-            // TODO: Apply any configuration to the device in this lambda
+            // Apply any configuration to the device in this lambda
+            std::string status = apply.attempt ? "successful" : "unsucessful";
+            log<NUClear::INFO>(fmt::format("StreamReactor PostConnect status: {}", status));
         });
 
         // Send servo targets to the device
@@ -62,6 +67,57 @@ namespace module::platform::NUsense {
         // Emit any messages sent by the device to the rest of the system
         on<Trigger<NUSenseFrame>>().then("From NUSense", [this](const NUSenseFrame& packet) {
             message::reflection::from_hash<EmitReflector>(packet.hash)->emit(powerplant, packet);
+        });
+
+        // TODO (JohanneMontano) Figure out why we only receive 17 servo targets on NUSense with frequencies above 50 Hz
+        on<Every<50, Per<std::chrono::seconds>>>().then([this] {
+            auto servo_targets = std::make_unique<ServoTargets>();
+
+            for (int i = 0; i < 20; ++i) {
+                auto servo_target      = std::make_unique<ServoTarget>();
+                servo_target->time     = NUClear::clock::now();
+                servo_target->id       = i + 1;
+                servo_target->position = 100.0;
+                servo_target->gain     = 100.0;
+                servo_target->torque   = 100.0;
+
+                servo_targets->targets.push_back(*servo_target);
+            }
+
+            emit(servo_targets);
+        });
+
+        on<Trigger<NUSense>>().then([this](const NUSense& nusense) {
+            log<NUClear::DEBUG>(
+                fmt::format("\nIMU Data\n"
+                            "\tAccel(xyz): {} - {} - {}\n"
+                            "\t Gyro(xyz): {} - {} - {}\n ",
+                            nusense.imu.accel.x,
+                            nusense.imu.accel.y,
+                            nusense.imu.accel.z,
+                            nusense.imu.gyro.x,
+                            nusense.imu.gyro.y,
+                            nusense.imu.gyro.z));
+
+            log<NUClear::DEBUG>("Logging servo states...");
+
+            for (const auto& [key, val] : nusense.servo_map) {
+                log<NUClear::DEBUG>(fmt::format("      key: {}", key));
+
+                log<NUClear::DEBUG>(fmt::format("       id: {}", val.id));
+                log<NUClear::DEBUG>(fmt::format("   hw_err: {}", val.hardware_error));
+                log<NUClear::DEBUG>(fmt::format("torque_en: {}", val.torque_enabled));
+                log<NUClear::DEBUG>(fmt::format("     ppwm: {}", val.present_pwm));
+                log<NUClear::DEBUG>(fmt::format("    pcurr: {}", val.present_current));
+                log<NUClear::DEBUG>(fmt::format("    pvelo: {}", val.present_velocity));
+                log<NUClear::DEBUG>(fmt::format("     ppos: {}", val.present_position));
+                log<NUClear::DEBUG>(fmt::format("     gpwm: {}", val.goal_pwm));
+                log<NUClear::DEBUG>(fmt::format("    gcurr: {}", val.goal_current));
+                log<NUClear::DEBUG>(fmt::format("    gvelo: {}", val.goal_velocity));
+                log<NUClear::DEBUG>(fmt::format("     gpos: {}", val.goal_position));
+                log<NUClear::DEBUG>(fmt::format("  voltage: {}", val.voltage));
+                log<NUClear::DEBUG>(fmt::format("     temp: {}", val.temperature));
+            }
         });
     }
 
