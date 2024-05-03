68dd6f7fdcf9813d64b724f36e6f835a632e4454
Tom0Brien <thomas16441@gmail.com>
2024-05-03 15:24:33 +1000

    Add back get_id

--- shared/utility/nbs/get_id.hpp
+++ shared/utility/nbs/get_id.hpp
@@ -44,6 +44,17 @@ namespace utility::nbs {
         return data.id;
     }
 
+    /// @brief Returns the id field of data or, if id does not exist, 0
+    template <typename T>
+    std::enable_if_t<!utility::type_traits::has_id<T>::value, uint32_t> get_id(const T& /*data*/) {
+        return 0;
+    }
+
+    template <typename T>
+    std::enable_if_t<utility::type_traits::has_id<T>::value, uint32_t> get_id(const T& data) {
+        return data.id;
+    }
+
 }  // namespace utility::nbs
 
 #endif  // UTILITY_NBS_GET_ID_HPP
