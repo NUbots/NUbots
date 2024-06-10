f3dd3e1f4ad133394e9bd7af83294d8db99ec157
Jesse Williamson <jnathanwilliamson@gmail.com>
2024-06-11 09:46:24 +1000

    Fix meta tag

--- nusight2/src/client/components/theme_switcher_button/view.tsx
+++ nusight2/src/client/components/theme_switcher_button/view.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useMemo, useState } from "react";
-
 import { IconButton, IconButtonProps } from "../icon_button/view";
 
 export default function ThemeSwitcherButton(props: IconButtonProps) {
     const [isDark, setIsDark] = useState(window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches);
-
     const mediaQuery = useMemo(() => window.matchMedia("(prefers-color-scheme: dark)"), []);
 
     useEffect(() => {
@@ -18,12 +16,18 @@ export default function ThemeSwitcherButton(props: IconButtonProps) {
     }, [mediaQuery]);
 
     useEffect(() => {
+        const themeColorMetaTag = document.querySelector('meta[name="color-scheme"]');
+
         if (isDark) {
             document.documentElement.classList.add("dark"); // Needed for styling Tailwind elements
-            document.documentElement.classList.add("[color-scheme:dark]"); // Needed for styling default browser HTML elements
+            if (themeColorMetaTag) {
+                themeColorMetaTag.setAttribute('content', 'dark'); // Needed for default browser styling
+            }
         } else {
             document.documentElement.classList.remove("dark");
-            document.documentElement.classList.remove("[color-scheme:dark]");
+            if (themeColorMetaTag) {
+                themeColorMetaTag.setAttribute('content', 'light');
+            }
         }
     }, [isDark]);
 
