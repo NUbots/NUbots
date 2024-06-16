b2b3c84016537d4c2758b182651ff6ddec099e54
Ysobel <ysobelsims@gmail.com>
2024-06-16 15:59:13 +1000

    simplify walk to field position to use walk path planning

--- module/strategy/WalkToFieldPosition/src/WalkToFieldPosition.cpp
+++ module/strategy/WalkToFieldPosition/src/WalkToFieldPosition.cpp
@@ -68,44 +68,12 @@ namespace module::strategy {
                                            std::sin(walk_to_field_position.heading),
                                            0.0);
 
-                // Transform the field position from field {f} space to robot {r} space
+                // Transform the field position and heading from field {f} space to robot {r} space
                 const Eigen::Vector3d rPRr(Hfr.inverse() * rPFf);
+                const Eigen::Vector3d uHRr(Hfr.inverse() * uHFf);
+                const double heading = std::atan2(uHRr.y(), uHRr.x());
 
-                // Compute the current position error and heading error in field {f} space
-                const double position_error = (Hfr.translation().head(2) - rPFf.head(2)).norm();
-                Eigen::Vector2d uXRf        = Hfr.rotation().col(0).head<2>();
-                const double heading_error  = std::acos(std::max(-1.0, std::min(1.0, uXRf.dot(uHFf.head<2>()))));
-
-                // If we have stopped and our position and heading error is below resume tolerance, then remain stopped
-                if (stopped && position_error < cfg.resume_tolerance && heading_error < cfg.resume_tolerance) {
-                    emit<Task>(std::make_unique<StandStill>());
-                    stopped = true;
-                    return;
-                }
-
-                // If the error in the desired field position and heading is low enough, stand still
-                if (!stopped && position_error < cfg.stop_tolerance && heading_error < cfg.stop_tolerance) {
-                    emit<Task>(std::make_unique<StandStill>());
-                    stopped = true;
-                    return;
-                }
-
-                // If we are getting close to the field position begin to align with the desired heading in field space
-                if (position_error < cfg.align_radius) {
-                    // Rotate the desired heading in field {f} space to robot space
-                    const Eigen::Vector3d uHRr(Hfr.inverse().linear() * uHFf);
-                    const double desired_heading = std::atan2(uHRr.y(), uHRr.x());
-                    emit<Task>(std::make_unique<WalkTo>(rPRr, desired_heading));
-                }
-                // Otherwise, walk directly to the field position
-                else {
-                    const double desired_heading = std::atan2(rPRr.y(), rPRr.x());
-                    emit<Task>(std::make_unique<WalkTo>(rPRr, desired_heading));
-                }
-
-                if (log_level <= NUClear::DEBUG) {
-                    log<NUClear::DEBUG>("Position error: ", position_error, " Heading error: ", heading_error);
-                }
+                emit<Task>(std::make_unique<WalkTo>(rPRr, heading));
             });
     }
 
