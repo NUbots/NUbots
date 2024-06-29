f66ba5a3d6c4f5a8e468cfc94d3edecdd126c216
Jesse Williamson <jnathanwilliamson@gmail.com>
2024-06-29 16:28:57 +1000

    Bring back the dark navbar

--- nusight2/src/client/components/navigation/view.tsx
+++ nusight2/src/client/components/navigation/view.tsx
@@ -14,7 +14,7 @@ const NavigationItemView = ({ url, Icon, children }: NavigationItemViewProps) =>
   <li className="whitespace-nowrap">
     <NavLink
       className={({ isActive }) => {
-        return `p-4 flex flex-col items-center text-xs hover:bg-auto-contrast-1 ${isActive ? "!bg-nusight-600 text-white" : ""
+        return `p-4 flex flex-col items-center text-xs hover:bg-auto-contrast-1 text-white ${isActive ? "!bg-nusight-600" : ""
           }`;
       }}
       to={url}
@@ -26,7 +26,7 @@ const NavigationItemView = ({ url, Icon, children }: NavigationItemViewProps) =>
 );
 
 export const NavigationView = ({ nav }: { nav: NavigationConfiguration }) => (
-  <div className="bg-auto-surface-1 border-r border-auto text-center flex flex-col justify-between items-center">
+  <div className="dark bg-gray-800 dark:bg-auto-surface-1 border-r border-auto text-center flex flex-col justify-between items-center">
     <header>
       <h1 className="flex justify-center items-center text-nusight-500 text-xl font-medium h-[60px]">NUsight</h1>
       <ul>
