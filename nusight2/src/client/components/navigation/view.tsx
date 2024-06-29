77dd9cb40397c0aa4f1ff0aea9920cb187b88f4e
Jesse Williamson <jnathanwilliamson@gmail.com>
2024-06-29 14:02:45 +1000

    Update navigation bar styling

--- nusight2/src/client/components/navigation/view.tsx
+++ nusight2/src/client/components/navigation/view.tsx
@@ -14,9 +14,8 @@ const NavigationItemView = ({ url, Icon, children }: NavigationItemViewProps) =>
   <li className="whitespace-nowrap">
     <NavLink
       className={({ isActive }) => {
-        return `p-4 flex flex-col items-center text-primary-auto text-xs hover:bg-auto-contrast-1 ${
-          isActive ? "!bg-nusight-600" : ""
-        }`;
+        return `p-4 flex flex-col items-center text-xs hover:bg-auto-contrast-1 ${isActive ? "!bg-nusight-600 text-white" : ""
+          }`;
       }}
       to={url}
     >
@@ -27,7 +26,7 @@ const NavigationItemView = ({ url, Icon, children }: NavigationItemViewProps) =>
 );
 
 export const NavigationView = ({ nav }: { nav: NavigationConfiguration }) => (
-  <div className="bg-auto-contrast-1 border-r border-auto text-center flex flex-col justify-between items-center dark">
+  <div className="bg-auto-surface-2 border-r border-auto text-center flex flex-col justify-between items-center">
     <header>
       <h1 className="flex justify-center items-center text-nusight-500 text-xl font-medium h-[60px]">NUsight</h1>
       <ul>
--- nusight2/src/client/components/profiler/icon.tsx
+++ nusight2/src/client/components/profiler/icon.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function IconProfiler(props: { className?: string }) {
   return (
-    <svg className={props.className} version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff">
+    <svg className={props.className} version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
       <path d="M128,384v96H32v-96H128z M192,0v480h-96V0H192z M320,256v224h-96V256H320z M384,128v352h-96V128H384z M512,192v288h-96V192H512z" />
     </svg>
   );
