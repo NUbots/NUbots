0bfa1523a55f7f1ea3f35b84091cdcfc0e1181bf
Jesse Williamson <jnathanwilliamson@gmail.com>
2024-06-29 15:45:37 +1000

    Revert shade change to icon button

--- nusight2/src/client/components/icon_button/view.tsx
+++ nusight2/src/client/components/icon_button/view.tsx
@@ -19,10 +19,10 @@ const SizeToClassName = {
 
 const ColorToClassName = {
   default:
-    "shadow-sm bg-white text-gray-500 ring-1 ring-inset ring-gray-300 enabled:hover:bg-gray-50 enabled:active:bg-gray-200 dark:bg-gray-700 dark:text-white dark:ring-gray-600 dark:enabled:hover:bg-gray-600 dark:enabled:hover:ring-gray-500 dark:enabled:active:bg-gray-700",
+    "shadow-sm bg-white text-gray-600 ring-1 ring-inset ring-gray-300 enabled:hover:bg-gray-50 enabled:active:bg-gray-200 dark:bg-gray-800 dark:text-white dark:ring-gray-700 dark:enabled:hover:bg-gray-700 dark:enabled:hover:ring-gray-600 dark:enabled:active:bg-gray-800",
   primary: "shadow-sm bg-blue-500 text-white enabled:hover:bg-blue-400 enabled:active:bg-blue-600",
   transparent:
-    "bg-transparent text-white ring-transparent enabled:hover:bg-black/10 enabled:active:bg-black/20 dark:enabled:hover:bg-white/10 dark:enabled:active:bg-black/20",
+    "bg-transparent text-blue-500 ring-transparent enabled:hover:bg-black/10 enabled:active:bg-black/20 dark:text-blue-500 dark:enabled:hover:bg-white/10 dark:enabled:active:bg-black/20",
 } as const;
 
 export function IconButton(props: IconButtonProps) {
