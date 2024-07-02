4eb5f7e70b5ca1c6474fd1157d42c4cc8c017f45
Jesse Williamson <jnathanwilliamson@gmail.com>
2024-07-02 17:31:50 +1000

    Formatting

--- nusight2/src/client/components/director/create.tsx
+++ nusight2/src/client/components/director/create.tsx
@@ -10,18 +10,18 @@ import { DirectorNetwork } from "./network";
 import { DirectorView } from "./view";
 
 export function createDirectorView({
-    appModel,
-    nusightNetwork,
-    Menu,
+  appModel,
+  nusightNetwork,
+  Menu,
 }: {
-    appModel: AppModel;
-    nusightNetwork: NUsightNetwork;
-    Menu: ComponentType;
+  appModel: AppModel;
+  nusightNetwork: NUsightNetwork;
+  Menu: ComponentType;
 }) {
-    const model = DirectorModel.of(appModel);
-    return () => {
-        const network = DirectorNetwork.of(nusightNetwork);
-        const controller = DirectorController.of(model);
-        return <DirectorView controller={controller} model={model} Menu={Menu} network={network} />;
-    }
+  const model = DirectorModel.of(appModel);
+  return () => {
+    const network = DirectorNetwork.of(nusightNetwork);
+    const controller = DirectorController.of(model);
+    return <DirectorView controller={controller} model={model} Menu={Menu} network={network} />;
+  };
 }
