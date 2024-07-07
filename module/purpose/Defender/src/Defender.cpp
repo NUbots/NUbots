8695bdd9261c02939a86ab74e007ae596821d975
A-Herfel <94888634+A-Herfel@users.noreply.github.com>
2024-07-07 12:49:57 +1000

    Remove unnecessary Defender configs (#1410)
    Co-authored-by: Ysobel <ysobelsims@gmail.com>
    Co-authored-by: Ysobel Sims <35280100+ysims@users.noreply.github.com>

--- module/purpose/Defender/src/Defender.cpp
+++ module/purpose/Defender/src/Defender.cpp
@@ -155,8 +155,7 @@ namespace module::purpose {
     void Defender::play() {
         // Walk to the ball and kick!
         // Second argument is priority - higher number means higher priority
-        emit<Task>(std::make_unique<FindBall>(),
-                   1);                                  // if the look/walk to ball tasks are not running, find the ball
+        emit<Task>(std::make_unique<FindBall>(), 1);    // if the look/walk to ball tasks are not running, find the ball
         emit<Task>(std::make_unique<LookAtBall>(), 2);  // try to track the ball
         emit<Task>(std::make_unique<WalkToKickBall>(), 3);  // try to walk to the ball and align towards opponents goal
         emit<Task>(std::make_unique<KickToGoal>(), 4);      // kick the ball if possible
