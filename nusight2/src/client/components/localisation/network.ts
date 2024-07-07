73b868dd54179055065e350e0e6c51700ec5d491
Ysobel <ysobelsims@gmail.com>
2024-07-07 15:27:02 +1000

    actually fix

--- nusight2/src/client/components/localisation/network.ts
+++ nusight2/src/client/components/localisation/network.ts
@@ -25,7 +25,7 @@ export class LocalisationNetwork {
     this.network.on(message.vision.Goals, this.onGoals);
     this.network.on(message.planning.WalkToDebug, this.onWalkToDebug);
     this.network.on(message.vision.FieldIntersections, this.onFieldIntersections);
-    this.network.on(message.input.Purpose, this.onPurposes);
+    this.network.on(message.purpose.Purpose, this.onPurposes);
   }
 
   static of(nusightNetwork: NUsightNetwork, model: LocalisationModel): LocalisationNetwork {
