aebbb9f7002d82966675a9b09a5166a3bb9aa671
Jesse Williamson <jnathanwilliamson@gmail.com>
2024-07-09 15:50:27 +1000

    Remove special behaviour from goalie penalty defence

--- module/purpose/Goalie/src/Goalie.hpp
+++ module/purpose/Goalie/src/Goalie.hpp
@@ -43,8 +43,6 @@ namespace module::purpose {
         struct Config {
             /// @brief Ready position to walk to (x, y, theta)
             Eigen::Vector3f ready_position = Eigen::Vector3f::Zero();
-            /// @brief Penalty defence position to walk to when defending from a penalty kick from the opposing team
-            Eigen::Vector3f penalty_defence_position = Eigen::Vector3f::Zero();
         } cfg;
 
     public:
