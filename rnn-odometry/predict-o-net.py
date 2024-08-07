208ea17dc5254206fc25e9f19ce94f15a3ecbd20
Crafty15 <crafty0410@gmail.com>
2024-08-07 20:28:49 +1000

    Implement basic data partitioning and simplify

--- rnn-odometry/predict-o-net.py
+++ rnn-odometry/predict-o-net.py
@@ -6,6 +6,21 @@ from keras.models import load_model
 from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.preprocessing import MinMaxScaler
 
+
+def partition_dataset(features, targets, window_size):
+    inputs = []
+    outputs = []
+
+    for i in range(0, len(features) - window_size):
+        window = features[i:i + window_size]
+        next_value = targets[i + window_size]
+
+        inputs.append(window)
+        outputs.append(next_value)
+
+    return np.array(inputs), np.array(outputs)
+
+
 # Load data
 test_data = np.load('datasets/input_data_train.npy')
 test_targets = np.load('datasets/input_targets_train.npy')
@@ -13,71 +28,22 @@ test_targets = np.load('datasets/input_targets_train.npy')
 # test_targets = test_targets[:4000]
 
 # Load model
-model = load_model('models/model-20240601-213604')
+model = load_model('models/model-20240807-201541')
 
-# Plot and inspect loaded data
-# num_channels = test_data.shape[1]
-# plt.figure(figsize=(10, 5))
-# # Plot each channel
-# for i in range(num_channels):
-#     plt.plot(test_data[:20000, i], label=f'Channel {i+1}')
-# # Add a legend
-# plt.legend()
-# plt.show()
-
-system_sample_rate = 115
-sequence_length = system_sample_rate * 1    # Look back 3 seconds
-sequence_stride = 1                         # Shift one sequence_length at a time (rolling window)
-sampling_rate = 1                           # Used for downsampling
-batch_size = 115
-#NOTE: Using return_sequences=True. sequence_length should be = axis 0 of array (total sequence length) (WRONG)
-# Create test dataset
-
-# sequence_length = test_data.shape[0]
-
-test_dataset = tf.keras.utils.timeseries_dataset_from_array(
-    data=test_data,
-    targets=None,
-    sequence_length=sequence_length,
-    sequence_stride=sequence_stride,
-    sampling_rate=sampling_rate,
-    batch_size=batch_size
-)
-# target_dataset = tf.keras.utils.timeseries_dataset_from_array(
-#     data=test_targets,
-#     targets=None,
-#     sequence_length=sequence_length,
-#     sequence_stride=sequence_stride,
-#     sampling_rate=sampling_rate,
-#     batch_size=batch_size
-# )
-# test_dataset = tf.keras.utils.timeseries_dataset_from_array(
-#     data=test_data,
-#     targets=None,
-#     sequence_length=sequence_length,
-#     sequence_stride=sequence_stride,
-#     sampling_rate=sampling_rate,
-#     batch_size=batch_size
-# )
-# Assuming input_targets_train is your original ground truth data
-targets_sequenced = np.array([test_targets[i: i + sequence_length] for i in range(test_targets.shape[0] - sequence_length + 1)])
+# Partition dataset
+window_size = 100
+test_data, test_targets = partition_dataset(test_data, test_targets, window_size)
 
 # Predict
-predictions = model.predict(test_dataset)
+predictions = model.predict(test_data)
 
 # Check shapes
-# print('test set shape:', test_targets)
 print('prediction shape:', predictions.shape)
-# print('prediction shape[0]:', predictions.shape[0])
-# print('prediction shape[1]:', predictions.shape[1])
 print('loaded target set shape:', test_targets.shape)
-print('Sequenced target set shape: ', targets_sequenced.shape)
+print('Sequenced target set shape: ', test_targets.shape)
 
-# single plot
-num_time_steps = predictions.shape[1]
-
-plt.figure(figsize=(10, 6))
-plt.plot(predictions[:, 100, :], label=f'Predictions')
-plt.plot(targets_sequenced[:, 100, :], label=f'Targets')
+# Plot predictions vs targets
+plt.plot(predictions, label='predictions')
+plt.plot(test_targets, label='targets')
 plt.legend()
 plt.show()
--- rnn-odometry/train-o-net.py
+++ rnn-odometry/train-o-net.py
@@ -1,103 +1,379 @@
 #!/usr/bin/env python3
 
 import argparse
+import datetime
 import os
 import subprocess
 
+import joblib
+import keras
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+from scipy.ndimage import gaussian_filter1d
 from sklearn.preprocessing import MinMaxScaler
-
-# def normalise(list):
-#     # convert list to np
-#     arr = np.array(list)
-#     # Convert the arrays to tensors
-#     # tensors = [tf.convert_to_tensor(array, dtype=tf.float32) for array in arrays]
-#     tensors = tf.convert_to_tensor(arr, dtype=tf.float32)
-
-#     # Concatenate all tensors into one
-#     concatenated = tf.concat(tensors, axis=0)
-
-#     # Calculate the min and max of the concatenated tensor
-#     min_val = tf.reduce_min(concatenated)
-#     max_val = tf.reduce_max(concatenated)
-
-#     # Normalize each tensor
-#     normalised_tensors = [(tensor - min_val) / (max_val - min_val) for tensor in tensors]
-#     return normalised_tensors
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Train an LSTM-RNN on servo and imu data with XYZ ground truth.")
-    parser.add_argument(
-        "path_to_data",
-        help="The path to the directory holding the training data." + "The program will search nested directories.",
+
+    def partition_dataset(features, targets, window_size):
+        inputs = []
+        outputs = []
+
+        for i in range(0, len(features) - window_size):
+            window = features[i:i + window_size]
+            next_value = targets[i + window_size]
+
+            inputs.append(window)
+            outputs.append(next_value)
+
+        return np.array(inputs), np.array(outputs)
+
+    def convert_to_relative(data):
+        """
+        This function takes a dataset of world coordinates and converts it to relative positions
+        based on the starting position (assumed to be the first index).
+
+        Args:
+            data: A NumPy array of shape (num_datapoints, 3) representing world coordinates.
+
+        Returns:
+            A NumPy array of shape (num_datapoints, 3) representing relative positions.
+        """
+
+        # Get the starting position (assuming first index)
+        starting_position = data[0]
+
+        # Calculate relative positions for all data points (excluding starting position)
+        relative_positions = data - starting_position
+
+        return relative_positions
+
+    def gaussian_smooth(data, window_size):
+        "Takes an array and separately applies gaussian filter to each dimension"
+
+        # Output array
+        smoothed_data = np.empty_like(data)
+
+        # Apply gaussian filter to each dimension
+        for i in range(data.shape[1]):
+            smoothed_data[:, i] = gaussian_filter1d(data[:, i], window_size, axis=0)
+
+        # print shapes to verify i didn't f up
+        # print("Original data shape: ", data.shape)
+        # print("Smoothed data shape: ", smoothed_data.shape)
+        return smoothed_data
+
+    # numpy arrays
+    first_file = 1
+    num_files = 4  # Number of files to load
+    prefix = "s"  # s for straight path
+    imu = []
+    # servos = []
+    truth_all = []
+    truth_start_end_indicator = []
+
+    for i in range(first_file, num_files):
+        imu_data = np.load(f"processed-outputs/numpy/{prefix}/{i}/{prefix}-imu-{i}.npy")
+        imu.append(imu_data)
+
+        # servos_data = np.load(f"processed-outputs/numpy/{prefix}/{i}/{prefix}-servos-{i}.npy")
+        # servos.append(servos_data)
+
+        truth_data = np.load(f"processed-outputs/numpy/{prefix}/{i}/{prefix}-truth-{i}.npy")
+        truth_all.append(truth_data)
+
+        # Create a chunk for the truth_start_end_indicator array
+        chunk_size = truth_data.shape[0]  # Assuming the first dimension is the one we're interested in
+        indicator_chunk = np.zeros(chunk_size)
+        indicator_chunk[0] = 1  # Start indicator
+        indicator_chunk[-1] = 1  # End indicator
+        truth_start_end_indicator.extend(indicator_chunk)
+
+    # Need to do the relative conversions here
+    # Use the convert_to_relative function to convert the truth data to relative positions
+    # NOTE: Htw is already relative to the starting point!! But the starting point can vary if not converted
+    truth_all = [convert_to_relative(truth) for truth in truth_all]
+
+    # Smoothing should be done here
+    # Loop through each truth array and smooth
+    # truth_all = [gaussian_smooth(truth, 50) for truth in truth_all]
+
+    # Loop through each array and concatenate into a numpy array
+    imu_joined = np.concatenate(imu, axis=0)
+    # servos_joined = np.concatenate(servos, axis=0)
+    truth_all_joined = np.concatenate(truth_all, axis=0)
+    truth_start_end_indicator_joined = np.array(truth_start_end_indicator)
+
+    # Print the shape of the joined arrays
+    print("IMU s joined: ", imu_joined.shape)
+    # print("Servos s joined: ", servos_joined.shape)
+    print("Truth s joined: ", truth_all_joined.shape)
+    print("Truth start/end indicator: ", truth_start_end_indicator_joined.shape)
+
+    # Slice out the arm and head servos
+    # servos_joined = servos_joined[:, 6:18]
+
+    # Plot and inspect each joined array
+    # num_channels = imu_joined.shape[1]
+    # plt.figure(figsize=(10, 5))
+    # # Plot each channel
+    # for i in range(num_channels):
+    #     plt.plot(imu_joined[0:10000, i], label=f'imu {i+1}')
+    # # Add a legend
+    # # plt.ylim(np.min(imu), np.max(imu))
+    # plt.autoscale(enable=True, axis="both")
+    # plt.legend()
+    # plt.show()
+
+    # NOTE: Using only position for first tests to see what happens. May add in heading later.
+    # Filter the truth down to just position -  this is the last column of the homogenous transform matrix
+    # NOTE: 17/04/2024 - Due to fluctuations in the z component, trying with just x, y
+    # NOTE: Remember to reshape if adding or removing features
+
+    # slice out the x and y positions (9:11) of the truth array
+    truth_joined_sliced = truth_all_joined[:, 9:11]
+
+    # Plot and inspect after slicing
+    # num_channels = servos_joined.shape[1]
+    # plt.figure(figsize=(10, 5))
+    # # Plot each channel
+    # for i in range(num_channels):
+    #     plt.plot(servos_joined[100000:, i], label=f'Servos {i+1}')
+    # # Add a legend
+    # plt.legend()
+    # plt.show()
+
+
+    # Join the data
+    # joined_data = np.concatenate([imu_joined, servos_joined, truth_joined_sliced], axis=1)
+
+    # Testing without the servos
+    joined_data = np.concatenate([imu_joined, truth_joined_sliced], axis=1)
+
+    print("Total joined data shape: ", joined_data.shape)
+
+    # Split the training data into training, validation and test sets
+    training_size = 0.5
+    validate_size = 0.2
+    num_time_steps = joined_data.shape[0]
+
+    num_train_max_idx = int(np.floor(num_time_steps * training_size))
+    num_val_max_idx = int(np.floor(num_time_steps * (training_size + validate_size)))
+
+    # import pdb
+    # pdb.set_trace()
+
+    train_arr = joined_data[:num_train_max_idx]
+    validate_arr = joined_data[num_train_max_idx: num_val_max_idx]
+    test_arr = joined_data[num_val_max_idx:]
+
+    # import pdb
+    # pdb.set_trace()
+
+    # array sizes
+    # num_train = train_arr.size
+    # num_val = validate_arr.size
+
+    #### Normalisation  ####
+    ## Standardise ##
+
+    # NOTE: mean and std from training dataset is used to standardise
+    # all of the datasets to prevent information leakage.
+    # mean and std from the training run will need to be used in production for de-standardise the predictions.
+    mean = train_arr.mean(axis=0)
+    std = train_arr.std(axis=0)
+    print("mean: ", mean)
+    print("std: ", std)
+
+    train_arr = (train_arr - mean) / std
+    validate_arr = (validate_arr - mean) / std
+    test_arr = (test_arr - mean) / std
+    # # # #
+
+    # # clip the outliers in the data
+    # train_arr_clipped = np.clip(train_arr, -4, 4)
+    # validate_arr_clipped = np.clip(validate_arr, -4, 4)
+    # test_arr_clipped = np.clip(test_arr, -4, 4)
+
+    ## End of standardisation ##
+
+    ## Min/Max Scaling ##
+
+    # scaler = MinMaxScaler(feature_range=(-1, 1))
+    # # Fit scaler to training data only
+    # scaler.fit(train_arr)
+    # # Transform the data
+    # train_arr = scaler.transform(train_arr)
+    # validate_arr = scaler.transform(validate_arr)
+    # test_arr = scaler.transform(test_arr)
+
+    ## End of Min/Max Scaling ##
+
+    # Print the shapes and min/max values of the datasets
+    print(f"Training set size: {train_arr.shape}")
+    print("Training set min:", np.min(train_arr))
+    print("Training set max:", np.max(train_arr))
+    print(f"Validation set size: {validate_arr.shape}")
+    print(f"Test set size: {test_arr.shape}")
+
+    # Plot and inspect after normalising
+    # num_channels = train_arr.shape[1]
+    # plt.figure(figsize=(10, 5))
+    # # Plot each channel
+    # for i in range(num_channels):
+    #     plt.plot(train_arr[0:80000, i], label=f'Channel {i+1}')
+    # # Add a legend
+    # plt.legend()
+    # plt.show()
+
+    #### End of normalisation ####
+
+
+
+    #### Split into data and targets ####
+    # NOTE: Remember to reshape if adding or removing features
+    # NOTE: Just using IMU data, so changing index slice from 18 to 6
+    # Training
+    input_data_train = train_arr[:, :6]  # imu and servos
+    input_targets_train = train_arr[:, 6:]  # truth
+
+    # Convert sliced targets to relative position
+    # input_targets_train = convert_to_relative(input_targets_train)
+
+    # Validation
+    input_data_validate = validate_arr[:, :6]  # imu and servos
+    input_targets_validate = validate_arr[:, 6:]  # truth
+
+    # Convert sliced targets to relative position
+    # input_targets_validate = convert_to_relative(input_targets_validate)
+
+    # Testing
+    input_data_test= test_arr[:, :6]  # imu and servos
+    input_targets_test = test_arr[:, 6:]  # truth
+
+    # Convert sliced targets to relative position
+    # input_targets_test = convert_to_relative(input_targets_test)
+
+    # Plot and inspect after splitting
+    # num_channels = input_targets_train.shape[1]
+    # plt.figure(figsize=(10, 5))
+    # # Plot each channel
+    # for i in range(num_channels):
+    #     plt.plot(input_targets_train[0:100000, i], label=f'Target {i+1}')
+    # # Add a legend
+    # plt.legend()
+    # plt.show()
+
+    #### End of splitting ####
+
+    #### Add indicator to the datasets ####
+    # Convert indicator list to a numpy array before reshaping
+    truth_start_end_indicator_2d = np.array(truth_start_end_indicator).reshape(-1, 1)
+
+    # Split the indicator array
+    train_indicator = truth_start_end_indicator_2d[:train_arr.shape[0]]
+    validate_indicator = truth_start_end_indicator_2d[train_arr.shape[0]:train_arr.shape[0] + validate_arr.shape[0]]
+    test_indicator = truth_start_end_indicator_2d[train_arr.shape[0] + validate_arr.shape[0]:]
+    # Concatenate the indicator arrays to the main datasets
+    input_data_train = np.concatenate((input_data_train, train_indicator.reshape(-1, 1)), axis=1)
+    input_data_validate = np.concatenate((input_data_validate, validate_indicator.reshape(-1, 1)), axis=1)
+    input_data_test = np.concatenate((input_data_test, test_indicator.reshape(-1, 1)), axis=1)
+    #### End of adding indicator ####
+
+    # print dataset shapes (will have an extra column for the indicator)
+    print(f"input_data_train: {input_data_train.shape}")
+    print(f"input_targets_train: {input_targets_train.shape}")
+    print(f"input_data_validate: {input_data_validate.shape}")
+    print(f"input_targets_validate: {input_targets_validate.shape}")
+    print(f"input_data_test: {input_data_test.shape}")
+    print(f"input_targets_test: {input_targets_test.shape}")
+
+     # Save the datasets (should break the dataset creation to another module eventually)
+    np.save('datasets/input_data_train.npy',input_data_train)
+    np.save('datasets/input_targets_train.npy',input_targets_train)
+    np.save('datasets/input_data_validate.npy', input_data_validate)
+    np.save('datasets/input_targets_validate.npy', input_targets_validate)
+    np.save('datasets/input_data_test.npy', input_data_test)
+    np.save('datasets/input_targets_test.npy', input_targets_test)
+
+    # Plot and inspect after adding indicator
+    # num_channels = input_data_train.shape[1]
+    # plt.figure(figsize=(10, 5))
+    # # Plot each channel
+    # for i in range(num_channels):
+    #     plt.plot(input_data_train[:, i], label=f'Data {i+1}')
+    # # Add a legend
+    # plt.legend()
+    # plt.show()
+
+    # Plot and inspect
+    # num_channels = train_arr_scaled.shape[1]
+    # plt.figure(figsize=(10, 5))
+    # # Plot each channel
+    # for i in range(num_channels):
+    #     if i < 6:
+    #         plt.plot(train_arr_scaled[0:50000, i], label=f'Imu {i+1}', color='g')
+    #     elif i < 18:
+    #         plt.plot(train_arr_scaled[0:50000, i], label=f'Servos {i+1}', color='b')
+    #     else:
+    #         plt.plot(train_arr_scaled[0:50000, i], label=f'Truth {i+1}', color='r')
+    # # Add a legend
+    # plt.legend()
+    # plt.show()
+
+    # # NOTE: Samples are roughly 115/sec
+    # system_sample_rate = 115
+    sequence_length = 100   # Look back n seconds (system_sample_rate * n). system_sample_rate was roughly calculated at 115/sec
+    # sequence_stride = 1                         # Shift one sequence_length at a time (rolling window)
+    # sampling_rate = 1                           # Used for downsampling
+    # batch_size = 150                         # Number of samples per gradient update (original: 64, seemed better?: 512)
+
+    # Partition the training and validation datasets into sequences
+    input_data_train, input_targets_train = partition_dataset(input_data_train, input_targets_train, sequence_length)
+    input_data_validate, input_targets_validate = partition_dataset(input_data_validate, input_targets_validate, sequence_length)
+    validation_data = (input_data_validate, input_targets_validate)
+
+    # Print the shapes of the partitioned datasets
+    print(f"input_data_train: {input_data_train.shape}")
+    print(f"input_targets_train: {input_targets_train.shape}")
+    print(f"input_data_validate: {input_data_validate.shape}")
+    # Print the shape of the second element in the training dataset
+
+
+    # Model parameters
+    learning_rate = 0.000018   # Controls how much to change the model in response to error.
+    epochs = 50
+    loss_function = keras.losses.MeanSquaredError()
+    optimizer = keras.optimizers.AdamW(learning_rate=learning_rate)
+
+    # Tensorboard
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_dir = "logs/fit/" + timestamp
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    # Model Layers
+    inputs = keras.layers.Input(shape=(sequence_length, input_data_train.shape[2]))
+    lstm = keras.layers.LSTM(64, kernel_initializer=keras.initializers.GlorotNormal(), return_sequences=False)(inputs)    # 32 originally
+    outputs = keras.layers.Dense(2)(lstm)
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    model.compile(optimizer=optimizer, loss=loss_function)
+    model.summary()
+
+    # Train the model
+    model.fit(
+        x=input_data_train,
+        y=input_targets_train,
+        validation_data=validation_data,
+        epochs=epochs,
+        callbacks=[tensorboard_callback]
+        # Unspecified batch size will default to 32
     )
 
-    args = parser.parse_args()
-    path_to_data = args.path_to_data
+    # Note add back the model save
+    model.save("models/model-" + timestamp)
 
-    # Declare both features and ground truth lists
-    servo_data_list = []
-    imu_data_list = []
-    truth_list = []
-    features = [[], []]
-    samples = 0
-
-    # Walk the given file path searching for data.
-    for root, dirs, files in os.walk(path_to_data):
-        for file in sorted(files):
-            # increment sample count
-            samples = samples + 1
-            # Concatenate the imu and servo data along the second axis
-            # This is necessary to maintain their temporal relationship
-            filepath = os.path.join(root, file)
-            if "servos" in file:
-                print("Servos: " + file)
-                servo_data_list.append(np.load(filepath))
-            if "imu" in file:
-                print("imu: " + file)
-                imu_data_list.append(np.load(filepath))
-            if "truth" in file:
-                print("truth: " + file)
-                truth_list.append(np.load(filepath))
-            # features[0].append([servo_data_list])
-            # features[1].append([imu_data_list])
-    # NP Concatenate or Stack?
-
-    # Create np arrays
-    # Servos
-    for item in servo_data_list:
-        print(item)
-        servo_data_arr = np.concatenate((item), axis=0)
-    # # Imu
-    # for item in imu_data_list:
-    #     imu_data_arr = np.concatenate(item)
-    # # Truth
-    # for item in truth_list:
-    #     truth_arr = np.concatenate(item)
-
-    # print("Servos list: ")
-    # print(servo_data_list)
-    # print("IMU list: ")
-    # print(imu_data_list)
-    # print("Ground truth list: ")
-    # print(samples)
-    # print(truth_list)
-    # print("SHAPES/LENGTHS")
-    print("servos")
-    print(servo_data_arr)
-    print(servo_data_arr.shape)
-    # print("imu")
-    # print(imu_data_arr)
-    # print(imu_data_arr.shape)
-    # print("imu")
-    # print(truth_arr)
-    # print(truth_arr.shape)
-    # normalise
-    # scaler = MinMaxScaler()
-    # normalised_servo_tensor = scaler.fit_transform(servo_data_list)
-    # normalised_imu_tensor = scaler.fit_transform(imu_data_list)
 
 
 if __name__ == "__main__":
