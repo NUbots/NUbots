d86906390a3c20f84e236c5d3b7969428845d776
Trent Houliston <trent@houliston.me>
2024-11-02 19:44:45 +1100

    Fix up types for the GroupInfoStore

--- shared/extension/behaviour/GroupInfo.hpp
+++ shared/extension/behaviour/GroupInfo.hpp
@@ -66,7 +66,7 @@ namespace extension::behaviour {
         struct GroupInfoStore {
         private:
             using ThreadStore = NUClear::dsl::store::ThreadStore<std::shared_ptr<const GroupInfo>>;
-            using GlobalStore = NUClear::util::TypeMap<T, T, const std::shared_ptr<GroupInfo>>;
+            using GlobalStore = NUClear::util::TypeMap<T, T, GroupInfo>;
 
         public:
             /**
