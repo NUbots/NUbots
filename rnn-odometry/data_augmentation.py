df742b0f475f7167c278042a459809f17af99173
Crafty15 <crafty0410@gmail.com>
2024-11-05 19:27:29 +1100

    fix time warp

--- rnn-odometry/data_augmentation.py
+++ rnn-odometry/data_augmentation.py
@@ -1,19 +1,35 @@
 import numpy as np
+from scipy.interpolate import CubicSpline
 
 
 def add_noise(data, noise_level=0.05):
     noise = np.random.normal(loc=0, scale=noise_level, size=data.shape)
     return data + noise
 
+# def time_warp(data, sigma=0.2, knot=4):
+
+#     orig_steps = np.arange(data.shape[0])
+#     random_warps = np.random.normal(loc=1.0, scale=sigma, size=(knot+2,))
+#     warp_steps = (np.cumsum(random_warps) * (data.shape[0]-1) / (knot+1)) \
+#                  [:knot+1]
+#     warper = CubicSpline(np.concatenate(([-1], warp_steps, [data.shape[0]])),
+#                          np.concatenate(([-1], orig_steps[warp_steps.astype(int)], [data.shape[0]])))
+#     return warper(orig_steps)
+
 def time_warp(data, sigma=0.2, knot=4):
-    from scipy.interpolate import CubicSpline
     orig_steps = np.arange(data.shape[0])
-    random_warps = np.random.normal(loc=1.0, scale=sigma, size=(knot+2,))
-    warp_steps = (np.cumsum(random_warps) * (data.shape[0]-1) / (knot+1)) \
-                 [:knot+1]
-    warper = CubicSpline(np.concatenate(([-1], warp_steps, [data.shape[0]])),
-                         np.concatenate(([-1], orig_steps[warp_steps.astype(int)], [data.shape[0]])))
-    return warper(orig_steps)
+    random_warps = np.random.normal(loc=1.0, scale=sigma, size=(knot + 2,))
+    warp_steps = np.linspace(0, data.shape[0] - 1, num=knot + 2)
+    warp_steps = np.clip(warp_steps, 0, data.shape[0] - 1)  # Ensure warp_steps are within bounds
+    warped_steps = np.cumsum(random_warps)
+    warped_steps = (warped_steps / warped_steps[-1]) * (data.shape[0] - 1)
+    warped_steps = np.clip(warped_steps, 0, data.shape[0] - 1)  # Ensure warped_steps are within bounds
+
+    spline = CubicSpline(warp_steps, warped_steps)
+    new_steps = spline(orig_steps)
+    new_steps = np.clip(new_steps, 0, data.shape[0] - 1).astype(int)  # Ensure new_steps are within bounds
+
+    return data[new_steps]
 
 def augment_batch(batch):
     augmented = []
@@ -35,7 +51,11 @@ def time_warp_batch(batch, sigma=0.2, knot=4):
 def time_warp_batch(batch, targets, sigma=0.2, knot=4):
     augmented_batch = []
     augmented_targets = []
+    index = 0
     for sequence, target in zip(batch, targets):
+        index += 1
+        # Debug print with an index to indicate that the time warp is being applied
+        print("Time warp applied to index: ", index)
         warped_sequence = time_warp(sequence, sigma, knot)
         warped_target = time_warp(target, sigma, knot)
         augmented_batch.append(warped_sequence)
