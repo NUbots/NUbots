ba0a9d5fdae4c8ddf0d918553c119eb51c1b9a30
Crafty15 <crafty0410@gmail.com>
2024-11-07 21:38:54 +1100

    remove dodgy time warp

--- rnn-odometry/data_augmentation.py
+++ rnn-odometry/data_augmentation.py
@@ -6,16 +6,6 @@ def add_noise(data, noise_level=0.05):
     noise = np.random.normal(loc=0, scale=noise_level, size=data.shape)
     return data + noise
 
-# def time_warp(data, sigma=0.2, knot=4):
-
-#     orig_steps = np.arange(data.shape[0])
-#     random_warps = np.random.normal(loc=1.0, scale=sigma, size=(knot+2,))
-#     warp_steps = (np.cumsum(random_warps) * (data.shape[0]-1) / (knot+1)) \
-#                  [:knot+1]
-#     warper = CubicSpline(np.concatenate(([-1], warp_steps, [data.shape[0]])),
-#                          np.concatenate(([-1], orig_steps[warp_steps.astype(int)], [data.shape[0]])))
-#     return warper(orig_steps)
-
 def time_warp(data, sigma=0.2, knot=4):
     orig_steps = np.arange(data.shape[0])
     random_warps = np.random.normal(loc=1.0, scale=sigma, size=(knot + 2,))
