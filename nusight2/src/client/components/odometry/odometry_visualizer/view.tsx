1e4e120356e48e8b0b9cb8ef04f3e1c5b2583937
Brendan Annable <brendan@annable.id.au>
2024-11-28 14:23:41 +1100

    [NUsight] Rewrite OdometryVisualizer with R3F

--- nusight2/src/client/components/odometry/odometry_visualizer/view.tsx
+++ nusight2/src/client/components/odometry/odometry_visualizer/view.tsx
@@ -3,12 +3,12 @@ import { action } from "mobx";
 import { computed } from "mobx";
 
 import { Vector2 } from "../../../../shared/math/vector2";
-import { Canvas } from "../../three/three";
-import { Three } from "../../three/three";
+import { Vector3 } from "../../../../shared/math/vector3";
+import { PerspectiveCamera, ThreeFiber } from "../../three/three_fiber";
 
 import { OdometryVisualizerModel } from "./model";
 import styles from "./style.module.css";
-import { OdometryVisualizerViewModel } from "./view_model";
+import * as THREE from "three";
 
 export class OdometryVisualizer extends React.Component<{ model: OdometryVisualizerModel }> {
   private dragger?: Dragger;
@@ -16,22 +16,27 @@ export class OdometryVisualizer extends React.Component<{ model: OdometryVisuali
   render() {
     return (
       <div className={styles.visualizer}>
-        <Three
-          stage={this.stage}
-          onWheel={this.onWheel}
-          onMouseDown={this.onMouseDown}
-          onMouseMove={this.onMouseMove}
-          onMouseUp={this.onMouseUp}
-        />
+        <ThreeFiber
+          // onWheel={this.onWheel}
+          // onMouseDown={this.onMouseDown}
+          // onMouseMove={this.onMouseMove}
+          // onMouseUp={this.onMouseUp}
+        >
+          <PerspectiveCamera fov={75} aspect={1} near={0.001} far={100} up={[0, 0, 1]}
+                             position={this.cameraPosition.toArray()} lookAt={this.rTWw}/>
+          {this.torso}
+          {this.floor}
+          {this.worldFrame}
+        </ThreeFiber>
         <div className={styles.legend}>
           <div className={styles.item}>
-            <div className={styles.color} style={{ backgroundColor: "red" }} />
-            <div className={styles.color} style={{ backgroundColor: "green" }} />
-            <div className={styles.color} style={{ backgroundColor: "blue" }} />
+            <div className={styles.color} style={{ backgroundColor: "red" }}/>
+            <div className={styles.color} style={{ backgroundColor: "green" }}/>
+            <div className={styles.color} style={{ backgroundColor: "blue" }}/>
             <span>Hwt</span>
           </div>
           <div className={styles.item}>
-            <div className={styles.color} style={{ backgroundColor: "white" }} />
+            <div className={styles.color} style={{ backgroundColor: "white" }}/>
             <span>Accelerometer</span>
           </div>
         </div>
@@ -39,42 +44,96 @@ export class OdometryVisualizer extends React.Component<{ model: OdometryVisuali
     );
   }
 
-  private readonly stage = (canvas: Canvas) => {
-    const cameraViewModel = OdometryVisualizerViewModel.of(canvas, this.props.model);
-    return computed(() => [cameraViewModel.stage]);
-  };
+  private get torso() {
+    return <object3D
+      position={this.rTWw.toArray()}
+      rotation={Vector3.fromThree(new THREE.Euler().setFromRotationMatrix(this.model.Hwt.toThree())).toArray()}
+    >
+      {this.basis}
+      {this.accelerometer}
+    </object3D>;
+  }
 
-  @action.bound
-  private onWheel(deltaY: number) {
-    const { camera } = this.props.model;
-    camera.distance = clamp(camera.distance + deltaY / 200, 0.01, 10, 0);
+  private get basis() {
+    return <object3D>
+      <arrowHelper args={[new THREE.Vector3(1, 0, 0), undefined, 1, 0xff0000]}/>
+      <arrowHelper args={[new THREE.Vector3(0, 1, 0), undefined, 1, 0x00ff00]}/>
+      <arrowHelper args={[new THREE.Vector3(0, 0, 1), undefined, 1, 0x0000ff]}/>
+    </object3D>;
+  }
+
+  private get worldFrame() {
+    return <object3D position={this.rTWw.toArray()}>
+      <arrowHelper args={[new THREE.Vector3(1, 0, 0), undefined, 0.2, 0xff0000]}/>
+      <arrowHelper args={[new THREE.Vector3(0, 1, 0), undefined, 0.2, 0x00ff00]}/>
+      <arrowHelper args={[new THREE.Vector3(0, 0, 1), undefined, 0.2, 0x0000ff]}/>
+    </object3D>;
+  }
+
+  private get accelerometer() {
+    return <arrowHelper args={[
+      this.model.accelerometer.normalize().toThree(),
+      undefined,
+      this.model.accelerometer.length / 9.8,
+      0xffffff,
+    ]}/>;
+  }
+
+  private get floor() {
+    return <gridHelper args={[100, 100]} rotation={[Math.PI / 2, 0, 0]}/>;
+  }
+
+  private get model(): OdometryVisualizerModel {
+    return this.props.model;
+  }
+
+  @computed
+  private get cameraPosition(): Vector3 {
+    const { distance, pitch, yaw } = this.model.camera;
+    const p = pitch - Math.PI / 2;
+    const y = -yaw + Math.PI;
+    const orbitPosition = new Vector3(Math.sin(p) * Math.cos(y), Math.sin(p) * Math.sin(y), Math.cos(p)).multiplyScalar(
+      -distance,
+    ); // rCTw
+    return orbitPosition.add(this.rTWw);
+  }
+
+  @computed
+  private get rTWw() {
+    return this.model.Hwt.t.vec3();
   }
 
   @action.bound
-  private onMouseDown(x: number, y: number) {
+  private onWheel(event: React.WheelEvent) {
+    const { camera } = this.props.model;
+    camera.distance = clamp(camera.distance + event.deltaY / 200, 0.01, 10, 0);
+  }
+
+  @action.bound
+  private onMouseDown(event: React.MouseEvent) {
     const {
       model,
       model: {
         camera: { pitch, yaw },
       },
     } = this.props;
-    this.dragger = new Dragger(model, pitch, yaw, Vector2.of(x, y));
+    this.dragger = new Dragger(model, pitch, yaw, Vector2.of(event.nativeEvent.layerX, event.nativeEvent.layerY));
   }
 
   @action.bound
-  private onMouseMove(x: number, y: number) {
+  private onMouseMove(event: React.MouseEvent) {
     if (!this.dragger) {
       return;
     }
-    this.dragger.to = Vector2.of(x, y);
+    this.dragger.to = Vector2.of(event.nativeEvent.layerX, event.nativeEvent.layerY);
   }
 
   @action.bound
-  private onMouseUp(x: number, y: number) {
+  private onMouseUp(event: React.MouseEvent) {
     if (!this.dragger) {
       return;
     }
-    this.dragger.to = Vector2.of(x, y);
+    this.dragger.to = Vector2.of(event.nativeEvent.layerX, event.nativeEvent.layerY);
     this.dragger = undefined;
   }
 }
