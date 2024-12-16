b9ea3d748b2a716973475f902f393a44b25abb60
nmthais <minhnguyenvan8866@gmail.com>
2024-12-16 14:48:26 +1100

    rewrite logic to avoid obstacles ,new function avoid code repetition

--- module/planning/PlanWalkPath/src/PlanWalkPath.hpp
+++ module/planning/PlanWalkPath/src/PlanWalkPath.hpp
@@ -78,8 +78,12 @@ namespace module::planning {
             double obstacle_radius = 0.0;
         } cfg;
 
+
         /// @brief Current magnitude of the translational velocity of the walk command
         double velocity_magnitude = 0.0;
+
+        /// @brief Just testing
+        Eigen::Vector2d walk_around_obstacle(std::vector<Eigen::Vector2d> obstacles, Eigen::Vector2d rDRr);
 
         /// @brief Constrain a velocity vector to ensure it is within the limits
         /// @param v velocity vector to constrain
