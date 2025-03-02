8d8419c1aad6641c0ba4eb3ccaa9c59499bd53c6
Tom0Brien <thomas16441@gmail.com>
2025-03-02 16:28:54 +0100

    Format

--- nusight2/src/client/components/localisation/network.ts
+++ nusight2/src/client/components/localisation/network.ts
@@ -16,7 +16,10 @@ import { FieldIntersection } from "./robot_model";
 import { Line } from "./robot_model";
 
 export class LocalisationNetwork {
-  constructor(private network: Network, private model: LocalisationModel) {
+  constructor(
+    private network: Network,
+    private model: LocalisationModel,
+  ) {
     this.network.on(message.input.Sensors, this.onSensors);
     this.network.on(message.localisation.Field, this.onField);
     this.network.on(message.localisation.Ball, this.onBall);
