d40ff529797cae37d82dd468f44ded2344efc895
Ysobel <ysobelsims@gmail.com>
2025-03-10 15:44:54 +1100

    reaction times unused

--- module/support/ReactionTimer/src/ReactionTimer.cpp
+++ module/support/ReactionTimer/src/ReactionTimer.cpp
@@ -36,7 +36,7 @@ namespace module::support {
     ReactionTimer::ReactionTimer(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
 
         on<Configuration>("ReactionTimer.yaml").then([this](const Configuration& config) {
-            // Use configuration here from file ReactionTimer.yaml
+            log_level = config["log_level"].as<NUClear::LogLevel>();
         });
 
         on<Trigger<ReactionStatistics>>().then([this](const ReactionStatistics& stats) {
