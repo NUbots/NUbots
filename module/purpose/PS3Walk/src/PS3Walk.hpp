b5b37f15d8fce39e0e49c63fc5063bead2473cea
Tom0Brien <thomas16441@gmail.com>
2025-05-11 18:02:11 +1000

    Add button/script yaml map

--- module/purpose/PS3Walk/src/PS3Walk.hpp
+++ module/purpose/PS3Walk/src/PS3Walk.hpp
@@ -31,6 +31,7 @@
 
 #include <Eigen/Core>
 #include <nuclear>
+#include <unordered_map>
 
 #include "Joystick.hpp"
 
@@ -78,6 +79,8 @@ namespace module::purpose {
             double maximum_rotational_velocity = 0;
             /// @brief Maximum allowed acceleration per second (in m/s²)
             double max_acceleration = 0.5;
+            /// @brief Maps button names to script filenames
+            std::unordered_map<std::string, std::string> button_scripts;
         } cfg;
 
         /// @brief Controls interactions with the PS3 controller
