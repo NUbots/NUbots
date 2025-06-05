f07053087ccc49b13e5266291a9c41a43f1f307d
Tom0Brien <thomas16441@gmail.com>
2025-06-05 12:55:39 +1000

    Fix pose

--- module/localisation/Mocap/src/Mocap.cpp
+++ module/localisation/Mocap/src/Mocap.cpp
@@ -1,3 +1,29 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 NUbots
+ *
+ * This file is part of the NUbots codebase.
+ * See https://github.com/NUbots/NUbots for further info.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include "Mocap.hpp"
 
 #include "extension/Configuration.hpp"
@@ -5,12 +31,17 @@
 #include "message/input/MotionCapture.hpp"
 #include "message/localisation/Field.hpp"
 
+#include "utility/math/euler.hpp"
+
 namespace module::localisation {
 
     using extension::Configuration;
 
     using message::input::MotionCapture;
     using message::localisation::RobotPoseGroundTruth;
+
+    using utility::math::euler::mat_to_rpy_intrinsic;
+    using utility::math::euler::rpy_intrinsic_to_mat;
 
     Mocap::Mocap(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
 
@@ -30,14 +61,19 @@ namespace module::localisation {
                     log<DEBUG>("Rigid Body Position: ", rigid_body.position);
                     log<DEBUG>("Rigid Body Rotation: ", rigid_body.rotation);
                     auto robot_pose_ground_truth = std::make_unique<message::localisation::RobotPoseGroundTruth>();
-                    Eigen::Isometry3d Hft        = Eigen::Isometry3d::Identity();
+
+                    // Build transform from field {b} frame to torso {t} frame
+                    Eigen::Isometry3d Hft = Eigen::Isometry3d::Identity();
                     Hft.translation() =
-                        Eigen::Vector3d(rigid_body.position.x(), rigid_body.position.y(), rigid_body.position.z());
-                    Eigen::Quaterniond quat(rigid_body.rotation(0),
-                                            rigid_body.rotation(1),
-                                            rigid_body.rotation(2),
-                                            rigid_body.rotation(3));
-                    Hft.linear()                 = quat.toRotationMatrix();
+                        Eigen::Vector3d(-rigid_body.position.y(), rigid_body.position.x(), rigid_body.position.z());
+                    auto rpy                      = mat_to_rpy_intrinsic(Eigen::Quaterniond(rigid_body.rotation(0),
+                                                                       rigid_body.rotation(1),
+                                                                       rigid_body.rotation(2),
+                                                                       rigid_body.rotation(3))
+                                                        .toRotationMatrix());
+                    Eigen::Vector3d rpy_corrected = Eigen::Vector3d(rpy.y(), -(rpy.z() - M_PI), rpy.x());
+                    Hft.linear()                  = rpy_intrinsic_to_mat(rpy_corrected);
+
                     robot_pose_ground_truth->Hft = Hft;
                     emit(robot_pose_ground_truth);
                 }
