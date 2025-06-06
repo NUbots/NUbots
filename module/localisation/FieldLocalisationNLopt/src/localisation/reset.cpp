81d69837def0ad59c3d1d3dee2607f5c16edbb73
Ysobel <ysobelsims@gmail.com>
2025-06-06 15:49:04 +1000

    a bit of clean up

--- module/localisation/FieldLocalisationNLopt/src/localisation/reset.cpp
+++ module/localisation/FieldLocalisationNLopt/src/localisation/reset.cpp
@@ -44,25 +44,23 @@ namespace module::localisation {
                                                    const std::shared_ptr<const FieldIntersections>& field_intersections,
                                                    const std::shared_ptr<const Goals>& goals,
                                                    const Eigen::Isometry3d& Hrw) {
-        Eigen::Isometry3d Hfw = compute_Hfw(last_certain_state);
-        Eigen::Isometry3d Hrf = Hrw * Hfw.inverse();
+        // Get robot position in field space
+        Eigen::Isometry3d Hrf = Hrw * compute_Hfw(last_certain_state).inverse();
+        Eigen::Vector3d rRFf  = Hrf.inverse().translation();
 
-        Eigen::Vector3d rRFf = Hrf.inverse().translation();  // Robot position in world space {w} [m]
-
-        // How much distance from the robot to the top of the field (fd.dimensions.field_length / 2 + 0.2)
-        // And so on for the other sides
+        // How much distance from the robot to each side of the field
         // This represents the allowed change in the robot and world
-        double x_min = (fd.dimensions.field_length / 2 + 0.2) - rRFf.x();
-        double x_max = -(fd.dimensions.field_length / 2 + 0.2) - rRFf.x();
-        double y_min = (fd.dimensions.field_width / 2 + 0.2) - rRFf.y();
-        double y_max = -(fd.dimensions.field_width / 2 + 0.2) - rRFf.y();
+        double x_min = -(fd.dimensions.field_length / 2 + 0.2) - rRFf.x();
+        double x_max = (fd.dimensions.field_length / 2 + 0.2) - rRFf.x();
+        double y_min = -(fd.dimensions.field_width / 2 + 0.2) - rRFf.y();
+        double y_max = (fd.dimensions.field_width / 2 + 0.2) - rRFf.y();
 
         // Define search window around last known good position
         const double window_size         = 2.0;
         const double step_size           = 0.3;
         const std::vector<double> angles = {0, M_PI_2, M_PI, -M_PI_2, M_PI_4, 3 * M_PI_4, -M_PI_4, -3 * M_PI_4};
 
-        std::vector<Eigen::Vector3d> hypotheses;
+        std::vector<std::pair<Eigen::Vector3d, double>> hypotheses;
         for (double dx = -window_size; dx <= window_size; dx += step_size) {
             for (double dy = -window_size; dy <= window_size; dy += step_size) {
                 // Try hypotheses around the last certain state (state is world position)
@@ -75,81 +73,69 @@ namespace module::localisation {
                 }
 
                 // Add hypotheses for each position with all compass and diagonal headings
+                // Calculate cost using NLopt
                 for (const auto& angle : angles) {
-                    hypotheses.emplace_back(x, y, angle);
+                    Eigen::Vector3d hypothesis(x, y, angle);
+                    double cost =
+                        run_field_line_optimisation(hypothesis, field_lines.rPWw, field_intersections, goals).second;
+                    hypotheses.emplace_back(hypothesis, cost);
                 }
             }
-        }
-
-        // Evaluate all hypotheses using NLopt
-        std::vector<std::pair<Eigen::Vector3d, double>> ranked_hypotheses;
-        for (const auto& hypothesis : hypotheses) {
-            double cost = run_field_line_optimisation(hypothesis, field_lines.rPWw, field_intersections, goals).second;
-            ranked_hypotheses.emplace_back(hypothesis, cost);
         }
 
         // Sort hypotheses by cost (ascending)
-        std::sort(ranked_hypotheses.begin(), ranked_hypotheses.end(), [](const auto& a, const auto& b) {
+        std::sort(hypotheses.begin(), hypotheses.end(), [](const auto& a, const auto& b) {
             return a.second < b.second;
         });
 
-        Eigen::Vector3d best_hypothesis = Eigen::Vector3d::Zero();
-        // Check if cost is below threshold, if not then check the whole field
-        if (ranked_hypotheses.empty() || ranked_hypotheses[0].second > 1.0) {
-            if (ranked_hypotheses.empty()) {
-                log<INFO>("Uncertainty reset (global): No hypotheses found, searching whole field");
-            }
-            else {
-                log<INFO>("Uncertainty reset (global): Cost too high, searching whole field",
-                          ranked_hypotheses[0].second);
-            }
-            hypotheses.clear();
-            for (double x = x_min; x <= x_max; x += step_size) {
-                for (double y = y_min; y <= y_max; y += step_size) {
-                    // Get field positions of robot considering world to field
-                    double pos_x = x;  // + rRWw.x();
-                    double pos_y = y;  // + rRWw.y();
+        // If local search is valid, use the lowest cost hypothesis
+        if (!ranked_hypothesis.empty() && hypotheses[0].second < cfg.cost_threshold) {
+            log<INFO>("Uncertainty reset (local): using best hypothesis", hypotheses[0].second);
+            log<INFO>("Best hypothesis: ", hypotheses[0].second.transpose());
 
-                    // Add hypotheses for each position with all compass and diagonal headings
-                    for (const auto& angle : angles) {
-                        hypotheses.emplace_back(pos_x, pos_y, angle);
-                    }
-                }
-            }
+            // Set the state to the best hypothesis
+            state = hypotheses[0].second;
+            kf.set_state(state);
+            kf.time(Eigen::Matrix<double, n_inputs, 1>::Zero(), 0);
+            return;
+        }
 
-            // Evaluate all hypotheses using NLopt
-            ranked_hypotheses.clear();
-            for (const auto& hypothesis : hypotheses) {
-                double cost =
-                    run_field_line_optimisation(hypothesis, field_lines.rPWw, field_intersections, goals).second;
-                ranked_hypotheses.emplace_back(hypothesis, cost);
-            }
-
-            // Sort hypotheses by cost (ascending)
-            std::sort(ranked_hypotheses.begin(), ranked_hypotheses.end(), [](const auto& a, const auto& b) {
-                return a.second < b.second;
-            });
-            // best_hypothesis = ranked_hypotheses[0].first;
-
-            // Fix mirror field issue by using the top 2 hypothesis closest to the last certain state
-            // If the second best hypothesis is too far away, use the first one
-            if (((ranked_hypotheses[0].first - last_certain_state).norm()
-                 > (ranked_hypotheses[1].first - last_certain_state).norm())
-                || (ranked_hypotheses[1].second > cfg.cost_threshold)) {
-                best_hypothesis = ranked_hypotheses[1].first;
-                log<INFO>("Uncertainty reset (global): Using second best hypothesis", ranked_hypotheses[1].second);
-            }
-            else {
-                log<INFO>("Uncertainty reset (global): Using best hypothesis", ranked_hypotheses[0].second);
-                best_hypothesis = ranked_hypotheses[0].first;
-            }
+        // The local search did not yield a valid hypothesis, use a global search
+        if (hypotheses.empty()) {
+            log<INFO>("Uncertainty reset (global): No hypotheses found, searching whole field");
         }
         else {
-            log<INFO>("Uncertainty reset (local): Cost below threshold, using best hypothesis",
-                      ranked_hypotheses[0].second);
-            best_hypothesis = ranked_hypotheses[0].first;
+            log<INFO>("Uncertainty reset (global): Cost too high, searching whole field", hypotheses[0].second);
         }
-        log<INFO>("Best hypothesis: ", best_hypothesis.x(), ", ", best_hypothesis.y());
+
+        // Iterate over the entire field area with a grid search
+        hypotheses.clear();
+        for (double x = x_min; x <= x_max; x += step_size) {
+            for (double y = y_min; y <= y_max; y += step_size) {
+                // Add hypotheses for each position with all compass and diagonal headings
+                // Calculate cost using NLopt
+                for (const auto& angle : angles) {
+                    Eigen::Vector3d hypothesis(x, y, angle);
+                    double cost =
+                        run_field_line_optimisation(hypothesis, field_lines.rPWw, field_intersections, goals).second;
+                    hypotheses.emplace_back(hypothesis, cost);
+                }
+            }
+        }
+
+        // Sort hypotheses by cost (ascending)
+        std::sort(hypotheses.begin(), hypotheses.end(), [](const auto& a, const auto& b) {
+            return a.second < b.second;
+        });
+
+        // If the best hypothesis is not on the same side, mirror it
+        bool same_side = (hypotheses[0].first.x() * last_certain_state.x() >= 0);
+        Eigen::Vector3d best_hypothesis =
+            same_side ? hypotheses[0].first
+                      : Eigen::Vector3d(-hypotheses[0].first.x(), -hypotheses[0].first.y(), hypotheses[0].first.z());
+        log<INFO>("Same side: ", same_side, " (", hypotheses[0].first.x(), ", ", last_certain_state.x(), ")");
+
+        log<INFO>("Best hypothesis: ", best_hypothesis.transpose());
         // Set the state to the best hypothesis
         state = best_hypothesis;
         kf.set_state(state);
