9bf118c71fdcab9a8edb0df33ea2302aa906df4e
Ysobel <ysobelsims@gmail.com>
2025-06-22 20:16:25 +1000

    change robotloc to use purpose message

--- module/localisation/RobotLocalisation/src/RobotLocalisation.hpp
+++ module/localisation/RobotLocalisation/src/RobotLocalisation.hpp
@@ -33,6 +33,7 @@
 #include "RobotModel.hpp"
 
 #include "message/localisation/Field.hpp"
+#include "message/purpose/Purpose.hpp"
 #include "message/support/FieldDescription.hpp"
 #include "message/vision/GreenHorizon.hpp"
 
@@ -81,15 +82,17 @@ namespace module::localisation {
             long missed_count = 0;
             /// @brief A unique identifier for the robot
             unsigned long id;
+
             /// @brief The unique identifier of the robot if it is a teammate
             /// If it is not a teammate, this will be 0
-            unsigned long teammate_id = 0;
-            /// @brief Penalisation state for teammate
-            /// This allows behaviour systems to ignore teammates that are not in the game, but keep tracking them
-            bool penalised = false;
+            bool teammate                     = false;
+            message::purpose::Purpose purpose = message::purpose::Purpose();
 
             /// @brief Constructor that sets the state for the UKF
-            TrackedRobot(const Eigen::Vector3d& initial_rRWw, const Config::UKF& cfg_ukf, const unsigned long next_id)
+            TrackedRobot(const Eigen::Vector3d& initial_rRWw,
+                         const Config::UKF& cfg_ukf,
+                         const unsigned long next_id,
+                         const std::unique_ptr<message::purpose::Purpose>& p = nullptr)
                 : id(next_id) {
                 NUClear::log<NUClear::LogLevel::DEBUG>("Making robot with id: ", id);
                 RobotModel<double>::StateVec initial_state;
@@ -104,6 +107,11 @@ namespace module::localisation {
                 process_noise.rRWw      = cfg_ukf.noise.process.position;
                 process_noise.vRw       = cfg_ukf.noise.process.velocity;
                 ukf.model.process_noise = process_noise;
+
+                if (p) {
+                    teammate = true;
+                    purpose  = *p;
+                }
             }
 
             // Get the robot's position in world space
@@ -132,8 +140,7 @@ namespace module::localisation {
         /// @param teammate_id The unique identifier of the robot if it is a teammate
         /// @param penalised Whether the robot is penalised or not
         void data_association(const std::vector<Eigen::Vector3d>& robots_rRWw,
-                              uint teammate_id = 0,
-                              bool penalised   = false);
+                              const std::unique_ptr<message::purpose::Purpose>& purpose = nullptr);
 
         /// @brief Run maintenance on the tracked robots
         /// This will remove any viewable robots that have been missed too many times or are too close to another robot
