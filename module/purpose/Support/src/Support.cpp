85b1c643ebefe83acba82f74a236051d7f2e7bfe
Ysobel <ysobelsims@gmail.com>
2025-06-24 21:32:41 +1000

    compile

--- module/purpose/Support/src/Support.cpp
+++ module/purpose/Support/src/Support.cpp
@@ -4,14 +4,25 @@
 #include "extension/Configuration.hpp"
 
 #include "message/localisation/Ball.hpp"
+#include "message/localisation/Field.hpp"
 #include "message/purpose/Player.hpp"
+#include "message/strategy/WalkToFieldPosition.hpp"
+#include "message/support/FieldDescription.hpp"
+
+#include "utility/math/euler.hpp"
 
 namespace module::purpose {
 
     using extension::Configuration;
 
+    using SupportMsg = message::purpose::Support;
+
     using message::localisation::Ball;
-    using message::purpose::Support;
+    using message::localisation::Field;
+    using message::strategy::WalkToFieldPosition;
+    using message::support::FieldDescription;
+
+    using utility::math::euler::pos_rpy_to_transform;
 
     Support::Support(std::unique_ptr<NUClear::Environment> environment) : BehaviourReactor(std::move(environment)) {
 
@@ -20,14 +31,20 @@ namespace module::purpose {
             this->log_level = config["log_level"].as<NUClear::LogLevel>();
         });
 
-        on<Provide<Support>, With<Ball>, With<Field>, With<FieldDescription>>.then(
+        on<Provide<SupportMsg>, With<Ball>, With<Field>, With<FieldDescription>>().then(
             [this](const Ball& ball, const Field& field, const FieldDescription& fd) {
                 // Get ball in field coordinates
                 Eigen::Vector3d rBFf = field.Hfw * ball.rBWw;
 
                 // Keep in line with the ball on the x-axis, but stay on the other side of the field on the y-axis
-                double y_position        = rBFf.y() < 0 ? rBFf.y() + cfg.y_offset : -fd.field_length / 2.0;
-                Eigen::Vecotr3d position = Eigen::Vector3d(rBFf.x(), fd.field_length / 2.0, 0.0);
+                Eigen::Vector3d position = rBFf;
+                position.y()             = rBFf.y() < 0 ? rBFf.y() + (fd.dimensions.field_width / 2)
+                                                        : rBFf.y() - (fd.dimensions.field_width / 2);
+
+                // Walk to the position
+                emit<Task>(
+                    std::make_unique<WalkToFieldPosition>(pos_rpy_to_transform(position, Eigen::Vector3d(0, 0, -M_PI)),
+                                                          true));
             });
     }
 
