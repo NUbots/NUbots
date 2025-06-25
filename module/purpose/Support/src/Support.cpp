2ded98344e904c6fdb97acb57beae65046a58093
Ysobel <ysobelsims@gmail.com>
2025-06-25 19:25:33 +1000

    support positioning

--- module/purpose/Support/src/Support.cpp
+++ module/purpose/Support/src/Support.cpp
@@ -3,6 +3,7 @@
 #include "extension/Behaviour.hpp"
 #include "extension/Configuration.hpp"
 
+#include "message/input/Sensors.hpp"
 #include "message/localisation/Ball.hpp"
 #include "message/localisation/Field.hpp"
 #include "message/purpose/Player.hpp"
@@ -17,6 +18,7 @@ namespace module::purpose {
 
     using SupportMsg = message::purpose::Support;
 
+    using message::input::Sensors;
     using message::localisation::Ball;
     using message::localisation::Field;
     using message::strategy::WalkToFieldPosition;
@@ -31,16 +33,28 @@ namespace module::purpose {
             this->log_level = config["log_level"].as<NUClear::LogLevel>();
         });
 
-        on<Provide<SupportMsg>, With<Ball>, With<Field>, With<FieldDescription>>().then(
-            [this](const Ball& ball, const Field& field, const FieldDescription& fd) {
+        on<Provide<SupportMsg>, With<Ball>, With<Sensors>, With<Field>, With<FieldDescription>>().then(
+            [this](const Ball& ball, const Sensors& sensors, const Field& field, const FieldDescription& fd) {
                 // Get ball in field coordinates
                 Eigen::Vector3d rBFf = field.Hfw * ball.rBWw;
+                Eigen::Vector3d rBRr = sensors.Hrw * ball.rBWw;
 
                 // Keep in line with the ball on the x-axis, but stay on the other side of the field on the y-axis
                 Eigen::Vector3d position = rBFf;
-                position.y()             = rBFf.y() < 0 ? rBFf.y() + (fd.dimensions.field_width / 4)
+                position.y()             = rBRr.y() < 0 ? rBFf.y() + (fd.dimensions.field_width / 4)
                                                         : rBFf.y() - (fd.dimensions.field_width / 4);
+
+                // If there isn't really space to walk to the desired side, walk to the other side
+                if (position.y() > fd.dimensions.field_width / 2) {
+                    position.y() = rBFf.y() - (fd.dimensions.field_width / 4);
+                }
+                else if (position.y() < -fd.dimensions.field_width / 2) {
+                    position.y() = rBFf.y() + (fd.dimensions.field_width / 4);
+                }
+
                 // Bound x by the penalty areas
+                double penalty_area = fd.dimensions.field_length / 2 - fd.dimensions.penalty_area_length;
+                position.x()        = std::clamp(position.x(), -penalty_area, penalty_area);
                 // Closest side, unless it wont fit
                 // Walk to the position
                 emit<Task>(
