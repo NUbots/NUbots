e59c550e434183fd05788c91382de5ffd7cafd2b
Ysobel <ysobelsims@gmail.com>
2025-07-06 19:47:25 +1000

    Config by part of body to relax rather than script name
    so that switching doesn't require code changes for the type of sequence

--- module/planning/FallingRelaxPlanner/src/FallingRelaxPlanner.cpp
+++ module/planning/FallingRelaxPlanner/src/FallingRelaxPlanner.cpp
@@ -30,40 +30,17 @@
 #include "extension/Configuration.hpp"
 
 #include "message/actuation/Limbs.hpp"
-#include "message/behaviour/state/Stability.hpp"
-#include "message/input/Sensors.hpp"
-#include "message/planning/RelaxWhenFalling.hpp"
 
-#include "utility/nusight/NUhelpers.hpp"
-#include "utility/skill/Script.hpp"
 #include "utility/support/yaml_expression.hpp"
 
 namespace module::planning {
 
     using extension::Configuration;
+    using message::actuation::ArmsSequence;
+    using message::actuation::BodySequence;
     using message::actuation::HeadSequence;
-    using message::actuation::LimbsSequence;
     using message::actuation::UpperBodySequence;
-    using message::behaviour::state::Stability;
-    using message::input::Sensors;
-    using message::planning::RelaxWhenFalling;
-    using utility::nusight::graph;
-    using utility::skill::load_script;
     using utility::support::Expression;
-
-    double smooth(double value, double new_value, double alpha) {
-        return alpha * value + (1.0 - alpha) * new_value;
-    }
-
-    /// @brief A state to categorise each of the properties we are monitoring
-    enum class State {
-        /// @brief This sensor believes the robot is stable
-        STABLE,
-        /// @brief This sensor believes the robot is unstable but is unsure if it is falling
-        UNSTABLE,
-        /// @brief This sensor believes the robot is falling
-        FALLING
-    };
 
     FallingRelaxPlanner::FallingRelaxPlanner(std::unique_ptr<NUClear::Environment> environment)
         : BehaviourReactor(std::move(environment)) {
@@ -83,94 +60,29 @@ namespace module::planning {
             cfg.acc_angle.unstable  = config["accelerometer_angle"]["unstable"].as<Expression>();
             cfg.acc_angle.falling   = config["accelerometer_angle"]["falling"].as<Expression>();
             cfg.acc_angle.smoothing = config["accelerometer_angle"]["smoothing"].as<Expression>();
-            cfg.fall_script         = config["fall_script"].as<std::string>();
+
+            std::string relax_part = config["relax"].as<std::string>();
+
+            if (relax_part == "Body") {
+                cfg.fall_script = "Relax.yaml";
+                make_relax<BodySequence>();
+            }
+            else if (relax_part == "UpperBody") {
+                cfg.fall_script = "RelaxUpperBody.yaml";
+                make_relax<UpperBodySequence>();
+            }
+            else if (relax_part == "Arms") {
+                cfg.fall_script = "RelaxArms.yaml";
+                make_relax<ArmsSequence>();
+            }
+            else if (relax_part == "Head") {
+                cfg.fall_script = "RelaxHead.yaml";
+                make_relax<HeadSequence>();
+            }
+            else {
+                log<ERROR>("Unknown relax part: ", relax_part);
+            }
         });
-
-        on<Provide<RelaxWhenFalling>, Uses<UpperBodySequence>, Trigger<Sensors>>().then(
-            [this](const RunReason& run_reason, const Uses<UpperBodySequence>& upper_body, const Sensors& sensors) {
-                // If this isn't a sensors update, keep doing what we were doing
-                if (run_reason != RunReason::OTHER_TRIGGER) {
-                    emit<Task>(std::make_unique<Continue>());
-                    return;
-                }
-
-                auto& a = sensors.accelerometer;
-                auto& g = sensors.gyroscope;
-
-                // Smooth the values we use to determine if we are falling
-                gyro_mag  = smooth(gyro_mag,
-                                  std::abs(std::abs(g.x()) + std::abs(g.y()) + std::abs(g.z()) - cfg.gyro_mag.mean),
-                                  cfg.gyro_mag.smoothing);
-                acc_mag   = smooth(acc_mag,  //
-                                 std::abs(a.norm()),
-                                 cfg.acc_mag.smoothing);
-                acc_angle = smooth(acc_angle,
-                                   std::acos(std::min(1.0, std::abs(a.normalized().z())) - cfg.acc_angle.mean),
-                                   cfg.acc_angle.smoothing);
-
-
-                // Check if we are stable according to each sensor
-                State gyro_mag_state  = gyro_mag < cfg.gyro_mag.unstable  ? State::STABLE
-                                        : gyro_mag < cfg.gyro_mag.falling ? State::UNSTABLE
-                                                                          : State::FALLING;
-                State acc_mag_state   = acc_mag > cfg.acc_mag.unstable  ? State::STABLE
-                                        : acc_mag > cfg.acc_mag.falling ? State::UNSTABLE
-                                                                        : State::FALLING;
-                State acc_angle_state = acc_angle < cfg.acc_angle.unstable  ? State::STABLE
-                                        : acc_angle < cfg.acc_angle.falling ? State::UNSTABLE
-                                                                            : State::FALLING;
-
-                // Falling if any 2 of 3 sensors report falling or the angle reports falling
-                bool falling = (gyro_mag_state == State::FALLING && acc_mag_state == State::FALLING)
-                               || (gyro_mag_state == State::FALLING && acc_angle_state == State::FALLING)
-                               || (acc_mag_state == State::FALLING && acc_angle_state == State::FALLING)
-                               || (acc_angle_state == State::FALLING);
-
-                emit(graph("Falling sensor: x:gyro, y:acc, z:angle",
-                           gyro_mag,
-                           acc_mag,
-                           acc_angle * 180 / 3.14159265358979));
-                emit(graph("Falling", falling));
-
-                if (falling) {
-                    log<DEBUG>("Falling:",
-                               "Gyroscope Magnitude:",
-                               gyro_mag,
-                               gyro_mag_state == State::FALLING    ? "FALLING"
-                               : gyro_mag_state == State::UNSTABLE ? "UNSTABLE"
-                                                                   : "STABLE",
-                               "Accelerometer Magnitude:",
-                               acc_mag,
-                               acc_mag_state == State::FALLING    ? "FALLING"
-                               : acc_mag_state == State::UNSTABLE ? "UNSTABLE"
-                                                                  : "STABLE",
-                               "Accelerometer Angle:",
-                               acc_angle,
-                               acc_angle_state == State::FALLING    ? "FALLING"
-                               : acc_angle_state == State::UNSTABLE ? "UNSTABLE"
-                                                                    : "STABLE");
-
-                    emit(std::make_unique<Stability>(Stability::FALLING));
-
-                    // We are falling but not running relax yet, run relax
-                    if (upper_body.run_state == RunState::NO_TASK) {
-                        log<INFO>("Falling!");
-                        emit<Task>(load_script<UpperBodySequence>(cfg.fall_script));
-                    }
-                    // Falling and already relaxing, continue relaxing
-                    else {
-                        emit<Task>(std::make_unique<Continue>());
-                    }
-                }
-                // Not falling, but still running, set stability to standing
-                else if (!falling && upper_body.run_state == RunState::RUNNING) {
-                    log<INFO>("Unfalling!");
-                    emit(std::make_unique<Stability>(Stability::STANDING));
-                }
-                else {
-                    log<INFO>("No task");
-                }
-            });
     }
 
 }  // namespace module::planning
