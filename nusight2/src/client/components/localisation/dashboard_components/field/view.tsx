6028c37a4c4025e1e98778e159b134b96835c948
Kim Khang Hoang <140460564+KimKhangHoang@users.noreply.github.com>
2025-07-08 15:06:55 +1000

    [NUsight] Merge dashboard and localisation tab (#1643)
    Co-authored-by: Tom0Brien <thomas16441@gmail.com>

--- nusight2/src/client/components/localisation/dashboard_components/field/view.tsx
+++ nusight2/src/client/components/localisation/dashboard_components/field/view.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Component } from "react";
 import { observer } from "mobx-react";
 
-import { Renderer } from "../../../render2d/renderer";
+import { Renderer } from "../../../../render2d/renderer";
 
-import { FieldModel } from "./model";
-import { FieldViewModel } from "./view_model";
+import { DashboardFieldModel } from "./model";
+import { DashboardFieldViewModel } from "./view_model";
 
 export type FieldProps = {
-  model: FieldModel;
+  model: DashboardFieldModel;
 };
 
 @observer
-export class Field extends Component<FieldProps> {
+export class DashboardFieldView extends Component<FieldProps> {
   render() {
     const model = this.props.model;
-    const viewModel = FieldViewModel.of(model);
+    const viewModel = DashboardFieldViewModel.of(model);
     return (
       <div className="h-full m-0 p-0 bottom-0 absolute w-full">
         <Renderer
