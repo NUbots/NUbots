3aa376fe6705c14358457e814f202d152986f4b8
Tom0Brien <41043317+Tom0Brien@users.noreply.github.com>
2025-07-08 03:31:18 +0000

    Deploying to gh-pages from @ NUbots/NUbots@39e345231d00ec62145a3f3315e0032abf5b0f94 🚀

--- navtreeindex17.js
+++ navtreeindex17.js
@@ -1,6 +1,5 @@
 var NAVTREEINDEX17 =
 {
-"structmodule_1_1support_1_1optimisation_1_1StrafeEvaluator_1_1Config.html":[3,0,3,12,2,10,0],
 "structmodule_1_1support_1_1optimisation_1_1StrafeEvaluator_1_1Config.html#a29006077dcf7a37a7f3bf77ee9ba5b05":[3,0,3,12,2,10,0,0],
 "structmodule_1_1support_1_1optimisation_1_1WalkEvaluator_1_1Config.html":[3,0,3,12,2,12,0],
 "structmodule_1_1support_1_1optimisation_1_1WalkEvaluator_1_1Config.html#a0243517121b347d2ea565df50bdda7c7":[3,0,3,12,2,12,0,0],
@@ -249,5 +248,6 @@ var NAVTREEINDEX17 =
 "structutility_1_1input_1_1LimbID.html#af112b2b27f4997918d1a55f1eebdde70":[3,0,7,2,1,21],
 "structutility_1_1input_1_1LimbID.html#afc7d69eaf9e751008379b9d8371f2df6":[3,0,7,2,1,11],
 "structutility_1_1input_1_1LimbID.html#afd8f97218aa8df4f7c682a6bd59b67c4":[3,0,7,2,1,27],
-"structutility_1_1input_1_1ServoID.html":[3,0,7,2,2]
+"structutility_1_1input_1_1ServoID.html":[3,0,7,2,2],
+"structutility_1_1input_1_1ServoID.html#a099d66316df7ca2bd3256dd435a3b272":[3,0,7,2,2,6]
 };
--- structmodule_1_1strategy_1_1WalkToBall_1_1Config.js
+++ structmodule_1_1strategy_1_1WalkToBall_1_1Config.js
@@ -5,7 +5,6 @@ var structmodule_1_1strategy_1_1WalkToBall_1_1Config =
     [ "avoid_opponent_offset", "structmodule_1_1strategy_1_1WalkToBall_1_1Config.html#aa6911ba8b1b2d062a672c52f9e25d893", null ],
     [ "ball_approach_distance", "structmodule_1_1strategy_1_1WalkToBall_1_1Config.html#a8c1f65493a26e6dc05d1ca9393386fa6", null ],
     [ "ball_kick_distance", "structmodule_1_1strategy_1_1WalkToBall_1_1Config.html#abb8208e99cfc7559c5f74c275fb08c46", null ],
-    [ "ball_search_timeout", "structmodule_1_1strategy_1_1WalkToBall_1_1Config.html#a37852bb8f11bf710e84cd8a44184accb", null ],
     [ "ball_y_offset", "structmodule_1_1strategy_1_1WalkToBall_1_1Config.html#a958bcdb230880540d2662b7bd4f33779", null ],
     [ "distance_behind_ball", "structmodule_1_1strategy_1_1WalkToBall_1_1Config.html#a0daf377729af79ce5322fb33d19ca066", null ],
     [ "goal_target_offset", "structmodule_1_1strategy_1_1WalkToBall_1_1Config.html#ae78d1a0a643cad341f63f9104243d32e", null ],
