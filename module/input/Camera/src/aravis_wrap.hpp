81d29956444bb58eba37e87a790a07f8651a3b34
Trent Houliston <trent@houliston.me>
2025-07-15 15:46:51 -0300

    Enhance Camera module with improved configuration and time synchronization features

--- module/input/Camera/src/aravis_wrap.hpp
+++ module/input/Camera/src/aravis_wrap.hpp
@@ -58,77 +58,77 @@ namespace module::input::arv {
     }
 
     inline ArvCamera* camera_new(const std::string& device_description) {
-        return wrap_return(arv_camera_new, device_description.c_str());
+        return wrap_return(::arv_camera_new, device_description.c_str());
     }
 
     inline ArvStream* camera_create_stream(ArvCamera* camera, ArvStreamCallback callback, void* user_data) {
         if (ARV_IS_CAMERA(camera) != TRUE) {
             throw(std::runtime_error("Invalid Aravis camera: camera_stop_acquisition"));
         }
-        return wrap_return(arv_camera_create_stream, camera, callback, user_data);
+        return wrap_return(::arv_camera_create_stream, camera, callback, user_data);
     }
 
     inline void device_execute_command(ArvDevice* device, const char* feature) {
         if (ARV_IS_DEVICE(device) != TRUE) {
             throw(std::runtime_error("Invalid Aravis device: device_execute_command"));
         }
-        wrap(arv_device_execute_command, device, feature);
+        wrap(::arv_device_execute_command, device, feature);
     }
 
     inline gint64 device_get_integer_feature_value(ArvDevice* device, const char* feature) {
         if (ARV_IS_DEVICE(device) != TRUE) {
             throw(std::runtime_error(fmt::format("Attempt to get feature '{}' from an invalid device", feature)));
         }
-        return wrap_return(arv_device_get_integer_feature_value, device, feature);
+        return wrap_return(::arv_device_get_integer_feature_value, device, feature);
     }
 
     inline bool device_get_boolean_feature_value(ArvDevice* device, const char* feature) {
         if (ARV_IS_DEVICE(device) != TRUE) {
             throw(std::runtime_error(fmt::format("Attempt to get feature '{}' from an invalid device", feature)));
         }
-        return wrap_return(arv_device_get_boolean_feature_value, device, feature) == TRUE;
+        return wrap_return(::arv_device_get_boolean_feature_value, device, feature) == TRUE;
     }
 
     inline void camera_start_acquisition(ArvCamera* camera) {
         if (ARV_IS_CAMERA(camera) != TRUE) {
             throw(std::runtime_error("Invalid Aravis camera: camera_start_acquisition"));
         }
-        wrap(arv_camera_start_acquisition, camera);
+        wrap(::arv_camera_start_acquisition, camera);
     }
 
     inline void camera_stop_acquisition(ArvCamera* camera) {
         if (ARV_IS_CAMERA(camera) != TRUE) {
             throw(std::runtime_error("Invalid Aravis camera: camera_stop_acquisition"));
         }
-        wrap(arv_camera_stop_acquisition, camera);
+        wrap(::arv_camera_stop_acquisition, camera);
     }
 
     inline void camera_set_region(ArvCamera* camera, gint x, gint y, gint width, gint height) {
         if (ARV_IS_CAMERA(camera) != TRUE) {
             throw(std::runtime_error("Invalid Aravis camera: camera_set_region"));
         }
-        wrap(arv_camera_set_region, camera, x, y, width, height);
+        wrap(::arv_camera_set_region, camera, x, y, width, height);
     }
 
     inline void camera_gv_set_packet_size(ArvCamera* camera, gint packet_size) {
         if (ARV_IS_CAMERA(camera) != TRUE) {
             throw(std::runtime_error("Invalid Aravis camera: camera_gv_set_packet_size"));
         }
-        wrap(arv_camera_gv_set_packet_size, camera, packet_size);
+        wrap(::arv_camera_gv_set_packet_size, camera, packet_size);
     }
 
     inline guint camera_get_payload(ArvCamera* camera) {
         if (ARV_IS_CAMERA(camera) != TRUE) {
             throw(std::runtime_error("Attempt to get payload from a non-valid Aravis camera"));
         }
-        return wrap_return(arv_camera_get_payload, camera);
+        return wrap_return(::arv_camera_get_payload, camera);
     }
 
     inline void stream_set_emit_signals(ArvStream* stream, bool emit_signals) {
         if (ARV_IS_STREAM(stream) != TRUE) {
             throw(std::runtime_error("Invalid Aravis stream: stream_set_emit_signals"));
         }
-        arv_stream_set_emit_signals(stream, emit_signals ? TRUE : FALSE);
+        ::arv_stream_set_emit_signals(stream, emit_signals ? TRUE : FALSE);
     }
 
 }  // namespace module::input::arv
