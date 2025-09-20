75b6784c410957fe4fc571a0cc2d975c44b0d5a4
Austin Bagnall <austinbagnall@live.com.au>
2025-09-21 02:03:06 +1000

    main task - tail-led recursion + fizzbuzz

--- module/onboarding/Pong/src/Pong.cpp
+++ module/onboarding/Pong/src/Pong.cpp
@@ -9,6 +9,45 @@ namespace module::onboarding {
 
     using extension::Configuration;
 
+    int TARGET = 50;
+
+    bool pre_check(int lower_bound, int upper_bound) {
+        // ensure valid range
+        if (lower_bound >= upper_bound) {
+            return false;
+        }
+        return true;
+    }
+
+    std::string fizz_buzz(int number) {
+        // fizz, buzz, fizzbuzz?
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+        else if (number % 3 == 0) {
+            return "Fizz";
+        }
+        else if (number % 5 == 0) {
+            return "Buzz";
+        }
+    }
+
+    void recursive_iterate(int current, int target) {
+        // tail led direct recursion
+        if (current > target) {
+            // robot nod here
+            return;
+        }
+        if (current % 3 == 0 || current % 5 == 0) {
+            std::cout << fizz_buzz(current) << std::endl;
+        }
+        else {
+            std::cout << current << std::endl;
+        }
+        recursive_iterate(current + 1, target);
+    }
+
+
     Pong::Pong(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
 
         using message::onboarding::Ping;
@@ -22,7 +61,12 @@ namespace module::onboarding {
         on<Startup>().then([this] {
             // Start the ping pong chain
             auto pong_msg = std::make_unique<Pong>();
-            emit(pong_msg);
+            // std::cout << "[DEBUG] Pre-check result: " << pre_check(1, TARGET) << std::endl;
+            if (pre_check(1, TARGET)) {
+                recursive_iterate(1, TARGET);
+            }
+
+            //  emit(pong_msg);
         });
 
         on<Trigger<Ping>>().then([this](const Ping& ping_msg) {
