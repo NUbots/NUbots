9f77cd5a229119f46c73f6b5ae9bdec7b379c461
dargem <genericz145@gmail.com>
2025-09-30 21:21:12 +1000

    ping pong judge implementation

--- module/onboarding/Ping/src/Ping.cpp
+++ module/onboarding/Ping/src/Ping.cpp
@@ -6,25 +6,28 @@
 #include "message/onboarding/Pong.hpp"
 
 namespace module::onboarding {
-
     using extension::Configuration;
 
     Ping::Ping(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+        // Responsible for responding to pongs with an incremented ping
 
         using message::onboarding::Ping;
         using message::onboarding::Pong;
 
         on<Configuration>("Ping.yaml").then([this](const Configuration& config) {
-            // Use configuration here from file Ping.yaml
             this->log_level = config["log_level"].as<NUClear::LogLevel>();
         });
 
         on<Startup>().then([this] {
-            // Vibe
+            // Pass
         });
 
         on<Trigger<Pong>>().then([this](const Pong& pong_msg) {
-            auto ping_msg = std::make_unique<Ping>();
+            // Constructs an increment ping from the pong, then emits it
+            auto ping_msg       = std::make_unique<Ping>();
+            ping_msg->value     = pong_msg.value + pong_msg.iteration + 1;
+            ping_msg->iteration = pong_msg.iteration + 1;
+            log<INFO>("Total is: " + std::to_string(ping_msg->value));
             log<INFO>("Ping");
             emit(ping_msg);
         });
--- module/onboarding/Pong/src/Pong.cpp
+++ module/onboarding/Pong/src/Pong.cpp
@@ -4,32 +4,44 @@
 
 #include "message/onboarding/Ping.hpp"
 #include "message/onboarding/Pong.hpp"
+#include "message/onboarding/VerifiedPing.hpp"
 
 namespace module::onboarding {
 
     using extension::Configuration;
 
     Pong::Pong(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
+        // Responsible for starting the ping reaction and responding to pings with pongs
 
         using message::onboarding::Ping;
         using message::onboarding::Pong;
+        using message::onboarding::VerifiedPing;
 
         on<Configuration>("Pong.yaml").then([this](const Configuration& config) {
-            // Use configuration here from file Pong.yaml
             this->log_level = config["log_level"].as<NUClear::LogLevel>();
         });
 
         on<Startup>().then([this] {
-            // Start the ping pong chain
-            auto pong_msg = std::make_unique<Pong>();
+            // Initiates the ping pong chain on startup
+            auto pong_msg       = std::make_unique<Pong>();
+            pong_msg->value     = 0;
+            pong_msg->iteration = 0;
             emit(pong_msg);
         });
 
         on<Trigger<Ping>>().then([this](const Ping& ping_msg) {
-            // Logs INFO level message of "Pong"
+            // Constructs then emits a pong message in response to a ping
             log<INFO>("Pong");
-            // Constructs then emits a pong message
-            auto pong_msg = std::make_unique<Pong>();
+            if (ping_msg.iteration >= 10) {
+                log<INFO>("Finished iterating, value is: " + std::to_string(ping_msg.value));
+                auto judged_msg = std::make_unique<VerifiedPing>();
+                emit(judged_msg);
+                return;
+            }
+
+            auto pong_msg       = std::make_unique<Pong>();
+            pong_msg->value     = ping_msg.value;
+            pong_msg->iteration = ping_msg.iteration;
             emit(pong_msg);
         });
     }
