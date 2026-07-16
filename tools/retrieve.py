5952ba4e36859eea099c3b43d5ff32b6d0775741
Miles Punch <miles.anyprintz@gmail.com>
2026-07-16 11:24:10 +1000

    init this new script i wrote

---
+++ tools/retrieve.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+#
+# MIT License
+#
+# Copyright (c) 2026 NUbots
+#
+# This file is part of the NUbots codebase.
+# See https://github.com/NUbots/NUbots for further info.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+import glob
+import os
+import subprocess
+
+from termcolor import cprint
+
+import b
+from utility.dockerise import run_on_docker
+
+
+@run_on_docker
+def register(command):
+    command.description = "Retrieve files from the target system to the local system"
+
+    command.add_argument("host", help="The host or directory to retrieve the files from")
+
+    command.add_argument("target", help="The target directory/files to get")
+
+    command.add_argument("local", help="The local directory to retrieve the files to")
+
+    command.add_argument("--user", "-u", help="The user to retrieve the files with")
+
+@run_on_docker
+def run(host, target, local, user=None, **kwargs):
+    if not os.path.exists(local):
+        os.makedirs(local)
+
+    cprint(f"Retrieving files from {host}:{target} to {local}", "green")
+
+    if user is None:
+        user = "nubots"
+
+    # Use rsync for efficient incremental transfers over SSH
+    subprocess.run([
+        "rsync",
+        "-aP", # partial progression, archive mode
+        "-e",  # specify the remote shell to use
+        "ssh",
+        f"{user}@{host}:{target}",
+        f"{local}",
+    ], check=True)
