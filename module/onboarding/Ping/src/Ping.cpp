37c741a0d1c0f7d65fc790d6babadba49a9e2b3d
Tom0Brien <41043317+Tom0Brien@users.noreply.github.com>
2026-07-27 13:37:59 +1000

    Add pong module

--- module/onboarding/Ping/src/Ping.cpp
+++ module/onboarding/Ping/src/Ping.cpp
@@ -25,7 +25,7 @@ namespace module::onboarding {
 
         on<Trigger<Pong>>().then([this](const Pong& pong_msg) {
             auto ping_msg = std::make_unique<Ping>();
-            log<NUClear::INFO>("Ping");
+            log<INFO>("Ping");
             emit(ping_msg);
         });
     }
