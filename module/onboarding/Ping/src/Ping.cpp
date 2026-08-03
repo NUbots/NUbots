eac47373914729aef2f471ade90e058599382f5e
Marvin Yustanto Rusli <marvinrusli27@gmail.com>
2026-08-03 13:47:35 +1000

    add in all modules

--- module/onboarding/Ping/src/Ping.cpp
+++ module/onboarding/Ping/src/Ping.cpp
@@ -4,6 +4,7 @@
 
 #include "message/onboarding/Ping.hpp"
 #include "message/onboarding/Pong.hpp"
+#include "message/onboarding/FoundAnswer.hpp"
 
 namespace module::onboarding {
 
@@ -13,6 +14,7 @@ namespace module::onboarding {
 
         using message::onboarding::Ping;
         using message::onboarding::Pong;
+        using message::onboarding::FoundAnswer;
 
         on<Configuration>("Ping.yaml").then([this](const Configuration& config) {
             // Use configuration here from file Ping.yaml
@@ -25,9 +27,24 @@ namespace module::onboarding {
 
         on<Trigger<Pong>>().then([this](const Pong& pong_msg) {
             auto ping_msg = std::make_unique<Ping>();
-            log<INFO>("Ping");
-            emit(ping_msg);
+
+            // Iterate the counters and track it
+            ping_msg->count = pong_msg.count + 1;
+            ping_msg->iter_count = ping_msg->count + pong_msg.iter_count;
+
+            if(ping_msg->iter_count == 55){
+                log<INFO>("Ping count: ", ping_msg->count, " Found Answer! ", ping_msg->iter_count);
+
+                auto found_answer_msg = std::make_unique<FoundAnswer>();
+                emit(found_answer_msg);
+            }
+            else{
+                log<INFO>("Ping count: ", ping_msg->count, " Ping iter_count: ", ping_msg->iter_count);
+                emit(ping_msg);
+            }
+
         });
+
     }
 
 }  // namespace module::onboarding
--- module/onboarding/Pong/src/Pong.cpp
+++ module/onboarding/Pong/src/Pong.cpp
@@ -17,20 +17,29 @@ Pong::Pong(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::mov
     on<Configuration>("Pong.yaml").then([this](const Configuration& config) {
         // Use configuration here from file Pong.yaml
         this->log_level = config["log_level"].as<NUClear::LogLevel>();
-
-
     });
 
     on<Startup>().then([this] {
             // Start the ping pong chain
             auto pong_msg = std::make_unique<Pong>();
-            log<INFO>("Pong");
+
+            // Start the counters and track it
+            pong_msg->count = 1;
+            pong_msg->iter_count = 1;
+
+            log<INFO>("Pong count: ", pong_msg->count, " Pong iter_count: ", pong_msg->iter_count);
             emit(pong_msg);
     });
 
         on<Trigger<Ping>>().then([this](const Ping& ping_msg) {
             auto pong_msg = std::make_unique<Pong>();
-            log<INFO>("Pong");
+            // log<INFO>("Pong");
+
+            // Increment the counters and track it
+            pong_msg->count = ping_msg.count + 1;
+            pong_msg->iter_count = pong_msg->count + ping_msg.iter_count;
+
+            log<INFO>("Pong count: ", pong_msg->count, " Pong iter_count: ", pong_msg->iter_count);
 
             emit(pong_msg);
         });
